d48c1c3afcaee48c352dee145ca1292dfc85425a
sjoerdk <w.s.kerkstra@gmail.com>
2012-06-19 10:54:12 +0200

    Now renders pages in sites individually using correct template. Improvements in pages admin

--- django/ComicSite/admin.py
+++ django/ComicSite/admin.py
@@ -8,5 +8,18 @@ from django.contrib import admin
 from models import ComicSite,Page
 
 admin.site.register(ComicSite)
-admin.site.register(Page)
 
+
+
+class PageAdmin(admin.ModelAdmin):
+    """Define the admin interface for pages"""
+    
+    #Show these page params in admin overview list 
+    list_display = ('title','ComicSite')
+    ordering = ['ComicSite']
+    
+    
+    
+
+
+admin.site.register(Page,PageAdmin)    
