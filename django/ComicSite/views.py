fd1f8d73d4e54936b1a46ff741be10425e3f62ce
sjoerdk <w.s.kerkstra@gmail.com>
2012-06-20 13:32:32 +0200

    now includes

--- django/ComicSite/views.py
+++ django/ComicSite/views.py
@@ -10,6 +10,9 @@ from django.http import HttpResponse
 from ComicSite.models import ComicSite,Page
 from django.http import Http404
 from django.shortcuts import render_to_response
+from DataProviders import FileSystemDataProvider
+from Comic import settings
+
 
 def index(request):
     return  HttpResponse("ComicSite index page.")
@@ -39,7 +42,21 @@ def page(request, site_name, page_title):
     #return HttpResponse(givePageHTML(p))
     
 
+def dataPage(request):
+    """ test function for data provider"""
+    #= r"D:\userdata\Sjoerd\Aptana Studio 3 Workspace\comic-django\django\static\files"
+    
+    path = r"D:\userdata\Sjoerd\Aptana Studio 3 Workspace\comic-django\django\static\files"
+    dp = FileSystemDataProvider.FileSystemDataProvider(path)
+    images = dp.getImages()
+    
+    htmlOut = "available files:"+", ".join(images)
+    p = createTestPage(html=htmlOut)
+    pages = [p]
+    
+    return render_to_response('page.html', {'site': p.ComicSite, 'page': p, "pages":pages })
 
+# ======================================== not called directly from urls.py =========================================
 
 def getSite(site_name):
     try:
@@ -58,6 +75,16 @@ def getPages(site_name):
         raise Http404
     return pages
 
+    
+# ======================================================  debug and test ==================================================
+def createTestPage(title="testPage",html=""):
+    """ Create a quick mockup on the ComicSite 'Test'"""
+    try: 
+        site = getSite("test")
+    except Http404:
+        raise ComicSite.DoesNotExist("To show a testpage you have to have a ComicSite called 'test' (called by /test)..")
+    
+    return Page(ComicSite=site,title=title,html=html)
     
 
 def givePageHTML(page):
--- django/Static/readme.txt
+++ django/Static/readme.txt
@@ -1,2 +1,2 @@
 6/6/2012 Sjoerd
-All static content for the HelloWorld example is served from here.
+All static content for the project is kept here, only during development. When going live files should be served by apache or similar.
