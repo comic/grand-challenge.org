95b08ada5a8beb756f1927a80511d46ff8650c41
sjoerdk <w.s.kerkstra@gmail.com>
2012-06-21 09:40:10 +0200

    replaced last hard links with relative using {% url %} added comments to urls.py

--- django/comic/urls.py
+++ django/comic/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView
 from django.contrib import admin
-from ComicSite.models import ComicSite 
+from ComicSite.models import ComicSite
+from django.views.generic.simple import redirect_to 
 
 admin.autodiscover()
 
-
+# prefix introduced to make it possible to serve django from non-root dir on server.
+# see https://github.com/comic/comic-django/issues/14 
 urlPrefix = "Comic/"
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Comic.views.home', name='home'),
@@ -15,6 +18,10 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    # for convenience in developing: redirect to not have to type /Comic 
+    # after 'localhost' to load site for the first time
+    (r'^$', redirect_to, {'url': '/'+urlPrefix}),
+     
     # Uncomment the next line to enable the admin:
     url(r'^'+urlPrefix+'admin/', include(admin.site.urls)),
     
