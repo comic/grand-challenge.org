b047267a63fe4f46094f47a3863b9f15ba065045
sjoerdk <w.s.kerkstra@gmail.com>
2012-06-29 10:04:26 +0200

    added way to order page objects. Added .order param, added move up and down methods and changed admin to display order for pages and be able te edit order via up and down methods.

--- django/ComicSite/admin.py
+++ django/ComicSite/admin.py
@@ -5,19 +5,44 @@ Created on Jun 18, 2012
 '''
 
 from django.contrib import admin
-from models import ComicSite,Page
+from django import forms
+from django.db import models 
 
-admin.site.register(ComicSite)
+from models import ComicSite,Page 
 
 
+
+
+class PageAdminForm(forms.ModelForm):
+    move = forms.CharField(widget=forms.Select)
+    move.required = False
+    move.widget.choices=(
+                         (models.BLANK_CHOICE_DASH[0]),
+                         ('FIRST', 'First'),
+                         ('UP', 'Up'),
+                         ('DOWN', 'Down'),
+                         ('LAST', 'Last'),
+                        )
+    class Meta:
+        model = Page
 
 class PageAdmin(admin.ModelAdmin):
     """Define the admin interface for pages"""
+    form = PageAdminForm
+    
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        move = form.cleaned_data['move']
+        obj.move(move)
     
     #Show these page params in admin overview list 
-    list_display = ('title','ComicSite')
-    ordering = ['ComicSite']
+    list_display = ('title','ComicSite','order')
+    
         
+
+admin.site.register(ComicSite)
+admin.site.register(Page,PageAdmin)
+
     
 
-admin.site.register(Page,PageAdmin)    
+    
--- django/ComicSite/models.py
+++ django/ComicSite/models.py
@@ -9,16 +9,19 @@ class ComicSite(Site):
     
     # add requirement to native django 'site' obejct that site name should be unique.
     # because names are used in URLS in COMIC, dublicate sitenames would make trouble.
-        
+    
     short_name = models.CharField(max_length = 50, default="", help_text = "short name used in url, specific css, files etc. No spaces allowed")
-    skin = models.CharField(max_length = 225)
+    skin = models.CharField(max_length = 225)    
+        
     comment = models.CharField(max_length = 1024, default="", blank=True)
         
 
 class Page(models.Model):
     """ A single editable page containing html and maybe special output plugins """
     
+    order = models.IntegerField(editable=False, default=1) #for determining order in which pages appear on site    
     ComicSite = models.ForeignKey("ComicSite")
+    
     title = models.CharField(max_length = 255)
     html = models.TextField()
     
@@ -27,10 +30,32 @@ class Page(models.Model):
         #TODO : do checking for scripts and hacks here? 
         return mark_safe(self.html)
     
+    def move(self, move):
+        if move == 'UP':
+            mm = Page.objects.get(ComicSite=self.ComicSite,order=self.order-1)
+            mm.order += 1
+            mm.save()
+            self.order -= 1
+            self.save()
+        if move == 'DOWN':
+            mm = Page.objects.get(ComicSite=self.ComicSite,order=self.order+1)
+            mm.order -= 1
+            mm.save()
+            self.order += 1
+            self.save()
+        if move == 'FIRST':
+            raise NotImplementedError("Somebody should implement this!")
+        if move == 'LAST':
+            raise NotImplementedError("Somebody should implement this!")
+    
+    
     class Meta:
         """special class holding meta info for this class"""
         # make sure a single site never has two pages with the same name
         unique_together = (("ComicSite", "title"),)
+        
+        # when getting a list of these objects this ordering is used
+        ordering = ['ComicSite','order']
     
     
 
