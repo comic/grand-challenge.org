54e0569c4ec8bcfd4a0cebd70f2589dd5dedded1
koekie <m.koek@erasmusmc.nl>
2012-07-05 16:58:05 +0200

    * added app profiles: this app is dependent on django-userena and south * added some stuff to css and comicsite * began to try to integrate social-auth and userena, but there are couple of things still to be figured out

---
+++ django/profiles/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+from userena.models import UserenaBaseProfile
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django_countries import CountryField
+
+class UserProfile(UserenaBaseProfile):
+    user = models.OneToOneField(User, 
+            unique=True, 
+            verbose_name=_('user'), 
+            related_name='user_profile')
+    institution = models.CharField(max_length = 100)
+    department = models.CharField(max_length = 100)
+    country = CountryField(default=_(u'NL'))
+    website = models.CharField(max_length = 150)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
