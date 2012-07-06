9bd879f6c9f12cc7784c2a6acc98cfd886072b32
koekie <m.koek@erasmusmc.nl>
2012-07-06 17:28:21 +0200

    begin adding pipelines to social auth

---
+++ django/profiles/social_auth/pipeline.py
@@ -0,0 +1,31 @@
+from userena.models import UserenaSignup
+from profiles.models import UserProfile as Profile
+from userena.managers import ASSIGNED_PERMISSIONS
+
+def userena_signup(user=None, profile=None, *args, **kwargs):
+    """ Create user using UserenaSignup
+    """
+    print user, profile
+    return {'user': UserenaSignup.objects.get_or_create(user=user) }
+
+def create_profile(user=None, profile=None, *args, **kwargs):
+    """ Create user profile if necessary
+    """
+    if profile:
+        return { 'profile':profile }
+    if not user:
+        return
+    return { 'profile': Profile.objects.get_or_create(user.user.profile)[0] }
+
+def set_guardian_permissions(user=None, profile=None, *args, **kwargs):
+    """ Give the user permission to modify themselves
+    """
+    if not user or not user.is_authenticated():
+        return
+    if profile:
+        # Give permissions to view and change profile
+        for perm in ASSIGNED_PERMISSIONS['profile']:
+            assign(perm[0], user, profile)
+    # Give permissions to view and change itself
+    for perm in ASSIGNED_PERMISSIONS['user']:
+        assign(perm[0], user, user)
