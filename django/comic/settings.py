1ba2828a1d2ff2434e0c4644af7e57ddf26265eb
koekie <m.koek@erasmusmc.nl>
2012-07-09 12:11:51 +0200

    Social auth pipeline now also creates a profile and gives guardian permissions.

--- django/comic/settings.py
+++ django/comic/settings.py
@@ -249,9 +249,8 @@ SOCIAL_AUTH_PIPELINE = (
         'social_auth.backends.pipeline.associate.associate_by_email',
         'social_auth.backends.pipeline.user.get_username',
         'social_auth.backends.pipeline.user.create_user',
-        'profile.social_auth.pipeline.create_profile',
-        'profile.social_auth.pipeline.set_guardian_permissions',
-        #'profile.social_auth.pipeline.userena_signup',
+        'profiles.social_auth.pipeline.create_profile',
+        'profiles.social_auth.pipeline.set_guardian_permissions',
         'social_auth.backends.pipeline.social.associate_user',
         'social_auth.backends.pipeline.social.load_extra_data',
         'social_auth.backends.pipeline.user.update_user_details',
@@ -260,7 +259,8 @@ SOCIAL_AUTH_PIPELINE = (
 SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
 SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
 SOCIAL_AUTH_EXPIRATION = 'expires'
-SOCAIL_AUTH_RAISE_EXCEPTIONS = DEBUG
+SOCIAL_AUTH_RAISE_EXCEPTIONS = DEBUG
+SOCIAL_AUTH_ASSOCIATE_BY_MAIL = True
 #SOCIAL_AUTH_USER_MODEL = 'auth.User'
 
 
