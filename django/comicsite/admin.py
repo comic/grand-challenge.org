f7630176d81ab04b5880fc46eae3b6b121e2d185
sjoerdk <w.s.kerkstra@gmail.com>
2012-07-10 17:05:37 +0200

    ComicSiteAdmin view now only shows sites to which user has object permission change_comicsite as defined by django guardian. To test register user, activate user, make user administrator but not superuser, give user regular ComicSite permissions, Go to a single ComicSite in admin, click 'object permissions' and add all permissions for the new user. Now when new user logs in to admin only ComicSites can be edited, only allowed ComicSite is shown

--- django/comicsite/admin.py
+++ django/comicsite/admin.py
@@ -7,10 +7,13 @@ Created on Jun 18, 2012
 from django.contrib import admin
 from django import forms
 from django.db import models 
+from django.contrib.auth.models import Group
+
 
 from guardian.admin import GuardedModelAdmin
+from guardian.shortcuts import get_objects_for_user
 from models import ComicSite,Page
-
+import pdb
 
 
 
@@ -38,11 +41,45 @@ class PageAdmin(GuardedModelAdmin):
     
     #Show these page params in admin overview list 
     list_display = ('title','ComicSite','order')
+    
 
 
 class ComicSiteAdmin(GuardedModelAdmin):
-    pass
     
+    def __init__(self,*args,**kwargs):
+        super(ComicSiteAdmin, self).__init__(*args, **kwargs)  
+        self.user_can_access_owned_objects_only = True
+    
+    def queryset(self, request):
+        """ overwrite this method to return only comicsites to which current user has access """
+        qs = super(admin.ModelAdmin, self).queryset(request)
+
+        if request.user.is_superuser:
+            return qs
+                
+        user_qs = get_objects_for_user(request.user, 'comicsite.change_comicsite')
+        return user_qs
+    
+    def save_model(self, request, obj, form, change):        
+        """ when saving for the first time, set object permissions; give all permissions to creator """
+        pdb.set_trace()
+        if obj.id is not None:      
+            # create admins group
+            group = Group.objects.create(name=self.short_name+"Admin")
+            pdb.set_trace()
+            # create participants group
+            
+            
+            # give all this sites permissions to admin group  
+            
+            # add current user to admins group
+            
+            
+            max_order = Page.objects.filter(ComicSite__pk=self.ComicSite.pk).aggregate(Max('order'))                
+            self.order = max_order["order__max"] + 1
+    
+        obj.save()
+        
     
         
 
