454b16fc9eab52fe69882da85be2b9c255c63e2b
sjoerdk <w.s.kerkstra@gmail.com>
2012-08-01 16:32:59 +0200

    ComicSite no longer extends Site
    This was a BIG hassle in terms of db migration.. hence 6 migrations.. Lots
    of errors with missing UNIQUE contraints, duplicate primary keys etc.. But
    I guess a good training for complex south migration.
    Running these migrations will not allow going back, the ComicSite.id will
    be changed and previous value not saved. But I think this is no problem

--- django/comicsite/models.py
+++ django/comicsite/models.py
@@ -25,7 +25,11 @@ class ComicSite(models.Model):
     short_name = models.CharField(max_length = 50, default="", help_text = "short name used in url, specific css, files etc. No spaces allowed")
     skin = models.CharField(max_length = 225)
     comment = models.CharField(max_length = 1024, default="", blank=True)
-        
+    
+    def __unicode__(self):
+        """ string representation for this object"""
+        return self.short_name
+    
     def clean(self):
         """ clean method is called automatically for each save in admin"""
         #TODO check whether short name is really clean and short!
@@ -45,7 +49,7 @@ class Page(models.Model):
     
     order = models.IntegerField(editable=False, default=1, help_text = "Determines order in which pages appear on site")
     ComicSiteTemp = models.IntegerField(default = 0, help_text = "just a temp var to hold foreign key while rebasing this model from ComicSite model")     
-    #ComicSite = models.ForeignKey("ComicSite")
+    ComicSite = models.ForeignKey("ComicSite")
     title = models.CharField(max_length = 255)
     html = models.TextField()
     
@@ -100,10 +104,10 @@ class Page(models.Model):
         """special class holding meta info for this class"""
         # make sure a single site never has two pages with the same name because page names
         # are used as keys in urls
-        #unique_together = (("ComicSite", "title"),)
+        unique_together = (("ComicSite", "title"),)
          
         # when getting a list of these objects this ordering is used
-        #ordering = ['ComicSite','order']
+        ordering = ['ComicSite','order']
     
     
 class ComicSiteException(Exception):
