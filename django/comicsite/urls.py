4cad71e14e710909dd016ba7b55d3a369aef98f5
sjoerdk <w.s.kerkstra@gmail.com>
2012-08-29 14:29:32 +0200

    minor bugfixes for showdate test page, just for debug

--- django/comicsite/urls.py
+++ django/comicsite/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
+    url(r'^test/showData/$','comicsite.views.dataPage'),
                     
-    url(r'^(?P<site_short_name>\w+)/$','comicsite.views.site'),
+    url(r'^(?P<site_short_name>\w+)/$','comicsite.views.site'),    
+    
+    url(r'^(?P<site_short_name>\w+)/admin/$','comicsite.views.page'),
     
     url(r'^(?P<site_short_name>\w+)/(?P<page_title>\w+)/$','comicsite.views.page'),
-
-    url(r'^test/showData/$','comicsite.views.dataPage'),
+        
+    
     
     
 )
