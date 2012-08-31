7d355507928a7e0086b8e9f4c15781085a23c0ff
sjoerdk <w.s.kerkstra@gmail.com>
2012-08-31 15:26:38 +0200

    improved comicsite admin: better links, returnto urls

--- django/comic/urls.py
+++ django/comic/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
         
-    url(r'^site/',include('comicsite.urls')),
+    url(r'^site/',include('comicsite.urls'),name='site'),
         
     
     # requirement for social_auth
