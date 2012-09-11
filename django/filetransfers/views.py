f0e65066d987e5bb2d3a7fd787152cc5ee92d04f
sjoerdk <w.s.kerkstra@gmail.com>
2012-09-11 13:30:04 +0200

    download link based on project/dataset/filename now works

--- django/filetransfers/views.py
+++ django/filetransfers/views.py
@@ -1,4 +1,7 @@
 import pdb
+from os import path
+
+from django.core.files import File
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -7,8 +10,9 @@ from django.views.generic.simple import direct_to_template
 
 from filetransfers.forms import UploadForm
 from filetransfers.models import UploadModel
-
 from filetransfers.api import prepare_upload, serve_file
+
+from comicmodels.models import FileSystemDataset 
 
 def upload_handler(request):
     view_url = reverse('filetransfers.views.upload_handler')
@@ -27,6 +31,18 @@ def upload_handler(request):
 def download_handler(request, pk):
     upload = get_object_or_404(UploadModel, pk=pk)
     return serve_file(request, upload.file, save_as=True)
+
+def download_handler_filename(request, project_name, dataset_title,filename):    
+    "offer file for download based on filename"
+    
+    dataset = FileSystemDataset.objects.get(comicsite__short_name=project_name,title=dataset_title)        
+    filefolder = dataset.get_data_dir()
+    filepath = path.join(filefolder,filename)
+    f = open(filepath, 'r')
+    file = File(f) # create django file object
+        
+    return serve_file(request, file, save_as=True)
+
 
 def delete_handler(request, pk):
     if request.method == 'POST':
