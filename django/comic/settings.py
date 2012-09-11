da4de68e00a4a397778ddcf6a190884a5b71a735
sjoerdk <w.s.kerkstra@gmail.com>
2012-09-11 11:01:54 +0200

    towards download dataset tag

--- django/comic/settings.py
+++ django/comic/settings.py
@@ -72,7 +72,9 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+# FIXME: put site source root here for testing purposes. This should be a real data drive 
+MEDIA_ROOT = os.path.join(SITE_ROOT,"static/media") 
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
--- django/comicmodels/models.py
+++ django/comicmodels/models.py
@@ -1,48 +1,77 @@
+import os
+import re
+import pdb
+
 from django.db import models
+from django.conf import settings
 
 from dataproviders import FileSystemDataProvider
 from comicsite.models import ComicSite 
 
 class UploadModel(models.Model):
+    
     title = models.CharField(max_length=64, blank=True)
     file = models.FileField(upload_to='uploads/%Y/%m/%d/%H/%M/%S/')
 
-    @property
+    @property    
     def filename(self):
         return self.file.name.rsplit('/', 1)[-1]
     
     
 class Dataset(models.Model):
-     """
-     Collection of files 
-     """
-     title = models.CharField(max_length=64, blank=True)
-     description = models.TextField()
-     comicsite = models.ForeignKey(ComicSite)
+    """
+    Collection of files 
+    """
+    title = models.CharField(max_length=64, blank=True)
+    description = models.TextField()
+    comicsite = models.ForeignKey(ComicSite)
     
-     def __unicode__(self):
-        """ string representation for this object"""
-        return self.title
-     
-     class Meta:
-        abstract = True
-        
+       
+    @property
+    def cleantitle(self):
+        return re.sub('[\[\]/{}., ]+', '',self.title)
+   
+    def __unicode__(self):
+       """ string representation for this object"""
+       return self.title
+                
+    class Meta:
+       abstract = True
+         
     
-
-
 class FileSystemDataset(Dataset):
     """
     A folder location on disk
     """
-    folder = models.FilePathField()
+    folder = models.FilePathField(editable=False)
         
     def get_all_files(self):
-         """ return array of all files in this folder
-         """
-         dp = FileSystemDataProvider.FileSystemDataProvider(self.folder)
-         filenames = dp.getFileNames()
-         htmlOut = "available files:"+", ".join(filenames)
-         return htmlOut
-         
+        """ return array of all files in this folder
+        """        
+        dp = FileSystemDataProvider.FileSystemDataProvider(self.folder)
+        filenames = dp.getFileNames()
+        htmlOut = "available files:"+", ".join(filenames)
+        return htmlOut
 
+    
+    def save(self):                    
+        data_dir = self.get_data_dir()
+        self.ensure_dir(data_dir)
+        self.folder = data_dir
+        super(FileSystemDataset,self).save()
+        
+    
+    def get_data_dir(self):
+        """ In which dir Where should this dataset be located?  
+        """        
+        #data_dir_path = os.path.join(settings.MEDIA_ROOT[0],self.comicsite.short_name,self.cleantitle)
+        data_dir_path = os.path.join(self.comicsite.short_name,self.cleantitle)
+        return data_dir_path
+    
+    def ensure_dir(self,dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+
+        
      
