7e1ed62bc080453f6ad7618aee2b71cc613e2f6a
sjoerdk <w.s.kerkstra@gmail.com>
2012-09-24 13:11:26 +0200

    Removed left over test code

--- django/comicsite/templatetags/template_tags.py
+++ django/comicsite/templatetags/template_tags.py
@@ -18,27 +18,6 @@ from comicmodels.models import FileSystemDataset #FIXME: abstract Dataset should
 # This is needed to use the @register.tag decorator
 register = template.Library()
 
-@register.tag(name="current_time")
-def do_current_time(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, format_string = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return CurrentTimeNode(format_string[1:-1])
-
-
-class CurrentTimeNode(template.Node):	
-	
-    def __init__(self, format_string):
-        self.format_string = format_string
-    def render(self, context):
-        return datetime.datetime.now().strftime(self.format_string)
-       
-       
-       
 @register.tag(name="filelist")
 def do_get_files(parser, token):	
     try:
