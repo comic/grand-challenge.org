b73c919ddc352b7514015ffa180d22510d9f93d2
sjoerdk <w.s.kerkstra@gmail.com>
2012-10-03 14:12:09 +0200

    very basic upload now working for <sitename>/files references #89

---
+++ django/comicmodels/migrations/0002_auto__add_field_uploadmodel_comicsite.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'UploadModel.comicsite'
+        db.add_column('comicmodels_uploadmodel', 'comicsite',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['comicsite.ComicSite']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'UploadModel.comicsite'
+        db.delete_column('comicmodels_uploadmodel', 'comicsite_id')
+
+
+    models = {
+        'comicmodels.filesystemdataset': {
+            'Meta': {'object_name': 'FileSystemDataset'},
+            'comicsite': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': "orm['comicsite.ComicSite']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'folder': ('django.db.models.fields.FilePathField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'})
+        },
+        'comicmodels.uploadmodel': {
+            'Meta': {'object_name': 'UploadModel'},
+            'comicsite': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['comicsite.ComicSite']"}),
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'})
+        },
+        'comicsite.comicsite': {
+            'Meta': {'object_name': 'ComicSite'},
+            'description': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'}),
+            'skin': ('django.db.models.fields.CharField', [], {'max_length': '225'})
+        }
+    }
+
+    complete_apps = ['comicmodels']
