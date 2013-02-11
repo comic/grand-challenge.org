e4e32b300645a4c80c1d86ebd37cb141d43ef873
sjoerdk <w.s.kerkstra@gmail.com>
2013-02-11 14:42:07 +0100

    ckeditor upload an browsing now working

--- django/comicsite/urls.py
+++ django/comicsite/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     
     url(r'^(?P<site_short_name>\w+)/files/$','comicmodels.views.upload_handler'),
     
+    url(r'^(?P<site_short_name>\w+)/serve/(?P<filepath>.+)/$','comicsite.views.inserted_file'),
+    
     url(r'^(?P<site_short_name>\w+)/_register/$','comicsite.views._register'),
     
     url(r'^(?P<site_short_name>\w+)/source/(?P<page_title>\w+)/$','comicsite.views.pagesource'),
