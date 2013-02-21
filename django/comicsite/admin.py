70c798bc545f43e6e21b5994b73bed8147c42628
sjoerdk <w.s.kerkstra@gmail.com>
2013-02-21 15:31:10 +0100

    links within admin now pointing correctly

--- django/comicsite/admin.py
+++ django/comicsite/admin.py
@@ -41,6 +41,10 @@ from django.contrib.admin.sites import AdminSite
 from django.contrib.admin.filters import (ListFilter, SimpleListFilter,
     FieldListFilter, BooleanFieldListFilter, RelatedFieldListFilter,
     ChoicesFieldListFilter, DateFieldListFilter, AllValuesFieldListFilter)
+from django.views.decorators.cache import never_cache
+from django.utils.text import capfirst
+from django.template.response import TemplateResponse
+
 
 class ProjectAdminSite(AdminSite):
     """ """
@@ -51,8 +55,7 @@ class ProjectAdminSite(AdminSite):
         from django.conf import settings
         from functools import update_wrapper
         from django.contrib.contenttypes import views as contenttype_views
-        
-        pdb.set_trace()
+            
         if settings.DEBUG:
             self.check_dependencies()
 
@@ -92,6 +95,73 @@ class ProjectAdminSite(AdminSite):
                     include(model_admin.urls))
             )
         return urlpatterns
+
+    @never_cache
+    def index(self, request, extra_context=None):
+        """                 
+        Displays the main admin index page, which lists all of the installed
+        apps that have been registered in this site.
+        
+        SJOERD: copied this completely from AdminSite in django.contrib.admin.sites. This seemed the only way
+        to get the urls for the apps list right (currently the apps registered to projectadminsite contain links
+        to regular admin. This is because the index method contains "reverse('admin:..." statements, when 
+        they should be "reverse('projectadmin". Just copying the whole thing seems a bad solution but I see 
+        no other way to overwrite. Why is the reverse not based on app_name property? Done that here. 
+        """
+        
+        app_dict = {}
+        user = request.user
+        for model, model_admin in self._registry.items():
+            app_label = model._meta.app_label
+            has_module_perms = user.has_module_perms(app_label)
+
+            if has_module_perms:
+                perms = model_admin.get_model_perms(request)
+
+                # Check whether user has any perm for this module.
+                # If so, add the module to the model_list.
+                if True in perms.values():
+                    info = (app_label, model._meta.module_name)
+                    model_dict = {
+                        'name': capfirst(model._meta.verbose_name_plural),
+                        'perms': perms,
+                    }
+                    if perms.get('change', False):
+                        try:
+                            model_dict['admin_url'] = reverse(self.app_name+':%s_%s_changelist' % info, current_app=self.name)
+                        except NoReverseMatch:
+                            pass
+                    if perms.get('add', False):
+                        try:
+                            model_dict['add_url'] = reverse(self.app_name+':%s_%s_add' % info, current_app=self.name)
+                        except NoReverseMatch:
+                            pass
+                    if app_label in app_dict:
+                        app_dict[app_label]['models'].append(model_dict)
+                    else:
+                        app_dict[app_label] = {
+                            'name': app_label.title(),
+                            'app_url': reverse(self.app_name+':app_list', kwargs={'app_label': app_label}, current_app=self.name),
+                            'has_module_perms': has_module_perms,
+                            'models': [model_dict],
+                        }
+
+        # Sort the apps alphabetically.
+        app_list = app_dict.values()
+        app_list.sort(key=lambda x: x['name'])
+
+        # Sort the models alphabetically within each app.
+        for app in app_list:
+            app['models'].sort(key=lambda x: x['name'])
+
+        context = {
+            'title': _('Site administration'),
+            'app_list': app_list,
+        }
+        context.update(extra_context or {})
+        return TemplateResponse(request, [
+            self.index_template or 'admin/index.html',
+        ], context, current_app=self.name)
 
 
 projectadminsite =  ProjectAdminSite(name="projectadmin",app_name="projectadmin")
