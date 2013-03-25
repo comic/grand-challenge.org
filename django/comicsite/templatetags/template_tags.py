ff81574f82d34b916315bce375c70591540ae4ca
sjoerdk <w.s.kerkstra@gmail.com>
2013-03-25 17:19:05 +0100

    Removed Comic main portal link from menu bar, removed link from 'COMIC:' in footer

--- django/comicsite/templatetags/template_tags.py
+++ django/comicsite/templatetags/template_tags.py
@@ -61,7 +61,7 @@ register = template.Library()
 @register.simple_tag
 def metafooterpages():
     """ Get the metafooter pages. """
-    html_string = "<div class='text'><a href=\"/\">COMIC:</a></div>"
+    html_string = "<div class='text'><span>COMIC:</span></div>"
     pages = comicsite.views.getPages('COMIC')
     for p in pages:
         if not p.hidden:
