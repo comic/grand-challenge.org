6843fa1362cad9c1cc3ccdd248bb4aebf466dba0
sjoerdk <w.s.kerkstra@gmail.com>
2013-03-27 13:19:18 +0100

    added email sending for new uploads

--- django/comicmodels/views.py
+++ django/comicmodels/views.py
@@ -3,23 +3,19 @@ import pdb
 from os import path
 import ntpath
 
-#from django.core.files import File
-#
 from django.contrib import messages
+from django.contrib.sites.models import get_current_site
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-#from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-#from django.views.generic.simple import direct_to_template
-#
+
 from comicmodels.models import ComicSite
 from comicmodels.forms import UploadForm,UserUploadForm
 from comicmodels.models import UploadModel,Page
+from comicmodels.signals import file_uploaded
 from comicsite.views import site_get_standard_vars,concatdicts,getSite,permissionMessage
 from filetransfers.api import prepare_upload, serve_file
-#
-#from comicmodels.models import FileSystemDataset
 
 
 def upload_handler(request,site_short_name):
@@ -42,7 +38,12 @@ def upload_handler(request,site_short_name):
             messages.success(request, "File '%s' sucessfully uploaded. An email has been sent to this\
                                        projects organizers." % filename)
             
-            #TODO: send email!
+            # send signal to be picked up by email notifier 03/2013 - Sjoerd. I'm not sure that sending
+            # signals is the best way to do this. Why not just call the method directly?
+            # typical case for a refactoring round.                 
+            file_uploaded.send(sender=UploadModel,uploader=request.user,filename=filename,comicsite=site
+                                   ,site=get_current_site(request))
+            
 
             return HttpResponseRedirect(view_url)
         else:
