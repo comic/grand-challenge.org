9ed97ed8fdd4a54ab993efd16a25b08a8492ac2b
Marcel Koek <koek.marcel@gmail.com>
2013-04-16 14:57:43 +0200

    after login go back to previous page; login/logout urls and footer now in overloadable blocks; use social-auth built-in get_username again

--- django/profiles/views.py
+++ django/profiles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 import pdb
 import userena.views as userena_views
 
+#FIXME: quick-n-dirty implementation to make things work for social-auth login redirect handling.
 
 def profile(request):
     """
@@ -14,3 +15,12 @@ def profile(request):
     else:
         return redirect('/accounts/signin')
 
+def profile_edit(request):
+    """
+    Redirect to the profile edit page of the currently signed in user.
+    """
+    if request.user.is_authenticated():
+        print "username: ", request.user.username
+        return redirect('/accounts/'+request.user.username+'/edit')
+    else:
+        return redirect('accounts/signin')
