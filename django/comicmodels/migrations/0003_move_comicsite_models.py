84c2a88d12cc8fdb1f39466e6d4de28b24544a02
sjoerdk <w.s.kerkstra@gmail.com>
2013-06-18 17:27:25 +0200

    Fixed error in comicmodels migration 0003 by setting south dependency

--- django/comicmodels/migrations/0003_move_comicsite_models.py
+++ django/comicmodels/migrations/0003_move_comicsite_models.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+             ("comicsite", "0001_initial"),
+    )
+
     def forwards(self, orm):
         
         # Adding model 'ComicSite'
