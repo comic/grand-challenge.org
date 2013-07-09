f9e463e74c1a8c695f1aa43cd325e675d0c7e499
sjoerdk <w.s.kerkstra@gmail.com>
2013-07-09 15:00:14 +0200

    test refactored. testing project creation and admin views closes #185

--- django/comicsite/tests.py
+++ django/comicsite/tests.py
@@ -6,6 +6,7 @@ when you run "manage.py test.
 """
 import pdb
 import re
+from random import choice
 
 from django.test import TestCase
 from django.test.client import RequestFactory
@@ -33,8 +34,6 @@ def get_or_create_user(username,password):
         return 
     
         
-        
-
 
 def create_comicsite_in_admin(user,short_name,description="test project"):
     """ Create a ComicSite object as if created through django admin interface.
@@ -69,6 +68,17 @@ def create_page_in_admin(comicsite,title,content="testcontent"):
     page_admin.first_save(page)
     return page
     
+def extract_form_errors(html):
+    """ If something in post to a form url fails, I want to know what the
+    problem was.
+    
+    """
+    errors = re.findall('<ul class="errorlist"(.*)</ul>',
+                         html,
+                         re.IGNORECASE)
+    
+    return errors
+    
         
 
 class SimpleTest(TestCase):
@@ -96,31 +106,97 @@ class ViewsTest(TestCase):
         # Siteadmin, cam do things to a site he or she owns. And logged in
         # user 
         
-        root = User.objects.create_user('root',
+        self.root = User.objects.create_user('root',
                                         'w.s.kerkstra@gmail.com',
-                                        'rootpassword')        
-        root.is_staff = True
-        root.is_superUser = True
-        root.save()
+                                        'testpassword')        
+        self.root.is_staff = True
+        self.root.is_superuser = True
+        self.root.save()
         
         # non-root users are created as if they signed up through the site,
         # to maximize test coverage.        
-        user1 = self._create_user({"username":"user1"})
-                        
-        siteadmin = User.objects.create_user('siteadmin1',
-                                        'w.s.kerkstra@gmail.com',
-                                        'password1')
-                
-        siteadmin.is_staff = True        
-        siteadmin.save()
+        self.registered_user = self._create_user({"username":"registered_user"})
+                                        
+        self.siteadmin = self._create_user({"username":"siteadmin",
+                                            "email":"df@rt.com"}) 
                     
-        testsite = create_comicsite_in_admin(root,"viewtest")                
-        create_page_in_admin(testsite,"testpage1")
-        create_page_in_admin(testsite,"testpage2")
+        self.testsite = create_comicsite_in_admin(self.siteadmin,"viewtest")                
+        create_page_in_admin(self.testsite,"testpage1")
+        create_page_in_admin(self.testsite,"testpage2")
+                
+        
+    
+    def test_registered_user_can_create_project(self):
+        """ A user freshly registered through the site can immediately create
+        a project
+        
+        """
+        user = self._create_user({"username":"user2","email":"ab@cd.com"})
+        testsite = create_comicsite_in_admin(user,"user1project")                
+        testpage1 = create_page_in_admin(testsite,"testpage1")
+        testpage2 = create_page_in_admin(testsite,"testpage2")
+        
+        self._test_page_can_be_viewed(user.username,testpage1)
+        self._test_page_can_be_viewed(self.root.username,testpage1)
         
         
+    
+    def test_page_permissions_view(self):
+        """ Test that the permissions page does not crash: for root
+        https://github.com/comic/comic-django/issues/180 
+        
+        """
+        
+        testpage1 = Page.objects.filter(title='testpage1')
+        self.assert_(testpage1.exists(),"could not find page 'testpage1'")                 
+        url = reverse("admin:comicmodels_page_permissions",
+                      args=[testpage1[0].pk])
+        
+        self._test_url_can_be_viewed(self.root.username,url)
+        
+        otheruser = self._create_random_user()
+        self._test_url_cannot_be_viewed(otheruser.username,url)
+        
+        
+    
+    def test_page_change_view(self):
+        """ Root can see a page 
+        
+        """
+        user = self._create_user({"username":"user3","email":"de@cd.com"})
+        testsite = create_comicsite_in_admin(user,"user3project")                
+        testpage1 = create_page_in_admin(testsite,"testpage1")
+        testpage2 = create_page_in_admin(testsite,"testpage2")                         
+        url = reverse("admin:comicmodels_page_change",
+                      args=[testpage1.pk])
+        
+        self._test_url_can_be_viewed(user.username,url)        
+        self._test_url_can_be_viewed(self.root.username,url)
+        
+        
+
+    def _test_page_can_be_viewed(self,username,page):
+        page_url = reverse('comicsite.views.page',
+                           kwargs={"site_short_name":page.comicsite.short_name,
+                                   "page_title":page.title})
+        
+        self._test_url_can_be_viewed(username,page_url)
+        
+                         
+    def _test_url_can_be_viewed(self,username,url):
+        self._login(username)
+        response = self.client.get(url)        
+        self.assertEqual(response.status_code, 200, "could not load page"
+                         "'%s' logged in as user '%s'"% (url,username))
+    
+    def _test_url_cannot_be_viewed(self,username,url):
+        self._login(username)
+        response = self.client.get(url)        
+        self.assertNotEqual(response.status_code, 200, "could load restricted " 
+                            "page'%s' logged in as user '%s'"% (url,username))
+       
     def _signup_user(self,overwrite_data={}):
-        """ Create a user in the same way as a new user is signed up on the site.
+        """Create a user in the same way as a new user is signed up on the site.
         any key specified in data overwrites default key passed to form.
         For example, signup_user({'username':'user1'}) to creates a user called 
         'user1' and fills the rest with default data.  
@@ -131,8 +207,8 @@ class ViewsTest(TestCase):
                 'last_name':'test',
                 'username':'test',            
                 'email':'test@test.com',
-                'password1':'test',
-                'password2':'test',
+                'password1':'testpassword',
+                'password2':'testpassword',
                 'institution':'test',
                 'department':'test', 
                 'country':'NL',
@@ -149,17 +225,30 @@ class ViewsTest(TestCase):
         # items(). Don't know how to better check for type here.
         list = [x[0] for x in signin_page.items()]
         
-        self.assertTrue('Location' in list, "could not create user. dumping"
-                        " page:\n %s"
-                        % signin_page.content)
         
+        self.assertTrue('Location' in list, "could not create user. errors in"
+                        " html:\n %s \n posted data: %s"                        
+                        %(extract_form_errors(signin_page.content),data))
+                        
+        
+        
+    def _create_random_user(self):
+        """ Sign up a user, saves me having to think of a unique name each time 
+        """
+        
+        username = "".join([choice('AEOUY')+choice('QWRTPSDFGHHKLMNB') for x in range(3)])
+        
+        data = {'username':username,
+                'email':username+"@test.com"}
+        
+        return self._create_user(data)
 
     def _create_user(self,data):
         """ Sign up user in a way as close to production as possible. Check a 
         lot of stuff
         
         """        
-        
+        username = data['username']
         self._signup_user(data)
         
         
@@ -168,9 +257,9 @@ class ViewsTest(TestCase):
                         " sent which had 'signup' in the subject line")
         
                     
-        self.assertEqual(self.client.get('/accounts/user1/').status_code,403,
-                         "Could access user account without using validation" 
-                         "link!")
+        self.assertEqual(self.client.get('/accounts/'+username+'/').status_code,
+                         403, "Was not locked out of user account which was not"
+                         "yet validated with link!"),
         
         # validate the user with the link that was emailed
         validationlink_result = re.search('/example.com(.*)\n',
@@ -181,62 +270,26 @@ class ViewsTest(TestCase):
                         "registration email")
         
         validationlink = validationlink_result.group(1)        
-        response = self.client.get(validationlink)
+        response = self.client.get(validationlink)        
         
-        self.assertEqual(self.client.get('/accounts/user1/').status_code,200,
-                         "Could not access user account after using validation" 
-                         "link!")
+        self.assertEqual(self.client.get('/accounts/'+username+'/').status_code,
+                         200,"Could not access user account after using" 
+                         "validation link!")
             
-        welcome_mail = mail.outbox[-1]
-        #TODO check welcome mail
         
-        
-        query_result = User.objects.filter(username=data['username'])        
+        query_result = User.objects.filter(username=username)        
         return query_result[0] 
-        
+     
 
-
-    def _login_as_root_user(self):
-        """ log in comicmodels.tests.ViewsTest object testcase as admin. Assert 
-        whether login was successful. Convenience function to save a few lines of 
-        code.  
-    
-        """        
-        success = self.client.login(username='root',password='rootpassword')    
-        return success
-
-    
-    def _test_as_root(self,url):
-        """ Log in as root and try to load url, will assert whether this works 
-        
-        """   
-                         
-        self._login_as_root_user()            
-        response = self.client.get(url)                        
-        self.assertEqual(response.status_code, 200, "loading %s as root "
-                        "failed, full response was %s" % (url,response.content))
-                        
-    
-    
-        
-    def test_page_permissions_view(self):
-        """ Test that the permissions page does not crash:
-        https://github.com/comic/comic-django/issues/180 
+    def _login(self,username,password="testpassword"):
+        """ convenience function. log in user an assert whether it worked
         
         """
+        self.client.logout()
+        success = self.client.login(username=username,password=password)
+        self.assertTrue(success, "could not log in as user "+username)        
         
-        testpage1 = Page.objects.filter(title='testpage1')
-        self.assert_(testpage1.exists(),"could not find page 'testpage1'")                 
-        url = reverse("admin:comicmodels_page_permissions",
-                      args=[testpage1[0].pk])
-        self._test_as_root(url)
     
-    def test_page_change_view(self):
-        """ Root can see a page 
+    
+    
         
-        """        
-        testpage1 = Page.objects.filter(title='testpage1')
-        self.assert_(testpage1.exists(),"could not find page 'testpage1'")                 
-        url = reverse("admin:comicmodels_page_change",
-                      args=[testpage1[0].pk])
-        self._test_as_root(url)
