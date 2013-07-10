095f09e42e7facfb182e934d6799224da76eb07d
sjoerdk <w.s.kerkstra@gmail.com>
2013-07-10 09:58:42 +0200

    split off reuasble test code into base class, password hashingsetting is now set for the entire class instead of just the setUp method

--- django/comicsite/tests.py
+++ django/comicsite/tests.py
@@ -81,100 +81,10 @@ def extract_form_errors(html):
     
         
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """ Tests that 1 + 1 always equals 2.
+class ComicframeworkTestCase(TestCase):
+    """ Contains methods for creating users using comicframework interface
+    """ 
         
-        """
-        self.assertEqual(1 + 1, 2)
-
-
-class ViewsTest(TestCase):
-    
-    @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.'
-                                     'EmailBackend')
-    
-    #use fast, non-safe password hashing to speed up testing
-    @override_settings(PASSWORD_HASHERS=('django.contrib.auth.hashers.'
-                                        'SHA1PasswordHasher',))
-    
-    def setUp(self):
-        """ Create some objects to work with, In part this is done through
-        admin views, meaning admin views are also tested here.
-        """
-        # Create three types of users that exist: Root, can do anything, 
-        # Siteadmin, cam do things to a site he or she owns. And logged in
-        # user 
-        
-        self.root = User.objects.create_user('root',
-                                        'w.s.kerkstra@gmail.com',
-                                        'testpassword')        
-        self.root.is_staff = True
-        self.root.is_superuser = True
-        self.root.save()
-        
-        # non-root users are created as if they signed up through the site,
-        # to maximize test coverage.        
-        self.registered_user = self._create_user({"username":"registered_user"})
-                                        
-        self.siteadmin = self._create_user({"username":"siteadmin",
-                                            "email":"df@rt.com"}) 
-                    
-        self.testsite = create_comicsite_in_admin(self.siteadmin,"viewtest")                
-        create_page_in_admin(self.testsite,"testpage1")
-        create_page_in_admin(self.testsite,"testpage2")
-                
-        
-    
-    def test_registered_user_can_create_project(self):
-        """ A user freshly registered through the site can immediately create
-        a project
-        
-        """
-        user = self._create_user({"username":"user2","email":"ab@cd.com"})
-        testsite = create_comicsite_in_admin(user,"user1project")                
-        testpage1 = create_page_in_admin(testsite,"testpage1")
-        testpage2 = create_page_in_admin(testsite,"testpage2")
-        
-        self._test_page_can_be_viewed(user.username,testpage1)
-        self._test_page_can_be_viewed(self.root.username,testpage1)
-        
-        
-    
-    def test_page_permissions_view(self):
-        """ Test that the permissions page does not crash: for root
-        https://github.com/comic/comic-django/issues/180 
-        
-        """
-        
-        testpage1 = Page.objects.filter(title='testpage1')
-        self.assert_(testpage1.exists(),"could not find page 'testpage1'")                 
-        url = reverse("admin:comicmodels_page_permissions",
-                      args=[testpage1[0].pk])
-        
-        self._test_url_can_be_viewed(self.root.username,url)
-        
-        otheruser = self._create_random_user()
-        self._test_url_cannot_be_viewed(otheruser.username,url)
-        
-        
-    
-    def test_page_change_view(self):
-        """ Root can see a page 
-        
-        """
-        user = self._create_user({"username":"user3","email":"de@cd.com"})
-        testsite = create_comicsite_in_admin(user,"user3project")                
-        testpage1 = create_page_in_admin(testsite,"testpage1")
-        testpage2 = create_page_in_admin(testsite,"testpage2")                         
-        url = reverse("admin:comicmodels_page_change",
-                      args=[testpage1.pk])
-        
-        self._test_url_can_be_viewed(user.username,url)        
-        self._test_url_can_be_viewed(self.root.username,url)
-        
-        
-
     def _test_page_can_be_viewed(self,username,page):
         page_url = reverse('comicsite.views.page',
                            kwargs={"site_short_name":page.comicsite.short_name,
@@ -287,9 +197,133 @@ class ViewsTest(TestCase):
         """
         self.client.logout()
         success = self.client.login(username=username,password=password)
-        self.assertTrue(success, "could not log in as user "+username)        
+        self.assertTrue(success, "could not log in as user %s using password %s"
+                        % (username,password))        
+
+
+# Decorators applied to the entire class: see 
+# https://docs.djangoproject.com/en/1.4/topics/testing/#django.test.utils.override_settings
+
+#don't send real emails, keep them in memory
+ComicframeworkTestCase = override_settings(EMAIL_BACKEND='django.core.mail.'
+                                         'backends.locmem.EmailBackend')(ComicframeworkTestCase)
+                                        
+#use fast, non-safe password hashing to speed up testing
+ComicframeworkTestCase = override_settings(PASSWORD_HASHERS=('django.contrib.'
+                                           'auth.hashers.SHA1PasswordHasher',))(ComicframeworkTestCase)
+                                          
+
+class ViewsTest(ComicframeworkTestCase):
+        
+    def setUp(self):
+        """ Create some objects to work with, In part this is done through
+        admin views, meaning admin views are also tested here.
+        """
+        # Create three types of users that exist: Root, can do anything, 
+        # Siteadmin, cam do things to a site he or she owns. And logged in
+        # user 
+        
+        self.root = User.objects.create_user('root',
+                                        'w.s.kerkstra@gmail.com',
+                                        'testpassword')        
+        self.root.is_staff = True
+        self.root.is_superuser = True
+        self.root.save()
+        
+        # non-root users are created as if they signed up through the site,
+        # to maximize test coverage.        
+        self.registered_user = self._create_user({"username":"registered_user"})
+                                        
+        self.siteadmin = self._create_user({"username":"siteadmin",
+                                            "email":"df@rt.com"}) 
+                    
+        self.testsite = create_comicsite_in_admin(self.siteadmin,"viewtest")                
+        create_page_in_admin(self.testsite,"testpage1")
+        create_page_in_admin(self.testsite,"testpage2")
+                
         
     
-    
-    
+    def test_registered_user_can_create_project(self):
+        """ A user freshly registered through the site can immediately create
+        a project
         
+        """
+        user = self._create_user({"username":"user2","email":"ab@cd.com"})
+        testsite = create_comicsite_in_admin(user,"user1project")                
+        testpage1 = create_page_in_admin(testsite,"testpage1")
+        testpage2 = create_page_in_admin(testsite,"testpage2")
+                
+        self._test_page_can_be_viewed(user.username,testpage1)
+        self._test_page_can_be_viewed(self.root.username,testpage1)
+        
+        
+    
+    def test_page_permissions_view(self):
+        """ Test that the permissions page does not crash: for root
+        https://github.com/comic/comic-django/issues/180 
+        
+        """
+        
+        testpage1 = Page.objects.filter(title='testpage1')
+        self.assert_(testpage1.exists(),"could not find page 'testpage1'")                 
+        url = reverse("admin:comicmodels_page_permissions",
+                      args=[testpage1[0].pk])
+        
+        self._test_url_can_be_viewed(self.root.username,url)
+        
+        otheruser = self._create_random_user()
+        self._test_url_cannot_be_viewed(otheruser.username,url)
+        
+        
+    
+    def test_page_change_view(self):
+        """ Root can see a page 
+        
+        """
+        user = self._create_user({"username":"user3","email":"de@cd.com"})
+        testsite = create_comicsite_in_admin(user,"user3project")                
+        testpage1 = create_page_in_admin(testsite,"testpage1")
+        testpage2 = create_page_in_admin(testsite,"testpage2")                         
+        url = reverse("admin:comicmodels_page_change",
+                      args=[testpage1.pk])
+        
+        self._test_url_can_be_viewed(user.username,url)        
+        self._test_url_can_be_viewed(self.root.username,url)
+        
+                
+    
+    
+class UploadTest(ComicframeworkTestCase):
+    
+    
+    def setUp(self):
+        """ Create some objects to work with, In part this is done through
+        admin views, meaning admin views are also tested here.
+        """
+        # Create three types of users that exist: Root, can do anything, 
+        # Siteadmin, cam do things to a site he or she owns. And logged in
+        # user 
+        
+        self.root = self._create_user('root',
+                                      'w.s.kerkstra@gmail.com',
+                                      'testpassword')        
+        self.root.is_staff = True
+        self.root.is_superuser = True
+        self.root.save()
+        
+        
+        # non-root users are created as if they signed up through the site,
+        # to maximize test coverage.        
+        self.registered_user = self._create_user({"username":"registered_user"})
+                                        
+        self.siteadmin = self._create_user({"username":"siteadmin",
+                                            "email":"df@rt.com"}) 
+                    
+        self.testsite = create_comicsite_in_admin(self.siteadmin,"viewtest")                
+        create_page_in_admin(self.testsite,"testpage1")
+        create_page_in_admin(self.testsite,"testpage2")
+    
+    
+    
+
+    
