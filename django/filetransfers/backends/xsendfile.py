64caca625b36a291c5d0b39fabb92497def60182
sjoerdk <w.s.kerkstra@gmail.com>
2013-07-23 12:27:24 +0200

    Fixed a wrong call in filetransfers xsendfile backend.

--- django/filetransfers/backends/xsendfile.py
+++ django/filetransfers/backends/xsendfile.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.utils.encoding import smart_str
+import pdb
 
 def serve_file(request, file, save_as, content_type, **kwargs):
+    pdb.set_trace()
     """Lets the web server serve the file using the X-Sendfile extension"""
     response = HttpResponse(content_type=content_type)
-    response['X-Sendfile'] = file.path
+    response['X-Sendfile'] = file.name
     if save_as:
         response['Content-Disposition'] = smart_str(u'attachment; filename=%s' % save_as)
     if file.size is not None:
