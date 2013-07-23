3b416c07e638ec04a5c1ec06d5d2994a4d66afd3
sjoerdk <w.s.kerkstra@gmail.com>
2013-07-23 11:34:33 +0200

    Activated xsendfile backend for serving files

--- django/filetransfers/api.py
+++ django/filetransfers/api.py
@@ -8,7 +8,7 @@ PREPARE_UPLOAD_BACKEND = getattr(settings,
     'filetransfers.backends.default.prepare_upload')
 SERVE_FILE_BACKEND = getattr(settings,
     'SERVE_FILE_BACKEND',
-    'filetransfers.backends.default.serve_file')
+    'filetransfers.backends.xsendfile.serve_file')
 PUBLIC_DOWNLOAD_URL_BACKEND = getattr(settings,
     'PUBLIC_DOWNLOAD_URL_BACKEND',
     'filetransfers.backends.default.public_download_url')
