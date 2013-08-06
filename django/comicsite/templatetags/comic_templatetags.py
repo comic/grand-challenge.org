eeb4c87cc0187b772440cec04001e551446987fb
sjoerdk <w.s.kerkstra@gmail.com>
2013-08-06 23:16:55 +0200

    Improved method description

--- django/comicsite/templatetags/comic_templatetags.py
+++ django/comicsite/templatetags/comic_templatetags.py
@@ -215,7 +215,7 @@ def sanitize_django_items(string):
 
 @register.simple_tag
 def metafooterpages():
-    """ Get the metafooter pages. """
+    """ Get html for links to general pages like 'contact' """
     html_string = "<div class='text'><span>COMIC:</span></div>"
     pages = comicsite.views.getPages(settings.MAIN_PROJECT_NAME)
     for p in pages:
