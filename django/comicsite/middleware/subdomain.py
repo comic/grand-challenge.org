75c72f4824942032c55cd11932d9e10458bb0d2d
sjoerdk <w.s.kerkstra@gmail.com>
2013-08-09 10:57:54 +0200

    small fix to work with sub-subdomains

--- django/comicsite/middleware/subdomain.py
+++ django/comicsite/middleware/subdomain.py
@@ -5,8 +5,12 @@ class SubdomainMiddleware:
     """
     def process_request(self, request):
         """Parse out the subdomain from the request"""
+        import pdb
+        pdb.set_trace()
         request.subdomain = None
         host = request.META.get('HTTP_HOST', '')
         host_s = host.replace('www.', '').split('.')        
         if len(host_s) > 2:
-            request.subdomain = ''.join(host_s[:-2])
+            request.subdomain = '.'.join(host_s[:-2])
+        
+                
