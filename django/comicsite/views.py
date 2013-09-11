de6c39070ba03fd835cce16f4155a65e483919e0
sjoerdk <w.s.kerkstra@gmail.com>
2013-09-11 17:11:35 +0200

    info messages are now also logged to log

--- django/comicsite/views.py
+++ django/comicsite/views.py
@@ -7,6 +7,10 @@ Testing views. Each of these views is referenced in urls.py
 '''
 import pdb
 import mimetypes
+import logging
+ 
+
+
 from os import path
 from django.conf import settings
 from django.contrib.admin.options import ModelAdmin
@@ -617,7 +621,17 @@ def send_email(request):
         
     return HttpResponse(text);
 
-        
+def test_logging(request):    
+    logger = logging.getLogger("django")
+    
+    logger.critical("This is critical")
+    logger.error("This is error")
+    logger.warning("This is warning")
+    logger.info("This is info")
+    logger.debug("This is debug")
+
+    return HttpResponse("logged")
+
 def throw_exception(request):
     """ Test handling of exceptions
     
