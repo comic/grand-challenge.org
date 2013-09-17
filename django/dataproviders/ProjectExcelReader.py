d3f466899398104670fd15b929c919fdc7517766
sjoerdk <w.s.kerkstra@gmail.com>
2013-09-17 11:40:24 +0200

    Projectlinks now sorted by date

--- django/dataproviders/ProjectExcelReader.py
+++ django/dataproviders/ProjectExcelReader.py
@@ -33,22 +33,40 @@ class ProjectExcelReader(object):
         f.write(links)
         f.close()
     
+    
+
     def get_project_links(self):
-        """ Read excel file and output html to show all challenge links
-        listed there 
+        """ Read excel file and with challenge listings and return an array of
+        projectlinks describing each 
         """
+        
         book = open_workbook(self.path)
         sheet = book.sheet_by_name(self.sheetname)
         
         html = ""
         items = self.get_excel_items(sheet)
+        projectlinks = []
         
         for item in items:
             if item["abreviation"] != "":
-                projectlink = ProjectLink(item)        
-                html += projectlink.render_to_HTML()
-
+                projectlink = ProjectLink(item)
+                projectlinks.append(projectlink)
+                        
         book.unload_sheet(self.sheetname)    
+        return projectlinks
+        
+    
+    def get_project_links_html(self):
+        """ Read excel file and output html to show all challenge links
+        listed there 
+        """
+        
+        html = ""
+        projectlinks = self.get_project_links()
+            
+        for projectlink in projectlinks:
+            html += projectlink.render_to_html()
+        
         return html
 
     
