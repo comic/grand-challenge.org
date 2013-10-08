a8ad7038674c47877f84b57ed9116f93fedfb423
sjoerdk <w.s.kerkstra@gmail.com>
2013-10-08 17:28:44 +0200

    silenced exceptions in contextprocessor as even Http404 is rendered as 500 server error because the exception is not thrown inside a view refs#219

--- django/comicsite/contextprocessors/contextprocessors.py
+++ django/comicsite/contextprocessors/contextprocessors.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.conf import settings
 
 from comicsite.views import site_get_standard_vars
+from django.http import Http404
 
 def comic_site(request):
     """ Find out in which comic site this request is loaded. If you cannot
@@ -15,7 +16,15 @@ def comic_site(request):
     """
         
     from django.core.urlresolvers import resolve
-    resolution = resolve(request.path)
+    
+    try:
+        resolution = resolve(request.path)
+    except Http404:
+        #fail silently beacuse any exeception here will cause a 500 server error
+        # on page. Let views show errors but not the context processor
+        resolution = resolve("/")
+        
+    
         
     if resolution.kwargs.has_key("site_short_name"):
         sitename = resolution.kwargs["site_short_name"]
