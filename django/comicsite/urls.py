3970c3a04faec643e9bc67e20d6f9b149e9c1c30
sjoerdk <w.s.kerkstra@gmail.com>
2013-10-10 14:06:44 +0200

    fixes #223, extended comicsite url regexes to also match hyphens

--- django/comicsite/urls.py
+++ django/comicsite/urls.py
@@ -20,35 +20,35 @@ urlpatterns = patterns('',
         
     #url(r'^admin/', include(projectadminsite.urls)),
                              
-    url(r'^(?P<site_short_name>\w+)/$','comicsite.views.site'),
+    url(r'^(?P<site_short_name>[\w-]+)/$','comicsite.views.site'),
         
-    url(r'^(?P<site_short_name>\w+)/admin/', include(projectadminsite.urls)),
+    url(r'^(?P<site_short_name>[\w-]+)/admin/', include(projectadminsite.urls)),
     
-    url(r'^(?P<site_short_name>\w+)/robots\.txt$', TemplateView.as_view(template_name='robots.html'),name="comicsite_robots_txt"),
+    url(r'^(?P<site_short_name>[\w-]+)/robots\.txt$', TemplateView.as_view(template_name='robots.html'),name="comicsite_robots_txt"),
     
     # these registration and account views are viewed in the context of a
     # project
-    url(r'^(?P<site_short_name>\w+)/accounts/signin/$','comicsite.views.signin',name="comicsite_signin"),
-    url(r'^(?P<site_short_name>\w+)/accounts/signup/$','comicsite.views.signup',{'signup_form':SignupFormExtra},name="comicsite_signup"),    
-    url(r'^(?P<site_short_name>\w+)/accounts/signup_complete/$','comicsite.views.signup_complete',name="comicsite_signup_complete"),
+    url(r'^(?P<site_short_name>[\w-]+)/accounts/signin/$','comicsite.views.signin',name="comicsite_signin"),
+    url(r'^(?P<site_short_name>[\w-]+)/accounts/signup/$','comicsite.views.signup',{'signup_form':SignupFormExtra},name="comicsite_signup"),    
+    url(r'^(?P<site_short_name>[\w-]+)/accounts/signup_complete/$','comicsite.views.signup_complete',name="comicsite_signup_complete"),
     
-    # url(r'^(?P<site_short_name>\w+)/accounts/signup/$','comicsite.views.signup',{'signup_form':SignupFormExtra},name="comicsite_signup"),
+    # url(r'^(?P<site_short_name>[\w-]+)/accounts/signup/$','comicsite.views.signup',{'signup_form':SignupFormExtra},name="comicsite_signup"),
     # tell (nice) bots what to do  
     
             
-    url(r'^(?P<site_short_name>\w+)/files/$','comicmodels.views.upload_handler'),
+    url(r'^(?P<site_short_name>[\w-]+)/files/$','comicmodels.views.upload_handler'),
             
     url(r'^(?P<project_name>\w+)/serve/(?P<path>.+)/$','filetransfers.views.serve',name="project_serve_file"),
     
-    url(r'^(?P<site_short_name>\w+)/_register/$','comicsite.views._register'),
+    url(r'^(?P<site_short_name>[\w-]+)/_register/$','comicsite.views._register'),
     
-    url(r'^(?P<site_short_name>\w+)/source/(?P<page_title>\w+)/$','comicsite.views.pagesource'),
+    url(r'^(?P<site_short_name>[\w-]+)/source/(?P<page_title>[\w-]+)/$','comicsite.views.pagesource'),
     
-    url(r'^(?P<site_short_name>\w+)/(?P<page_title>\w+)/db/(?P<dropboxname>\w+)/(?P<dropboxpath>.+)/$','comicsite.views.dropboxpage'),
+    url(r'^(?P<site_short_name>[\w-]+)/(?P<page_title>[\w-]+)/db/(?P<dropboxname>\w+)/(?P<dropboxpath>.+)/$','comicsite.views.dropboxpage'),
     
-    url(r'^(?P<site_short_name>\w+)/(?P<page_title>\w+)/insert/(?P<dropboxpath>.+)/$','comicsite.views.insertedpage'),
+    url(r'^(?P<site_short_name>[\w-]+)/(?P<page_title>[\w-]+)/insert/(?P<dropboxpath>.+)/$','comicsite.views.insertedpage'),
     
-    url(r'^(?P<site_short_name>\w+)/(?P<page_title>\w+)/$','comicsite.views.page'),
+    url(r'^(?P<site_short_name>[\w-]+)/(?P<page_title>[\w-]+)/$','comicsite.views.page'),
     
 )
     
