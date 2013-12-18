0f777b9642b933b04a1f788bd9eced6ba45ca867
sjoerdk <w.s.kerkstra@gmail.com>
2013-12-18 13:57:32 +0100

    refs #228 now filtering RegistrationRequests by project.. all were shown before

--- django/comicsite/middleware/project.py
+++ django/comicsite/middleware/project.py
@@ -17,6 +17,9 @@ class ProjectMiddleware:
         """
                     
         resolution = resolve(request.path)
+                
+        request = self.add_project_pk(request)
+        
         if resolution.kwargs.has_key("site_short_name"):
             projectname = resolution.kwargs["site_short_name"]
         elif resolution.kwargs.has_key("project_name"):
@@ -27,4 +30,15 @@ class ProjectMiddleware:
         request.projectname = projectname
         
 
-        
+    def add_project_pk(self,request):
+        # if the requested url is in admin try to infer from url which project
+        # this is regarding. TODO: the best way to fix this is to have seperate
+        # admin sites for each project #181..
+        #         
+        resolution = resolve(request.path)
+        if resolution.url_name == "comicmodels_comicsite_participantrequests":
+            project_pk = resolution.kwargs["object_pk"]
+        else:
+            project_pk = -1 
+        request.project_pk = project_pk
+        return request
