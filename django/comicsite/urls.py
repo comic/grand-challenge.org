c1b42ec9def3c28d08ddee37f13fc9ec440a77a1
sjoerdk <w.s.kerkstra@gmail.com>
2014-04-24 16:44:16 +0200

    fixes image upload issue: ckeditor urls are now also available under /site/projectname/

--- django/comicsite/urls.py
+++ django/comicsite/urls.py
@@ -25,6 +25,9 @@ urlpatterns = patterns('',
     
     url(r'^(?P<site_short_name>[\w-]+)/robots\.txt$', TemplateView.as_view(template_name='robots.html'),name="comicsite_robots_txt"),
     
+    
+    #url(r'^(?P<site_short_name>[\w-]+)/ckeditor/', include('ckeditor.urls')),
+    
     # these registration and account views are viewed in the context of a
     # project
     url(r'^(?P<site_short_name>[\w-]+)/accounts/signin/$','comicsite.views.signin',name="comicsite_signin"),
