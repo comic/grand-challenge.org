820438519291d11780af6cb0f0b59a352871b98e
sjoerdk <w.s.kerkstra@gmail.com>
2014-09-19 16:49:27 +0200

    Image viewer is now working

--- django/comicsite/api.py
+++ django/comicsite/api.py
@@ -10,14 +10,29 @@ from comicsite.views import get_data_folder_path,get_dirnames
 
 
 def get_public_results(request,project_name):
-        
-    data_folder_path = get_data_folder_path(project_name)    
-    public_results_path = path.join(data_folder_path,"results/public/")    
-    
+    """ Return an array of strings respresenting all results that can be publically
+    viewed.
+    """
     try:
-        dirnames = get_dirnames(public_results_path)
+        public_results = get_public_results_by_project_name(project_name)
     except OSError:
         return HttpResponse("Cannot list public results. Public results should be in folder'results/public/' but this folder could not be found'")
     
-    return HttpResponse(json.dumps(dirnames))
+    return json.dumps(public_results) 
+        
+        
+    
+def get_public_results_by_project_name(project_name):
+    """ Made a separate method here to be able to also call this API function from
+    within other parts of the code.
+    
+    raises OSError when no results folder can be found for this project
+    """
+        
+    data_folder_path = get_data_folder_path(project_name)    
+    public_results_path = path.join(data_folder_path,"results/public/")
+    dirnames = get_dirnames(public_results_path)
+        
+
+    return dirnames
     
