1ebc401466b879baad378d353e0f5d784d38d4f0
james <james.meakin@radboudumc.nl>
2017-04-19 09:23:27 +0200

    Added Rollbar and refactored settings for python 3.

--- django/comic/settings.py
+++ django/comic/settings.py
@@ -1,8 +1,13 @@
 # Django settings for comic project.
+import glob
 import os
-import glob 
+import six
+
 from django.core.exceptions import ImproperlyConfigured
 
+# These need to be set in the conf files
+MEDIA_ROOT = ''
+DROPBOX_ROOT = ''
 
 # Get *.conf from the directory this file is in and execute these in order. 
 # To include your own local settings, put these in a  a 'XX-local.conf' file in the
@@ -10,21 +15,22 @@ from django.core.exceptions import ImproperlyConfigured
 # Executed last overwrites previous settings.  
 
 path = os.path.join(os.path.dirname(__file__), 'settings', '*.conf')
-conffiles = glob.glob(path)
+conf_files = glob.glob(path)
 
-if len(conffiles) == 0:
+if len(conf_files) == 0:
     msg = "Could not find any files matching '" + path + "'. There should be at least one configuration file containing django settings at that location."
-    raise ImproperlyConfigured(msg) 
+    raise ImproperlyConfigured(msg)
 
-conffiles.sort()
-for f in conffiles:
-    execfile(os.path.abspath(f))
-
+conf_files.sort()
+for conf_file in conf_files:
+    with open(conf_file) as f:
+        code = compile(f.read(), conf_file, 'exec')
+        six.exec_(code)
 
 if MEDIA_ROOT[-1] != "/":
-    msg = "MEDIA_ROOT setting should end in a slash. Found '" +MEDIA_ROOT+ "'. Please add a slash"
+    msg = "MEDIA_ROOT setting should end in a slash. Found '" + MEDIA_ROOT + "'. Please add a slash"
     raise ImproperlyConfigured(msg)
 
 if DROPBOX_ROOT[-1] != "/":
-    msg = "DROPBOX_ROOT setting should end in a slash. Found '" +DROPBOX_ROOT+ "'. Please add a slash"
+    msg = "DROPBOX_ROOT setting should end in a slash. Found '" + DROPBOX_ROOT + "'. Please add a slash"
     raise ImproperlyConfigured(msg)
