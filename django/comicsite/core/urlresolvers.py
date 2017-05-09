8341e93773b08502738d99ae26093b7739aed151
James Meakin <jamesmeakin@gmail.com>
2017-05-09 14:31:00 +0200

    Fixes apps arent loaded

--- django/comicsite/core/urlresolvers.py
+++ django/comicsite/core/urlresolvers.py
@@ -1,6 +1,5 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse as reverse_org
-from comic import settings
-from comicsite.templatetags.comic_templatetags import subdomain_is_projectname
 
 
 def reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None, current_app=None):
@@ -16,11 +15,13 @@ def reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None, current
     # See for example the custom url template tag in comicsite.templatetags, which 
     # does almost the same as this method but in an even more complex way. Both
     # These methods should be refactored until they shine.  
-    
+
+    from comicsite.templatetags.comic_templatetags import subdomain_is_projectname
+
     if viewname == 'comicsite.views.site' and subdomain_is_projectname() and args[0] is not None:
-        protocol,domainname = settings.MAIN_HOST_NAME.split("//")
-        url = protocol + "//" +args[0] +"."+ domainname
-        
-        return url    
+        protocol, domainname = settings.MAIN_HOST_NAME.split("//")
+        url = protocol + "//" + args[0] + "." + domainname
+
+        return url
     else:
         return reverse_org(viewname, urlconf, args, kwargs, prefix, current_app)
