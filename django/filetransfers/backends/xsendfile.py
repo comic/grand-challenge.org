aa60d3bd9cf3a0bbd0b0752138cc770ea1c83e52
J Meakin <jamesmeakin@gmail.com>
2017-05-19 14:34:51 +0200

    Update xsendfile.py

--- django/filetransfers/backends/xsendfile.py
+++ django/filetransfers/backends/xsendfile.py
@@ -6,7 +6,7 @@ from django.utils.encoding import smart_str
 def serve_file(request, file, save_as, content_type, **kwargs):
     """Lets the web server serve the file using the X-Sendfile extension"""
     response = HttpResponse(content_type=content_type)
-    response['X-Sendfile'] = file.name
+    response['X-Accel-Redirect'] = file.name
     if save_as:
         response['Content-Disposition'] = smart_str(u'attachment; filename=%s' % save_as)
     if file.size is not None:
