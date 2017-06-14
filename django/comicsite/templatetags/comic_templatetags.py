7eb6549c77260c75d165f8a0b7dff798acf8e04f
James Meakin <jamesmeakin@gmail.com>
2017-06-14 12:10:43 +0200

    Removes filelist

--- django/comicsite/templatetags/comic_templatetags.py
+++ django/comicsite/templatetags/comic_templatetags.py
@@ -352,36 +352,6 @@ def metafooterpages():
     return html_string
 
 
-@register.tag(name="filelist")
-def do_get_files(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, filefolder = token.split_contents()
-        format_string = "\"%Y-%m-%d %I:%M %p\""
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return FileListNode(format_string[1:-1], filefolder[1:-1])
-
-
-class FileListNode(template.Node):
-    """ Show list of files in given dir
-    """
-
-    def __init__(self, format_string, filefolder):
-        self.format_string = format_string
-        self.filefolder = filefolder
-
-    def render(self, context):
-        dp = FileSystemDataProvider.FileSystemDataProvider(self.filefolder)
-        images = dp.getImages()
-
-        htmlOut = "available files:" + ", ".join(images)
-        return htmlOut
-
-
-# ========#========#========#========#========#========#========#========
 @register.tag(name="dataset",
               usagestr="""Tag usage: {% dataset <datasetname>,<comicsitename> %}. <comicsitename> can be\
                   omitted, defaults to current site"""
