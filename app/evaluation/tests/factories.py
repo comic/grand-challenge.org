a10a61a7c21b1cd06a136728f210c1629d6a022f
James Meakin <jamesmeakin@gmail.com>
2017-06-20 17:35:21 +0200

    Use factory_boy for faster model testing

---
+++ app/evaluation/tests/factories.py
@@ -0,0 +1,27 @@
+from factory import Factory, SubFactory
+from comicmodels.models import ComicSite
+from evaluation.models import Submission, Job, Method, Result
+
+class ComicSiteFactory(Factory):
+    class Meta:
+        model = ComicSite
+
+    short_name = 'test_challenge'
+
+class SubmissionFactory(Factory):
+    class Meta:
+        model = Submission
+
+    challenge = SubFactory(ComicSiteFactory)
+
+class JobFactory(Factory):
+    class Meta:
+        model = Job
+
+class MethodFactory(Factory):
+    class Meta:
+        model = Method
+
+class ResultFactory(Factory):
+    class Meta:
+        model = Result
---
+++ app/evaluation/tests/test_models.py
@@ -0,0 +1,5 @@
+from .factories import SubmissionFactory
+
+def test_submission():
+    submission = SubmissionFactory()
+    assert submission.challenge.short_name == 'test_challenge'
