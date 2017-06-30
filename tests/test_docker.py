07599c4fe40f81889f77d13054a0da4ab229097f
James Meakin <jamesmeakin@gmail.com>
2017-06-30 15:46:26 +0200

    Adds basic authentication to the registry

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -9,6 +9,19 @@ def test_docker_registry_push():
         ['docker', 'tag', 'alpine:latest', 'localhost:5000/my-alpine'])
     assert response == 0
 
+    # This should fail as we have basic auth inplace
+    response = subprocess.call(
+        ['docker', 'push', 'localhost:5000/my-alpine']
+    )
+    assert response == 1
+
+    # Authenticate
+    response = subprocess.call(
+        ['docker', 'login', '-u', 'testuser', '-p', 'testpassword', 'localhost:5000']
+    )
+    assert response == 0
+
+    # Now it should work
     response = subprocess.call(
         ['docker', 'push', 'localhost:5000/my-alpine']
     )
@@ -26,5 +39,10 @@ def test_docker_registry_push():
 
     response = subprocess.call(
         ['docker', 'image', 'pull', 'localhost:5000/my-alpine']
+    )
+    assert response == 0
+
+    response = subprocess.call(
+        ['docker', 'logout', 'localhost:5000']
     )
     assert response == 0
