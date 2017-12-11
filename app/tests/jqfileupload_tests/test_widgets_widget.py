dabdb50fe5007d2e77607763a9200590e74a2f5a
Paul K. Gerke <pkcakeout@gmail.com>
2017-12-11 17:35:00 +0100

    Added rfc7233 based upload tests (happy route only)
    TRAC#7301

--- app/tests/jqfileupload_tests/test_widgets_widget.py
+++ app/tests/jqfileupload_tests/test_widgets_widget.py
@@ -1,10 +1,12 @@
 import os
 import uuid
-
-import pytest
+import random
 import json
+import time
 
 from datetime import timedelta
+
+import pytest
 
 from django.http.response import JsonResponse
 from django.test.client import RequestFactory
@@ -19,6 +21,8 @@ def load_test_data():
             'rb') as f:
         return f.read()
 
+def generate_new_upload_id(sender, content):
+    return f"{id(sender)}_{hash(content)}_{time.time()}_{random.random()}"
 
 def create_upload_file_request(
         rf: RequestFactory,
@@ -26,7 +30,8 @@ def create_upload_file_request(
         boundary: str="RandomBoundaryFTWBlablablablalba8923475278934578",
         content: bytes=None,
         csrf_token: str="tests_csrf_token",
-        extra_fields: dict={}):
+        extra_fields: dict={},
+        extra_headers: dict={}):
     if content is None:
         content = load_test_data()
 
@@ -49,12 +54,43 @@ Content-Disposition: form-data; name="{key}"\r
 """.lstrip().encode()
         data = extra_field_data + data
 
+    headers = {
+        "X-CSRFToken": csrf_token
+    }
+    for key, value in extra_headers.items():
+        headers[key] = value
+
     post_request = rf.post(
         "/ajax",
         data=data,
         content_type=f"multipart/form-data; boundary={boundary}",
-        **{"X-CSRFToken": csrf_token})
+        **headers)
     post_request.META["CSRF_COOKIE"] = csrf_token
+
+    return post_request
+
+
+def create_partial_upload_file_request(
+        rf: RequestFactory,
+        upload_identifer: str,
+        content: bytes,
+        start_byte: int,
+        end_byte: int,
+        filename: str = "test.bin"):
+    content_range = f"bytes {start_byte}-{end_byte-1}/{len(content)}"
+
+    post_request = create_upload_file_request(
+        rf,
+        filename=filename,
+        content=content[start_byte:end_byte],
+        extra_headers={
+            "Content-Range": content_range
+        },
+        extra_fields={
+            "X-Upload-ID": upload_identifer
+        })
+
+    post_request.META["HTTP_CONTENT_RANGE"] = content_range
 
     return post_request
 
@@ -67,7 +103,6 @@ def test_one_big_upload(rf: RequestFactory):
     filename = 'test.bin'
     post_request = create_upload_file_request(rf, filename=filename)
     response = widget.handle_ajax(post_request)
-
     assert isinstance(response, JsonResponse)
 
     parsed_json = json.loads(response.content)
@@ -79,8 +114,36 @@ def test_one_big_upload(rf: RequestFactory):
 
     staged_file = StagedAjaxFile(uuid.UUID(parsed_json[0]["uuid"]))
     with staged_file.open() as f:
-        assert f.read() == load_test_data()
+        staged_content = f.read()
 
+    assert staged_content == load_test_data()
 
+@pytest.mark.django_db
 def test_rfc7233_implementation(rf: RequestFactory):
-    pass
+    content = load_test_data()
+    upload_id = generate_new_upload_id(test_rfc7233_implementation, content)
+
+    part_1 = create_partial_upload_file_request(
+        rf, upload_id, content, 0, 10)
+    part_2 = create_partial_upload_file_request(
+        rf, upload_id, content, 10, len(content) // 2)
+    part_3 = create_partial_upload_file_request(
+        rf, upload_id, content, len(content) // 2, len(content))
+
+    widget = AjaxUploadWidget(ajax_target_path="/ajax")
+    widget.timeout = timedelta(seconds=1)
+
+    response = widget.handle_ajax(part_1)
+    assert isinstance(response, JsonResponse)
+    response = widget.handle_ajax(part_2)
+    assert isinstance(response, JsonResponse)
+    response = widget.handle_ajax(part_3)
+    assert isinstance(response, JsonResponse)
+
+    parsed_json = json.loads(response.content)
+    staged_file = StagedAjaxFile(uuid.UUID(parsed_json[0]["uuid"]))
+
+    with staged_file.open() as f:
+        staged_content = f.read()
+
+    assert staged_content == content
