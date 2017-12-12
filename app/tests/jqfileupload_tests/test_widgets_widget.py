469cce0150ddadddb2dfa77fcd270ac9b19bb5f5
Paul K. Gerke <pkcakeout@gmail.com>
2017-12-12 12:00:49 +0100

    Added tests for the form field
    TRAC#7301

--- app/tests/jqfileupload_tests/test_widgets_widget.py
+++ app/tests/jqfileupload_tests/test_widgets_widget.py
@@ -7,13 +7,15 @@ import time
 from datetime import timedelta
 
 import pytest
+from django.core.exceptions import ValidationError
 
 from django.http.response import JsonResponse, HttpResponseForbidden, \
     HttpResponseBadRequest
 from django.test.client import RequestFactory
 
 from jqfileupload.models import StagedFile
-from jqfileupload.widgets.uploader import AjaxUploadWidget, StagedAjaxFile
+from jqfileupload.widgets.uploader import AjaxUploadWidget, StagedAjaxFile, \
+    UploadedAjaxFileList
 
 
 def load_test_data():
@@ -207,3 +209,26 @@ def test_render():
     assert isinstance(render_result, str)
     render_result = widget.render("some_name", (uuid.uuid4(), uuid.uuid4()))
     assert isinstance(render_result, str)
+
+
+def test_form_field_to_python():
+    form_field = UploadedAjaxFileList()
+
+    uuid_string = "4dec34db-930f-48be-bb65-d7f8319ff654," + \
+        "5d901b2c-7cd1-416e-9952-d30b6a0edcba," + \
+        "4a3c5731-0050-4489-8364-282278f7190f"
+    staged_files = form_field.to_python(uuid_string)
+
+    assert ",".join(str(sf.uuid) for sf in staged_files) == uuid_string
+
+    with pytest.raises(ValidationError):
+        form_field.to_python("asdfasdfsdafasdf")
+
+    other_string = uuid_string[10:]
+    with pytest.raises(ValidationError):
+        form_field.to_python(other_string)
+
+
+def test_form_field_prepare_value_not_implemented():
+    form_field = UploadedAjaxFileList()
+    assert form_field.prepare_value("") is None
