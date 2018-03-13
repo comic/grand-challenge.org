22bb6ef5ac0f00e788a4fed247aa76fd984137c9
James Meakin <jamesmeakin@gmail.com>
2018-03-13 08:57:41 +0100

    TRAC #7494 Test uploading

--- app/tests/uploads_tests/test_ckeditor.py
+++ app/tests/uploads_tests/test_ckeditor.py
@@ -1,19 +1,30 @@
 import pytest
 
+from comicmodels.models import UploadModel
 from tests.utils import get_view_for_user
 
 
 @pytest.mark.django_db
 def test_upload_with_ckeditor(client, tmpdir, TwoChallengeSets):
-    p = tmpdir.mkdir("sub").join("hello.txt")
+    filename = 'hello.txt'
+
+    p = tmpdir.mkdir("sub").join(filename)
     p.write("content")
 
-    response = get_view_for_user(
-        viewname='uploads:ck-create',
-        challenge=TwoChallengeSets.ChallengeSet1.challenge,
-        user=TwoChallengeSets.admin12,
-        client=client,
-    )
+    num_files = UploadModel.objects.all().count()
 
-    assert response.status_code == 200
+    with open(p, 'rb') as f:
+        response = get_view_for_user(
+            viewname='uploads:ck-create',
+            challenge=TwoChallengeSets.ChallengeSet1.challenge,
+            user=TwoChallengeSets.admin12,
+            client=client,
+            method=client.post,
+            data={
+                'upload': f,
+            },
+            format='multipart',
+        )
 
+    assert response.status_code == 302
+    assert UploadModel.objects.all().count() == num_files + 1
