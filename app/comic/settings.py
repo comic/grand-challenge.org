50bb1584994d7fffe0cc191652441978e72f162e
James Meakin <jamesmeakin@gmail.com>
2018-03-13 09:43:52 +0100

    TRAC #7494 WIP Fix staticfiles
    The staticfile finding on travis is failing, trying to move the stuff
    into the app to fix.

--- app/comic/settings.py
+++ app/comic/settings.py
@@ -165,15 +165,6 @@ SESSION_COOKIE_DOMAIN = None
 # Serve files using django (debug only)
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-
-    SITE_ROOT + "/" + "static",
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
