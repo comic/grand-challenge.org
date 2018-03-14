57b67237ead4753369e63e5eb8309cba0f0d9c4b
James Meakin <jamesmeakin@gmail.com>
2018-03-14 17:08:44 +0100

    404 wp-login at nginx

--- app/comic/settings.py
+++ app/comic/settings.py
@@ -271,7 +271,7 @@ INSTALLED_APPS = (
     'challenges',
 )
 
-ADMIN_URL = f'^{os.environ.get("DJANGO_ADMIN_URL", "admin")}/'
+ADMIN_URL = f'^{os.environ.get("DJANGO_ADMIN_URL", "django-admin")}/'
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
