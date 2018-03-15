5e371351422ec1a717ec241fb2d179b5106c62a2
James Meakin <jamesmeakin@gmail.com>
2018-03-15 14:22:19 +0100

    Fix #426 Adds user profile link filter

---
+++ app/profiles/templatetags/profiles.py
@@ -0,0 +1,23 @@
+from django import template
+from django.contrib.auth.models import AbstractUser
+from django.utils.html import format_html
+
+from comicsite.core.urlresolvers import reverse
+
+register = template.Library()
+
+
+@register.filter
+def user_profile_link(user: AbstractUser) -> str:
+    return format_html(
+        ('<div style="vertical-align:middle; display:inline;">'
+         '  <a href="{0}">'
+         '    <img class="mugshot" src="{1}" alt="User Mugshot" '
+         '         style="height: 1.5em; vertical-align: middle;"/>'
+         '    {2}'
+         '  </a>'
+         '</div>'),
+        reverse('userena_profile_detail', args=[user]),
+        user.user_profile.get_mugshot_url(),
+        user.username,
+    )
