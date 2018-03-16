2d740aa40211b3804d204eee90310b5a9e0140ce
James Meakin <jamesmeakin@gmail.com>
2018-03-16 13:07:49 +0100

    Fix #412 Improve sign up form
    There is an issue in the production db right now in that it cannot
    handle utf-8, so make the sign up form force ASCII.

--- app/profiles/forms.py
+++ app/profiles/forms.py
@@ -1,34 +1,82 @@
 import userena.forms as userena_forms
 from django import forms
+from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 from django_countries import countries
 
 
 class SignupFormExtra(userena_forms.SignupForm):
-    first_name = forms.CharField(label=_(u'First Name'),
-                                 max_length=30,
-                                 required=True)
-    last_name = forms.CharField(label=_(u'Last Name'),
-                                max_length=30,
-                                required=True)
-    institution = forms.CharField(label=_(u'Institution'),
-                                  max_length=100,
-                                  required=True,
-                                  help_text=_(
-                                      u'Institution you are affiliated to.'))
-    department = forms.CharField(label=_(u'Department'),
-                                 max_length=100,
-                                 required=True,
-                                 help_text=_(u'Department you represent.'))
+    username = forms.RegexField(
+        regex=r'^[a-zA-Z0-9]+$',
+        max_length=30,
+        widget=forms.TextInput(
+            attrs=userena_forms.attrs_dict),
+        label=_("Username"),
+        error_messages={'invalid': _(
+            'Username must contain only letters and numbers.')
+        }
+    )
+    first_name = forms.CharField(
+        label=_(u'First Name'),
+        max_length=30,
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z]+$',
+                message=_(u"First name must only contain the characters a-z."),
+            )
+        ],
+    )
+    last_name = forms.CharField(
+        label=_(u'Last Name'),
+        max_length=30,
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z-\s]+$',
+                message=_(
+                    u"Last name must only contain the characters a-z, spaces or hypens."),
+            ),
+        ],
+    )
+    institution = forms.CharField(
+        label=_(u'Institution'),
+        max_length=100,
+        required=True,
+        help_text=_(
+            u'Institution you are affiliated to.'),
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z-\s]+$',
+                message=_(
+                    u"Institution must only contain the characters a-z, spaces or hypens."),
+            ),
+        ],
+    )
+    department = forms.CharField(
+        label=_(u'Department'),
+        max_length=100,
+        required=True,
+        help_text=_(u'Department you represent.'),
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z-\s]+$',
+                message=_(
+                    u"Department must only contain the characters a-z, spaces or hypens."),
+            ),
+        ],
+    )
     country = forms.ChoiceField(label=_(u'Country'),
                                 choices=tuple(
                                     [('00', _('-' * 9))] + list(countries)),
                                 required=True)
-    website = forms.CharField(label=_(u'Website'),
-                              max_length=150,
-                              required=False,
-                              help_text=_(
-                                  u'A website which describes you or your department'))
+    website = forms.URLField(
+        label=_(u'Website'),
+        max_length=150,
+        required=False,
+        help_text=_(
+            u'A website which describes you or your department')
+    )
 
     def __init__(self, *args, **kw):
         super(SignupFormExtra, self).__init__(*args, **kw)
