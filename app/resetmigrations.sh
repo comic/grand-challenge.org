2f55066f469b42ca156f9a78ad639b6d35817bac
James Meakin <jamesmeakin@gmail.com>
2018-03-20 18:41:10 +0100

    Adds a script to reset the migrations

---
+++ app/resetmigrations.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for app in uploads teams participants pages evaluation comicmodels profiles jqfileupload
+do
+    python manage.py migrate --fake $app zero
+    find . -path "./$app/migrations/*.py" -not -name "__init__.py" -delete
+    find . -path "./$app/migrations/*.pyc"  -delete
+done
+
+python manage.py makemigrations
+python manage.py migrate --fake-initial
