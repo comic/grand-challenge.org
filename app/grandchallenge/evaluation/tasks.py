15d93ac169e6998f886ba15b9b0c73194c6fb547
James Meakin <jamesmeakin@gmail.com>
2018-04-10 13:38:19 +0200

    Retries dropped mysql connections
    Fix #462

--- app/grandchallenge/evaluation/tasks.py
+++ app/grandchallenge/evaluation/tasks.py
@@ -3,6 +3,7 @@ import tarfile
 import uuid
 
 from celery import shared_task
+from django.db import OperationalError
 from django.db.models import Q
 
 from grandchallenge.challenges.models import Challenge
@@ -12,6 +13,46 @@ from grandchallenge.evaluation.backends.dockermachine.evaluator import (
 from grandchallenge.evaluation.exceptions import EvaluationException
 from grandchallenge.evaluation.models import Job, Result, Method
 from grandchallenge.evaluation.utils import generate_rank_dict
+
+
+def retry_if_dropped(func):
+    """
+    Sometimes the Mysql connection will drop for long running jobs. This is
+    a decorator that will retry a function that relies on a usable connection.
+    """
+
+    def wrapper(*args, **kwargs):
+        n_tries = 0
+        max_tries = 2
+        err = None
+
+        while n_tries < max_tries:
+            n_tries += 1
+
+            try:
+                return func(*args, **kwargs)
+            except OperationalError as e:
+                err = e
+
+                # This needs to be a local import
+                from django.db import connection
+                connection.close()
+
+        raise err
+
+    return wrapper
+
+
+@retry_if_dropped
+def get_job(*, job_pk) -> Job:
+    return Job.objects.get(pk=job_pk)
+
+
+@retry_if_dropped
+def create_result(*, metrics, job_pk):
+    job = get_job(job_pk=job_pk)
+    Result.objects.create(job=job, metrics=metrics, challenge=job.challenge)
+    job.update_status(status=Job.SUCCESS)
 
 
 @shared_task
@@ -27,16 +68,19 @@ def evaluate_submission(*, job_pk: uuid.UUID = None, job: Job = None) -> dict:
     :return:
     """
     if (job_pk is None and job is None) or (
-        job_pk is not None and job is not None
+            job_pk is not None and job is not None
     ):
         raise TypeError(
             'You need to provide either a job or a job_id as '
             'arguments to evaluate_submission, not none or both.'
         )
 
-    if job_pk:
-        job = Job.objects.get(pk=job_pk)  # type: Job
+    if job:
+        job_pk = job.pk
+
+    job = get_job(job_pk=job_pk)
     job.update_status(status=Job.STARTED)
+
     if not job.method.ready:
         # TODO: email admin
         job.update_status(
@@ -47,18 +91,19 @@ def evaluate_submission(*, job_pk: uuid.UUID = None, job: Job = None) -> dict:
 
     try:
         with Evaluator(
-            job_id=job.pk,
-            input_file=job.submission.file,
-            eval_image=job.method.image,
-            eval_image_sha256=job.method.image_sha256,
+                job_id=job.pk,
+                input_file=job.submission.file,
+                eval_image=job.method.image,
+                eval_image_sha256=job.method.image_sha256,
         ) as e:
-            metrics = e.evaluate()
+            metrics = e.evaluate() # This call is potentially very long
     except EvaluationException as exc:
+        job = get_job(job_pk=job_pk)
         job.update_status(status=Job.FAILURE, output=exc.message)
         return {}
 
-    Result.objects.create(job=job, metrics=metrics, challenge=job.challenge)
-    job.update_status(status=Job.SUCCESS)
+    create_result(metrics=metrics, job_pk=job_pk)
+
     return metrics
 
 
