7db13100007f45ccee36c6c75b26ac5c3e6087e2
James Meakin <jamesmeakin@gmail.com>
2018-04-12 21:23:52 +0200

    Adds chunked uploading
    There's a bug when running minio on a non standard port, so there is a
    workaround from this report: https://github.com/TTLabs/EvaporateJS/issues/317

--- app/grandchallenge/minioupload/urls.py
+++ app/grandchallenge/minioupload/urls.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from grandchallenge.minioupload.views import presigned_url, MinioFileUpload
+from grandchallenge.minioupload.views import presigned_url, MinioFileUpload, \
+    EvaporateFileUpload, presign_string
 
 urlpatterns =[
     path('presign/<path:filename>/', presigned_url),
-    path('upload/', MinioFileUpload.as_view())
+    path('presign/', presign_string),
+    path('upload/', MinioFileUpload.as_view()),
+    path('evaporate/', EvaporateFileUpload.as_view()),
 ]
--- app/grandchallenge/minioupload/views.py
+++ app/grandchallenge/minioupload/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import datetime
+import hashlib
+import hmac
 from datetime import timedelta
 
 from django.conf import settings
@@ -6,11 +9,15 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from minio import Minio
 from minio.error import BucketAlreadyOwnedByYou, BucketAlreadyExists
-from minio.signer import presign_v4
+from minio.signer import presign_v4, generate_signing_key
+
+
+def get_bucket_name():
+    return 'testbucket'
 
 
 def presigned_url(request, filename):
-    bucket_name = 'testbucket'
+    bucket_name = get_bucket_name()
     access_key = settings.MINIO_ACCESS_KEY
     secret_key = settings.MINIO_SECRET_KEY
 
@@ -40,5 +47,44 @@ def presigned_url(request, filename):
     return HttpResponse(url)
 
 
+def presign_string(request):
+    # TODO: Try this with a real AWS bucket
+    to_sign = request.GET['to_sign']
+    date = request.GET['datetime']
+
+    canonical_request = request.GET['canonical_request']
+    current_canonical_signature = hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
+
+    new_canonical_request = canonical_request.replace('host:localhost', 'host:localhost:9000')
+    new_canonical_signature = hashlib.sha256(new_canonical_request.encode('utf-8')).hexdigest()
+
+    to_sign = to_sign.replace(current_canonical_signature, new_canonical_signature)
+
+
+    signing_key = generate_signing_key(datetime.datetime.strptime(date,
+                                                                  '%Y%m%dT%H%M%SZ'),
+                                       'us-east-1',
+                                       settings.MINIO_SECRET_KEY)
+    signature = hmac.new(signing_key, to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
+
+    return HttpResponse(signature, content_type='text/HTML')
+
+
 class MinioFileUpload(TemplateView):
     template_name = 'minio.html'
+
+
+class EvaporateFileUpload(TemplateView):
+    template_name = 'evaporate.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update(
+            {
+                'MINIO_ACCESS_KEY': settings.MINIO_ACCESS_KEY,
+                'MINIO_BUCKET_NAME': get_bucket_name(),
+            }
+        )
+
+        return context
