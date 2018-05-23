4f3563485f813a3efe33b24ea78ba6b4940c1a7a
James Meakin <jamesmeakin@gmail.com>
2018-05-23 17:24:14 +0200

    Removes minio direct upload

--- app/grandchallenge/minioupload/views.py
+++ app/grandchallenge/minioupload/views.py
@@ -2,49 +2,15 @@
 import datetime
 import hashlib
 import hmac
-from datetime import timedelta
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from minio import Minio
-from minio.error import BucketAlreadyOwnedByYou, BucketAlreadyExists
-from minio.signer import presign_v4, generate_signing_key
+from minio.signer import generate_signing_key
 
 
 def get_bucket_name():
     return settings.MINIO_DEFAULT_BUCKET_NAME
-
-
-def presigned_url(request, filename):
-    bucket_name = get_bucket_name()
-    access_key = settings.MINIO_ACCESS_KEY
-    secret_key = settings.MINIO_SECRET_KEY
-
-    minio_client = Minio(
-        'minio:9000',  # the minio url as seen by the web container
-        access_key=access_key,
-        secret_key=secret_key,
-        secure=False,
-    )
-
-    try:
-        minio_client.make_bucket(bucket_name)
-    except (BucketAlreadyExists, BucketAlreadyOwnedByYou):
-        pass
-
-    # Usually you would use minio_client.presigned_put_object, but the client
-    # cannot see this url. So, create this ourselves by using the publically
-    # available url.
-    url = presign_v4(
-        'PUT',
-        f'{settings.MINIO_PUBLIC_URL}/{bucket_name}/{filename}',
-        access_key=access_key,
-        secret_key=secret_key,
-        expires=int(timedelta(days=1).total_seconds()),
-    )
-
-    return HttpResponse(url)
 
 
 def presign_string(request):
@@ -74,10 +40,6 @@ def presign_string(request):
     ).hexdigest()
 
     return HttpResponse(signature, content_type='text/HTML')
-
-
-class MinioFileUpload(TemplateView):
-    template_name = 'minio.html'
 
 
 class EvaporateFileUpload(TemplateView):
