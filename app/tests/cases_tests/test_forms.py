10a6444d0ed45daa545a6ac884b9c7a527d456f4
Paul K. Gerke <pkcakeout@gmail.com>
2018-06-26 16:49:40 +0200

    Add more tests for user-facing forms, refactor tests to use django-client through grandchallenge utility functions
    Issue #479

--- app/tests/cases_tests/test_forms.py
+++ app/tests/cases_tests/test_forms.py
@@ -1,42 +1,52 @@
+import re
+from uuid import UUID
+
 import pytest
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponse
 from django.test import Client, RequestFactory
-from django.views import View
 
-from grandchallenge.cases.views import UploadRawFiles
-from tests.factories import UserFactory
+from grandchallenge.cases import signals
+from grandchallenge.cases.models import RawImageUploadSession
+from tests.cases_tests import RESOURCE_PATH
+from tests.factories import SUPER_SECURE_TEST_PASSWORD
+from tests.jqfileupload_tests.external_test_support import \
+    create_file_from_filepath
+
+
+signals.PREVENT_JOB_CREATION_ON_SAVE = True
 
 
 @pytest.mark.django_db
-def test_upload_some_images(client: Client, rf: RequestFactory):
-    """
-    Not working?! :-(
-     - something with template tags goes wrong here...
-
+def test_upload_some_images(client: Client, rf: RequestFactory, ChallengeSet):
+    test_user = ChallengeSet.participant
 
     response = client.get("/cases/upload/")
     assert response.status_code != 200
 
-    client.force_login(test_user)
+    assert client.login(username=test_user.username, password=SUPER_SECURE_TEST_PASSWORD)
 
     response = client.get("/cases/upload/")
-    assert response.status_code == 200
-    """
-    test_user = UserFactory()
-
-    upload_view = UploadRawFiles.as_view()
-    upload_view: View
-
-    req = rf.get("/cases/upload/")
-    req.user = AnonymousUser()
-    response = upload_view(req)
-    response: HttpResponse
-    assert response.status_code != 200
-
-    req.user = test_user
-    response = upload_view(req)
     response: HttpResponse
     assert response.status_code == 200
 
+    file1 = create_file_from_filepath(RESOURCE_PATH / "image10x10x10.mha")
+
+    response = client.post(
+        "/cases/upload/",
+        data={
+            "files": f"{file1.uuid}",
+        }
+    )
+    response: HttpResponse
+    assert response.status_code == 302
+
+    redirect_match = re.match(
+        r"/cases/uploaded/(?P<uuid>[^/]+)/?$",
+        response["Location"])
+    assert redirect_match is not None
+    assert RawImageUploadSession.objects.filter(
+        pk=UUID(redirect_match.group("uuid"))).exists()
+
+    response = client.get(response["Location"])
+    response: HttpResponse
+    assert response.status_code == 200
