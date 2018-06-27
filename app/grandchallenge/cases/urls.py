df9c340da619f0b0a694629c5406db5af3d3a328
Paul K. Gerke <pkcakeout@gmail.com>
2018-06-27 16:06:34 +0200

    Add upload filter checking for uploaded files in one session
    Issue #479

--- app/grandchallenge/cases/urls.py
+++ app/grandchallenge/cases/urls.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from grandchallenge.cases.forms import upload_raw_files_widget
+from grandchallenge.cases.forms import upload_raw_files_widget, \
+    duplicate_file_rejecter
 from grandchallenge.cases.views import UploadRawFiles, \
     ShowUploadSessionState, ViewImage
 
@@ -11,7 +12,8 @@ urlpatterns = [
     path('upload/', UploadRawFiles.as_view(), name='create'),
     path(
         f"upload/{upload_raw_files_widget.ajax_target_path}",
-        upload_raw_files_widget.handle_ajax,
+        duplicate_file_rejecter.install_request_updater(
+            upload_raw_files_widget.handle_ajax),
         name="upload-raw-image-files-ajax",
     ),
     path(
