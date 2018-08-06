5500e8b1c7673a53961d6d4c3d332f3c9c9c8e46
James Meakin <jamesmeakin@gmail.com>
2018-08-06 14:30:04 +0200

    Adds favicon views for browsers that do not follow links

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.template.response import TemplateResponse
-from django.urls import re_path
+from django.urls import re_path, path
 from django.views.generic import TemplateView, RedirectView
 
+from grandchallenge.pages.views import FaviconView
 from grandchallenge.core.views import comicmain
 from grandchallenge.uploads.views import serve
 
@@ -26,6 +27,34 @@ urlpatterns = [
             template_name='robots.txt', content_type='text/plain'
         ),
     ),
+
+    # Favicons
+    path(
+        'favicon.ico/',
+        FaviconView.as_view(rel='shortcut icon'),
+        name='favicon',
+    ),
+    path(
+        'apple-touch-icon.png/',
+        FaviconView.as_view(rel='apple-touch-icon'),
+        name='apple-touch-icon',
+    ),
+    path(
+        'apple-touch-icon-precomposed.png/',
+        FaviconView.as_view(rel='apple-touch-icon-precomposed'),
+        name='apple-touch-icon-precomposed',
+    ),
+    path(
+        'apple-touch-icon-<int:size>x<int>.png/',
+        FaviconView.as_view(rel='apple-touch-icon'),
+        name='apple-touch-icon-sized',
+    ),
+    path(
+        'apple-touch-icon-<int:size>x<int>-precomposed.png/',
+        FaviconView.as_view(rel='apple-touch-icon-precomposed'),
+        name='apple-touch-icon-precomposed-sized',
+    ),
+
     url(settings.ADMIN_URL, admin.site.urls),
     url(r'^site/', include('grandchallenge.core.urls'), name='site'),
     # Do not change the namespace without updating the view names in
