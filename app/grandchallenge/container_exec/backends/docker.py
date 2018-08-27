579eec10aad5e6b5ecd50885c93d706bf9a5f7f0
James Meakin <jamesmeakin@gmail.com>
2018-08-27 11:46:31 +0200

    Fix AttributeError

--- app/grandchallenge/container_exec/backends/docker.py
+++ app/grandchallenge/container_exec/backends/docker.py
@@ -192,7 +192,7 @@ def put_file(*, container: ContainerApiMixin, src: File, dest: str) -> ():
 def get_file(*, container: ContainerApiMixin, src: Path) -> File:
     tarstrm, info = container.get_archive(src)
 
-    if info.size > 2E9:
+    if info["size"] > 2E9:
         raise ValueError(f"File {src} is too big to be decompressed.")
 
     file_obj = io.BytesIO()
