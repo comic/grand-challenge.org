3326a987bf723cbec1c3c3275a4894636cc6536f
James Meakin <jamesmeakin@gmail.com>
2018-09-04 14:08:07 +0200

    Types the values in the csv file

--- app/grandchallenge/datasets/utils.py
+++ app/grandchallenge/datasets/utils.py
@@ -2,11 +2,36 @@
 import csv
 import itertools
 from codecs import iterdecode
+from typing import Union
 
 
 def lower_first(iterator):
     """ Lowers the first line of a file """
     return itertools.chain([next(iterator).lower()], iterator)
+
+
+def infer_type(value: str) -> Union[str, float, int]:
+    """
+    Tries to convert the value to its implicit type, returns the original if
+    unsuccessful
+    """
+    operations = [int, float]
+
+    for op in operations:
+        try:
+            return op(value)
+        except ValueError:
+            continue
+
+    return value
+
+
+def type_values(input: dict) -> dict:
+    """
+    Takes a dictionary of strings and tries to convert the data to the
+    intended type
+    """
+    return {k: infer_type(v) for k, v in input.items()}
 
 
 def process_csv_file(filehandle):
@@ -15,4 +40,4 @@ def process_csv_file(filehandle):
         lower_first(iterdecode(filehandle, encoding="utf-8")),
         skipinitialspace=True,
     )
-    return [row for row in reader]
+    return [type_values(row) for row in reader]
