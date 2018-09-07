423b06feb8cafb6d1e2f8e30e49093e523e758ff
James Meakin <jamesmeakin@gmail.com>
2018-09-07 12:23:35 +0200

    Replace url with path

--- app/grandchallenge/evaluation/urls.py
+++ app/grandchallenge/evaluation/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from grandchallenge.evaluation.forms import (
     method_upload_widget,
@@ -24,60 +24,48 @@ from grandchallenge.evaluation.views import (
 app_name = "evaluation"
 
 urlpatterns = [
-    url(r"^config/$", ConfigUpdate.as_view(), name="config-update"),
-    url(r"^methods/$", MethodList.as_view(), name="method-list"),
-    url(r"^methods/create/$", MethodCreate.as_view(), name="method-create"),
-    url(
+    path("config/", ConfigUpdate.as_view(), name="config-update"),
+    path("methods/", MethodList.as_view(), name="method-list"),
+    path("methods/create/", MethodCreate.as_view(), name="method-create"),
+    path(
         f"^methods/create/{method_upload_widget.ajax_target_path}$",
         method_upload_widget.handle_ajax,
         name="method-upload-ajax",
     ),
-    url(
-        r"^methods/(?P<pk>[0-9a-fA-F-]+)/$",
-        MethodDetail.as_view(),
-        name="method-detail",
-    ),
-    url(r"^submissions/$", SubmissionList.as_view(), name="submission-list"),
-    url(
-        r"^submissions/create/$",
+    path("methods/<uuid:pk>/", MethodDetail.as_view(), name="method-detail"),
+    path("submissions/", SubmissionList.as_view(), name="submission-list"),
+    path(
+        "submissions/create/",
         SubmissionCreate.as_view(),
         name="submission-create",
     ),
-    url(
-        r"^submissions/create-legacy/$",
+    path(
+        "submissions/create-legacy/",
         LegacySubmissionCreate.as_view(),
         name="submission-create-legacy",
     ),
-    url(
-        f"^submissions/create-legacy/{submission_upload_widget.ajax_target_path}$",
+    path(
+        f"submissions/create-legacy/{submission_upload_widget.ajax_target_path}",
         submission_upload_widget.handle_ajax,
         name="submission-upload-legacy-ajax",
     ),
-    url(
-        f"^submissions/create/{submission_upload_widget.ajax_target_path}$",
+    path(
+        f"submissions/create/{submission_upload_widget.ajax_target_path}",
         submission_upload_widget.handle_ajax,
         name="submission-upload-ajax",
     ),
-    url(
-        r"^submissions/(?P<pk>[0-9a-fA-F-]+)/$",
+    path(
+        "submissions/<uuid:pk>/",
         SubmissionDetail.as_view(),
         name="submission-detail",
     ),
-    url(r"^jobs/$", JobList.as_view(), name="job-list"),
-    url(r"^jobs/create/$", JobCreate.as_view(), name="job-create"),
-    url(
-        r"^jobs/(?P<pk>[0-9a-fA-F-]+)/$",
-        JobDetail.as_view(),
-        name="job-detail",
-    ),
-    url(r"^results/$", ResultList.as_view(), name="result-list"),
-    url(
-        r"^results/(?P<pk>[0-9a-fA-F-]+)/$",
-        ResultDetail.as_view(),
-        name="result-detail",
-    ),
-    url(
-        r"^results/(?P<pk>[0-9a-fA-F-]+)/update/$",
+    path("jobs/", JobList.as_view(), name="job-list"),
+    path("jobs/create/", JobCreate.as_view(), name="job-create"),
+    path("jobs/<uuid:pk>/", JobDetail.as_view(), name="job-detail"),
+    path("results/", ResultList.as_view(), name="result-list"),
+    path("results/<uuid:pk>/", ResultDetail.as_view(), name="result-detail"),
+    path(
+        "results/<uuid:pk>/update/",
         ResultUpdate.as_view(),
         name="result-update",
     ),
