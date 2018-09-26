70f72e24428ec1d7fde1526186bd6d120a46bf84
James Meakin <jamesmeakin@gmail.com>
2018-09-26 16:57:55 +0200

    Use make to build and deploy the containers

--- dockerfiles/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login --username grandchallenge --password-stdin
-
-docker push grandchallenge/http:"$TRAVIS_BUILD_NUMBER"
-docker push grandchallenge/web:"$TRAVIS_BUILD_NUMBER"
