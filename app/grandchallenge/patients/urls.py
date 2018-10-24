323169db3156a77c54478aec742657d53d46110a
Karel <karelgerbrands@gmail.com>
2018-10-24 14:35:27 +0200

    - Fixes the URL app name to the correct format
    #570

--- app/grandchallenge/patients/urls.py
+++ app/grandchallenge/patients/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from grandchallenge.patients import views
 
-app_name = "api"
+app_name = "patients"
 urlpatterns = [
     url(r'^patients/$', views.PatientTable.as_view()),
     url(r'^patients/(?P<pk>[0-9]+)$', views.PatientRecord.as_view())
