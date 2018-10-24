5424133dd03c659cd71649d9b3704683ade4e675
Karel <karelgerbrands@gmail.com>
2018-10-24 15:42:54 +0200

    - Speculative fix for regex paths
    #570

--- app/grandchallenge/patients/urls.py
+++ app/grandchallenge/patients/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from grandchallenge.patients import views
 
 app_name = "patients"
 urlpatterns = [
-    path('patients/&', views.PatientTable.as_view(), name="patients"),
-    path('patients/(?P<pk>[0-9]+)$', views.PatientRecord.as_view(), name="patient")
+    path('patients/', views.PatientTable.as_view(), name="patients"),
+    re_path('patients/(?P<pk>[0-9]+)$', views.PatientRecord.as_view(), name="patient")
 ]
--- app/grandchallenge/studies/urls.py
+++ app/grandchallenge/studies/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from grandchallenge.patients import views
 
 app_name = "studies"
 urlpatterns = [
-    path('studies/&', views.StudyTable.as_view(), name="patients"),
-    path('studies/(?P<pk>[0-9]+)$', views.StudyRecord.as_view(), name="patient")
+    path('studies/', views.StudyTable.as_view(), name="patients"),
+    re_path('studies/(?P<pk>[0-9]+)$', views.StudyRecord.as_view(), name="patient")
 ]
