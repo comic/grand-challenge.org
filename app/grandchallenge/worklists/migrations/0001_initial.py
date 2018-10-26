9ed9b01d21330bc8f28b5594881808dd03bbc877
Karel <karelgerbrands@gmail.com>
2018-10-26 12:37:49 +0200

    - Fixes small bug in Worklists migration
    #570

--- app/grandchallenge/worklists/migrations/0001_initial.py
+++ app/grandchallenge/worklists/migrations/0001_initial.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255, verbose_name='Title of Work list')),
                 ('group', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='worklists.Group')),
-                ('parent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='worklist.Worklist')),
+                ('parent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='worklists.Worklist')),
             ],
         ),
     ]
