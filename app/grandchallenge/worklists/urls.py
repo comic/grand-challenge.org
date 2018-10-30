ca64588011bc9119dbc6511d24728c07262a2782
Karel <karelgerbrands@gmail.com>
2018-10-30 14:33:33 +0100

    - Fixes url issues, caused by the renaming of group to WorklistGroup
    #570

--- app/grandchallenge/worklists/urls.py
+++ app/grandchallenge/worklists/urls.py
@@ -3,8 +3,8 @@ from grandchallenge.worklists import views
 
 app_name = "worklists"
 urlpatterns = [
-    path('worklist_groups/', views.GroupTable.as_view(), name="worklist_groups"),
-    re_path(r'^worklist_groups/(?P<pk>[0-9]+)$', views.GroupRecord.as_view(), name="worklist_group"),
+    path('worklist_groups/', views.WorklistGroupTable.as_view(), name="worklist_groups"),
+    re_path(r'^worklist_groups/(?P<pk>[0-9]+)$', views.WorklistGroupRecord.as_view(), name="worklist_group"),
     path('worklists/', views.WorklistTable.as_view(), name="worklists"),
     re_path(r'^worklists/(?P<pk>[0-9]+)$', views.WorklistRecord.as_view(), name="worklist")
 ]
