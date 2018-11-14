083b526c01fe584a8056580965436909553be02c
Karel <karelgerbrands@gmail.com>
2018-11-14 17:42:29 +0100

    - Adds incomplete tests that check patient table access and crud functionality
    #607

--- app/tests/patients_tests/test_api.py
+++ app/tests/patients_tests/test_api.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 from django.utils.encoding import force_text
 from rest_framework.authtoken.models import Token
 
-from tests.factories import UserFactory
+from grandchallenge.patients.serializer import PatientSerializer
+from tests.factories import UserFactory, PatientFactory
 
 
 def get_staff_user_with_token():
@@ -18,13 +19,20 @@ def get_staff_user_with_token():
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     "test_input, expected",
-    [("patients", "Patient Table")], #("patient", "Patient Record")
+    [("patients", "Patient Table")],  # ("patient", "Patient Record")
 )
-def test_api_pages(client, test_input, expected):
+def test_table_pages(client, test_input, expected):
     _, token = get_staff_user_with_token()
-
-    # Check for the correct HTML view
     url = reverse(f"patients:{test_input}")
+    patient = PatientFactory()
+
+    # Checks the HTML View
+    test_table_access(client, url, token, expected)
+    # Checks insertions and acquires id
+    id = test_table_insert(client, url, token, patient)
+
+
+def test_table_access(client, url, token, expected):
     response = client.get(
         url, HTTP_ACCEPT="text/html", HTTP_AUTHORIZATION="Token " + token
     )
@@ -39,3 +47,21 @@ def test_api_pages(client, test_input, expected):
     )
     assert response.status_code == 200
     assert not json.loads(response.content)
+
+
+def test_table_insert(client, url, token, patient):
+    response = client.post(url, PatientSerializer.serialize("json", patient), HTTP_ACCEPT="application/json", HTTP_AUTHORIZATION="Token " + token)
+    json_response = json.loads(response.loads)
+
+    assert response.status_code == 200
+    return json_response["id"]
+
+
+
+def test_record_display(client, url, token, id):
+
+
+def test_record_update():
+
+
+def test_record_delete():
