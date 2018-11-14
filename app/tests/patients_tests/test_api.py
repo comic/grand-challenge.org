d1f18e1c7d828644714c4cd32c674eb9fd7d8fec
Karel <karelgerbrands@gmail.com>
2018-11-14 15:24:05 +0100

    - Speculative fix for patient view tests - Adds API page access tests
    #607

---
+++ app/tests/patients_tests/test_api.py
@@ -0,0 +1,42 @@
+import json
+
+import pytest
+from django.urls import reverse
+from django.utils.encoding import force_text
+from rest_framework.authtoken.models import Token
+
+from grandchallenge.patients import Patient
+from tests.factories import UserFactory
+
+
+def get_staff_user_with_token():
+    user = UserFactory(is_staff=True)
+    token = Token.objects.create(user=user)
+
+    return user, token.key
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [("patients", "Patient Table"), ("patient", "Patient Record")],
+)
+def test_api_pages(client, test_input, expected):
+    _, token = get_staff_user_with_token()
+
+    # Check for the correct HTML view
+    url = reverse(f"patients:{test_input}")
+    response = client.get(
+        url, HTTP_ACCEPT="text/html", HTTP_AUTHORIZATION="Token " + token
+    )
+    assert expected in force_text(response.content)
+    assert response.status_code == 200
+
+    # There should be no content, but we should be able to do json.loads
+    response = client.get(
+        url,
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token,
+    )
+    assert response.status_code == 200
+    assert not json.loads(response.content)
--- app/tests/patients_tests/test_views.py
+++ app/tests/patients_tests/test_views.py
@@ -8,10 +8,7 @@ from tests.utils import get_view_for_user
 
 @pytest.mark.django_db
 def test_patient_list(client):
-    visible = PatientFactory(hidden=False)
-    #invisible = PatientFactory(hidden=True)
-
+    visible = PatientFactory()
     response = get_view_for_user(client=client, viewname="patients:patient-list")
-
     assert str(visible.id) in response.rendered_content
-    #assert invisible.id not in response.rendered_content
+
