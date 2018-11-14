931f252f04edc1c9400208a2c2827db05465b1d5
Karel <karelgerbrands@gmail.com>
2018-11-14 11:09:15 +0100

    - Disables study tests for now - Adds a bare test for patient list views
    #606 #607

--- app/tests/patients_tests/test_views.py
+++ app/tests/patients_tests/test_views.py
@@ -6,10 +6,10 @@ from tests.utils import get_view_for_user
 
 @pytest.mark.django_db
 def test_patient_list(client):
-    visible = PatientFactory(hidden=False)
-    invisible = PatientFactory(hidden=True)
+    visible = PatientFactory()
+    #invisible = PatientFactory(hidden=True)
 
     response = get_view_for_user(client=client, viewname="patients:patient-list")
 
     assert visible.id in response.rendered_content
-    assert invisible.id not in response.rendered_content
+    #assert invisible.id not in response.rendered_content
