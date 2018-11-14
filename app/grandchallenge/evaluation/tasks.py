93a76293bfac3306112580769b2080145c7b2c10
James Meakin <jamesmeakin@gmail.com>
2018-11-14 17:19:08 +0100

    Refactor rank calculations

--- app/grandchallenge/evaluation/tasks.py
+++ app/grandchallenge/evaluation/tasks.py
@@ -5,15 +5,55 @@ from django.db.models import Q
 
 from grandchallenge.challenges.models import Challenge
 from grandchallenge.evaluation.models import Result, Config
-from grandchallenge.evaluation.utils import generate_rank_dict
+from grandchallenge.evaluation.utils import generate_ranks
+
+
+def filter_results_by_most_recent(*, results):
+    # Go through the results and only pass through the most recent
+    # submission for each user
+    users_seen = set()
+    filtered_qs = []
+
+    for r in results:
+        creator = r.job.submission.creator
+
+        if creator not in users_seen:
+            users_seen.add(creator)
+            filtered_qs.append(r)
+
+    return filtered_qs
+
+
+def filter_results_by_users_best(*, results, ranks):
+
+    best_result_per_user = {}
+
+    for r in results:
+        creator = r.job.submission.creator
+
+        try:
+            this_rank = ranks[str(r.pk)]
+        except KeyError:
+            # This result was not ranked
+            continue
+
+        if creator not in best_result_per_user or (
+            this_rank < ranks[str(best_result_per_user[creator].pk)]
+        ):
+            best_result_per_user[creator] = r
+
+    return [r for r in best_result_per_user.values()]
 
 
 @shared_task
 def calculate_ranks(*, challenge_pk: uuid.UUID):
     challenge = Challenge.objects.get(pk=challenge_pk)
-    score_path = challenge.evaluation_config.score_jsonpath
-    default_sort = challenge.evaluation_config.score_default_sort
     display_choice = challenge.evaluation_config.result_display_choice
+
+    metric_paths = (challenge.evaluation_config.score_jsonpath,)
+    metric_reverse = (
+        challenge.evaluation_config.score_default_sort == Config.DESCENDING,
+    )
 
     valid_results = (
         Result.objects.filter(Q(challenge=challenge), Q(published=True))
@@ -22,57 +62,28 @@ def calculate_ranks(*, challenge_pk: uuid.UUID):
     )
 
     if display_choice == Config.MOST_RECENT:
-        # Go through the results and only pass through the most recent
-        # submission for each user
-        users_seen = set()
-        queryset = []
-
-        for r in valid_results:
-            creator = r.job.submission.creator
-
-            if creator not in users_seen:
-                users_seen.add(creator)
-                queryset.append(r)
-
+        queryset = filter_results_by_most_recent(results=valid_results)
     elif display_choice == Config.BEST:
-
-        all_ranks = generate_rank_dict(
-            queryset=valid_results,
-            metric_paths=(score_path,),
-            metric_reverse=(default_sort == Config.DESCENDING,),
+        queryset = filter_results_by_users_best(
+            results=valid_results,
+            ranks=generate_ranks(
+                queryset=valid_results,
+                metric_paths=metric_paths,
+                metric_reverse=metric_reverse,
+            ),
         )
-
-        best_result_per_user = {}
-
-        for r in valid_results:
-            creator = r.job.submission.creator
-
-            try:
-                this_rank = all_ranks[str(r.pk)][score_path]
-            except KeyError:
-                # This result was not ranked
-                continue
-
-            if creator not in best_result_per_user or (
-                this_rank
-                < all_ranks[str(best_result_per_user[creator].pk)][score_path]
-            ):
-                best_result_per_user[creator] = r
-
-        queryset = [r for r in best_result_per_user.values()]
-
     else:
         queryset = valid_results
 
-    ranks = generate_rank_dict(
+    ranks = generate_ranks(
         queryset=queryset,
-        metric_paths=(score_path,),
-        metric_reverse=(default_sort == Config.DESCENDING,),
+        metric_paths=metric_paths,
+        metric_reverse=metric_reverse,
     )
 
     for res in Result.objects.filter(Q(challenge=challenge)):
         try:
-            rank = ranks[str(res.pk)][score_path]
+            rank = ranks[str(res.pk)]
         except KeyError:
             # This result will be excluded from the display
             rank = 0
--- app/grandchallenge/evaluation/utils.py
+++ app/grandchallenge/evaluation/utils.py
@@ -7,7 +7,8 @@ from grandchallenge.evaluation.templatetags.evaluation_extras import (
 )
 
 
-def generate_rank_dict(
+def generate_ranks(
+    *,
     queryset: Tuple[Result, ...],
     metric_paths: Tuple[str, ...],
     metric_reverse: Tuple[bool, ...],
@@ -16,35 +17,41 @@ def generate_rank_dict(
     Generates a dictionary that contains the ranking of results based on a
     given metric path.
     """
-    rank = defaultdict(dict)
-    pk_val = namedtuple("pk_val", ["pk", "val"])
+
+    metric_rank = defaultdict(dict)
 
     for (metric_path, reverse) in zip(metric_paths, metric_reverse):
         # Extract the value of the metric for this primary key and sort on the
         # value of the metric
-        pk_vals = [
-            pk_val(str(res.pk), get_jsonpath(res.metrics, metric_path))
+        metric_results = [
+            {
+                "pk": str(res.pk),
+                "value": get_jsonpath(res.metrics, metric_path),
+            }
             for res in queryset
             if get_jsonpath(res.metrics, metric_path) != ""
         ]
-        pk_vals.sort(key=lambda x: x.val, reverse=reverse)
+        metric_results.sort(key=lambda x: x["value"], reverse=reverse)
 
         # Assign the ranks
         try:
-            current_val = pk_vals[0].val
+            current_val = metric_results[0]["value"]
             current_rank = 1
         except IndexError:
             # No results to work with for this metric
             continue
 
-        for idx, result_pk_val in enumerate(pk_vals):
+        for idx, result in enumerate(metric_results):
 
             # If the values of the metrics are the same, keep the rank
             # position the same
-            if result_pk_val.val != current_val:
-                current_val = result_pk_val.val
+            if result["value"] != current_val:
+                current_val = result["value"]
                 current_rank = idx + 1
 
-            rank[result_pk_val.pk][metric_path] = current_rank
+            metric_rank[result["pk"]][metric_path] = current_rank
 
-    return rank
+    if len(metric_paths) == 1:
+        return {pk: r[metric_paths[0]] for pk, r in metric_rank.items()}
+    else:
+        raise NotImplementedError
