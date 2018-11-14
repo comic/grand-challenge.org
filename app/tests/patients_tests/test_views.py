ecb4c744f90366b0100ecc87ab8febaf648e9633
Karel <karelgerbrands@gmail.com>
2018-11-14 14:47:00 +0100

    - Speculative fix of list-view test
    #607

--- app/tests/patients_tests/test_views.py
+++ app/tests/patients_tests/test_views.py
@@ -3,13 +3,15 @@ import pytest
 from tests.factories import PatientFactory
 from tests.utils import get_view_for_user
 
+"""" Tests the forms available for Patient CRUD """
+
 
 @pytest.mark.django_db
 def test_patient_list(client):
-    visible = PatientFactory()
+    visible = PatientFactory(hidden=False)
     #invisible = PatientFactory(hidden=True)
 
     response = get_view_for_user(client=client, viewname="patients:patient-list")
 
-    assert visible.id in response.rendered_content
+    assert str(visible.id) in response.rendered_content
     #assert invisible.id not in response.rendered_content
