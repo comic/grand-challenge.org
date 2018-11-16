fc62024b83d55cd2beb7077a8152cf9dcdc1be3f
Karel <karelgerbrands@gmail.com>
2018-11-16 10:30:00 +0100

    - Adds crispy forms for patient updates and creates
    #570

--- app/grandchallenge/patients/forms.py
+++ app/grandchallenge/patients/forms.py
@@ -1,8 +1,42 @@
 from django.forms import ModelForm
 from grandchallenge.patients.models import Patient
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 class PatientDetailForm(ModelForm):
     class Meta:
         model = Patient
         fields = ("name", "sex", "height")
+
+
+class PatientCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout.append(Submit("create", "Create"))
+
+    class Meta:
+        model = Patient
+        fields = [
+            "name",
+            "sex",
+            "height",
+        ]
+
+
+class PatientUpdateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout.append(Submit("save", "Save"))
+
+    class Meta:
+        model = Patient
+        fields = [
+            "id",
+            "name",
+            "sex",
+            "height",
+        ]
