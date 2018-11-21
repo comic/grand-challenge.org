84639b406269b656653051447476c493feb2ec11
Karel <karelgerbrands@gmail.com>
2018-11-21 12:20:37 +0100

    - Disables an unused import
    #606

--- app/tests/worklist_tests/test_permissions.py
+++ app/tests/worklist_tests/test_permissions.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.factories import WorklistFactory
+#from tests.factories import WorklistFactory
 from tests.utils import validate_staff_only_view
 
 """" Tests the permission access for Patient Forms """
