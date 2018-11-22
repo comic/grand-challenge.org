676131ea6d44f7e39f350a7e09f56f0c7c05f821
Karel <karelgerbrands@gmail.com>
2018-11-22 17:22:43 +0100

    - Fixes an error where no user was created for testing - Fixes an error where a table wasn't named correctly for testing
    #607 #622

--- app/tests/patients_tests/test_views.py
+++ app/tests/patients_tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.factories import PatientFactory
+from tests.factories import PatientFactory, UserFactory
 from tests.utils import get_view_for_user
 
 """" Tests the forms available for Patient CRUD """
@@ -9,5 +9,7 @@ from tests.utils import get_view_for_user
 @pytest.mark.django_db
 def test_patient_list(client):
     visible = PatientFactory()
+
+    staff_user = UserFactory(is_staff=True)
     response = get_view_for_user(client=client, viewname="patients:patient-list", user=staff_user)
     assert str(visible.id) in response.rendered_content
--- app/tests/worklist_tests/test_api.py
+++ app/tests/worklist_tests/test_api.py
@@ -6,7 +6,7 @@ from tests.api_utils import assert_api_crud
 @pytest.mark.parametrize(
     "table_reverse, record_reverse, expected_table, object_factory",
     [("worklists:worklists", "worklists:worklists", "Worklist Table", WorklistFactory),
-     ("worklists:sets", "worklists:sets", "WorklistSet Table", WorklistSetFactory)],
+     ("worklists:sets", "worklists:sets", "Worklist Set Table", WorklistSetFactory)],
 )
 def test_api_pages(client, table_reverse, record_reverse, expected_table, object_factory):
     assert_api_crud(client, table_reverse, record_reverse, expected_table, object_factory)
