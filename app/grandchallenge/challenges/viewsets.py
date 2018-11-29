7f0cbe27d6f46a3b94922c2673de6e1c89c6d13c
Roel Zinkstok <rzinkstok@gmail.com>
2018-11-29 11:51:31 +0100

    Add API challenge create/update, including tests

--- app/grandchallenge/challenges/viewsets.py
+++ app/grandchallenge/challenges/viewsets.py
@@ -1,15 +1,34 @@
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.permissions import BasePermission
+from rest_framework.viewsets import ModelViewSet
 
 from grandchallenge.challenges.models import Challenge
 from grandchallenge.challenges.serializers import ChallengeSerializer
 
 
+class ChallengeAccessPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "GET":
+            # List
+            return True
+        if request.method == "POST":
+            # Create: only authenticated users
+            return request.user and request.user.is_authenticated
+        if request.method == "PUT":
+            # Update: only authenticated users (object permission is checked next)
+            return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, challenge):
+        if request.method == "PUT":
+            # Update: only challenge admins
+            return challenge.is_admin(request.user)
+        return True
+
+
 class ChallengeViewSet(ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (ChallengeAccessPermission,)
 
-#    def perform_create(self, serializer):
-#        serializer.save(creator=self.request.user)
-
+    def perform_create(self, serializer):
+        # Add the logged in user as the challenge creator
+        serializer.save(creator=self.request.user)
--- app/tests/challenges_tests/test_api.py
+++ app/tests/challenges_tests/test_api.py
@@ -21,23 +21,86 @@ def get_staff_user_with_token():
 
 
 @pytest.mark.django_db
-def test_api_challenge(client):
+def test_api_challenge_get(client):
     _, token = get_normal_user_with_token()
 
-    n_challenges = 2
-    for i in range(n_challenges):
-        _ = ChallengeFactory()
+    n_challenges = 5
+    challenges = [ChallengeFactory() for i in range(n_challenges)]
 
     url = reverse(f"api:challenge-list")
     response = client.get(
-        url, HTTP_ACCEPT="text/html", HTTP_AUTHORIZATION="Token " + token
+        url,
+        HTTP_ACCEPT="text/html",
+        HTTP_AUTHORIZATION="Token " + token
     )
     assert response.status_code == 200
     assert "Challenge List" in force_text(response.content)
 
     response = client.get(
-        url, HTTP_ACCEPT="application/json", HTTP_AUTHORIZATION="Token " + token
+        url,
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token
     )
     assert response.status_code == 200
     j = json.loads(response.content)
     assert len(j) == n_challenges
+    for i, c in enumerate(challenges):
+        assert j[i]['short_name'] == f'test_challenge_{i}'
+        assert j[i]['creator'] == challenges[i].creator.pk
+
+    url = reverse("api:challenge-detail", kwargs={"pk": challenges[0].pk})
+    response = client.get(
+        url,
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token
+    )
+    assert response.status_code == 200
+    j = json.loads(response.content)
+    assert j["short_name"] == challenges[0].short_name
+
+
+@pytest.mark.django_db
+def test_api_challenge_post(client):
+    user, token = get_normal_user_with_token()
+    short_name = "test-challenge"
+
+    url = reverse(f"api:challenge-list")
+    response = client.post(
+        url,
+        {"short_name": short_name},
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token
+    )
+    assert response.status_code == 201
+    j = json.loads(response.content)
+    assert j["short_name"] == short_name
+    assert j["creator"] == user.pk
+
+
+@pytest.mark.django_db
+def test_api_challenge_put(client):
+    c = ChallengeFactory()
+    token = Token.objects.create(user=c.creator).key
+    updated_short_name = "updated-short-name"
+
+    url = reverse(f"api:challenge-detail", kwargs={"pk": c.pk})
+    response = client.put(
+        url,
+        {"short_name": updated_short_name},
+        content_type="application/json",
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token
+    )
+    assert response.status_code == 200
+    j = json.loads(response.content)
+    assert j["short_name"] == updated_short_name
+
+    user2, token2 = get_normal_user_with_token()
+    response = client.put(
+        url,
+        {"short_name": "this-goes-wrong"},
+        content_type="application/json",
+        HTTP_ACCEPT="application/json",
+        HTTP_AUTHORIZATION="Token " + token2
+    )
+    assert response.status_code == 403
