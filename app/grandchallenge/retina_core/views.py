91db9cc166d9c81253735ff44b8b5a63e809ebb9
Harm van Zeeland <harm.van.zeeland@gmail.com>
2018-12-04 17:31:06 +0100

    Replace loginrequiredmixin with retina specific mixin for index page ref #639

--- app/grandchallenge/retina_core/views.py
+++ app/grandchallenge/retina_core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from rest_framework import permissions
 from django.contrib.auth.mixins import LoginRequiredMixin
+from grandchallenge.retina_api.mixins import RetinaAPIPermissionMixin
 from django.views import View
 
 
-class IndexView(LoginRequiredMixin, View):  #TODO replace mixin with right authentication mixin
+class IndexView(RetinaAPIPermissionMixin, View):
     def get(self, request):
         return render(request, "pages/home.html")
 
