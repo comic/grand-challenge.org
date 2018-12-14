bd686edf66f459b17963af18430d1447bd2f43d9
Harm van Zeeland <harm.van.zeeland@gmail.com>
2018-12-14 15:11:31 +0100

    Add admin for cases

---
+++ app/grandchallenge/cases/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import ImageFile, Image
+
+
+class ImageFileInline(admin.StackedInline):
+    model = ImageFile
+    extra = 0
+
+
+class ImageAdmin(admin.ModelAdmin):
+    search_fields = (
+        "pk",
+        "name",
+    )
+    inlines = [ImageFileInline]
+
+
+admin.site.register(Image, ImageAdmin)
