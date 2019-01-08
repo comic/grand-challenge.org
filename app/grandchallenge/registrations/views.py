d62265f481638e796b3dedc61fb4498936fcb108
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-01-08 15:30:45 +0100

    Change registration viewset permissions to IsAdminUser

--- app/grandchallenge/registrations/views.py
+++ app/grandchallenge/registrations/views.py
@@ -4,6 +4,10 @@ from .serializers import OctObsRegistrationSerializer
 
 
 class OctObsRegistrationViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for OctObsRegistration.
+    This is currently only used in retina_importers app. Therefore, admin permissions are required.
+    """
     queryset = OctObsRegistration.objects.all()
     serializer_class = OctObsRegistrationSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
