602c3da28521ee2e4d7cba0246f4a6960dcf62e0
Karel <karelgerbrands@gmail.com>
2019-01-11 09:51:11 +0100

    - Removes stacktrace reporting

--- app/grandchallenge/cases/image_builders/tiff.py
+++ app/grandchallenge/cases/image_builders/tiff.py
@@ -6,9 +6,6 @@ from pathlib import Path
 from django.core.files import File
 
 import tifffile as tiff_lib
-import traceback
-import sys
-
 from grandchallenge.cases.image_builders import ImageBuilderResult
 from grandchallenge.cases.models import Image, ImageFile
 
@@ -112,7 +109,6 @@ def validate_tiff(path: Path):
 
         return True, ""
     except Exception as e:
-        traceback.print_exc(file=sys.stdout)
         return False, str(e)
 
 
