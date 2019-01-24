e28b5575b313df69455212183973e1adf8cad252
Karel <karelgerbrands@gmail.com>
2019-01-24 10:51:03 +0100

    #681
    - Attempts to fix the TIFF tests

--- app/tests/cases_tests/test_tiff.py
+++ app/tests/cases_tests/test_tiff.py
@@ -10,22 +10,23 @@ from tests.cases_tests import RESOURCE_PATH
 
 
 @pytest.mark.parametrize(
-     "resource, expected_error_message"[
-         (Path(RESOURCE_PATH / "valid_tiff.tif"), ""),
-         #(
-         #    RESOURCE_PATH / "invalid_meta_data_tiff.tif",
-         #    "Image contains unauthorized information",
-         #),
-         #(
-         #    RESOURCE_PATH / "invalid_resolutions_tiff.tif",
-         #    "Image only has a single resolution level",
-         #),
-         #(
-         #    RESOURCE_PATH / "invalid_tiles_tiff.tif",
-         #    "Image has incomplete tile information",
-         #),
-     ]
- )
+    "resource, expected_error_message",
+    [
+        (Path(RESOURCE_PATH / "valid_tiff.tif"), ""),
+        # (
+        #    RESOURCE_PATH / "invalid_meta_data_tiff.tif",
+        #    "Image contains unauthorized information",
+        # ),
+        # (
+        #    RESOURCE_PATH / "invalid_resolutions_tiff.tif",
+        #    "Image only has a single resolution level",
+        # ),
+        # (
+        #    RESOURCE_PATH / "invalid_tiles_tiff.tif",
+        #    "Image has incomplete tile information",
+        # ),
+    ],
+)
 def test_tiff_validation(resource, expected_error_message):
     error_message = ""
 
