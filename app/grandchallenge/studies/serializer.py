8d2c8f97a9e579daf3440a921bd8c70c6a36843f
Karel <karelgerbrands@gmail.com>
2019-02-13 14:17:46 +0100

    #695
    - Removes old serializers for Patients and Studies
    - Removes pathology app

--- app/grandchallenge/studies/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from grandchallenge.studies.models import Study
-
-
-class StudySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Study
-        fields = ("id", "code", "region_of_interest")
