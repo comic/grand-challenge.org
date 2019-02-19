9c4ac6ed8cd9ec38a8020600270162d5ad30f707
Roel Zinkstok <rzinkstok@gmail.com>
2019-02-19 09:38:55 +0100

    WIP k8s

--- app/grandchallenge/container_exec/backends/k8s.py
+++ app/grandchallenge/container_exec/backends/k8s.py
@@ -14,8 +14,159 @@ Create a pod with two containers. One is the data provisioner: it loads the requ
 input data from the object storage (Digital Ocean Spaces)
 """
 
+REGION = "ams3"
+ACCESS_KEY = "V5OTPTWOTJX4LSA3DBNP"
+SECRET_KEY = "wyewQ5K30HvnBii/t7bqfI5ovAjKJruHFXfLGVUgeOI"
+BUCKET = "eyra-datasets"
+
 
 class K8sJob(object):
+    def __init__(self, job_id, namespace, image, volume_defs):
+        self.job_id = str(job_id)
+        self.namespace = namespace
+        self.image = image
+        self.volume_defs = volume_defs
+        self.pvcs = []
+        self.pvcss = []
+        self.volumes = []
+        self.volume_mounts = []
+
+        self.pods = []
+
+        incluster = False
+
+        if incluster:
+            load_incluster_config()
+        else:
+            load_kube_config()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Clean up all resources
+        for pod in self.pods:
+            pass
+
+        for pvc in self.pvcs:
+            pass
+
+    def create_io_volumes(self):
+        core_v1 = client.CoreV1Api()
+
+        self.pvcs = []
+        self.pvcss = []
+        self.volumes = []
+        self.volume_mounts = []
+
+        for volume_name, mount_point in self.volume_defs.items():
+            self.pvcss.append(
+                client.V1PersistentVolumeClaimVolumeSource(
+                    claim_name=volume_name
+                )
+            )
+            self.volumes.append(
+                client.V1Volume(
+                    name=volume_name, persistent_volume_claim=self.pvcss[-1]
+                )
+            )
+            self.volume_mounts.append(
+                client.V1VolumeMount(mount_path=mount_point, name=volume_name)
+            )
+
+            meta = client.V1ObjectMeta(name=volume_name)
+            resources = client.V1ResourceRequirements(
+                requests={"storage": "8Gi"}
+            )
+            spec = client.V1PersistentVolumeClaimSpec(
+                access_modes=["ReadWriteOnce"], resources=resources
+            )
+            self.pvcs.append(
+                client.V1PersistentVolumeClaim(metadata=meta, spec=spec)
+            )
+            core_v1.create_namespaced_persistent_volume_claim(
+                self.namespace, self.pvcs[-1]
+            )
+
+            print("Checking PVC status...")
+            while True:
+                r = core_v1.read_namespaced_persistent_volume_claim_status(
+                    volume_name, self.namespace
+                )
+                print(r.status.phase)
+                if r.status.phase == "Bound":
+                    break
+                time.sleep(1)
+            print("Done")
+
+    def run_pod(self):
+        batch_v1 = client.BatchV1Api()
+        meta = client.V1ObjectMeta(name=self.job_id)
+
+        init_container = client.V1Container(
+            name=self.job_id + "-init",
+            image="docker-registry.roel.dev.eyrabenchmark.net/eyra-dataloader",
+            volume_mounts=self.volume_mounts,
+            command=[
+                "bash",
+                "-c",
+                "python /tmp/load_input.py ams3 V5OTPTWOTJX4LSA3DBNP wyewQ5K30HvnBii/t7bqfI5ovAjKJruHFXfLGVUgeOI eyra-datasets test_data/data.txt",
+            ],
+        )
+
+        container = client.V1Container(
+            name=self.job_id,
+            image=self.image,
+            volume_mounts=self.volume_mounts,
+        )
+
+        exit_container = client.V1Container(
+            name=self.job_id + "-exit",
+            image="docker-registry.roel.dev.eyrabenchmark.net/eyra-resultloader",
+            volume_mounts=self.volume_mounts,
+            command=[
+                "bash",
+                "-c",
+                "python /tmp/store_result.py ams3 V5OTPTWOTJX4LSA3DBNP wyewQ5K30HvnBii/t7bqfI5ovAjKJruHFXfLGVUgeOI eyra-datasets test_data/result.txt",
+            ],
+        )
+        podspec = client.V1PodSpec(
+            restart_policy="Never",
+            init_containers=[init_container, container],
+            containers=[exit_container],
+            volumes=self.volumes,
+        )
+
+        template = client.V1PodTemplateSpec(metadata=meta, spec=podspec)
+        jobspec = client.V1JobSpec(template=template, backoff_limit=3)
+        job = client.V1Job(metadata=meta, spec=jobspec)
+
+        r = batch_v1.create_namespaced_job(self.namespace, job)
+
+        print("Executing job...")
+        while True:
+            r = batch_v1.read_namespaced_job_status(
+                self.job_id, self.namespace
+            )
+            print(r.status)
+            if not r.status.active:
+                if r.status.failed or r.status.succeeded:
+                    break
+            time.sleep(1)
+
+        if r.status.failed:
+            print("Job failed!")
+        if r.status.succeeded:
+            print("Job succeeded!")
+
+        return
+
+    def execute(self):
+        self.create_io_volumes()
+        self.run_pod()
+
+
+class K8sJobOld(object):
     def __init__(self, job_id, namespace, image, volume_defs):
         self.job_id = str(job_id)
         self.namespace = namespace
