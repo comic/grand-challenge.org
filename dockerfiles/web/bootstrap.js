881a91ac077e90fe6c7f50bbb7316f9c9b005993
James Meakin <code@jmsmkn.com>
2019-02-25 12:39:38 +0100

    Copy jQuery from dist

--- dockerfiles/web/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css";
-
-// jquery ui must be before bootstrap, but is required by blueimp-file-upload
-require("jquery-ui");
-require("popper.js");
-require("bootstrap");
