be065b67f647644aa63b565ec4b2b75a9cd64394
Roel Zinkstok <rzinkstok@gmail.com>
2019-02-25 16:49:09 +0100

    First setup for algorithm execution task

--- app/grandchallenge/container_exec/tasks2.py
+++ app/grandchallenge/container_exec/tasks2.py
@@ -3,12 +3,28 @@ if __name__ == "__main__":
     setup()
 
 from grandchallenge.eyra_benchmarks.models import Benchmark, Submission
+from grandchallenge.container_exec.backends.k8s import K8sJob
 
 
 if __name__ == "__main__":
-    submission = Submission.objects.get(pk=)
-    benchmark = Benchmark.objects
-    f = Benchmark.objects.all()[0].test_datafile.file
+    submission = Submission.objects.all()[0]
 
-    print(f.name)
-    print(f.url)
+    benchmark = submission.benchmark
+    algorithm = submission.algorithm
+    f = benchmark.test_datafile.file
+
+    job_id = f"submission-{submission.pk}"
+    namespace = f"benchmark-{benchmark.pk}"
+    image = algorithm.container
+    s3_bucket = ""
+    input_object_keys = [benchmark.test_datafile, benchmark.ground_truth_datafile]
+    output_object_key = ""
+
+    job = K8sJob(
+        job_id=job_id,
+        namespace=namespace,
+        image=image,
+        s3_bucket=s3_bucket,
+        input_object_keys=input_object_keys,
+        output_object_key=output_object_key
+    )
