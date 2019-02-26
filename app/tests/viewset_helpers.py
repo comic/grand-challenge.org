3f70e1ef08f2eb4d0717e27feefe434133fb01c2
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-02-26 18:01:38 +0100

    Add helper function for viewsets

--- app/tests/viewset_helpers.py
+++ app/tests/viewset_helpers.py
@@ -1,11 +1,14 @@
-from rest_framework.test import force_authenticate
-import factory
 import json
-from grandchallenge.subdomains.utils import reverse
+
+import factory
+from rest_framework.test import force_authenticate
 from rest_framework import status
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, AnonymousUser
 from tests.factories import UserFactory
 from django.conf import settings
+
+from grandchallenge.subdomains.utils import reverse
 
 # Endpoints to check
 VIEWSET_ACTIONS = (
@@ -183,3 +186,85 @@ def create_test_method(
                 assert response_status == status.HTTP_403_FORBIDDEN
 
     return test_method
+
+
+def get_user_from_user_type(user_type, grader=None):
+    if user_type is None:
+        return AnonymousUser()
+    if user_type == "retina_grader":
+        user = grader if grader else UserFactory()
+        user.groups.add(
+            Group.objects.get(name=settings.RETINA_GRADERS_GROUP_NAME)
+        )
+    elif user_type == "retina_grader_non_allowed":
+        user = UserFactory()
+        user.groups.add(
+            Group.objects.get(name=settings.RETINA_GRADERS_GROUP_NAME)
+        )
+    elif user_type == "retina_admin":
+        user = UserFactory()
+        user.groups.add(
+            Group.objects.get(name=settings.RETINA_ADMINS_GROUP_NAME)
+        )
+    else:  # normal_user
+        user = grader if grader else UserFactory()
+    return user
+
+
+def get_viewset_user_kwargs_url(
+    user_type, namespace, basename, grader, polygon_set, url_name
+):
+    user = get_user_from_user_type(user_type, grader=grader)
+    kwargs = {"user_id": grader.id}
+    if url_name == "detail":
+        kwargs.update({"pk": polygon_set.pk})
+    url = reverse(f"{namespace}:{basename}-{url_name}", kwargs=kwargs)
+    return user, kwargs, url
+
+
+def view_test(action, user_type, namespace, basename, grader, polygon_set, rf, viewset, data=None):
+    if action == "list" or action == "create":
+        url_name = "list"
+    else:
+        url_name = "detail"
+    user, kwargs, url = get_viewset_user_kwargs_url(
+        user_type,
+        namespace,
+        basename,
+        grader,
+        polygon_set,
+        url_name,
+    )
+
+    method = "get"  # list or retrieve
+    if action == "create":
+        method = "post"
+    elif action == "update":
+        method = "put"
+    elif action == "partial_update":
+        method = "patch"
+    elif action == "destroy":
+        method = "delete"
+
+    request = getattr(rf, method)(url)  # list, retrieve, destroy
+    if action in ("create", "update", "partial_update"):
+        request = getattr(rf, method)(url, data=data, content_type="application/json")
+
+    view = viewset.as_view(actions={method: action})
+    force_authenticate(request, user=user)
+    response = view(request, **kwargs)
+
+    if (
+            user_type is None
+            or user_type == "normal_user"
+            or user_type == "retina_grader_non_allowed"
+    ):
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    else:
+        if action in ("list", "retrieve", "update", "partial_update"):
+            assert response.status_code == status.HTTP_200_OK
+        elif action == "create":
+            assert response.status_code == status.HTTP_201_CREATED
+        elif action == "destroy":
+            assert response.status_code == status.HTTP_204_NO_CONTENT
+    return response.data
