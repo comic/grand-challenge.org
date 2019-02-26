b13ee313b7be1a7f2037412d3aa965f233b88f75
Tom Klaver <tomklav@gmail.com>
2019-02-26 11:52:18 +0100

    update demo challenge creation according to new data model

--- app/grandchallenge/eyra_benchmarks/management/commands/create_demo_benchmark.py
+++ app/grandchallenge/eyra_benchmarks/management/commands/create_demo_benchmark.py
@@ -8,7 +8,7 @@ from urllib.parse import urlsplit
 
 from django.contrib.auth.models import User
 from grandchallenge.evaluation.models import Method
-from grandchallenge.eyra_algorithms.models import AlgorithmInput, Algorithm
+from grandchallenge.eyra_algorithms.models import Input, Algorithm, Interface
 from grandchallenge.eyra_benchmarks.models import Benchmark, Submission
 from grandchallenge.eyra_data.models import DataFile, DataType
 
@@ -93,29 +93,53 @@ class Command(BaseCommand):
 
             return test_images, gt_images
 
-        def create_evaluator(user: User) -> Algorithm:
+        def create_eval_interface() -> Interface:
+            interface = Interface(
+                name='Segmentation evaluation interface',
+                output_type = DataType.objects.get(name='OutputMetrics'),
+            )
+            interface.save()
+
+            predictions_input = Input(
+                name='predictions',
+                type=DataType.objects.get(name='GrayScaleImageSet'),
+                interface=interface
+            )
+            predictions_input.save()
+
+            ground_truth_input = Input(
+                name='ground_truth',
+                type=DataType.objects.get(name='GrayScaleImageSet'),
+                interface=interface
+            )
+            ground_truth_input.save()
+
+            return interface
+
+        def create_predict_interface() -> Interface:
+            interface = Interface(
+                name='Segmentation predictor interface',
+                output_type = DataType.objects.get(name='GrayScaleImageSet'),
+            )
+            interface.save()
+
+            test_input = Input(
+                name='test',
+                type=DataType.objects.get(name='GrayScaleImageSet'),
+                interface=interface
+            )
+            test_input.save()
+            return interface
+
+        def create_evaluator(user: User, interface: Interface) -> Algorithm:
             algo = Algorithm(
                 creator=user,
                 name='Segmentation evaluation',
                 description='Segmentation evaluation',
-                output_type=DataType.objects.get(name='OutputMetrics'),
-                container='segmentation_a_dummy'
+                container='segmentation_a_dummy',
+                interface=interface
             )
             algo.save()
-
-            algo_predictions_input = AlgorithmInput(
-                name='predictions',
-                type=DataType.objects.get(name='GrayScaleImageSet'),
-                algorithm=algo
-            )
-            algo_predictions_input.save()
-            algo_ground_truth_input = AlgorithmInput(
-                name='ground_truth',
-                type=DataType.objects.get(name='GrayScaleImageSet'),
-                algorithm=algo
-            )
-            algo_ground_truth_input.save()
-
             return algo
 
 
@@ -129,28 +153,21 @@ class Command(BaseCommand):
                         ''',
                 creator=user,
                 evaluator=evaluator,
-                test_datafile=test_data,
-                ground_truth_datafile=gt_data
+                test_data_file=test_data,
+                test_ground_truth_data_file=gt_data
             )
             demo.save()
             return demo
 
-        def create_predictor(user: User) -> Algorithm:
+        def create_predictor(user: User, interface: Interface) -> Algorithm:
             algo = Algorithm(
                 creator=user,
                 name='Algorithm A',
                 description='Algorithm A is the best algorithm. Even though it takes 24 hours to train',
-                output_type=DataType.objects.get(name='GrayScaleImageSet'),
-                container='algorithm_a_0148a9ce-34f6-11e9-b346-00155d544bd9'
+                container='algorithm_a_0148a9ce-34f6-11e9-b346-00155d544bd9',
+                interface=interface
             )
             algo.save()
-
-            algo_input = AlgorithmInput(
-                name='test',
-                type=DataType.objects.get(name='GrayScaleImageSet'),
-                algorithm=algo
-            )
-            algo_input.save()
 
             return algo
 
@@ -169,8 +186,9 @@ class Command(BaseCommand):
         demouser = create_user()
         create_test_data_types(demouser)
         test_data, gt_data = create_data_files(demouser)
-        evaluator = create_evaluator(demouser)
+        eval_interface = create_eval_interface()
+        evaluator = create_evaluator(demouser, eval_interface)
         demo_benchmark = create_demo_benchmark(demouser, evaluator, test_data, gt_data)
-
-        predictor_a = create_predictor(demouser)
+        predictor_interface = create_predict_interface()
+        predictor_a = create_predictor(demouser, predictor_interface)
         submission_a = create_submission(demouser, demo_benchmark, predictor_a)
