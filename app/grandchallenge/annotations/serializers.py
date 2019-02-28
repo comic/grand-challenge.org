56b9ad7a83f373ef34b511a8434f96bfec50464b
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-02-28 16:00:40 +0100

    Move validators to abstract serializers to implement in all annotation models

--- app/grandchallenge/annotations/serializers.py
+++ app/grandchallenge/annotations/serializers.py
@@ -12,29 +12,19 @@ from .models import (
 from .validators import validate_grader_is_current_retina_user
 
 
-class ETDRSGridAnnotationSerializer(serializers.ModelSerializer):
+class AbstractAnnotationSerializer(serializers.ModelSerializer):
+    def validate_grader(self, value):
+        """
+        Validate that grader is the user creating the object for retina_graders group
+        """
+        validate_grader_is_current_retina_user(value, self.context)
+        return value
+
     class Meta:
-        model = ETDRSGridAnnotation
-        fields = ("grader", "created", "image", "fovea", "optic_disk")
+        abstract = True
 
 
-class MeasurementAnnotationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeasurementAnnotation
-        fields = ("image", "grader", "created", "start_voxel", "end_voxel")
-
-
-class BooleanClassificationAnnotationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BooleanClassificationAnnotation
-        fields = ("image", "grader", "created", "name", "value")
-
-
-class SinglePolygonAnnotationSerializer(serializers.ModelSerializer):
-    annotation_set = serializers.PrimaryKeyRelatedField(
-        queryset=PolygonAnnotationSet.objects.all()
-    )
-
+class AbstractSingleAnnotationSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """
         Validate that the user that is creating this object equals the annotation_set.grader for retina_graders
@@ -43,21 +33,41 @@ class SinglePolygonAnnotationSerializer(serializers.ModelSerializer):
         return data
 
     class Meta:
+        abstract = True
+
+
+class ETDRSGridAnnotationSerializer(AbstractAnnotationSerializer):
+    class Meta:
+        model = ETDRSGridAnnotation
+        fields = ("grader", "created", "image", "fovea", "optic_disk")
+
+
+class MeasurementAnnotationSerializer(AbstractAnnotationSerializer):
+    class Meta:
+        model = MeasurementAnnotation
+        fields = ("image", "grader", "created", "start_voxel", "end_voxel")
+
+
+class BooleanClassificationAnnotationSerializer(AbstractAnnotationSerializer):
+    class Meta:
+        model = BooleanClassificationAnnotation
+        fields = ("image", "grader", "created", "name", "value")
+
+
+class SinglePolygonAnnotationSerializer(AbstractSingleAnnotationSerializer):
+    annotation_set = serializers.PrimaryKeyRelatedField(
+        queryset=PolygonAnnotationSet.objects.all()
+    )
+
+    class Meta:
         model = SinglePolygonAnnotation
         fields = ("id", "value", "annotation_set")
 
 
-class PolygonAnnotationSetSerializer(serializers.ModelSerializer):
+class PolygonAnnotationSetSerializer(AbstractAnnotationSerializer):
     singlepolygonannotation_set = SinglePolygonAnnotationSerializer(
         many=True, read_only=True
     )
-
-    def validate_grader(self, value):
-        """
-        Validate that grader is the user creating the object for retina_graders group
-        """
-        validate_grader_is_current_retina_user(value, self.context)
-        return value
 
     class Meta:
         model = PolygonAnnotationSet
@@ -71,13 +81,13 @@ class PolygonAnnotationSetSerializer(serializers.ModelSerializer):
         )
 
 
-class LandmarkAnnotationSetSerializer(serializers.ModelSerializer):
+class LandmarkAnnotationSetSerializer(AbstractAnnotationSerializer):
     class Meta:
         model = LandmarkAnnotationSet
         fields = ("grader", "created")
 
 
-class SingleLandmarkAnnotationSerializer(serializers.ModelSerializer):
+class SingleLandmarkAnnotationSerializer(AbstractSingleAnnotationSerializer):
     class Meta:
         model = SingleLandmarkAnnotation
         fields = ("image", "annotation_set", "landmarks")
