940b91c83261d90e71b421237c7f74c84bba9ae9
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-03-12 18:19:10 +0100

    Run black

--- app/grandchallenge/core/serializers.py
+++ app/grandchallenge/core/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username')
+        fields = ("id", "username")
