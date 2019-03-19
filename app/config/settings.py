998159b62193e04c0008dae9599a14ed86a3d132
James Meakin <code@jmsmkn.com>
2019-03-19 11:53:31 +0100

    Revert "Adds mlmodel view set"
    This reverts commit d5e49651cf37cd0054cf5592c86ad911573373a1.

--- app/config/settings.py
+++ app/config/settings.py
@@ -539,7 +539,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAdminUser",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ),
 }
 
--- app/config/urls.py
+++ app/config/urls.py
@@ -73,10 +73,6 @@ urlpatterns = [
         "algorithms/",
         include("grandchallenge.algorithms.urls", namespace="algorithms"),
     ),
-    path(
-        "models/",
-        include("grandchallenge.mlmodels.urls", namespace="mlmodels"),
-    ),
     path("summernote/", include("django_summernote.urls")),
     path(
         "retina/",
