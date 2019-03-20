999b5644b74bc98d07f93ccb63ba3f051a82a35a
Karel <karelgerbrands@gmail.com>
2019-03-20 16:33:52 +0100

    - Ensures permissions are only set if an object was succesfully created

--- app/grandchallenge/worklists/models.py
+++ app/grandchallenge/worklists/models.py
@@ -18,9 +18,10 @@ class WorklistSet(UUIDModel):
         return Worklist.objects.filter(set=self.pk)
 
     def save(self, *args, **kwargs):
+        created = self._state.adding
         super(WorklistSet, self).save(*args, **kwargs)
 
-        if self.user is not None:
+        if created and self.user is not None:
             assign_perm("view_worklistset", self.user, self)
 
     def __str__(self):
@@ -39,10 +40,11 @@ class Worklist(UUIDModel):
     )
 
     def save(self, *args, **kwargs):
+        created = self._state.adding
         super(Worklist, self).save(*args, **kwargs)
 
         set = WorklistSet.objects.get(pk=self.set)
-        if set.user is not None:
+        if created and set.user is not None:
             assign_perm("view_worklist", set.user, self)
             assign_perm("change_worklist", set.user, self)
             assign_perm("delete_worklist", set.user, self)
