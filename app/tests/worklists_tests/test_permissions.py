cb5dad5cae388504b1c754e81b3f16e26067b94b
Karel <karelgerbrands@gmail.com>
2019-04-01 13:22:36 +0200

    - Adds record permissions tests for Worklists
    #760

--- app/tests/worklists_tests/test_permissions.py
+++ app/tests/worklists_tests/test_permissions.py
@@ -1,9 +1,53 @@
 import pytest
 
+from guardian.shortcuts import assign_perm
+from tests.factories import UserFactory
 from tests.worklists_tests.factories import WorklistFactory, WorklistSetFactory
-from tests.utils import validate_staff_only_view
+from tests.utils import get_view_for_user, validate_staff_only_view
 
-"""" Tests the permission access for Worklist Forms """
+""" Tests the permission access for Worklist API Tables """
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "view, factory",
+    [
+        "worklists:list",
+        "worklists:set",
+    ],
+)
+def test_worklist_api_access(view, factory, client):
+    permitted_user = UserFactory()
+    blocked_user = UserFactory()
+    instance = factory()
+
+    # Assigns permissions to user
+    model_name = factory._meta.model._meta.model_name
+    for permission_type in factory._meta.model._meta.default_permissions:
+        permission_name = f"{permission_type}_{model_name}"
+        assign_perm(permission_name, permitted_user, instance)
+
+    # Asserts whether or not both users have access
+    permitted_response = get_view_for_user(
+        viewname="worklists:list-create",
+        client=client,
+        method=client.get,
+        user=permitted_user,
+    )
+
+    blocked_response = permitted_response = get_view_for_user(
+        viewname="worklists:list-create",
+        client=client,
+        method=client.get,
+        user=blocked_user,
+        kwargs={"pk": instance.pk}
+    )
+
+    assert permitted_response.status_code == 200
+    assert blocked_response.status_code == 403
+
+
+""" Tests the permission access for Worklist Forms """
 
 
 @pytest.mark.django_db
@@ -27,7 +71,7 @@ def test_worklist_form_access(view, client):
     )
 
 
-"""" Tests the permission access for Worklist Set Forms """
+""" Tests the permission access for Worklist Set Forms """
 
 
 @pytest.mark.django_db
