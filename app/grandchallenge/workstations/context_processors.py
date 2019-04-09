d921858302f8bba715a7f4e63eaec68dfe04927a
James Meakin <code@jmsmkn.com>
2019-04-09 14:05:40 +0200

    Handle no user at all

--- app/grandchallenge/workstations/context_processors.py
+++ app/grandchallenge/workstations/context_processors.py
@@ -6,13 +6,17 @@ def workstation_session(request):
 
     s = None
 
-    if not request.user.is_anonymous:
-        s = (
-            Session.objects.filter(creator=request.user)
-            .exclude(status__in=[Session.QUEUED, Session.STOPPED])
-            .order_by("-created")
-            .select_related("workstation_image__workstation")
-            .first()
-        )
+    try:
+        if not request.user.is_anonymous:
+            s = (
+                Session.objects.filter(creator=request.user)
+                .exclude(status__in=[Session.QUEUED, Session.STOPPED])
+                .order_by("-created")
+                .select_related("workstation_image__workstation")
+                .first()
+            )
+    except AttributeError:
+        # No user
+        pass
 
     return {"workstation_session": s}
