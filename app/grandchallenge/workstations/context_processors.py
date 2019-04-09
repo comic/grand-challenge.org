bc55dc93fbceef55dd73caae88525bb4c1c9526d
James Meakin <code@jmsmkn.com>
2019-04-09 13:54:21 +0200

    Handle anonymous users

--- app/grandchallenge/workstations/context_processors.py
+++ app/grandchallenge/workstations/context_processors.py
@@ -3,12 +3,16 @@ from grandchallenge.workstations.models import Session
 
 def workstation_session(request):
     """ Adds workstation_session. request.user must be set """
-    return {
-        "workstation_session": (
+
+    s = None
+
+    if not request.user.is_anonymous:
+        s = (
             Session.objects.filter(creator=request.user)
             .exclude(status__in=[Session.QUEUED, Session.STOPPED])
             .order_by("-created")
             .select_related("workstation_image__workstation")
             .first()
         )
-    }
+
+    return {"workstation_session": s}
