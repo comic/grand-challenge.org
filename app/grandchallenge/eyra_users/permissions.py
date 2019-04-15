160597302b7c5984ec1885c34d4702d38c558aab
Tom Klaver <tomklav@gmail.com>
2019-04-15 17:06:34 +0200

    fix permmissions (no raise in object permission checker

--- app/grandchallenge/eyra_users/permissions.py
+++ app/grandchallenge/eyra_users/permissions.py
@@ -96,24 +96,7 @@ class EyraDjangoObjectPermissions(EyraDjangoModelPermissions):
 
         perms = self.get_required_object_permissions(request.method, model_cls)
 
-        if not user.has_perms(perms, obj):
-            # If the user does not have permissions we need to determine if
-            # they have read permissions to see 403, or not, and simply see
-            # a 404 response.
-
-            if request.method in SAFE_METHODS:
-                # Read permissions already checked and failed, no need
-                # to make another lookup.
-                raise Http404
-
-            read_perms = self.get_required_object_permissions('GET', model_cls)
-            if not user.has_perms(read_perms, obj):
-                raise Http404
-
-            # Has read permissions.
-            return False
-
-        return True
+        return user.has_perms(perms, obj)
 
 
 # EyraPermissions:
