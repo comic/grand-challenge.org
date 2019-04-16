add87ae0bd7f1554c1609f45c3d2fb817c75d9ac
Karel <karelgerbrands@gmail.com>
2019-04-16 17:45:16 +0200

    - Refactors the api urls - Refactors the form urls
    #789

--- app/grandchallenge/api/urls.py
+++ app/grandchallenge/api/urls.py
@@ -5,12 +5,16 @@ from rest_framework import routers
 from grandchallenge.api.views import SubmissionViewSet
 from grandchallenge.cases.views import ImageViewSet
 from grandchallenge.patients.views_api import PatientsViewSet
+from grandchallenge.studies.views_api import StudyViewSet
+from grandchallenge.worklists.views_api import WorklistViewSet
 
 app_name = "api"
 
 router = routers.DefaultRouter()
 router.register(r"submissions", SubmissionViewSet)
 router.register(r"patients", PatientsViewSet, basename="patient")
+router.register(r"studies", StudyViewSet, basename="study")
+router.register(r"worklists", WorklistViewSet, basename="worklist")
 router.register(r"cases/images", ImageViewSet, basename="image")
 urlpatterns = [
     # Do not namespace the router.urls without updating the view names in
--- app/grandchallenge/worklists/urls.py
+++ app/grandchallenge/worklists/urls.py
@@ -1,46 +1,17 @@
 from django.urls import path
-from grandchallenge.worklists import views
+from grandchallenge.worklists.views_forms import (
+    WorklistListView,
+    WorklistCreateView,
+    WorklistDetailView,
+    WorklistDeleteView,
+    WorklistUpdateView,
+)
 
 app_name = "worklists"
 urlpatterns = [
-    path("list/", views.WorklistTable.as_view(), name="lists"),
-    path("list/<uuid:pk>/", views.WorklistRecord.as_view(), name="list"),
-    path(
-        "list/create/", views.WorklistCreateView.as_view(), name="list-create"
-    ),
-    path(
-        "list/remove/<uuid:pk>/",
-        views.WorklistRemoveView.as_view(),
-        name="list-remove",
-    ),
-    path(
-        "list/update/<uuid:pk>/",
-        views.WorklistUpdateView.as_view(),
-        name="list-update",
-    ),
-    path(
-        "list/display/",
-        views.WorklistDisplayView.as_view(),
-        name="list-display",
-    ),
-    path("set/", views.WorklistSetTable.as_view(), name="sets"),
-    path("set/<uuid:pk>/", views.WorklistSetRecord.as_view(), name="set"),
-    path(
-        "set/create/", views.WorklistSetCreateView.as_view(), name="set-create"
-    ),
-    path(
-        "set/remove/<uuid:pk>/",
-        views.WorklistSetRemoveView.as_view(),
-        name="set-remove",
-    ),
-    path(
-        "set/update/<uuid:pk>/",
-        views.WorklistSetUpdateView.as_view(),
-        name="set-update",
-    ),
-    path(
-        "set/display/",
-        views.WorklistSetDisplayView.as_view(),
-        name="set-display",
-    ),
+    path("", WorklistListView.as_view(), name="list"),
+    path("create", WorklistCreateView.as_view(), name="create"),
+    path("<uuid:pk>/detail/", WorklistDeleteView.as_view(), name="detail"),
+    path("<uuid:pk>/delete/", WorklistDeleteView.as_view(), name="remove"),
+    path("<uuid:pk>/update/", WorklistUpdateView.as_view(), name="update"),
 ]
