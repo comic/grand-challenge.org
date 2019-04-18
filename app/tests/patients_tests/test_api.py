29d8aeec7545637128276fc7e981d19ca5f77caf
Karel <karelgerbrands@gmail.com>
2019-04-18 14:12:08 +0200

    - Fixes assertion errors

--- app/tests/patients_tests/test_api.py
+++ app/tests/patients_tests/test_api.py
@@ -6,7 +6,7 @@ from tests.api_utils import assert_api_read_only
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     "table_reverse, expected_table, factory",
-    [("api:patient-list", "Patient Table", PatientFactory)],
+    [("api:patient-list", "Patient List", PatientFactory)],
 )
 def test_api_pages(client, table_reverse, expected_table, factory):
     assert_api_read_only(client, table_reverse, expected_table, factory)
