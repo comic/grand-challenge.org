95f11966af43bb620c24fd89e4649a48ac52559f
Karel <karelgerbrands@gmail.com>
2019-04-23 16:50:57 +0200

    - Changes Worklist API view to rely on integrity errors, rather than custom code

--- app/grandchallenge/worklists/views_api.py
+++ app/grandchallenge/worklists/views_api.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from grandchallenge.cases.models import Image
 from grandchallenge.worklists.models import Worklist
 from grandchallenge.worklists.serializers import WorklistSerializer
 
@@ -13,21 +15,22 @@ class WorklistViewSet(viewsets.ModelViewSet):
         data = request.data
         user = request.user
         title = data.get("title")
-        images = data.get("image", "")
-
-        if title is None or len(data["title"]) == 0:
-            return Response(
-                "Title field is not set.", status=status.HTTP_400_BAD_REQUEST
-            )
+        images = data.get("images", "")
 
         if "user" in data and len(data["user"]) > 0:
             user = User.objects.get(pk=data["user"])
 
-        worklist = Worklist.objects.create(
-            title=title, user=user, images=images
-        )
-        serialized = WorklistSerializer(worklist)
-        return Response(serialized.data, status=status.HTTP_201_CREATED)
+        # Creates worklist, then iterates over the list to add the image relations
+        try:
+            worklist = Worklist.objects.create(title=title, user=user)
+            for image in images.split():
+                worklist.images.add(Image.objects.get(pk=image))
+            worklist.save()
+
+            serialized = WorklistSerializer(worklist)
+            return Response(serialized.data, status=status.HTTP_201_CREATED)
+        except IntegrityError:
+            return Response(serialized.data, status=status.HTTP_400_BAD_REQUEST)
 
     def get_queryset(self):
         queryset = Worklist.objects.filter(
