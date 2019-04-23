ea8e3bdf871ebe608e16bc71e397202f14daad55
Karel <karelgerbrands@gmail.com>
2019-04-23 16:50:22 +0200

    - Fixes URL error

--- app/grandchallenge/worklists/urls.py
+++ app/grandchallenge/worklists/urls.py
@@ -10,7 +10,7 @@ from grandchallenge.worklists.views_forms import (
 app_name = "worklists"
 urlpatterns = [
     path("", WorklistListView.as_view(), name="list"),
-    path("create", WorklistCreateView.as_view(), name="create"),
+    path("create/", WorklistCreateView.as_view(), name="create"),
     path("<uuid:pk>/detail/", WorklistDeleteView.as_view(), name="detail"),
     path("<uuid:pk>/delete/", WorklistDeleteView.as_view(), name="remove"),
     path("<uuid:pk>/update/", WorklistUpdateView.as_view(), name="update"),
