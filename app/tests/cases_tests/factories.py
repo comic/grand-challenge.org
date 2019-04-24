0ad00fb7b2b91f623e929523f0631ed7700a4730
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-04-24 14:32:46 +0200

    Make ImageFactoryWithImageFile return 2d mhd images by default

--- app/tests/cases_tests/factories.py
+++ app/tests/cases_tests/factories.py
@@ -20,6 +20,14 @@ class ImageFileFactoryWithRAWFile(ImageFileFactory):
     )
 
 
+class ImageFileFactoryWithMHDFile2D(ImageFileFactory):
+    file = factory.django.FileField(from_path=RESOURCE_PATH / "image3x4.mhd")
+
+
+class ImageFileFactoryWithRAWFile2D(ImageFileFactory):
+    file = factory.django.FileField(from_path=RESOURCE_PATH / "image3x4.zraw")
+
+
 class ImageFactoryWithImageFile(ImageFactory):
     @factory.post_generation
     def files(self, create, extracted, **kwargs):
@@ -30,8 +38,8 @@ class ImageFactoryWithImageFile(ImageFactory):
             for image in extracted:
                 self.files.add(image)
         if create and not extracted:
-            ImageFileFactoryWithMHDFile(image=self)
-            ImageFileFactoryWithRAWFile(image=self)
+            ImageFileFactoryWithMHDFile2D(image=self)
+            ImageFileFactoryWithRAWFile2D(image=self)
 
     eye_choice = factory.Iterator([x[0] for x in Image.EYE_CHOICES])
     stereoscopic_choice = factory.Iterator(
@@ -39,7 +47,7 @@ class ImageFactoryWithImageFile(ImageFactory):
     )
     field_of_view = factory.Iterator([x[0] for x in Image.FOV_CHOICES])
     study = factory.SubFactory(StudyFactory)
-    name = factory.Sequence(lambda n: "RetinaImage {}".format(n))
+    name = factory.Sequence(lambda n: f"RetinaImage {n}")
     modality = factory.SubFactory(
         ImagingModalityFactory, modality=settings.MODALITY_CF
     )
