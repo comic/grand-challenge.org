a8d4c8e3d9bef35d7b219676833735e4c4f3fd76
Tom Klaver <tomklav@gmail.com>
2019-04-26 11:28:23 +0200

    set sphinx master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ django.setup()
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
