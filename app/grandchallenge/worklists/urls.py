59290c9687d9ca6e72dc4ad5ba112479b3319476
Karel <karelgerbrands@gmail.com>
2019-05-07 13:49:12 +0200

    - Fixes the worklist detail page calling the delete view instead - Fixes the worklist form url ordering - Fixes the worklist list template calling details rather than detail

--- app/grandchallenge/worklists/urls.py
+++ app/grandchallenge/worklists/urls.py
@@ -11,7 +11,7 @@ app_name = "worklists"
 urlpatterns = [
     path("", WorklistListView.as_view(), name="list"),
     path("create/", WorklistCreateView.as_view(), name="create"),
-    path("<uuid:pk>/detail/", WorklistDeleteView.as_view(), name="detail"),
-    path("<uuid:pk>/delete/", WorklistDeleteView.as_view(), name="remove"),
+    path("<uuid:pk>/detail/", WorklistDetailView.as_view(), name="detail"),
     path("<uuid:pk>/update/", WorklistUpdateView.as_view(), name="update"),
+    path("<uuid:pk>/delete/", WorklistDeleteView.as_view(), name="delete"),
 ]
