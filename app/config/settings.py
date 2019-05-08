ebe4688d29c72357bdade3e8a03dddb90a3853dd
James Meakin <code@jmsmkn.com>
2019-05-08 12:44:23 +0200

    Limit lists to 1000 items

--- app/config/settings.py
+++ app/config/settings.py
@@ -552,7 +552,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "grandchallenge.api.pagination.MaxLimit1000OffsetPagination",
     "PAGE_SIZE": 100,
 }
 
