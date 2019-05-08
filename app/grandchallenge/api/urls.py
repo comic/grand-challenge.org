096420dd24e88541b5dbf0a946b6938596cdd14b
James Meakin <code@jmsmkn.com>
2019-05-08 12:30:13 +0200

    Removes the submission api

--- app/grandchallenge/api/urls.py
+++ app/grandchallenge/api/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 
-from grandchallenge.api.views import SubmissionViewSet
 from grandchallenge.cases.views import ImageViewSet
 from grandchallenge.workstations.views import SessionViewSet
 
 app_name = "api"
 
 router = routers.DefaultRouter()
-router.register(r"submissions", SubmissionViewSet)
 router.register(r"cases/images", ImageViewSet)
 router.register(r"workstations/sessions", SessionViewSet)
 urlpatterns = [
