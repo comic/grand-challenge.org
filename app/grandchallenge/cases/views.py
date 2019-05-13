6ba962bddfea7761737d2eaa2b528ad24c5ba483
James Meakin <code@jmsmkn.com>
2019-05-13 15:49:52 +0200

    Remove dead import

--- app/grandchallenge/cases/views.py
+++ app/grandchallenge/cases/views.py
@@ -10,7 +10,6 @@ from grandchallenge.cases.models import (
 )
 from grandchallenge.cases.serializers import ImageSerializer
 from grandchallenge.core.permissions.mixins import UserIsStaffMixin
-from grandchallenge.core.utils.query import filter_queryset_fields
 
 
 class UploadRawFiles(UserIsStaffMixin, CreateView):
