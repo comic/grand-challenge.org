ff5edf6d49bb3429bcf3ef96285527e9efc146b2
James Meakin <code@jmsmkn.com>
2019-05-13 15:21:41 +0200

    Removes unused function

--- app/grandchallenge/core/utils/query.py
+++ app/grandchallenge/core/utils/query.py
@@ -13,23 +13,6 @@ def index(queryset, obj):
     return -1
 
 
-def filter_queryset_field(field, value, model=None, queryset=None) -> QuerySet:
-    """
-    Filters a passed model or queryset based on the field and value passed.
-    """
-    if queryset is None and model is None:
-        raise ValueError("Unable to initialize or utilize queryset.")
-
-    if model is not None:
-        queryset = model.objects.all()
-
-    if value is not None:
-        filter_argument = {field: value}
-        queryset = queryset.filter(**filter_argument)
-
-    return queryset
-
-
 def filter_queryset_fields(
     field_filters, model=None, queryset=None
 ) -> QuerySet:
