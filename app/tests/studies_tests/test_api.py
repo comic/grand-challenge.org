ec24bf3ac87645ab38b472609b8711b21fba2fcc
Karel <karelgerbrands@gmail.com>
2019-05-15 11:25:31 +0200

    - Updates black formatting

--- app/tests/studies_tests/test_api.py
+++ app/tests/studies_tests/test_api.py
@@ -9,4 +9,6 @@ from tests.api_utils import assert_api_read_only
     [("api:study-list", "Study List", "", StudyFactory)],
 )
 def test_api_pages(client, table_reverse, expected_table, user_field, factory):
-    assert_api_read_only(client, table_reverse, expected_table, user_field, factory)
+    assert_api_read_only(
+        client, table_reverse, expected_table, user_field, factory
+    )
