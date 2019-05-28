b7758d0a71a0a1ae828cffdde921ce3a26a32fad
James Meakin <code@jmsmkn.com>
2019-05-28 11:44:46 +0200

    Simplify challenge context processor

--- app/grandchallenge/core/context_processors.py
+++ app/grandchallenge/core/context_processors.py
@@ -11,30 +11,20 @@ def challenge(request):
     try:
         challenge = request.challenge
     except AttributeError:
-        challenge = None
-
-        # build_absolute_uri does not exist in some cases (eg, in tests)
-        try:
-            warning_url = request.build_absolute_url()
-        except AttributeError:
-            warning_url = request.path
-
-        logger.warning(f"Could not get challenge for request: {warning_url}")
-
-    if challenge is None:
+        logger.warning(f"Could not get challenge for request: {request}")
         return {}
-    else:
-        try:
-            user = request.user
-        except AttributeError:
-            user = get_anonymous_user()
 
-        return {
-            "challenge": challenge,
-            "challenge_perms": get_perms(user, challenge),
-            "user_is_participant": challenge.is_participant(user),
-            "pages": challenge.page_set.all(),
-        }
+    try:
+        user = request.user
+    except AttributeError:
+        user = get_anonymous_user()
+
+    return {
+        "challenge": challenge,
+        "challenge_perms": get_perms(user, challenge),
+        "user_is_participant": challenge.is_participant(user),
+        "pages": challenge.page_set.all(),
+    }
 
 
 def google_keys(*_, **__):
