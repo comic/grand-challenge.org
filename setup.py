e73a92723051e3fbc188a3c86b83f7384a304ab1
Tom Klaver <tomklav@gmail.com>
2019-06-07 12:28:26 +0200

    add generic setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="comic",
+    version="1.0.0",
+    author="Eyra team",
+    author_email="t.klaver@esciencecenter.nl",
+    description="Comic",
+    long_description="Comic",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
