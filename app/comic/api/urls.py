0b94c32764b67498fd0037a80cc6b638d7d218df
Tom Klaver <tomklav@gmail.com>
2019-06-17 17:40:41 +0200

    improve redoc api docs

--- app/comic/api/urls.py
+++ app/comic/api/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import routers, permissions
 from rest_framework.authtoken.views import obtain_auth_token
-# from rest_framework_swagger.views import get_swagger_view
 
 from comic.api.views import (
     UserViewSet,
@@ -23,8 +21,6 @@ from comic.eyra_users.viewsets import RegisterViewSet, LoginView
 app_name = "api"
 
 router = routers.DefaultRouter()
-# router.register(r"submissions", SubmissionViewSet)
-# router.register(r"cases/images", ImageViewSet)
 
 router.register(r"benchmarks", BenchmarkViewSet)
 router.register(r"submissions", SubmissionViewSet)
@@ -32,14 +28,9 @@ router.register(r"implementations", ImplementationViewSet)
 router.register(r"algorithms", AlgorithmViewSet)
 router.register(r"interfaces", InterfaceViewSet)
 router.register(r"jobs", JobViewSet)
-# router.register(r"challenges", ChallengeViewSet)
-
-
 router.register(r"data_files", DataFileViewSet)
 router.register(r"data_types", DataTypeViewSet)
 router.register(r"data_sets", DataSetViewSet)
-# router.register(r"datasetfiles", DataSetFileViewSet)
-
 router.register(r"users", UserViewSet)
 router.register(r"groups", GroupViewSet)
 
@@ -48,34 +39,35 @@ urlpatterns_social = [
     path("complete/<backend>/", rest_api_complete, name="complete"),
 ]
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   validators=['flex', 'ssv'],
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
     path("v1/auth/register/", RegisterViewSet.as_view({'post': 'create'})),
     path("v1/auth/login/", LoginView.as_view()),
     path("v1/algorithmSubmission/", algorithm_submission),
-
-    # path('v1/datasetfiles/<str:uuid>/', upload_file),
-    path("v1/me/", CurrentUserView.as_view()),
-    # path("v1/spec/", get_swagger_view(title="Comic API")),
     path("v1/social/", include((urlpatterns_social, "social"))),
+    path("v1/me/", CurrentUserView.as_view()),
     path("v1/login/", obtain_auth_token),
-    # Do not namespace the router.urls without updating the view names in
-    # evaluation.serializers
-    url(r'^v1/$', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
-
     path("v1/", include(router.urls)),
 ]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Eyra Benchmark REST API",
+      default_version='v1',
+      description="Eyra Benchmark REST API v1.",
+      terms_of_service="https://www.eyrabenchmark.net/",
+      contact=openapi.Contact(email="info@eyrabenchmark.net"),
+      license=openapi.License(name="Apache 2.0"),
+   ),
+   validators=['flex', 'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   patterns=urlpatterns[-3:],
+)
+
+urlpatterns = [
+    *urlpatterns[:-1],
+    url(r'^v1/$', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
+    urlpatterns[-1]
+]
+
 
