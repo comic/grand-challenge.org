ca13917ef6a0128f3edb64bbaf49bab35a720e0b
Haimasree <haimasree.diag@gmail.com>
2019-07-22 13:10:38 +0200

    viewsets added ISSUE #850

--- app/grandchallenge/algorithms/views.py
+++ app/grandchallenge/algorithms/views.py
@@ -9,6 +9,11 @@ from grandchallenge.cases.forms import UploadRawImagesForm
 from grandchallenge.cases.models import RawImageUploadSession
 from grandchallenge.core.permissions.mixins import UserIsStaffMixin
 from grandchallenge.subdomains.utils import reverse
+from grandchallenge.algorithms.serializers import AlgorithmSerializer, ResultSerializer, JobSerializer
+from grandchallenge.algorithms.models import Algorithm, Job, Result
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 logger = logging.getLogger(__name__)
 
@@ -54,3 +59,21 @@ class AlgorithmExecutionSessionCreate(
         return reverse(
             "algorithms:detail", kwargs={"slug": self.kwargs["slug"]}
         )
+
+    
+class AlgorithmViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Algorithm.objects.all()
+    serializer_class = AlgorithmSerializer
+    permission_classes = [IsAdminUser]
+
+
+class ResultViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+    permission_classes = [IsAdminUser]
+
+
+class JobViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
