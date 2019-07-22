6a07c32344d4613501b660c1479b67e5f536c583
Haimasree <haimasree.diag@gmail.com>
2019-07-22 13:12:13 +0200

    urls updated with router ISSUE #850

--- app/grandchallenge/api/urls.py
+++ app/grandchallenge/api/urls.py
@@ -7,6 +7,7 @@ from grandchallenge.patients.views import PatientViewSet
 from grandchallenge.studies.views import StudyViewSet
 from grandchallenge.worklists.views import WorklistViewSet
 from grandchallenge.workstations.views import SessionViewSet
+from grandchallenge.algorithms.views import AlgorithmViewSet, JobViewSet, ResultViewSet
 
 app_name = "api"
 
@@ -16,6 +17,9 @@ router.register(r"studies", StudyViewSet, basename="study")
 router.register(r"worklists", WorklistViewSet, basename="worklist")
 router.register(r"cases/images", ImageViewSet, basename="image")
 router.register(r"workstations/sessions", SessionViewSet)
+router.register(r'algorithms/', AlgorithmViewSet)
+router.register(r'algorithms/jobs', JobViewSet)
+router.register(r'algorithms/results', ResultViewSet)
 
 urlpatterns = [
     # Do not namespace the router.urls without updating the view names in
