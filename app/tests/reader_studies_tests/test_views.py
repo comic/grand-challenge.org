36ed047a44bd76764c1b1f2017f4f3a86c995c54
James Meakin <code@jmsmkn.com>
2019-07-29 15:25:18 +0200

    Adds permission tests to the update views

--- app/tests/reader_studies_tests/test_views.py
+++ app/tests/reader_studies_tests/test_views.py
@@ -7,6 +7,13 @@ from tests.reader_studies_tests.factories import ReaderStudyFactory
 from tests.utils import get_view_for_user
 
 
+def get_rs_creator():
+    creator = UserFactory()
+    g = Group.objects.get(name=settings.READER_STUDY_CREATORS_GROUP_NAME)
+    g.user_set.add(creator)
+    return creator
+
+
 @pytest.mark.django_db
 def test_rs_list_permissions(client):
     # Users should login
@@ -14,9 +21,7 @@ def test_rs_list_permissions(client):
     assert response.status_code == 302
     assert response.url.startswith(settings.LOGIN_URL)
 
-    creator = UserFactory()
-    g = Group.objects.get(name=settings.READER_STUDY_CREATORS_GROUP_NAME)
-    g.user_set.add(creator)
+    creator = get_rs_creator()
 
     # Creators should be able to see the create button
     response = get_view_for_user(
@@ -67,9 +72,7 @@ def test_rs_create_permissions(client):
     assert response.status_code == 302
     assert response.url.startswith(settings.LOGIN_URL)
 
-    creator = UserFactory()
-    g = Group.objects.get(name=settings.READER_STUDY_CREATORS_GROUP_NAME)
-    g.user_set.add(creator)
+    creator = get_rs_creator()
 
     # Creators should be able to get the create view
     response = get_view_for_user(
@@ -83,3 +86,131 @@ def test_rs_create_permissions(client):
         viewname="reader-studies:create", client=client, user=u
     )
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_rs_detail_view(client):
+    creator = get_rs_creator()
+    rs1, rs2 = ReaderStudyFactory(), ReaderStudyFactory()
+    editor1, reader1, editor2, reader2 = (
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+    )
+    rs1.add_editor(user=editor1)
+    rs2.add_editor(user=editor2)
+    rs1.add_reader(user=reader1)
+    rs2.add_reader(user=reader2)
+    u = UserFactory()
+
+    tests = (
+        (None, rs1, 302),
+        (None, rs2, 302),
+        (creator, rs1, 403),
+        (creator, rs2, 403),
+        (editor1, rs1, 200),
+        (editor1, rs2, 403),
+        (reader1, rs1, 200),
+        (reader1, rs2, 403),
+        (editor2, rs1, 403),
+        (editor2, rs2, 200),
+        (reader2, rs1, 403),
+        (reader2, rs2, 200),
+        (u, rs1, 403),
+        (u, rs2, 403),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            url=test[1].get_absolute_url(), client=client, user=test[0]
+        )
+        assert response.status_code == test[2]
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "view_name",
+    [
+        "update",
+        "add-images",
+        "add-question",
+        "editors-update",
+        "readers-update",
+    ],
+)
+def test_rs_edit_view(client, view_name):
+    creator = get_rs_creator()
+    rs1, rs2 = ReaderStudyFactory(), ReaderStudyFactory()
+    editor1, reader1, editor2, reader2 = (
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+    )
+    rs1.add_editor(user=editor1)
+    rs2.add_editor(user=editor2)
+    rs1.add_reader(user=reader1)
+    rs2.add_reader(user=reader2)
+    u = UserFactory()
+
+    tests = (
+        (None, rs1, 302),
+        (None, rs2, 302),
+        (creator, rs1, 403),
+        (creator, rs2, 403),
+        (editor1, rs1, 200),
+        (editor1, rs2, 403),
+        (reader1, rs1, 403),
+        (reader1, rs2, 403),
+        (editor2, rs1, 403),
+        (editor2, rs2, 200),
+        (reader2, rs1, 403),
+        (reader2, rs2, 403),
+        (u, rs1, 403),
+        (u, rs2, 403),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname=f"reader-studies:{view_name}",
+            client=client,
+            user=test[0],
+            reverse_kwargs={"slug": test[1].slug},
+        )
+        assert response.status_code == test[2]
+
+
+@pytest.mark.django_db
+def test_user_autocomplete(client):
+    creator = get_rs_creator()
+    rs1, rs2 = ReaderStudyFactory(), ReaderStudyFactory()
+    editor1, reader1, editor2, reader2 = (
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+        UserFactory(),
+    )
+    rs1.add_editor(user=editor1)
+    rs2.add_editor(user=editor2)
+    rs1.add_reader(user=reader1)
+    rs2.add_reader(user=reader2)
+    u = UserFactory()
+
+    tests = (
+        (None, 302),
+        (creator, 403),
+        (editor1, 200),
+        (reader1, 403),
+        (editor2, 200),
+        (reader2, 403),
+        (u, 403),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="reader-studies:users-autocomplete",
+            client=client,
+            user=test[0],
+        )
+        assert response.status_code == test[1]
