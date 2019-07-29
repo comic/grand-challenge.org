468d7494b0648ed85ebabd94138987557218c35d
James Meakin <code@jmsmkn.com>
2019-07-29 16:44:33 +0200

    Adds api permissions tests

--- app/tests/reader_studies_tests/test_permissions.py
+++ app/tests/reader_studies_tests/test_permissions.py
@@ -3,7 +3,11 @@ from django.conf import settings
 from django.contrib.auth.models import Group
 
 from tests.factories import UserFactory
-from tests.reader_studies_tests.factories import ReaderStudyFactory
+from tests.reader_studies_tests.factories import (
+    ReaderStudyFactory,
+    QuestionFactory,
+    AnswerFactory,
+)
 from tests.utils import get_view_for_user
 
 
@@ -195,3 +199,260 @@ def test_user_autocomplete_permissions(client):
             user=test[0],
         )
         assert response.status_code == test[1]
+
+
+@pytest.mark.django_db
+def test_api_rs_list_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    tests = (
+        (None, 401, []),
+        (rs_set.creator, 200, []),
+        (rs_set.editor1, 200, [rs_set.rs1.pk]),
+        (rs_set.reader1, 200, [rs_set.rs1.pk]),
+        (rs_set.editor2, 200, [rs_set.rs2.pk]),
+        (rs_set.reader2, 200, [rs_set.rs2.pk]),
+        (rs_set.u, 200, []),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-study-list",
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+        if test[1] != 401:
+            # We provided auth details and get a response
+            assert response.json()["count"] == len(test[2])
+
+            pks = [obj["pk"] for obj in response.json()["results"]]
+
+            for pk in test[2]:
+                assert str(pk) in pks
+
+
+@pytest.mark.django_db
+def test_api_rs_detail_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    tests = (
+        (None, 401),
+        (rs_set.creator, 404),
+        (rs_set.editor1, 200),
+        (rs_set.reader1, 200),
+        (rs_set.editor2, 404),
+        (rs_set.reader2, 404),
+        (rs_set.u, 404),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-study-detail",
+            reverse_kwargs={"pk": rs_set.rs1.pk},
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+
+@pytest.mark.django_db
+def test_api_rs_question_list_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    q1, q2 = (
+        QuestionFactory(reader_study=rs_set.rs1),
+        QuestionFactory(reader_study=rs_set.rs2),
+    )
+
+    tests = (
+        (None, 401, []),
+        (rs_set.creator, 200, []),
+        (rs_set.editor1, 200, [q1.pk]),
+        (rs_set.reader1, 200, [q1.pk]),
+        (rs_set.editor2, 200, [q2.pk]),
+        (rs_set.reader2, 200, [q2.pk]),
+        (rs_set.u, 200, []),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-studies-question-list",
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+        if test[1] != 401:
+            # We provided auth details and get a response
+            assert response.json()["count"] == len(test[2])
+
+            pks = [obj["pk"] for obj in response.json()["results"]]
+
+            for pk in test[2]:
+                assert str(pk) in pks
+
+
+@pytest.mark.django_db
+def test_api_rs_question_detail_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    q1 = QuestionFactory(reader_study=rs_set.rs1)
+
+    tests = (
+        (None, 401),
+        (rs_set.creator, 404),
+        (rs_set.editor1, 200),
+        (rs_set.reader1, 200),
+        (rs_set.editor2, 404),
+        (rs_set.reader2, 404),
+        (rs_set.u, 404),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-studies-question-detail",
+            reverse_kwargs={"pk": q1.pk},
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+
+@pytest.mark.django_db
+def test_api_rs_answer_list_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    q1, q2 = (
+        QuestionFactory(reader_study=rs_set.rs1),
+        QuestionFactory(reader_study=rs_set.rs2),
+    )
+
+    reader11 = UserFactory()
+    rs_set.rs1.add_reader(reader11)
+
+    a1, a11, a2 = (
+        AnswerFactory(question=q1, creator=rs_set.reader1, answer=""),
+        AnswerFactory(question=q1, creator=reader11, answer=""),
+        AnswerFactory(question=q2, creator=rs_set.reader2, answer=""),
+    )
+
+    tests = (
+        (None, 401, []),
+        (rs_set.creator, 200, []),
+        (rs_set.editor1, 200, [a1.pk, a11.pk]),
+        (rs_set.reader1, 200, [a1.pk]),
+        (reader11, 200, [a11.pk]),
+        (rs_set.editor2, 200, [a2.pk]),
+        (rs_set.reader2, 200, [a2.pk]),
+        (rs_set.u, 200, []),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-studies-answer-list",
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+        if test[1] != 401:
+            # We provided auth details and get a response
+            assert response.json()["count"] == len(test[2])
+
+            pks = [obj["pk"] for obj in response.json()["results"]]
+
+            for pk in test[2]:
+                assert str(pk) in pks
+
+
+@pytest.mark.django_db
+def test_api_rs_answer_detail_permissions(client):
+    rs_set = TwoReaderStudies()
+
+    q1 = QuestionFactory(reader_study=rs_set.rs1)
+
+    reader11 = UserFactory()
+    rs_set.rs1.add_reader(reader11)
+
+    a1 = AnswerFactory(question=q1, creator=rs_set.reader1, answer="")
+
+    tests = (
+        (None, 401),
+        (rs_set.creator, 404),
+        (rs_set.editor1, 200),
+        (rs_set.reader1, 200),
+        (reader11, 404),
+        (rs_set.editor2, 404),
+        (rs_set.reader2, 404),
+        (rs_set.u, 404),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-studies-answer-detail",
+            reverse_kwargs={"pk": a1.pk},
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+
+@pytest.mark.django_db
+def test_api_rs_answer_mine_list_permissions(client):
+    """
+    For the "mine" endpoint the list should be filtered by the users own
+    answers
+    """
+
+    rs_set = TwoReaderStudies()
+
+    q1, q2 = (
+        QuestionFactory(reader_study=rs_set.rs1),
+        QuestionFactory(reader_study=rs_set.rs2),
+    )
+
+    reader11 = UserFactory()
+    rs_set.rs1.add_reader(reader11)
+
+    a1, a11, a2 = (
+        AnswerFactory(question=q1, creator=rs_set.reader1, answer=""),
+        AnswerFactory(question=q1, creator=reader11, answer=""),
+        AnswerFactory(question=q2, creator=rs_set.reader2, answer=""),
+    )
+
+    tests = (
+        (None, 401, []),
+        (rs_set.creator, 200, []),
+        (rs_set.editor1, 200, []),
+        (rs_set.reader1, 200, [a1.pk]),
+        (reader11, 200, [a11.pk]),
+        (rs_set.editor2, 200, []),
+        (rs_set.reader2, 200, [a2.pk]),
+        (rs_set.u, 200, []),
+    )
+
+    for test in tests:
+        response = get_view_for_user(
+            viewname="api:reader-studies-answer-mine",
+            client=client,
+            user=test[0],
+            content_type="application/json",
+        )
+        assert response.status_code == test[1]
+
+        if test[1] != 401:
+            # We provided auth details and get a response
+            assert response.json()["count"] == len(test[2])
+
+            pks = [obj["pk"] for obj in response.json()["results"]]
+
+            for pk in test[2]:
+                assert str(pk) in pks
