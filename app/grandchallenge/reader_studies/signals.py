e68cb41a3fea696635e366cd98a70aa37aded288
James Meakin <code@jmsmkn.com>
2019-07-30 13:01:16 +0200

    Adds a bulk action for images

--- app/grandchallenge/reader_studies/signals.py
+++ app/grandchallenge/reader_studies/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from guardian.shortcuts import assign_perm, remove_perm
 
+from grandchallenge.cases.models import Image
 from grandchallenge.reader_studies.models import ReaderStudy
 
 
@@ -16,23 +17,24 @@ def update_image_permissions(instance, action, reverse, model, pk_set, **_):
         # nothing to do for the other actions
         return
 
-    op = assign_perm if "add" in action else remove_perm
-
     if reverse:
-        images = [instance]
+        images = Image.objects.filter(pk=instance.pk)
         try:
             reader_studies = model.objects.filter(pk__in=pk_set)
         except TypeError:
             # When a clear method is used pk_set is none and a TypeError raised
-            reader_studies = images[0].readerstudies.all()
+            reader_studies = instance.readerstudies.all()
+
+        reader_studies = reader_studies.select_related("readers_group")
     else:
         reader_studies = [instance]
         try:
             images = model.objects.filter(pk__in=pk_set)
         except TypeError:
             # When a clear method is used pk_set is none and a TypeError raised
-            images = reader_studies[0].images.all()
+            images = instance.images.all()
 
-    for im in images:
-        for rs in reader_studies:
-            op("view_image", rs.readers_group, im)
+    op = assign_perm if "add" in action else remove_perm
+
+    for rs in reader_studies:
+        op("view_image", rs.readers_group, images)
