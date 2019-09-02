7b4d1c1ba3ba14aa5c0019662427c41dadd69c75
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-02 17:10:01 +0200

    Adapt test so that it also tests for archive-only images, dual archive images, image with ImageFiles, image in study but not belonging to archive

--- app/tests/archives_tests/test_models.py
+++ app/tests/archives_tests/test_models.py
@@ -2,7 +2,10 @@ import pytest
 from django.core.exceptions import ObjectDoesNotExist
 
 from tests.archives_tests.factories import ArchiveFactory
-from tests.cases_tests.factories import ImageFactoryWithImageFile
+from tests.cases_tests.factories import (
+    ImageFactoryWithImageFile,
+    ImageFactoryWithoutImageFile,
+)
 from tests.model_helpers import do_test_factory
 
 
@@ -32,15 +35,22 @@ class TestCascadeDelete:
         annotation_set = AnnotationSetForImage(
             retina_grader=True, image=apsi_set.images111[0]
         )
-        image_not_in_archive = ImageFactoryWithImageFile(
+        image_not_in_archive = ImageFactoryWithoutImageFile(
             study=apsi_set.study111
         )
+        image_with_files = ImageFactoryWithImageFile(study=apsi_set.study111)
+        archive_only_images = ImageFactoryWithoutImageFile.create_batch(
+            4, study=None
+        )
+        dual_archive_images = ImageFactoryWithoutImageFile.create_batch(4)
+        apsi_set.archive1.images.add(image_with_files)
+        apsi_set.archive1.images.add(*archive_only_images)
+        apsi_set.archive1.images.add(*dual_archive_images)
+        apsi_set.archive2.images.add(*dual_archive_images)
         apsi_set.archive1.delete()
         all_deleted_models = (
             apsi_set.archive1,
-            apsi_set.patient11,
             apsi_set.patient12,
-            apsi_set.study111,
             apsi_set.study112,
             apsi_set.study113,
             apsi_set.study121,
@@ -57,17 +67,23 @@ class TestCascadeDelete:
             annotation_set.singlelandmarks[0],
             annotation_set.etdrs,
             annotation_set.integer,
-            image_not_in_archive,
+            *archive_only_images,
+            image_with_files,
+            *image_with_files.files.all(),
         )
         for model in all_deleted_models:
             with pytest.raises(ObjectDoesNotExist):
                 assert model.refresh_from_db()
 
         not_deleted_models = (
+            apsi_set.patient11,
+            apsi_set.study111,
             apsi_set.archive2,
             *apsi_set.images211,
             annotation_set.landmark,
             *annotation_set.singlelandmarks[1:],
+            image_not_in_archive,
+            *dual_archive_images,
         )
         for model in not_deleted_models:
             assert model.refresh_from_db() is None
