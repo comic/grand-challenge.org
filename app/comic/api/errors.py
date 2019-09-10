ea331cc580d8a997433f702062251ab934bcb387
Tom Klaver <tomklav@gmail.com>
2019-09-10 17:35:32 +0200

    #https://github.com/NLeSC/team-eyra/issues/108 add error codes, adds submission error test.

--- app/comic/api/errors.py
+++ app/comic/api/errors.py
@@ -9,8 +9,18 @@ logger = logging.getLogger(__name__)
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
+    from rest_framework.utils.serializer_helpers import ReturnDict
+    from rest_framework.exceptions import ErrorDetail, ValidationError
     from rest_framework.views import exception_handler
     response = exception_handler(exc, context)
+
+    # bad request.. probably validationError
+    if isinstance(exc, ValidationError) and response.status_code == 400 and isinstance(response.data, ReturnDict):
+        for (key, value) in response.data.items():
+            response.data[key] = [{
+                'message': str(error),
+                'code': error.code
+            } for error in value]
 
     # If error is not handled already by DRF (handles user errors (validation etc.))
     if response is None:
---
+++ app/tests/core_tests/test_submission.py
@@ -0,0 +1,20 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from tests.factories import UserFactory
+
+
+class SubmissionCreateTest(APITestCase):
+    def test_root(self):
+        user = UserFactory()
+        token, created = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+        url = '/api/v1/submissions/'
+        data = {
+            'name': "bladiebla",
+            'benchmark': 123,
+        }
+        response = self.client.post(url, data=data, format="json")
+        self.assertEqual(400, response.status_code)
+        self.assertEqual(response.data['benchmark'][0]['code'], 'does_not_exist')
+        self.assertEqual(response.data['implementation'][0]['code'], 'required')
