7eedcfc933ef9ac7245a8ab9ce0b5ef1dd1ad9bb
Haimasree <haimasree.diag@gmail.com>
2019-10-03 12:57:34 +0200

    Adds RawImageUploadSession API (#931)
    This only supports setting the upload session for algorithms right now.

---
+++ app/tests/cases_tests/test_api.py
@@ -0,0 +1,101 @@
+import pytest
+from tests.utils import get_view_for_user
+from tests.factories import UserFactory
+from tests.cases_tests.factories import RawImageUploadSessionFactory
+from tests.algorithms_tests.factories import AlgorithmImageFactory
+
+from grandchallenge.cases.models import RawImageUploadSession
+
+
+@pytest.mark.django_db
+def test_upload_session_list(client):
+    upload_session_1, upload_session_2 = (
+        RawImageUploadSessionFactory(),
+        RawImageUploadSessionFactory(),
+    )
+
+    user = UserFactory(is_staff=True)
+    response = get_view_for_user(
+        viewname="api:upload-session-list", user=user, client=client
+    )
+    assert response.status_code == 200
+    assert response.json()["count"] == 2
+
+    response = get_view_for_user(
+        viewname="api:upload-session-detail",
+        reverse_kwargs={"pk": upload_session_1.pk},
+        user=user,
+        client=client,
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_upload_sessions_create(client):
+    algo = AlgorithmImageFactory()
+    user = UserFactory(is_staff=True)
+
+    response = get_view_for_user(
+        viewname="api:upload-session-list",
+        user=user,
+        client=client,
+        method=client.post,
+        data={"algorithm": algo.api_url},
+        content_type="application/json",
+    )
+    assert response.status_code == 201
+
+    upload_session = RawImageUploadSession.objects.get(
+        pk=response.data.get("pk")
+    )
+    assert upload_session.algorithm == algo
+
+
+@pytest.mark.django_db
+def test_invalid_upload_sessions(client):
+    user = UserFactory(is_staff=True)
+
+    response = get_view_for_user(
+        viewname="api:upload-session-list",
+        user=user,
+        client=client,
+        method=client.post,
+        data={"algorithm": None},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+    assert response.json() == {"algorithm": ["This field may not be null."]}
+
+
+@pytest.mark.django_db
+def test_empty_data_upload_sessions(client):
+    user = UserFactory(is_staff=True)
+
+    response = get_view_for_user(
+        viewname="api:upload-session-list",
+        user=user,
+        client=client,
+        method=client.post,
+        data={},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+    assert response.json() == {"algorithm": ["This field is required."]}
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "is_staff, expected_response", [(False, 403), (True, 201)]
+)
+def test_upload_session_post_permissions(client, is_staff, expected_response):
+    user = UserFactory(is_staff=is_staff)
+    algo = AlgorithmImageFactory()
+    response = get_view_for_user(
+        viewname="api:upload-session-list",
+        user=user,
+        client=client,
+        method=client.post,
+        data={"algorithm": algo.api_url},
+        content_type="application/json",
+    )
+    assert response.status_code == expected_response
