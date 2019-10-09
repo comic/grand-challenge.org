7a6675e427d0dc75b348496d122d65141157f5fb
James Meakin <code@jmsmkn.com>
2019-10-09 09:59:51 +0200

    Adds algorithm to admin

--- app/grandchallenge/algorithms/admin.py
+++ app/grandchallenge/algorithms/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from grandchallenge.algorithms.models import AlgorithmImage, Job, Result
+from grandchallenge.algorithms.models import (
+    Algorithm,
+    AlgorithmImage,
+    Job,
+    Result,
+)
 
+admin.site.register(Algorithm)
 admin.site.register(AlgorithmImage)
 admin.site.register(Job)
 admin.site.register(Result)
