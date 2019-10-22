dbc77e2c0f61c5cda26d1a227158e5c0db410c1c
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-10-22 13:33:45 +0200

    Remove SessionAuthentication meant for debugging

--- app/grandchallenge/retina_api/views.py
+++ app/grandchallenge/retina_api/views.py
@@ -911,10 +911,7 @@ class RetinaImagePathologyAnnotationViewSet(viewsets.ModelViewSet):
 
 class ArchiveAPIView(APIView):
     permission_classes = (RetinaAPIPermission,)
-    authentication_classes = (
-        authentication.TokenAuthentication,
-        authentication.SessionAuthentication,
-    )  # TODO remove sessionsAuth
+    authentication_classes = (authentication.TokenAuthentication,)
     pagination_class = None
 
     def get(self, request, pk=None):
