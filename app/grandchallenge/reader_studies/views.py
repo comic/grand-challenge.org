2fc94638b940e26b74542b97b95601e63b6724a2
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2019-11-05 15:57:56 +0100

    Add csv export endpoints for reader study questions and answers

--- app/grandchallenge/reader_studies/views.py
+++ app/grandchallenge/reader_studies/views.py
@@ -1,3 +1,6 @@
+import csv
+import re
+
 from dal import autocomplete
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -6,6 +9,7 @@ from django.contrib.auth.mixins import (
     UserPassesTestMixin,
 )
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import Http404, HttpResponse
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -236,13 +240,62 @@ class ReadersUpdate(ReaderStudyUserGroupUpdateMixin):
     success_message = "Readers successfully updated"
 
 
-class ReaderStudyViewSet(ReadOnlyModelViewSet):
+class ExportCSVMixin(object):
+    def _check_export_perms(self, user, obj):
+        if not (user and user.has_perm(self.export_permission, obj)):
+            raise Http404()
+
+    def _create_csv_response(self, data, filename="export.csv"):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = f'attachment; filename="{filename}"'
+        writer = csv.writer(response, quoting=csv.QUOTE_ALL, escapechar="\\")
+
+        for line in data:
+            entry = map(lambda x: re.sub(r"[\n\r\t]", " ", str(x)), line)
+            writer.writerow(entry)
+
+        return response
+
+
+class ReaderStudyViewSet(ExportCSVMixin, ReadOnlyModelViewSet):
     serializer_class = ReaderStudySerializer
     queryset = ReaderStudy.objects.all().prefetch_related(
         "images", "questions"
     )
     permission_classes = [DjangoObjectPermissions]
     filter_backends = [ObjectPermissionsFilter]
+    export_permission = (
+        f"{ReaderStudy._meta.app_label}.change_{ReaderStudy._meta.model_name}"
+    )
+
+    @action(detail=True)
+    def export_questions(self, request, pk=None):
+        reader_study = self.get_object()
+        self._check_export_perms(request.user, reader_study)
+
+        data = []
+        for question in reader_study.questions.all():
+            data.append(
+                [question.question_text, question.get_answer_type_display()]
+            )
+
+        return self._create_csv_response(
+            data, filename=f"{reader_study.slug}-questions.csv"
+        )
+
+    @action(detail=True)
+    def export_answers(self, request, pk=None):
+        reader_study = self.get_object()
+        self._check_export_perms(request.user, reader_study)
+
+        fields = ["creator__username", "question__question_text", "answer"]
+        data = Answer.objects.filter(
+            question__reader_study=reader_study
+        ).values_list(*fields)
+
+        return self._create_csv_response(
+            data, filename=f"{reader_study.slug}-answers.csv"
+        )
 
 
 class QuestionViewSet(ReadOnlyModelViewSet):
