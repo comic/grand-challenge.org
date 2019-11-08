37f5b8a20a7d4c82d768e06c2862ef35b691b43d
Mike Overkamp <57257130+MikeOverkamp-diag@users.noreply.github.com>
2019-11-08 09:45:39 +0100

    Remove images from reader study admin (#976)

--- app/grandchallenge/reader_studies/admin.py
+++ app/grandchallenge/reader_studies/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from grandchallenge.reader_studies.models import Answer, Question, ReaderStudy
 
-admin.site.register(ReaderStudy)
+
+class ReaderStudyAdmin(admin.ModelAdmin):
+    exclude = ("images",)
+
+
+admin.site.register(ReaderStudy, ReaderStudyAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
