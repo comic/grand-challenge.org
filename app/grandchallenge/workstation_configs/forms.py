50c78b6142a162f59554d1846b552669886cd511
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2019-12-10 16:22:35 +0100

    Specify fields rather than exclude for form

--- app/grandchallenge/workstation_configs/forms.py
+++ app/grandchallenge/workstation_configs/forms.py
@@ -7,4 +7,16 @@ from grandchallenge.workstation_configs.models import WorkstationConfig
 class WorkstationConfigForm(SaveFormInitMixin, ModelForm):
     class Meta:
         model = WorkstationConfig
-        exclude = ("creator",)
+        fields = (
+            "title",
+            "description",
+            "window_presets",
+            "default_window_preset",
+            "default_slab_thickness_mm",
+            "default_slab_render_method",
+            "default_orientation",
+            "default_overlay_lut",
+            "default_overlay_interpolation",
+            "show_image_info_plugin",
+            "show_display_plugin",
+        )
