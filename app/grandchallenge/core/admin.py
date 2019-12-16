18785388c144443d979fb83daf4fc92d4ad3d6a3
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-12-16 14:19:19 +0100

    Reregister admin for Token with user search fields

--- app/grandchallenge/core/admin.py
+++ app/grandchallenge/core/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import Token
 
 admin.site.unregister(Group)
+admin.site.unregister(Token)
 
 
 class ReadOnlyUserInLine(admin.TabularInline):
@@ -21,3 +23,16 @@ class ReadOnlyUserInLine(admin.TabularInline):
 @admin.register(Group)
 class GroupWithUsers(GroupAdmin):
     inlines = [ReadOnlyUserInLine]
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ("key", "user", "created")
+    fields = ("user",)
+    ordering = ("-created",)
+    search_fields = (
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
