3d55f16db462301fc6145cbac6ee8ceb81e1644e
silvandeleemput <silvandeleemput@gmail.com>
2020-01-07 12:25:53 +0100

    splitted add_manual_fields to reduce cognitive complexity #1017

--- app/grandchallenge/api/swagger.py
+++ app/grandchallenge/api/swagger.py
@@ -8,16 +8,20 @@ def add_manual_fields(self, serializer_or_field, schema):
     meta = getattr(serializer_or_field, "Meta", None)
     swagger_schema_fields = getattr(meta, "swagger_schema_fields", {})
     if swagger_schema_fields:
-        for attr, val in swagger_schema_fields.items():
-            if isinstance(val, dict) and isinstance(
-                getattr(schema, attr, None), dict
-            ):
-                to_update = dict(
-                    list(getattr(schema, attr).items()) + list(val.items())
-                )
-                setattr(schema, attr, to_update)
-            else:
-                setattr(schema, attr, val)
+        update_swagger_schema_fields(schema, swagger_schema_fields)
+
+
+def update_swagger_schema_fields(schema, swagger_schema_fields):
+    for attr, val in swagger_schema_fields.items():
+        if isinstance(val, dict) and isinstance(
+            getattr(schema, attr, None), dict
+        ):
+            to_update = dict(
+                list(getattr(schema, attr).items()) + list(val.items())
+            )
+            setattr(schema, attr, to_update)
+        else:
+            setattr(schema, attr, val)
 
 
 # This overrides the default behavior when the swagger_schema_fields attribute is set
