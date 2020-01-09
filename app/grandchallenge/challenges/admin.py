9955f877933096e8304800ed26341867b3b768b3
James Meakin <code@jmsmkn.com>
2020-01-09 10:01:17 +0100

    Adds challenge series to admin

--- app/grandchallenge/challenges/admin.py
+++ app/grandchallenge/challenges/admin.py
@@ -4,6 +4,7 @@ from grandchallenge.challenges.models import (
     BodyRegion,
     BodyStructure,
     Challenge,
+    ChallengeSeries,
     ExternalChallenge,
     ImagingModality,
     TaskType,
@@ -11,6 +12,7 @@ from grandchallenge.challenges.models import (
 
 admin.site.register(Challenge)
 admin.site.register(ExternalChallenge)
+admin.site.register(ChallengeSeries)
 admin.site.register(BodyRegion)
 admin.site.register(BodyStructure)
 admin.site.register(ImagingModality)
