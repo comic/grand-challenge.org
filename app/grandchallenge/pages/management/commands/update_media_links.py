09547906dd1042bf37428ebfdddec0fb6cdde218
James Meakin <code@jmsmkn.com>
2020-01-22 14:21:10 +0100

    Clean media prefix

--- app/grandchallenge/pages/management/commands/update_media_links.py
+++ app/grandchallenge/pages/management/commands/update_media_links.py
@@ -8,5 +8,16 @@ from grandchallenge.pages.models import Page
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for p in Page.objects.filter(html__contains="/serve/"):
+            print(f"Updating serve in {p.get_absolute_url()}")
             p.html = re.sub(r"/site/([^/]+)/serve/", r"/media/\1/", p.html)
+            p.save()
+
+        for p in Page.objects.filter(html__contains="org/media/"):
+            print(f"Updating media in {p.get_absolute_url()}")
+            p.html = re.sub(
+                r"https?://[^.]+.grand-challenge.org/media/", "/media/", p.html
+            )
+            p.html = re.sub(
+                r"https?://grand-challenge.org/media/", "/media/", p.html
+            )
             p.save()
--- app/tests/pages_tests/test_media_rewrite.py
+++ app/tests/pages_tests/test_media_rewrite.py
@@ -34,3 +34,38 @@ def test_site_serve_rewrite(html_in, html_out):
 
     p.refresh_from_db()
     assert p.html == html_out
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "html_in,html_out",
+    (
+        ("http://myChallenge-18.grand-challenge.org/media/", "/media/"),
+        ("https://myChallenge-18.grand-challenge.org/media/", "/media/"),
+        (
+            "https://myChallenge-18.grand-challenge.org/notmedia/",
+            "https://myChallenge-18.grand-challenge.org/notmedia/",
+        ),
+        (
+            "https://myChallenge-18.grand-challenge.org/media ",
+            "https://myChallenge-18.grand-challenge.org/media ",
+        ),
+        ("http://grand-challenge.org/media/", "/media/"),
+        ("https://grand-challenge.org/media/", "/media/"),
+        (
+            "https://grand-challenge.org/notmedia/",
+            "https://grand-challenge.org/notmedia/",
+        ),
+        (
+            "https://grand-challenge.org/media ",
+            "https://grand-challenge.org/media ",
+        ),
+    ),
+)
+def test_media_rewrite(html_in, html_out):
+    p = PageFactory(html=html_in)
+
+    call_command("update_media_links")
+
+    p.refresh_from_db()
+    assert p.html == html_out
