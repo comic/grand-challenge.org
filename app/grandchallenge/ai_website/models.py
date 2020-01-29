195e3a89b19f22e03a602e404a7c769ac2768b26
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2020-01-29 12:39:13 +0100

    Use choices for charfields where appropriate

--- app/grandchallenge/ai_website/models.py
+++ app/grandchallenge/ai_website/models.py
@@ -9,6 +9,10 @@ def get_logo_path(instance, filename):
     return f"logos/{instance.__class__.__name__.lower()}/{instance.pk}/{get_valid_filename(filename)}"
 
 
+def get_images_path(instance, filename):
+    return f"product_images/{instance.__class__.__name__.lower()}/{instance.pk}/{get_valid_filename(filename)}"
+
+
 class CompanyEntry(models.Model):
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_date = models.DateField(default=timezone.now)
@@ -39,7 +43,7 @@ class CompanyEntry(models.Model):
 
 class ProductImage(models.Model):
     img = models.ImageField(
-        upload_to="media/product_images/", blank=True, default=""
+        upload_to=get_images_path, storage=public_s3_storage, blank=True,
     )
 
 
@@ -82,10 +86,51 @@ class ProductBasic(models.Model):
 
 # Create your models here.
 class ProductEntry(ProductBasic):
-    verified = models.CharField(max_length=5, default='no')
-    ce_status = models.CharField(max_length=500, default="no")
+    STATUS_CERTIFIED = 'cer'
+    STATUS_YES = 'yes'
+    STATUS_NO = 'no'
+    STATUS_NA = 'na'
+    STATUS_CLEARED = 'cle'
+    STATUS_DE_NOVO_CLEARED = 'dnc'
+    STATUS_PMA_APPROVED = 'pma'
+    STATUS_UNKNOWN = 'unk'
+
+    VERFIFIED_CHOICES = (
+        (STATUS_YES, 'Yes'),
+        (STATUS_NO, 'No')
+    )
+
+    CE_STATUS_CHOICES = (
+        (STATUS_CERTIFIED, 'Certified'),
+        (STATUS_NO, 'No or not yet'),
+        (STATUS_NA, 'Not applicable'),
+        (STATUS_UNKNOWN, 'Unknown'),
+    )
+
+    FDA_STATUS_CHOICES = (
+        (STATUS_CLEARED, '510(k) cleared'),
+        (STATUS_DE_NOVO_CLEARED, 'De novo 510(k) cleared'),
+        (STATUS_PMA_APPROVED, 'PMA approved'),
+        (STATUS_NO, 'No or not yet'),
+        (STATUS_NA, 'Not applicable'),
+        (STATUS_UNKNOWN, 'Unknown'),
+    )
+
+    ICONS = {
+        STATUS_CERTIFIED: 'icon_check.png',
+        STATUS_YES: 'icon_check.png',
+        STATUS_NO: 'icon_no.png',
+        STATUS_NA: 'icon_na.png',
+        STATUS_CLEARED: 'icon_check.png',
+        STATUS_DE_NOVO_CLEARED: 'icon_check.png',
+        STATUS_PMA_APPROVED: 'icon_check.png',
+        STATUS_UNKNOWN: 'icon_question.png',
+    }
+
+    verified = models.CharField(choices=VERFIFIED_CHOICES, max_length=3, default='no')
+    ce_status = models.CharField(choices=CE_STATUS_CHOICES, max_length=3, default="no")
     ce_class = models.CharField(max_length=500, default="unknown")
-    fda_status = models.CharField(max_length=500, default="unknown")
+    fda_status = models.CharField(choices=FDA_STATUS_CHOICES, max_length=3, default="unknown")
     fda_class = models.CharField(max_length=500, default="unknown")
 
     integration = models.CharField(max_length=500, default="unknown")
