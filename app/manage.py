3d7285351d14ea3f9121706dcfdc8b2f354b98e0
James <code@jmsmkn.com>
2020-01-29 20:28:44 +0100

    Adds asgi config (#1130)

--- app/manage.py
+++ app/manage.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
+
+def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-    from django.core.management import execute_from_command_line
-
-    # Appending app_path to path allows us to easily keep the apps in the
-    # subfolder
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "grandchallenge"))
-
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
