c7f677c2412711612ff7484057cef3f9e2f81b0b
HarmvZ <HarmvZ@users.noreply.github.com>
2020-01-30 16:51:36 +0100

    Improve performance of annotation admin (#1136)
    * Improve admin performance by making all fields read only
    
    * Make PolygonAnnotationSet searchable by image name

--- app/grandchallenge/annotations/admin.py
+++ app/grandchallenge/annotations/admin.py
@@ -19,23 +19,33 @@ class BooleanClassificationAnnotationAdmin(admin.ModelAdmin):
 class SinglePolygonAnnotationInline(admin.StackedInline):
     model = SinglePolygonAnnotation
     extra = 0
+    readonly_fields = (
+        "annotation_set",
+        "value",
+        "x_axis_orientation",
+        "y_axis_orientation",
+        "z",
+    )
 
 
 class PolygonAnnotationSetAdmin(admin.ModelAdmin):
-    search_fields = ("grader__username", "created", "name")
+    search_fields = ("grader__username", "created", "name", "image__name")
     list_filter = ("created", "grader__username", "name")
     inlines = [SinglePolygonAnnotationInline]
+    readonly_fields = ("grader", "image", "name", "created")
 
 
 class SingleLandmarkAnnotationInline(admin.StackedInline):
     model = SingleLandmarkAnnotation
     extra = 0
+    readonly_fields = ("image", "landmarks")
 
 
 class LandmarkAnnotationSetAdmin(admin.ModelAdmin):
     search_fields = ("grader__username", "created")
     list_filter = ("created", "grader__username")
     inlines = [SingleLandmarkAnnotationInline]
+    readonly_fields = ("grader", "created")
 
 
 admin.site.register(ETDRSGridAnnotation)
