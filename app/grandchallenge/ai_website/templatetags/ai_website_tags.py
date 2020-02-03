d9365adc4ed39caee1eb143f5d19b5869cc41524
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2020-02-03 15:50:08 +0100

    Move navbar to template tag

--- app/grandchallenge/ai_website/templatetags/ai_website_tags.py
+++ app/grandchallenge/ai_website/templatetags/ai_website_tags.py
@@ -6,6 +6,39 @@ from grandchallenge.ai_website.models import ProductEntry
 register = template.Library()
 
 
+@register.inclusion_tag("ai_website/partials/navbar.html", takes_context=True)
+def navbar(context):
+    url = context.request.resolver_match.url_name
+    return {
+        "items": [
+            {
+                "url": "product_list",
+                "active": url in ["product_list", "product_page"],
+                "icon": "fa-th-large",
+                "title": "Products",
+            },
+            {
+                "url": "company_list",
+                "active": url in ["company_list", "company_page"],
+                "icon": "fa-th-large",
+                "title": "Companies",
+            },
+            {
+                "url": "about",
+                "active": url == "about",
+                "icon": "fa-user",
+                "title": "About",
+            },
+            {
+                "url": "contact",
+                "active": url == "contact",
+                "icon": "fa-envelope",
+                "title": "Contact",
+            },
+        ],
+    }
+
+
 @register.simple_tag
 def icon(obj, field):
     value = getattr(obj, field, None)
