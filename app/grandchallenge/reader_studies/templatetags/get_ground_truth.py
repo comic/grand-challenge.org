aeffd116b3c75ab45cdf6cd5b3a903432d9f2639
Mike Overkamp <57257130+MikeOverkamp-diag@users.noreply.github.com>
2020-02-11 12:13:51 +0100

    Only show completed message on completed reader studies (#1162)
    * Only show completed message on completed reader studies
    
    * Fix docstring

--- app/grandchallenge/reader_studies/templatetags/get_ground_truth.py
+++ app/grandchallenge/reader_studies/templatetags/get_ground_truth.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_ground_truth(obj, image, question):
-    """Get the auth token for the user."""
+    """Get the ground truth value for the image/question combination in reader
+    study obj."""
     ground_truths = obj.statistics["ground_truths"]
     return ground_truths[image][question]
