7e0c7f541b5a7fa684c23d7dd0e0924006868345
Haimasree Bhattacharya <haimasree.diag@gmail.com>
2020-02-12 16:22:49 +0100

    Serializing status ISSUE #1164

--- app/grandchallenge/algorithms/serializers.py
+++ app/grandchallenge/algorithms/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField
+from rest_framework.fields import CharField, SerializerMethodField
 from rest_framework.relations import HyperlinkedRelatedField
 
 from grandchallenge.algorithms.models import (
@@ -68,7 +68,8 @@ class JobSerializer(serializers.ModelSerializer):
     image = HyperlinkedRelatedField(
         queryset=Image.objects.all(), view_name="api:image-detail"
     )
+    status = CharField(source="get_status_display", read_only=True)
 
     class Meta:
         model = Job
-        fields = ["pk", "api_url", "algorithm_image", "image"]
+        fields = ["pk", "api_url", "algorithm_image", "image", "status"]
