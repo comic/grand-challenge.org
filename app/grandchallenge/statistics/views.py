88cf2b833d9ee75abc2761aa0f05953fd3987eba
James <12661555+jmsmkn@users.noreply.github.com>
2020-04-13 18:14:24 +0200

    Adds prometheus endpoint (#1300)

--- app/grandchallenge/statistics/views.py
+++ app/grandchallenge/statistics/views.py
@@ -1,16 +1,26 @@
 import json
 from datetime import timedelta
 
+import prometheus_client
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.db.models import Count, Sum
 from django.utils import timezone
 from django.views.generic import TemplateView
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from grandchallenge.algorithms.models import Algorithm, Job
 from grandchallenge.challenges.models import Challenge
-from grandchallenge.evaluation.models import Result, Submission
+from grandchallenge.evaluation.models import (
+    Job as EvaluationJob,
+    Result,
+    Submission,
+)
 from grandchallenge.reader_studies.models import Answer, Question, ReaderStudy
+from grandchallenge.statistics.renderers import PrometheusRenderer
 from grandchallenge.workstations.models import Session, Workstation
 
 
@@ -118,3 +128,58 @@ class StatisticsDetail(TemplateView):
         context.update(extra)
 
         return context
+
+
+workstation_sessions_active = prometheus_client.Gauge(
+    "grandchallenge_workstation_sessions_active_total",
+    "The number of active workstation sessions",
+)
+algorithm_jobs_pending = prometheus_client.Gauge(
+    "grandchallenge_algorithm_jobs_pending_total",
+    "The number of pending algorithm jobs",
+)
+algorithm_jobs_active = prometheus_client.Gauge(
+    "grandchallenge_algorithm_jobs_active_total",
+    "The number of active algorithm jobs",
+)
+evaluation_jobs_pending = prometheus_client.Gauge(
+    "grandchallenge_evaluation_jobs_pending_total",
+    "The number of pending evaluation jobs",
+)
+evaluation_jobs_active = prometheus_client.Gauge(
+    "grandchallenge_evaluation_jobs_active_total",
+    "The number of active evaluation jobs",
+)
+
+build_version = prometheus_client.Info(
+    "grandchallenge_build_version", "The build version"
+)
+build_version.info({"grandchallenge_commit_id": settings.COMMIT_ID})
+
+
+class MetricsAPIView(APIView):
+    renderer_classes = [PrometheusRenderer]
+    permission_classes = [IsAdminUser]
+
+    def get(self, request, format=None):
+        workstation_sessions_active.set(
+            Session.objects.filter(status=Session.STARTED).count()
+        )
+        algorithm_jobs_pending.set(
+            Job.objects.filter(status=Job.PENDING).count()
+        )
+        algorithm_jobs_active.set(
+            Job.objects.filter(status=Job.STARTED).count()
+        )
+        evaluation_jobs_pending.set(
+            EvaluationJob.objects.filter(status=Job.PENDING).count()
+        )
+        evaluation_jobs_active.set(
+            EvaluationJob.objects.filter(status=Job.STARTED).count()
+        )
+
+        registry = prometheus_client.REGISTRY
+        metrics = prometheus_client.generate_latest(registry)
+        return Response(
+            metrics, content_type=prometheus_client.CONTENT_TYPE_LATEST
+        )
