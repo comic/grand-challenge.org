9925186c925a5a22e00eb3a1c8b875d4f0b81517
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-06-17 15:05:11 +0200

    Adds input serialization

--- app/tests/algorithms_tests/test_api.py
+++ app/tests/algorithms_tests/test_api.py
@@ -67,3 +67,14 @@ def test_keys_used_in_algorithm_session_js(client):
     )
     assert response.status_code == 200
     assert response.json()["job_set"] == [j.api_url]
+
+
+@pytest.mark.django_db
+def test_inputs_are_serialized(client):
+    u = UserFactory()
+    j = AlgorithmJobFactory(creator=u)
+
+    response = get_view_for_user(client=client, url=j.api_url, user=u)
+    assert response.json()["inputs"][0]["image"]["pk"] == str(
+        j.inputs.first().image.pk
+    )
