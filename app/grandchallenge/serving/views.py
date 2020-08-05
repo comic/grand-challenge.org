c010dec307b4a6f93a0305fc356ab4caf5dececc
James <12661555+jmsmkn@users.noreply.github.com>
2020-08-05 14:47:38 +0200

    Initialise the internal protected storage only once (#1448)
    Initialising the internal protected storage is quite slow, so move this
    out of the view.

--- app/grandchallenge/serving/views.py
+++ app/grandchallenge/serving/views.py
@@ -8,7 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
 from grandchallenge.cases.models import Image
-from grandchallenge.core.storage import ProtectedS3Storage
+from grandchallenge.core.storage import internal_protected_s3_storage
 from grandchallenge.evaluation.models import Submission
 from grandchallenge.serving.permissions import (
     user_can_download_image,
@@ -20,17 +20,15 @@ from grandchallenge.serving.tasks import create_download
 def protected_storage_redirect(*, name):
     # Get the storage with the internal redirect and auth. This will prepend
     # settings.PROTECTED_S3_STORAGE_KWARGS['endpoint_url'] to the url
-    storage = ProtectedS3Storage(internal=True)
-
-    if not storage.exists(name=name):
+    if not internal_protected_s3_storage.exists(name=name):
         raise Http404("File not found.")
 
     if settings.PROTECTED_S3_STORAGE_USE_CLOUDFRONT:
         response = HttpResponseRedirect(
-            storage.cloudfront_signed_url(name=name)
+            internal_protected_s3_storage.cloudfront_signed_url(name=name)
         )
     else:
-        url = storage.url(name=name)
+        url = internal_protected_s3_storage.url(name=name)
         response = HttpResponseRedirect(url)
 
     return response
