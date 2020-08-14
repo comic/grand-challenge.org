27845be1deb5b591600bdb13af6995e683bee363
Nils Hendrix <47858231+njmhendrix@users.noreply.github.com>
2020-08-14 18:53:46 +0200

    Add additional DICOM headers: PhotometricInterpretation, ImagerPixelSpacing, and PixelSpacing.

--- app/grandchallenge/cases/image_builders/metaio_utils.py
+++ app/grandchallenge/cases/image_builders/metaio_utils.py
@@ -56,6 +56,9 @@ LENGTH_LIMIT_MATCH_REGEXP: Pattern = re.compile(r"^.{0,128}$")
 STUDYDATE_MATCH_REGEXP: Pattern = re.compile(r"^\d{4}\d{1,2}\d{1,2}$")
 
 ADDITIONAL_HEADERS: Dict[str, Pattern] = {
+    "PhotometricInterpretation": LENGTH_LIMIT_MATCH_REGEXP,
+    "ImagerPixelSpacing": FLOAT_LIST_MATCH_REGEXP,
+    "PixelSpacing": FLOAT_LIST_MATCH_REGEXP,
     "Laterality": LENGTH_LIMIT_MATCH_REGEXP,
     "PatientID": LENGTH_LIMIT_MATCH_REGEXP,
     "PatientName": LENGTH_LIMIT_MATCH_REGEXP,
