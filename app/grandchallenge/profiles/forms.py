70604ddaf4ba4b210ae11b08646598bf0732c63a
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-09-25 19:18:53 +0200

    Do not allow editing of is_verified

--- app/grandchallenge/profiles/forms.py
+++ app/grandchallenge/profiles/forms.py
@@ -146,3 +146,6 @@ class EditProfileForm(userena_forms.EditProfileForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         del self.fields["privacy"]
+
+    class Meta(userena_forms.EditProfileForm.Meta):
+        exclude = ["is_verified", "user"]
