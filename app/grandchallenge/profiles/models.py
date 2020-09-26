86949303e4a386a5dcc78f5cfd62d18b0896b5fe
James <12661555+jmsmkn@users.noreply.github.com>
2020-09-26 11:57:59 +0200

    Adds pyswot to verify academic email addresses (#1551)
    * Adds pyswot
    
    * Use pyswot to update verification status
    
    * Verify on each save

--- app/grandchallenge/profiles/models.py
+++ app/grandchallenge/profiles/models.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 from django_countries.fields import CountryField
+from pyswot import is_academic
 from userena.models import UserenaBaseProfile
 
 from grandchallenge.core.utils import disable_for_loaddata
@@ -12,7 +13,7 @@ from grandchallenge.subdomains.utils import reverse
 
 class UserProfile(UserenaBaseProfile):
     user = models.OneToOneField(
-        User,
+        get_user_model(),
         unique=True,
         verbose_name=_("user"),
         related_name="user_profile",
@@ -30,6 +31,12 @@ class UserProfile(UserenaBaseProfile):
         return reverse(
             "userena_profile_detail", kwargs={"username": self.user.username}
         )
+
+    def save(self, *args, **kwargs):
+        # For now, only academic emails are verified
+        self.is_verified = is_academic(self.user.email)
+
+        super().save(*args, **kwargs)
 
 
 @disable_for_loaddata
