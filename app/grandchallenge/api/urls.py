a16235091a344da4eb3b9e50388e4d584640ba7a
James <12661555+jmsmkn@users.noreply.github.com>
2020-09-30 18:39:23 +0200

    Upgrade to Django 3.1 (#1557)
    * Update to django 3.1
    
    * Be more specific about ignored warnings

--- app/grandchallenge/api/urls.py
+++ app/grandchallenge/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.conf.urls import include, url
-from django.urls import path
+from django.conf.urls import include
+from django.urls import path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions, routers
@@ -179,7 +179,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    url(
+    re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(),
         name="schema-json",
