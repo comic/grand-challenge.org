d28f4088adedc0769a0efc80003183b7b665e386
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-10-15 05:51:15 +0200

    Fix testing endpoints before modal is displayed

--- app/grandchallenge/workstations/static/workstations/js/session-region.js
+++ app/grandchallenge/workstations/static/workstations/js/session-region.js
@@ -46,20 +46,22 @@ function ping_regions(regions, endpoint) {
 $(document).ready(function () {
     const session_modal = $("#sessionModal");
 
+    session_modal.on("shown.bs.modal", function () {
+        const ping_endpoint = JSON.parse(document.getElementById("ping-endpoint-data").textContent);
+        const region_selection = document.getElementById("id_region");
+        const ping_widget = document.getElementById("id_ping_times");
+        const regions = [...region_selection.options];
+
+        let timings = ping_regions(regions, ping_endpoint);
+
+        ping_widget.value = JSON.stringify(timings);
+
+        const server = timings.reduce((prev, current) => (prev.ping < current.ping) ? prev : current);
+        region_selection.value = server.id;
+
+        setModalLoadingMessage(`Connecting to ${region_selection.value}...`);
+        region_selection.form.submit();
+    })
+
     session_modal.modal({show: true});
-
-    const ping_endpoint = JSON.parse(document.getElementById("ping-endpoint-data").textContent);
-    const region_selection = document.getElementById("id_region");
-    const ping_widget = document.getElementById("id_ping_times");
-    const regions = [...region_selection.options];
-
-    let timings = ping_regions(regions, ping_endpoint);
-
-    ping_widget.value = JSON.stringify(timings);
-
-    const server = timings.reduce((prev, current) => (prev.ping < current.ping) ? prev : current);
-    region_selection.value = server.id;
-
-    setModalLoadingMessage(`Connecting to ${region_selection.value}...`);
-    region_selection.form.submit();
 })
