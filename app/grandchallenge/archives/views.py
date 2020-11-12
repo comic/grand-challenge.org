16b090eef6bf68113415cd2110d30470a4d8ad75
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-11-12 15:46:16 +0100

    Show algorithm job results on archive cases list page

--- app/grandchallenge/archives/views.py
+++ app/grandchallenge/archives/views.py
@@ -460,7 +460,10 @@ class ArchiveCasesList(
         qs = super().get_queryset(*args, **kwargs)
         return (
             qs.filter(archive=self.archive)
-            .prefetch_related("files")
+            .prefetch_related(
+                "files",
+                "componentinterfacevalue_set__algorithms_jobs_as_input__outputs__image__files",
+            )
             .select_related(
                 "origin__creator__user_profile",
                 "origin__creator__verification",
