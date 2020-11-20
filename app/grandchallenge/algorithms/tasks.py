d817ad49489a23d5c32b7bdfa86cdd84bff7106b
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-11-20 16:37:03 +0100

    Add archive groups to job viewer groups and make creator optional

--- app/grandchallenge/algorithms/tasks.py
+++ app/grandchallenge/algorithms/tasks.py
@@ -1,5 +1,6 @@
 from celery import group, shared_task
 from django.conf import settings
+from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 
@@ -24,17 +25,23 @@ def create_algorithm_jobs_for_session(*_, upload_session_pk):
     create_algorithm_jobs(
         algorithm_image=session.algorithm_image,
         images=session.image_set.all(),
-        session_pk=upload_session_pk,
+        session=session,
     )
 
 
 @shared_task
 def create_algorithm_jobs_for_archive_images(archive_pks, image_pks):
     for archive in Archive.objects.filter(pk__in=archive_pks).all():
+        groups = [
+            archive.editors_group,
+            archive.uploaders_group,
+            archive.users_group,
+        ]
         for algorithm in archive.algorithms.all():
             create_algorithm_jobs(
                 algorithm_image=algorithm.latest_ready_image,
                 images=Image.objects.filter(pk__in=image_pks).all(),
+                extra_viewer_groups=groups,
             )
 
 
@@ -42,13 +49,21 @@ def create_algorithm_jobs_for_archive_images(archive_pks, image_pks):
 def create_algorithm_jobs_for_archive_algorithms(archive_pks, algorithm_pks):
     for algorithm in Algorithm.objects.filter(pk__in=algorithm_pks).all():
         for archive in Archive.objects.filter(pk__in=archive_pks).all():
+            groups = [
+                archive.editors_group,
+                archive.uploaders_group,
+                archive.users_group,
+            ]
             create_algorithm_jobs(
                 algorithm_image=algorithm.latest_ready_image,
                 images=archive.images.all(),
+                extra_viewer_groups=groups,
             )
 
 
-def create_algorithm_jobs(algorithm_image, images, session_pk=None):
+def create_algorithm_jobs(
+    algorithm_image, images, session=None, extra_viewer_groups=None
+):
     default_input_interface = ComponentInterface.objects.get(
         slug=DEFAULT_INPUT_INTERFACE_SLUG
     )
@@ -57,7 +72,7 @@ def create_algorithm_jobs(algorithm_image, images, session_pk=None):
 
     if algorithm_image:
         for image in images:
-            creator = image.origin.creator
+            creator = None if session is None else session.creator
             if not ComponentInterfaceValue.objects.filter(
                 interface=default_input_interface,
                 image=image,
@@ -79,8 +94,25 @@ def create_algorithm_jobs(algorithm_image, images, session_pk=None):
     if jobs:
         job_signatures = [j.signature for j in jobs]
         job_pks = [j.pk for j in jobs]
-        workflow = group(*job_signatures) | send_failed_jobs_email.signature(
-            kwargs={"job_pks": job_pks, "session_pk": session_pk}
+        # TODO decision - who should be failed jobs email receivers?
+        permission_jobs = []
+        if extra_viewer_groups is not None:
+            group_pks = [g.pk for g in extra_viewer_groups]
+            for job in jobs:
+                permission_jobs.append(
+                    add_job_viewer_groups.signature(
+                        kwargs={"job_pk": job.pk, "group_pks": group_pks}
+                    )
+                )
+        workflow = (
+            group(*job_signatures)
+            | send_failed_jobs_email.signature(
+                kwargs={
+                    "job_pks": job_pks,
+                    "session_pk": None if session is None else session.pk,
+                }
+            )
+            | group(*permission_jobs)
         )
         workflow.apply_async()
 
@@ -132,3 +164,10 @@ def send_failed_jobs_email(*_, job_pks, session_pk=None):
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[email],
             )
+
+
+@shared_task
+def add_job_viewer_groups(job_pk, group_pks):
+    job = Job.objects.get(pk=job_pk)
+    groups = Group.objects.filter(pk__in=group_pks)
+    job.viewer_groups.add(*groups)
