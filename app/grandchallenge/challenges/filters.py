63833584a232a1c57935b5def69161594d1f49fb
James <12661555+jmsmkn@users.noreply.github.com>
2020-11-23 08:18:37 +0100

    Adds filtering back to the challenge list page (#1602)
    * Replace custom search with Django Filter
    
    * Use select2 for widgets
    
    * Use a collapsable element to hide the filter form

---
+++ app/grandchallenge/challenges/filters.py
@@ -0,0 +1,77 @@
+from functools import reduce
+from operator import or_
+
+from django.db.models import Q
+from django_filters import (
+    CharFilter,
+    FilterSet,
+    ModelMultipleChoiceFilter,
+)
+from django_select2.forms import Select2MultipleWidget
+
+from grandchallenge.challenges.forms import ChallengeFilterForm
+from grandchallenge.challenges.models import (
+    BodyRegion,
+    BodyStructure,
+    Challenge,
+    ChallengeSeries,
+    ImagingModality,
+    TaskType,
+)
+
+
+class ChallengeFilter(FilterSet):
+    search = CharFilter(method="search_filter", label="Title or Description")
+    modalities = ModelMultipleChoiceFilter(
+        queryset=ImagingModality.objects.all(),
+        widget=Select2MultipleWidget,
+        label="Modality",
+    )
+    task_types = ModelMultipleChoiceFilter(
+        queryset=TaskType.objects.all(),
+        widget=Select2MultipleWidget,
+        label="Task Type",
+    )
+    structures = ModelMultipleChoiceFilter(
+        queryset=BodyStructure.objects.all(),
+        widget=Select2MultipleWidget,
+        label="Anatomical Structure",
+    )
+    structures__region = ModelMultipleChoiceFilter(
+        queryset=BodyRegion.objects.all(),
+        widget=Select2MultipleWidget,
+        label="Anatomical Region",
+    )
+    series = ModelMultipleChoiceFilter(
+        queryset=ChallengeSeries.objects.all(),
+        widget=Select2MultipleWidget,
+        label="Challenge Series",
+    )
+
+    class Meta:
+        model = Challenge
+        fields = (
+            "search",
+            "series",
+            "task_types",
+            "modalities",
+            "structures",
+            "structures__region",
+            "educational",
+        )
+        form = ChallengeFilterForm
+
+    def search_filter(self, queryset, name, value):
+        search_fields = [
+            "title",
+            "short_name",
+            "description",
+            "event_name",
+        ]
+        return queryset.filter(
+            reduce(
+                or_,
+                [Q(**{f"{f}__icontains": value}) for f in search_fields],
+                Q(),
+            )
+        )
