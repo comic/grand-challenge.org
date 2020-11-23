9d792ff39bdc7450f120b7ef302ac34eb989aad0
Miriam Groeneveld <miriam.groeneveld.diag@gmail.com>
2020-11-23 14:32:31 +0100

    Fixed confilcting migration files

--- app/grandchallenge/algorithms/migrations/0033_algorithm_job_credit.py
+++ app/grandchallenge/algorithms/migrations/0033_algorithm_job_credit.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("algorithms", "0031_auto_20201117_0746"),
+        ("algorithms", "0032_job_stderr"),
     ]
 
     operations = [
