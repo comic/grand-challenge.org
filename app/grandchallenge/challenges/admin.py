2c2b91c60cae8f85ccb2a6a6548b128048aa6eb9
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-11-24 12:37:29 +0100

    Moves BodyStructure to Anatomy app

--- app/grandchallenge/challenges/admin.py
+++ app/grandchallenge/challenges/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from grandchallenge.challenges.models import (
-    BodyStructure,
     Challenge,
     ChallengeSeries,
     ExternalChallenge,
@@ -24,6 +23,5 @@ class ExternalChallengeAdmin(ModelAdmin):
 admin.site.register(Challenge, ChallengeAdmin)
 admin.site.register(ExternalChallenge, ExternalChallengeAdmin)
 admin.site.register(ChallengeSeries)
-admin.site.register(BodyStructure)
 admin.site.register(ImagingModality)
 admin.site.register(TaskType)
