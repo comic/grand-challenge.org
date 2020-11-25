58d77dd8033f7d559ab70c558a6e7b529d542261
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-11-25 13:41:13 +0100

    Adds user search in credits admin

--- app/grandchallenge/credits/admin.py
+++ app/grandchallenge/credits/admin.py
@@ -5,7 +5,15 @@ from grandchallenge.credits.models import Credit
 
 
 class CreditAdmin(ModelAdmin):
+    list_display = (
+        "user",
+        "credits",
+    )
     autocomplete_fields = ("user",)
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
 
 
 admin.site.register(Credit, CreditAdmin)
