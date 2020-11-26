c31976c0e1b3fd610f96c41eddc92af76ea8c17a
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-11-26 14:20:14 +0100

    Add tests to check if signals trigger correct tasks

--- app/tests/archives_tests/test_signals.py
+++ app/tests/archives_tests/test_signals.py
@@ -1,5 +1,12 @@
+from unittest.mock import call
+
 import pytest
 
+from grandchallenge.algorithms.tasks import (
+    create_algorithm_jobs_for_archive_algorithms,
+    create_algorithm_jobs_for_archive_images,
+)
+from tests.algorithms_tests.factories import AlgorithmFactory
 from tests.archives_tests.utils import TwoArchives
 from tests.factories import ImageFactory
 from tests.utils import get_view_for_user
@@ -81,3 +88,127 @@ def test_user_can_download_images(client, reverse):  # noqa: C901
         assert response.json()["count"] == 1
     else:
         assert response.json()["count"] == 0
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("reverse", [True, False])
+def test_adding_images_triggers_task(reverse, mocker):
+    mocker.patch(
+        "grandchallenge.algorithms.tasks.create_algorithm_jobs_for_archive_images.apply_async"
+    )
+    create_algorithm_jobs_for_archive_images.apply_async.assert_not_called()
+
+    arch_set = TwoArchives()
+
+    create_algorithm_jobs_for_archive_images.apply_async.assert_has_calls(
+        [
+            call(
+                args=([arch_set.arch1.pk], [arch_set.arch1.images.first().pk])
+            ),
+            call(
+                args=([arch_set.arch2.pk], [arch_set.arch2.images.first().pk])
+            ),
+        ]
+    )
+    create_algorithm_jobs_for_archive_images.apply_async.reset_mock()
+
+    im1, im2, im3, im4 = (
+        ImageFactory(),
+        ImageFactory(),
+        ImageFactory(),
+        ImageFactory(),
+    )
+
+    if not reverse:
+        arch_set.arch1.images.add(im1, im2, im3, im4)
+        args = create_algorithm_jobs_for_archive_images.apply_async.call_args.kwargs[
+            "args"
+        ]
+        create_algorithm_jobs_for_archive_images.apply_async.assert_called_once()
+        assert args[0] == [arch_set.arch1.pk]
+        assert set(args[1]) == {im1.pk, im2.pk, im3.pk, im4.pk}
+        create_algorithm_jobs_for_archive_images.apply_async.reset_mock()
+
+        arch_set.arch1.images.remove(im3, im4)
+        arch_set.arch1.images.clear()
+        create_algorithm_jobs_for_archive_images.apply_async.assert_not_called()
+    else:
+        for im in [im1, im2, im3, im4]:
+            im.archive_set.add(arch_set.arch1, arch_set.arch2)
+            args = create_algorithm_jobs_for_archive_images.apply_async.call_args.kwargs[
+                "args"
+            ]
+            create_algorithm_jobs_for_archive_images.apply_async.assert_called_once()
+            assert set(args[0]) == {arch_set.arch1.pk, arch_set.arch2.pk}
+            assert args[1] == [im.pk]
+            create_algorithm_jobs_for_archive_images.apply_async.reset_mock()
+        for im in [im3, im4]:
+            im.archive_set.remove(arch_set.arch1, arch_set.arch2)
+        for im in [im1, im2]:
+            im.archive_set.remove(arch_set.arch2)
+        im1.archive_set.clear()
+        create_algorithm_jobs_for_archive_images.apply_async.assert_not_called()
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("reverse", [True, False])
+def test_adding_algorithms_triggers_task(reverse, mocker):
+    mocker.patch(
+        "grandchallenge.algorithms.tasks.create_algorithm_jobs_for_archive_algorithms.apply_async"
+    )
+    create_algorithm_jobs_for_archive_algorithms.apply_async.assert_not_called()
+
+    arch_set = TwoArchives()
+    create_algorithm_jobs_for_archive_algorithms.apply_async.assert_has_calls(
+        [
+            call(
+                args=(
+                    [arch_set.arch1.pk],
+                    [arch_set.arch1.algorithms.first().pk],
+                )
+            ),
+            call(
+                args=(
+                    [arch_set.arch2.pk],
+                    [arch_set.arch2.algorithms.first().pk],
+                )
+            ),
+        ]
+    )
+    create_algorithm_jobs_for_archive_algorithms.apply_async.reset_mock()
+    algorithms = (
+        AlgorithmFactory(),
+        AlgorithmFactory(),
+        AlgorithmFactory(),
+        AlgorithmFactory(),
+    )
+
+    if not reverse:
+        arch_set.arch1.algorithms.add(*algorithms)
+        args = create_algorithm_jobs_for_archive_algorithms.apply_async.call_args.kwargs[
+            "args"
+        ]
+        create_algorithm_jobs_for_archive_algorithms.apply_async.assert_called_once()
+        assert args[0] == [arch_set.arch1.pk]
+        assert set(args[1]) == {a.pk for a in algorithms}
+        create_algorithm_jobs_for_archive_algorithms.apply_async.reset_mock()
+
+        arch_set.arch1.algorithms.remove(algorithms[0], algorithms[1])
+        arch_set.arch1.algorithms.clear()
+        create_algorithm_jobs_for_archive_algorithms.apply_async.assert_not_called()
+    else:
+        for alg in algorithms:
+            alg.archive_set.add(arch_set.arch1, arch_set.arch2)
+            args = create_algorithm_jobs_for_archive_algorithms.apply_async.call_args.kwargs[
+                "args"
+            ]
+            create_algorithm_jobs_for_archive_algorithms.apply_async.assert_called_once()
+            assert set(args[0]) == {arch_set.arch1.pk, arch_set.arch2.pk}
+            assert args[1] == [alg.pk]
+            create_algorithm_jobs_for_archive_algorithms.apply_async.reset_mock()
+        for im in algorithms[-2:]:
+            im.archive_set.remove(arch_set.arch1, arch_set.arch2)
+        for im in algorithms[:2]:
+            im.archive_set.remove(arch_set.arch2)
+        algorithms[0].archive_set.clear()
+        create_algorithm_jobs_for_archive_algorithms.apply_async.assert_not_called()
