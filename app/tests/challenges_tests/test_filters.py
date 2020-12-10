0e0cbf3b64ffb8518e58c1ebef545417285d9a7e
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-12-10 12:27:09 +0100

    Use Pagination for the UsersChallengeList
    Closes #1643

--- app/tests/challenges_tests/test_filters.py
+++ app/tests/challenges_tests/test_filters.py
@@ -6,28 +6,25 @@ from tests.utils import get_view_for_user
 
 @pytest.mark.django_db
 def test_challenge_list_is_filtered(client, two_challenge_sets):
-    c1 = two_challenge_sets.challenge_set_1.challenge.short_name
-    c2 = two_challenge_sets.challenge_set_2.challenge.short_name
+    c1 = two_challenge_sets.challenge_set_1.challenge
+    c2 = two_challenge_sets.challenge_set_2.challenge
     tests = [
-        ([], [c1, c2], two_challenge_sets.challenge_set_1.non_participant),
-        ([c1], [c2], two_challenge_sets.challenge_set_1.participant),
-        ([c1], [c2], two_challenge_sets.challenge_set_1.participant1),
-        ([c1], [c2], two_challenge_sets.challenge_set_1.creator),
-        ([c1], [c2], two_challenge_sets.challenge_set_1.admin),
-        ([], [c1, c2], two_challenge_sets.challenge_set_2.non_participant),
-        ([c2], [c1], two_challenge_sets.challenge_set_2.participant),
-        ([c2], [c1], two_challenge_sets.challenge_set_2.participant1),
-        ([c2], [c1], two_challenge_sets.challenge_set_2.creator),
-        ([c2], [c1], two_challenge_sets.challenge_set_2.admin),
-        ([c1, c2], [], two_challenge_sets.admin12),
-        ([c1, c2], [], two_challenge_sets.participant12),
-        ([c1, c2], [], two_challenge_sets.admin1participant2),
+        (set(), two_challenge_sets.challenge_set_1.non_participant),
+        ({c1}, two_challenge_sets.challenge_set_1.participant),
+        ({c1}, two_challenge_sets.challenge_set_1.participant1),
+        ({c1}, two_challenge_sets.challenge_set_1.creator),
+        ({c1}, two_challenge_sets.challenge_set_1.admin),
+        (set(), two_challenge_sets.challenge_set_2.non_participant),
+        ({c2}, two_challenge_sets.challenge_set_2.participant),
+        ({c2}, two_challenge_sets.challenge_set_2.participant1),
+        ({c2}, two_challenge_sets.challenge_set_2.creator),
+        ({c2}, two_challenge_sets.challenge_set_2.admin),
+        ({c1, c2}, two_challenge_sets.admin12),
+        ({c1, c2}, two_challenge_sets.participant12),
+        ({c1, c2}, two_challenge_sets.admin1participant2),
     ]
-    for test in tests:
+    for expected_challenges, user in tests:
         response = get_view_for_user(
-            url=reverse("challenges:users-list"), client=client, user=test[2]
+            url=reverse("challenges:users-list"), client=client, user=user
         )
-        for short_name in test[0]:
-            assert short_name in response.rendered_content
-        for short_name in test[1]:
-            assert short_name not in response.rendered_content
+        assert expected_challenges == {*response.context[-1]["object_list"]}
