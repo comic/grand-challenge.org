9983e0d21c3222cfa7b803c83822b076a319de62
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2020-12-10 18:01:20 +0100

    Adds test for job permissions

--- app/tests/algorithms_tests/test_permissions.py
+++ app/tests/algorithms_tests/test_permissions.py
@@ -1,19 +1,35 @@
 import pytest
 from django.conf import settings
 from django.contrib.auth.models import Group
+from django.test import TestCase
 from guardian.shortcuts import (
     assign_perm,
     get_group_perms,
     get_perms,
+    get_users_with_perms,
 )
 
+from grandchallenge.algorithms.models import Job
+from grandchallenge.algorithms.tasks import (
+    create_algorithm_jobs_for_archive,
+    create_algorithm_jobs_for_evaluation,
+    create_algorithm_jobs_for_session,
+)
 from tests.algorithms_tests.factories import (
     AlgorithmFactory,
     AlgorithmImageFactory,
     AlgorithmJobFactory,
 )
 from tests.algorithms_tests.utils import TwoAlgorithms
-from tests.factories import UserFactory, WorkstationFactory
+from tests.archives_tests.factories import ArchiveFactory
+from tests.evaluation_tests.factories import EvaluationFactory
+from tests.evaluation_tests.test_permissions import get_groups_with_set_perms
+from tests.factories import (
+    ImageFactory,
+    UploadSessionFactory,
+    UserFactory,
+    WorkstationFactory,
+)
 from tests.utils import get_view_for_user
 
 
@@ -245,3 +261,105 @@ def test_public_job_group_permissions():
 
     assert "view_job" not in get_perms(g_reg, algorithm_job)
     assert "view_job" not in get_perms(g_reg_anon, algorithm_job)
+
+
+class TestJobPermissions(TestCase):
+    """The permissions for jobs will depend on their creation"""
+
+    def test_job_permissions_for_session(self):
+        ai = AlgorithmImageFactory(ready=True)
+
+        u = UserFactory()
+        s = UploadSessionFactory(creator=u)
+        im = ImageFactory()
+        s.image_set.set([im])
+
+        create_algorithm_jobs_for_session(
+            upload_session_pk=s.pk, algorithm_image_pk=ai.pk
+        )
+
+        job = Job.objects.get()
+
+        # Editors and viewers should be able to view the job
+        assert get_groups_with_set_perms(job) == {
+            ai.algorithm.editors_group: {"view_job"},
+            job.viewers: {"view_job"},
+        }
+        # The Session Creator should be able to change the job
+        assert get_users_with_perms(
+            job, attach_perms=True, with_group_users=False
+        ) == {u: ["change_job"]}
+        # The only member of the viewers group should be the creator
+        assert {*job.viewers.user_set.all()} == {u}
+
+    def test_job_permissions_for_archive(self):
+        ai = AlgorithmImageFactory(ready=True)
+        archive = ArchiveFactory()
+
+        # Fake an image upload via a session
+        u = UserFactory()
+        s = UploadSessionFactory(creator=u)
+        im = ImageFactory()
+        s.image_set.set([im])
+
+        archive.images.set([im])
+        archive.algorithms.set([ai.algorithm])
+
+        create_algorithm_jobs_for_archive(archive_pks=[archive.pk])
+
+        job = Job.objects.get()
+
+        # The archive editors, users and uploaders, algorithm editors and job
+        # viewers should be able to view the job
+        assert get_groups_with_set_perms(job) == {
+            archive.editors_group: {"view_job"},
+            archive.users_group: {"view_job"},
+            archive.uploaders_group: {"view_job"},
+            ai.algorithm.editors_group: {"view_job"},
+            job.viewers: {"view_job"},
+        }
+        # No-one should be able to change the job
+        assert (
+            get_users_with_perms(
+                job, attach_perms=True, with_group_users=False
+            )
+            == {}
+        )
+        # No-one should be in the viewers group
+        assert {*job.viewers.user_set.all()} == set()
+
+    def test_job_permissions_for_challenge(self):
+        ai = AlgorithmImageFactory(ready=True)
+        archive = ArchiveFactory()
+        evaluation = EvaluationFactory(
+            submission__phase__archive=archive, submission__algorithm_image=ai
+        )
+
+        # Fake an image upload via a session
+        u = UserFactory()
+        s = UploadSessionFactory(creator=u)
+        im = ImageFactory()
+        s.image_set.set([im])
+
+        archive.images.set([im])
+
+        create_algorithm_jobs_for_evaluation(evaluation_pk=evaluation.pk)
+
+        job = Job.objects.get()
+
+        # Only the challenge admins and job viewers should be able to view the
+        # job. NOTE: NOT THE ALGORITHM EDITORS, they are the participants
+        # to the challenge and should not be able to see the test data
+        assert get_groups_with_set_perms(job) == {
+            evaluation.submission.phase.challenge.admins_group: {"view_job"},
+            job.viewers: {"view_job"},
+        }
+        # No-one should be able to change the job
+        assert (
+            get_users_with_perms(
+                job, attach_perms=True, with_group_users=False
+            )
+            == {}
+        )
+        # No-one should be in the viewers group
+        assert {*job.viewers.user_set.all()} == set()
