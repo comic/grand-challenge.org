e27763504fdd5b47c6837d209f48ac7785070031
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2020-12-17 11:29:59 +0100

    Fix failing test

--- app/tests/cases_tests/test_api.py
+++ app/tests/cases_tests/test_api.py
@@ -135,7 +135,7 @@ def test_empty_data_upload_sessions(client):
     assert response.status_code == 400
     assert response.json() == {
         "non_field_errors": [
-            "1 of algorithm, archive or reader study must be set"
+            "1 of algorithm, archive, answer or reader study must be set"
         ]
     }
 
