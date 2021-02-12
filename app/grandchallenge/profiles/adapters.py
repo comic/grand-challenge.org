2e12d748dfb58ddb06e5d5169d2652e268c5d033
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-02-12 13:51:23 +0100

    Adds next link validation

--- app/grandchallenge/profiles/adapters.py
+++ app/grandchallenge/profiles/adapters.py
@@ -1,10 +1,28 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django import forms
+from django.conf import settings
+from django.utils.http import url_has_allowed_host_and_scheme
 
-from config import settings
+from grandchallenge.challenges.models import Challenge
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    def is_safe_url(self, url):
+        challenge_domains = {
+            f"{c.short_name.lower()}{settings.SESSION_COOKIE_DOMAIN}"
+            for c in Challenge.objects.all()
+        }
+        workstation_domains = {
+            f"{r}{settings.SESSION_COOKIE_DOMAIN}"
+            for r in settings.WORKSTATIONS_ACTIVE_REGIONS
+        }
+
+        return url_has_allowed_host_and_scheme(
+            url=url,
+            allowed_hosts={*challenge_domains, *workstation_domains},
+            require_https=True,
+        )
+
     def clean_email(self, email):
         email = super().clean_email(email=email)
 
