b32602bb5167405e8338dfbbc656f1bfd8987a86
Anne Mickan <amickan1990@gmail.com>
2021-02-15 10:51:51 +0100

    7z submissions to evaluations now get marked as failed  (#1717)
    * 7z submission now get marked as failed and admins get an email when there is no evaluation method yet
    
    * changed evaluation to submission in send email function

--- app/grandchallenge/evaluation/emails.py
+++ app/grandchallenge/evaluation/emails.py
@@ -1,37 +1,45 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
+from django.utils.html import format_html
 
 from grandchallenge.subdomains.utils import reverse
 
 
 def send_failed_evaluation_email(evaluation):
     site = Site.objects.get_current()
-    message = (
-        f"Dear {{}},\n\n"
-        f"Unfortunately the evaluation for the submission to "
-        f"{evaluation.submission.phase.challenge.short_name} failed with an "
-        f"error. The error message is:\n\n"
-        f"{evaluation.error_message}\n\n"
-        f"You may wish to try and correct this, or contact the challenge "
-        f"organizers. The following information may help them:\n"
-        f"User: {evaluation.submission.creator.username}\n"
-        f"Evaluation ID: {evaluation.pk}\n"
-        f"Submission ID: {evaluation.submission.pk}\n\n"
-        f"Regards,\n"
-        f"{site.name}\n\n"
-        f"This is an automated service email from {site.domain}."
-    )
     recipients = list(evaluation.submission.phase.challenge.get_admins())
     recipients.append(evaluation.submission.creator)
     for recipient in recipients:
+        message = format_html(
+            "Dear {recipient},\n\n"
+            "Unfortunately the evaluation for the submission to {challenge} "
+            "failed with an error. The error message is:\n\n"
+            "{error_message}\n\n"
+            "You may wish to try and correct this, or contact the challenge "
+            "organizers. The following information may help them:\n"
+            "User: {user}\n"
+            "Evaluation ID: {evaluation_id}\n"
+            "Submission ID: {submission_id}\n\n"
+            "Regards,\n"
+            "{site}\n\n"
+            "This is an automated service email from {domain}",
+            recipient=recipient.username,
+            challenge=evaluation.submission.phase.challenge.short_name,
+            error_message=evaluation.error_message,
+            user=evaluation.submission.creator.username,
+            evaluation_id=evaluation.pk,
+            submission_id=evaluation.submission.pk,
+            site=site.name,
+            domain=site.domain,
+        )
         send_mail(
             subject=(
                 f"[{site.domain.lower()}] "
                 f"[{evaluation.submission.phase.challenge.short_name.lower()}] "
                 f"Evaluation Failed"
             ),
-            message=message.format(recipient.username),
+            message=message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[recipient.email],
         )
@@ -42,43 +50,83 @@ def send_successful_evaluation_email(evaluation):
     challenge = evaluation.submission.phase.challenge
 
     recipients = list(challenge.get_admins())
-    message = (
-        f"Dear {{}},\n\n"
-        f"There is a new result for {challenge.short_name} from "
-        f"{evaluation.submission.creator.username}. "
-    )
+
     if evaluation.published:
-        leaderboard_url = reverse(
-            "evaluation:leaderboard",
-            kwargs={
-                "challenge_short_name": challenge.short_name,
-                "slug": evaluation.submission.phase.slug,
-            },
-        )
-        message += (
-            f"You can view the result on the leaderboard here: "
-            f"{leaderboard_url}"
-        )
         recipients.append(evaluation.submission.creator)
-    else:
-        message += (
-            f"You can publish the result on the leaderboard here: "
-            f"{evaluation.get_absolute_url()}"
-        )
-    message += (
-        f"\n\n"
-        f"Regards,\n"
-        f"{site.name}\n\n"
-        f"This is an automated service email from {site.domain}."
-    )
+
     for recipient in recipients:
+        message = format_html(
+            "Dear {recipient},\n\n"
+            "There is a new result for {challenge} from {user}.",
+            recipient=recipient.username,
+            challenge=challenge.short_name,
+            user=evaluation.submission.creator.username,
+        )
+        if evaluation.published:
+            leaderboard_url = reverse(
+                "evaluation:leaderboard",
+                kwargs={
+                    "challenge_short_name": challenge.short_name,
+                    "slug": evaluation.submission.phase.slug,
+                },
+            )
+            message += format_html(
+                "You can view the result on the leaderboard here: {leaderboard}\n\n",
+                leaderboard=leaderboard_url,
+            )
+        else:
+            message += format_html(
+                "You can publish the result on the leaderboard here: {leaderboard}\n\n",
+                leaderboard=evaluation.get_absolute_url(),
+            )
+        message += format_html(
+            "Regards,\n"
+            "{site}\n\n"
+            "This is an automated service email from {domain}",
+            site=site.name,
+            domain=site.domain,
+        )
         send_mail(
             subject=(
                 f"[{site.domain.lower()}] "
                 f"[{challenge.short_name.lower()}] "
                 f"New Result"
             ),
-            message=message.format(recipient.username),
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[recipient.email],
+        )
+
+
+def send_missing_method_email(submission):
+    site = Site.objects.get_current()
+    challenge = submission.phase.challenge
+
+    recipients = challenge.get_admins()
+
+    for recipient in recipients:
+        message = format_html(
+            "Dear {recipient},\n\n"
+            "The submission from {user} could not be evaluated, "
+            "because there is no valid evaluation method for {challenge} in the {phase} evaluation phase. "
+            "Please upload an evaluation method container.\n\n"
+            "Regards,\n"
+            "{site}\n\n"
+            "This is an automated service email from {domain}.",
+            recipient=recipient.username,
+            user=submission.creator.username,
+            challenge=challenge.short_name,
+            phase=submission.phase.slug,
+            site=site.name,
+            domain=site.domain,
+        )
+        send_mail(
+            subject=(
+                f"[{site.domain.lower()}] "
+                f"[{challenge.short_name.lower()}] "
+                f"No evaluation method"
+            ),
+            message=message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[recipient.email],
         )
