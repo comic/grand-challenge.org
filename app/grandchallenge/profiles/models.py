76c0ff10c069f3de869ec5b51edaec29d0512502
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-02-17 15:00:35 +0100

    Fix warnings

--- app/grandchallenge/profiles/models.py
+++ app/grandchallenge/profiles/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 from easy_thumbnails.fields import ThumbnailerImageField
 from guardian.shortcuts import assign_perm
