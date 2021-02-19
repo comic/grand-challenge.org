14bc3dd4002756e9cf4a32bb0f238859a9175252
James <12661555+jmsmkn@users.noreply.github.com>
2021-02-19 16:30:08 +0100

    Flatten only 1 level of data (#1728)

--- app/grandchallenge/core/renderers.py
+++ app/grandchallenge/core/renderers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework_csv.renderers import CSVRenderer
 
 
@@ -9,3 +11,19 @@ class PaginatedCSVRenderer(CSVRenderer):
             data = data[self.results_field]
 
         return super().render(data, *args, **kwargs)
+
+    def flatten_data(self, data):
+        """
+        Create a dictionary that is 1 level deep, with nested values serialized
+        as json. This means that the header rows are now consistent.
+        """
+        for row in data:
+            flat_row = {k: self._flatten_value(v) for k, v in row.items()}
+            yield flat_row
+
+    @staticmethod
+    def _flatten_value(value):
+        if isinstance(value, (dict, list)):
+            return json.dumps(value)
+        else:
+            return value
