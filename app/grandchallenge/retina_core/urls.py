1ee6954078a55624eebc759bbdd243b8705684a6
Harm van Zeeland <harm.van.zeeland@gmail.com>
2021-02-26 13:55:21 +0100

    Remove templates, submodule and core views

--- app/grandchallenge/retina_core/urls.py
+++ app/grandchallenge/retina_core/urls.py
@@ -1,14 +1,11 @@
 from django.urls import include, path
 
 from grandchallenge.retina_core.views import (
-    IndexView,
-    NumpyView,
     ThumbnailView,
 )
 
 app_name = "retina"
 urlpatterns = [
-    path("", IndexView.as_view(), name="home"),
     path("api/", include("grandchallenge.retina_api.urls", namespace="api")),
     path(
         "retina_importers/",
@@ -18,10 +15,5 @@ urlpatterns = [
         "image/thumbnail/<uuid:image_id>/",
         ThumbnailView.as_view(),
         name="image-thumbnail",
-    ),
-    path(
-        "image/numpy/<uuid:image_id>/",
-        NumpyView.as_view(),
-        name="image-numpy",
     ),
 ]
