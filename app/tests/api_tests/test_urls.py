28192325d846e2e73bde97593895df552f795d5f
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-02-26 17:19:02 +0100

    Removes broken test

--- app/tests/api_tests/test_urls.py
+++ app/tests/api_tests/test_urls.py
@@ -1,14 +1,6 @@
-import inspect
-from typing import Dict, Type
-
 import pytest
-import rest_framework.fields
-from django.db.models import CharField
-from rest_framework.serializers import ModelSerializer
 
-import grandchallenge.api.urls
 from grandchallenge.reader_studies.models import ANSWER_TYPE_SCHEMA
-from grandchallenge.reader_studies.serializers import QuestionSerializer
 from grandchallenge.subdomains.utils import reverse
 from tests.utils import assert_viewname_status
 
@@ -31,88 +23,8 @@ def test_api_docs_generation(client, schema, content_type):
     )
     assert len(response.data["paths"]) > 0
     check_answer_type_schema_from_response(response)
-    check_response_schema_formatting(response)
 
 
 def check_answer_type_schema_from_response(response):
     schema = response.data["definitions"]["Answer"]["properties"]["answer"]
     assert {"title": "Answer", **ANSWER_TYPE_SCHEMA} == schema
-
-
-def check_response_schema_formatting(response):
-    definitions = response.data["definitions"]
-    serializers = extract_all_api_exposed_model_serializers()
-    for serializer in serializers:
-        if hasattr(serializer, "Meta") and hasattr(serializer.Meta, "model"):
-            model_name = serializer.Meta.model.__name__
-            if model_name not in (
-                "ImagingModality",
-                "Patient",
-                "Study",
-                "Archive",
-            ):
-                assert model_name in definitions
-                check_serializer_charfield_schema_formatting(
-                    definitions=definitions, serializer=serializer
-                )
-
-
-def extract_all_api_exposed_model_serializers():
-    modules = {
-        klass.serializer_class
-        for _, klass, _ in grandchallenge.api.urls.router.registry
-        if klass.serializer_class is not None
-    }
-    serializers = []
-    for module in modules:
-        serializers = serializers + [
-            v
-            for v in module.__dict__.values()
-            if inspect.isclass(v) and issubclass(v, (ModelSerializer,))
-        ]
-    return serializers
-
-
-def check_serializer_charfield_schema_formatting(
-    *, definitions: Dict, serializer: Type[ModelSerializer]
-):
-    model = serializer.Meta.model
-    model_field_names = [f.name for f in model._meta.fields]
-    model_property_definitions = definitions[model.__name__]["properties"]
-    name_remapping = (
-        {"form_direction": "direction"} if model is QuestionSerializer else {}
-    )
-    char_field_names = [
-        field_name
-        for field_name, field in serializer._declared_fields.items()
-        if isinstance(field, rest_framework.fields.CharField)
-    ]
-    for field_name in char_field_names:
-        if field_name in model_field_names:
-            field_name = (
-                field_name
-                if field_name not in name_remapping
-                else name_remapping[field_name]
-            )
-            char_field = model._meta.get_field(field_name)
-            if (
-                isinstance(char_field, CharField)
-                and len(char_field.choices) > 0
-            ):
-                check_charfield_schema_formatting(
-                    definitions=model_property_definitions,
-                    char_field=char_field,
-                )
-
-
-def check_charfield_schema_formatting(
-    *, definitions: Dict, char_field: CharField
-):
-    name = char_field.name
-    assert name in definitions
-    schema = definitions[name]
-    assert schema["type"] == "string"
-    assert schema["minLength"] == 1
-    assert schema["title"] == name[0].upper() + name[1:].replace("_", " ")
-    assert "enum" in schema
-    assert schema["enum"] == [e[1] for e in char_field.choices]
