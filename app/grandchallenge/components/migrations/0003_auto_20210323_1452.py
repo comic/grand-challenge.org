a23abba91cdc32b8d0abcfafee5c642aadb724da
miriam <miriam.groeneveld.diag@gmail.com>
2021-03-23 16:50:31 +0100

    Black

--- app/grandchallenge/components/migrations/0003_auto_20210323_1452.py
+++ app/grandchallenge/components/migrations/0003_auto_20210323_1452.py
@@ -5,14 +5,15 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('components', '0002_auto_20210203_0747'),
-    ]
+    dependencies = [("components", "0002_auto_20210203_0747")]
 
     operations = [
         migrations.AlterField(
-            model_name='componentinterface',
-            name='store_in_database',
-            field=models.BooleanField(default=True, help_text='Should the value be saved in a database field, only valid for outputs.'),
-        ),
+            model_name="componentinterface",
+            name="store_in_database",
+            field=models.BooleanField(
+                default=True,
+                help_text="Should the value be saved in a database field, only valid for outputs.",
+            ),
+        )
     ]
