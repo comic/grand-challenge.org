65cdc140042719fd0b7c8dbb51ba57c4e3b35be8
amickan <amickan1990@gmail.com>
2021-04-19 10:18:42 +0200

    remove duplicate code on pages detail html

--- app/grandchallenge/challenges/static/js/challenges/dropdown.js
+++ app/grandchallenge/challenges/static/js/challenges/dropdown.js
@@ -1,23 +1,40 @@
 
 function handle_nav_tab_dropdown() {
-    if (window.innerWidth < 992) {
+    if (window.innerWidth < 576) {
+        // 576 px is the bootstrap breakpoint for sm
+        navs_to_dropdown(false, true);
+    } else if (window.innerWidth < 992) {
         // 992 px is the bootstrap breakpoint for lg
         navs_to_dropdown();
     } else {
         navs_to_dropdown(true);
-    }
+        }
 }
 
-function navs_to_dropdown(reverse = false) {
+function navs_to_dropdown(reverse = false, horiz_page_pills = false) {
     // Get all elements that need to change class
-    // const tabContainers = document.getElementsByClassName("nav-tab-dropdown-container");
     const containers = document.querySelectorAll('.nav-tab-dropdown-container,.nav-pill-dropdown-container');
+    const page_containers = document.getElementsByClassName("nav-pill-pages-container");
 
     const ulDropdownClasses = ["dropdown-menu", "dropdown-menu-left"];
     const ulTabClasses = ["nav", "nav-tabs", "border-0"];
     const itemDropdownClasses = ["dropdown-item", "challengeDropdown"];
     const itemTabPillClasses = ["nav-item"];
     const ulPillClasses = ["nav", "nav-pills", "col-12", "mb-3"];
+    const ulHorizPagePillClasses = ["d-flex", "align-items-stretch", "mb-3"];
+    const ulVertPagePillClasses = ["flex-column"];
+
+    for (let page_container of page_containers) {
+        let ul_pages  = page_container.querySelector("ul");
+
+        if (horiz_page_pills === true) {
+            ul_pages.classList.remove(...ulVertPagePillClasses);
+            ul_pages.classList.add(...ulHorizPagePillClasses);
+        } else {
+            ul_pages.classList.add(...ulVertPagePillClasses);
+            ul_pages.classList.remove(...ulHorizPagePillClasses);
+        }
+    }
 
     // Change the classes
     for (let container of containers) {
