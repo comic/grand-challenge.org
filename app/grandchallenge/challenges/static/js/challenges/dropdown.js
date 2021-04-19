36e4c79a18a831b08683407def46de28d23241a0
amickan <amickan1990@gmail.com>
2021-04-19 08:13:14 +0200

    clean duplicate code from leaderboard html

--- app/grandchallenge/challenges/static/js/challenges/dropdown.js
+++ app/grandchallenge/challenges/static/js/challenges/dropdown.js
@@ -10,12 +10,14 @@ function handle_nav_tab_dropdown() {
 
 function navs_to_dropdown(reverse = false) {
     // Get all elements that need to change class
-    const containers = document.getElementsByClassName("nav-tab-dropdown-container");
+    // const tabContainers = document.getElementsByClassName("nav-tab-dropdown-container");
+    const containers = document.querySelectorAll('.nav-tab-dropdown-container,.nav-pill-dropdown-container');
 
     const ulDropdownClasses = ["dropdown-menu", "dropdown-menu-left"];
     const ulTabClasses = ["nav", "nav-tabs", "border-0"];
     const itemDropdownClasses = ["dropdown-item", "challengeDropdown"];
-    const itemTabClasses = ["nav-item"];
+    const itemTabPillClasses = ["nav-item"];
+    const ulPillClasses = ["nav", "nav-pills", "col-12", "pl-3", "mb-3"];
 
     // Change the classes
     for (let container of containers) {
@@ -23,29 +25,38 @@ function navs_to_dropdown(reverse = false) {
 
         if (reverse === true) {
             container.classList.remove("dropdown");
-
-            ul.classList.add(...ulTabClasses);
             ul.classList.remove(...ulDropdownClasses);
+
+            if (container.classList.contains("nav-tab-dropdown-container")) {
+                ul.classList.add(...ulTabClasses);
+            } else if (container.classList.contains("nav-pill-dropdown-container")) {
+                ul.classList.add(...ulPillClasses);
+            }
         }
         else {
             container.classList.add("dropdown");
-
             ul.classList.add(...ulDropdownClasses);
-            ul.classList.remove(...ulTabClasses);
+
+            if (container.classList.contains("nav-tab-dropdown-container")) {
+                ul.classList.remove(...ulTabClasses);
+            } else if (container.classList.contains("nav-pill-dropdown-container")) {
+                ul.classList.remove(...ulPillClasses);
+            }
         }
 
         let items = container.querySelectorAll("li");
 
         for (let item of items) {
             if (reverse === true) {
-                item.classList.add(...itemTabClasses);
+                item.classList.add(...itemTabPillClasses);
                 item.classList.remove(...itemDropdownClasses);
             } else {
                 item.classList.add(...itemDropdownClasses);
-                item.classList.remove(...itemTabClasses);
+                item.classList.remove(...itemTabPillClasses);
             }
         }
     }
+
 }
 
 window.addEventListener('resize', handle_nav_tab_dropdown);
