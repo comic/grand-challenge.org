daa05408ad4fcc45b1862a67ff932ef28caafc10
Anne Mickan <amickan1990@gmail.com>
2021-04-21 13:38:06 +0200

    Removed duplicate code in challenge layout (#1808)
    * fixed nav-pill width for pages
    
    * clean duplicate code from leaderboard html
    
    * clean duplicate code from submission form and list html
    
    * fix column width for pages nav-pills
    
    * remove duplicate code on pages detail html

--- app/grandchallenge/challenges/static/js/challenges/dropdown.js
+++ app/grandchallenge/challenges/static/js/challenges/dropdown.js
@@ -1,21 +1,40 @@
 
 function handle_nav_tab_dropdown() {
-    if (window.innerWidth < 992) {
+    if (window.innerWidth < 576) {
+        // 576 px is the bootstrap breakpoint for sm
+        navs_to_dropdown(false, true);
+    } else if (window.innerWidth < 992) {
         // 992 px is the bootstrap breakpoint for lg
         navs_to_dropdown();
     } else {
         navs_to_dropdown(true);
-    }
+        }
 }
 
-function navs_to_dropdown(reverse = false) {
+function navs_to_dropdown(reverse = false, horiz_page_pills = false) {
     // Get all elements that need to change class
-    const containers = document.getElementsByClassName("nav-tab-dropdown-container");
+    const containers = document.querySelectorAll('.nav-tab-dropdown-container,.nav-pill-dropdown-container');
+    const page_containers = document.getElementsByClassName("nav-pill-pages-container");
 
     const ulDropdownClasses = ["dropdown-menu", "dropdown-menu-left"];
     const ulTabClasses = ["nav", "nav-tabs", "border-0"];
     const itemDropdownClasses = ["dropdown-item", "challengeDropdown"];
-    const itemTabClasses = ["nav-item"];
+    const itemTabPillClasses = ["nav-item"];
+    const ulPillClasses = ["nav", "nav-pills", "col-12", "mb-3"];
+    const ulHorizPagePillClasses = ["d-flex", "align-items-stretch", "mb-3"];
+    const ulVertPagePillClasses = ["flex-column"];
+
+    for (let page_container of page_containers) {
+        let ul_pages  = page_container.querySelector("ul");
+
+        if (horiz_page_pills === true) {
+            ul_pages.classList.remove(...ulVertPagePillClasses);
+            ul_pages.classList.add(...ulHorizPagePillClasses);
+        } else {
+            ul_pages.classList.add(...ulVertPagePillClasses);
+            ul_pages.classList.remove(...ulHorizPagePillClasses);
+        }
+    }
 
     // Change the classes
     for (let container of containers) {
@@ -23,29 +42,38 @@ function navs_to_dropdown(reverse = false) {
 
         if (reverse === true) {
             container.classList.remove("dropdown");
-
-            ul.classList.add(...ulTabClasses);
             ul.classList.remove(...ulDropdownClasses);
+
+            if (container.classList.contains("nav-tab-dropdown-container")) {
+                ul.classList.add(...ulTabClasses);
+            } else if (container.classList.contains("nav-pill-dropdown-container")) {
+                ul.classList.add(...ulPillClasses);
+            }
         }
         else {
             container.classList.add("dropdown");
-
             ul.classList.add(...ulDropdownClasses);
-            ul.classList.remove(...ulTabClasses);
+
+            if (container.classList.contains("nav-tab-dropdown-container")) {
+                ul.classList.remove(...ulTabClasses);
+            } else if (container.classList.contains("nav-pill-dropdown-container")) {
+                ul.classList.remove(...ulPillClasses);
+            }
         }
 
         let items = container.querySelectorAll("li");
 
         for (let item of items) {
             if (reverse === true) {
-                item.classList.add(...itemTabClasses);
+                item.classList.add(...itemTabPillClasses);
                 item.classList.remove(...itemDropdownClasses);
             } else {
                 item.classList.add(...itemDropdownClasses);
-                item.classList.remove(...itemTabClasses);
+                item.classList.remove(...itemTabPillClasses);
             }
         }
     }
+
 }
 
 window.addEventListener('resize', handle_nav_tab_dropdown);
