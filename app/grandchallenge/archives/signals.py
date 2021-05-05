73fefb1b980e98e0fdd90fef0d1e4dc62a86a02f
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2021-05-05 13:57:53 +0200

    Remove image changed signal

--- app/grandchallenge/archives/signals.py
+++ app/grandchallenge/archives/signals.py
@@ -1,74 +1,9 @@
 from django.db.models.signals import m2m_changed
 from django.db.transaction import on_commit
 from django.dispatch import receiver
-from guardian.shortcuts import assign_perm, remove_perm
 
 from grandchallenge.algorithms.tasks import create_algorithm_jobs_for_archive
-from grandchallenge.archives.models import Archive, ArchiveItem
-from grandchallenge.cases.models import Image
-from grandchallenge.components.models import (
-    ComponentInterface,
-    ComponentInterfaceValue,
-)
-
-
-@receiver(m2m_changed, sender=Archive.images.through)
-def on_archive_images_changed(instance, action, reverse, model, pk_set, **_):
-    if action not in ["post_add", "post_remove", "pre_clear"]:
-        # nothing to do for the other actions
-        return
-
-    if reverse:
-        images = Image.objects.filter(pk=instance.pk)
-        if pk_set is None:
-            # When using a _clear action, pk_set is None
-            # https://docs.djangoproject.com/en/2.2/ref/signals/#m2m-changed
-            archives = instance.archive_set.all()
-        else:
-            archives = model.objects.filter(pk__in=pk_set)
-
-        archive_pks = archives.values_list("pk", flat=True)
-        archives = archives.select_related("users_group", "editors_group")
-    else:
-        archives = [instance]
-        archive_pks = [instance.pk]
-        if pk_set is None:
-            # When using a _clear action, pk_set is None
-            # https://docs.djangoproject.com/en/2.2/ref/signals/#m2m-changed
-            images = instance.images.all()
-        else:
-            images = model.objects.filter(pk__in=pk_set)
-
-    # TODO: This is a temporary workaround. The images field on Archives should
-    # not be used anymore. Instead, ArchiveItems should be directly created.
-    # The civs sent to the task should then be grouped by AchiveItem.
-    interface = ComponentInterface.objects.get(slug="generic-medical-image")
-    civs = []
-    for image in images:
-        for archive in image.archive_set.all():
-            civ, _ = ComponentInterfaceValue.objects.get_or_create(
-                interface=interface, image=image
-            )
-            civs.append(civ.pk)
-            if not ArchiveItem.objects.filter(
-                archive=archive, values__in=[civ.pk]
-            ).exists():
-                item = ArchiveItem.objects.create(archive=archive)
-                item.values.set([civ])
-
-    op = assign_perm if "add" in action else remove_perm
-
-    for archive in archives:
-        op("view_image", archive.editors_group, images)
-        op("view_image", archive.uploaders_group, images)
-        op("view_image", archive.users_group, images)
-
-    if "add" in action:
-        on_commit(
-            lambda: create_algorithm_jobs_for_archive.apply_async(
-                kwargs={"archive_pks": list(archive_pks), "civ_pks": civs},
-            )
-        )
+from grandchallenge.archives.models import Archive
 
 
 @receiver(m2m_changed, sender=Archive.algorithms.through)
--- app/grandchallenge/archives/tasks.py
+++ app/grandchallenge/archives/tasks.py
@@ -1,5 +1,8 @@
 from celery import shared_task
+from django.db.transaction import on_commit
+from guardian.shortcuts import assign_perm
 
+from grandchallenge.algorithms.tasks import create_algorithm_jobs_for_archive
 from grandchallenge.archives.models import Archive, ArchiveItem
 from grandchallenge.cases.models import Image
 from grandchallenge.components.models import (
@@ -15,9 +18,21 @@ def add_images_to_archive(*, upload_session_pk, archive_pk):
     # TODO: this should be configurable
     interface = ComponentInterface.objects.get(slug="generic-medical-image")
 
+    civs = []
     for image in images:
         civ = ComponentInterfaceValue.objects.create(
             interface=interface, image=image
         )
+        civs.append(civ)
         item = ArchiveItem.objects.create(archive=archive)
         item.values.set([civ])
+
+    assign_perm("view_image", archive.editors_group, images)
+    assign_perm("view_image", archive.uploaders_group, images)
+    assign_perm("view_image", archive.users_group, images)
+
+    on_commit(
+        lambda: create_algorithm_jobs_for_archive.apply_async(
+            kwargs={"archive_pks": list([archive.pk]), "civ_pks": civs},
+        )
+    )
