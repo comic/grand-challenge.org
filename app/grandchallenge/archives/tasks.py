01ada6124362f9de8c295d1fe68e57bdce5a165b
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2021-05-05 13:26:11 +0200

    Set archive items on image upload

--- app/grandchallenge/archives/tasks.py
+++ app/grandchallenge/archives/tasks.py
@@ -1,12 +1,23 @@
 from celery import shared_task
 
-from grandchallenge.archives.models import Archive
+from grandchallenge.archives.models import Archive, ArchiveItem
 from grandchallenge.cases.models import Image
+from grandchallenge.components.models import (
+    ComponentInterface,
+    ComponentInterfaceValue,
+)
 
 
 @shared_task
 def add_images_to_archive(*, upload_session_pk, archive_pk):
     images = Image.objects.filter(origin_id=upload_session_pk)
     archive = Archive.objects.get(pk=archive_pk)
+    # TODO: this should be configurable
+    interface = ComponentInterface.objects.get(slug="generic-medical-image")
 
-    archive.images.add(*images.all())
+    for image in images:
+        civ = ComponentInterfaceValue.objects.create(
+            interface=interface, image=image
+        )
+        item = ArchiveItem.objects.create(archive=archive)
+        item.values.set([civ])
