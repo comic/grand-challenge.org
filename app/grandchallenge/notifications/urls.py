53729f9fdc45c7eca00b5f57000db2057fa12358
amickan <amickan1990@gmail.com>
2021-05-18 18:05:42 +0200

    notifcation update view added, no tests yet for updating and deleting

--- app/grandchallenge/notifications/urls.py
+++ app/grandchallenge/notifications/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from grandchallenge.notifications.views import NotificationList
+from grandchallenge.notifications.views import (
+    NotificationList,
+    NotificationUpdate,
+)
 
 app_name = "notifications"
 
 urlpatterns = [
     path("", NotificationList.as_view(), name="list"),
+    path("<int:pk>/update", NotificationUpdate.as_view(), name="update"),
 ]
--- app/grandchallenge/notifications/views.py
+++ app/grandchallenge/notifications/views.py
@@ -1,7 +1,13 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin as ObjectPermissionRequiredMixin,
+)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import FormView, ListView
 
+from grandchallenge.notifications.forms import NotificationForm
 from grandchallenge.notifications.models import Notification
+from grandchallenge.subdomains.utils import reverse, reverse_lazy
 
 
 class NotificationList(LoginRequiredMixin, ListView):
@@ -19,8 +25,37 @@ class NotificationList(LoginRequiredMixin, ListView):
         context = super().get_context_data(*args, **kwargs)
         profile = self.request.user.user_profile
         context.update(
-            {"notifications_last_read_at": profile.notifications_last_read_at}
+            {
+                "notifications_last_read_at": profile.notifications_last_read_at,
+                "mark_as_read_action": NotificationForm.MARK_READ,
+                "mark_as_unread_action": NotificationForm.MARK_UNREAD,
+                "delete_notification": NotificationForm.REMOVE,
+            }
         )
         # TODO side-effect, not nice
         profile.update_notifications_last_read_timestep()
         return context
+
+
+class NotificationUpdate(
+    LoginRequiredMixin,
+    ObjectPermissionRequiredMixin,
+    SuccessMessageMixin,
+    FormView,
+):
+    form_class = NotificationForm
+    template_name = "notifications/notification_update_form.html"
+    success_message = "Notification successfully updated"
+    permission_required = "change_notification"
+    raise_exception = True
+    login_url = reverse_lazy("account_login")
+
+    def get_permission_object(self):
+        return self.request.notification
+
+    def get_success_url(self):
+        return reverse("notifications:list",)
+
+    def form_valid(self, form):
+        form.update()
+        return super().form_valid(form)
