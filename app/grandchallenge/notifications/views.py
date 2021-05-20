81ba083e5250eb56c7f090b14fa49f95c72ff8ef
amickan <amickan1990@gmail.com>
2021-05-20 09:50:55 +0200

    queryset optimization

--- app/grandchallenge/notifications/views.py
+++ app/grandchallenge/notifications/views.py
@@ -18,6 +18,11 @@ class NotificationList(LoginRequiredMixin, ListView):
             super()
             .get_queryset()
             .filter(user=self.request.user)
+            .prefetch_related(
+                "action__actor__user_profile",
+                "action__actor__verification",
+                "action__target__forum",
+            )
             .order_by("-action__timestamp")
         )
 
