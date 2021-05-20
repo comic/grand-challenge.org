0b766df018287f2eeecb7c6d784470a5772f2624
amickan <amickan1990@gmail.com>
2021-05-20 10:32:05 +0200

    added preliminary unsucscribe option

--- app/grandchallenge/notifications/forms.py
+++ app/grandchallenge/notifications/forms.py
@@ -1,4 +1,7 @@
+from actstream.actions import unfollow
 from django import forms
+from django.contrib.auth import get_user_model
+from django.db.models.query_utils import Q
 
 from grandchallenge.notifications.models import Notification
 
@@ -7,10 +10,15 @@ class NotificationForm(forms.Form):
     MARK_READ = "MARK_READ"
     MARK_UNREAD = "MARK_UNREAD"
     REMOVE = "REMOVE"
+    UNFOLLOW = "UNFOLLOW"
     CHOICES = (
         (MARK_READ, "Mark as read"),
         (MARK_UNREAD, "Mark as unread"),
         (REMOVE, "Remove notification"),
+        (UNFOLLOW, "Unfollow notification target"),
+    )
+    user = forms.ModelChoiceField(
+        queryset=get_user_model().objects.all(), widget=forms.HiddenInput()
     )
     notification = forms.ModelChoiceField(
         queryset=Notification.objects.all(), widget=forms.HiddenInput()
@@ -33,4 +41,19 @@ class NotificationForm(forms.Form):
         elif self.cleaned_data["action"] == NotificationForm.REMOVE:
             Notification.objects.filter(
                 id=self.cleaned_data["notification"].id
+            ).delete()
+        elif self.cleaned_data["action"] == NotificationForm.UNFOLLOW:
+            # user will no longer receive notifications for this target in future
+            unfollow(
+                self.cleaned_data["user"],
+                self.cleaned_data["notification"].action.target,
+            )
+            # delete all notifications relating to this target for this user
+            Notification.objects.filter(
+                Q(
+                    action__target_object_id=self.cleaned_data[
+                        "notification"
+                    ].action.target.id
+                )
+                & Q(user_id=self.cleaned_data["user"])
             ).delete()
