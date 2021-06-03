b37f7f9ea5a85117dd096fe45a32a69d8ddb1532
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-06-03 09:00:51 +0200

    Split task into add and remove operations

--- app/grandchallenge/archives/tasks.py
+++ app/grandchallenge/archives/tasks.py
@@ -36,7 +36,9 @@ def add_images_to_archive(*, upload_session_pk, archive_pk, interface_pk=None):
 
 
 @shared_task
-def add_values_to_archive_item(*, archive_item_pk, civ_pks):
+def update_archive_item_values(
+    *, archive_item_pk, civ_pks_to_remove, civ_pks_to_add,
+):
     archive_item = ArchiveItem.objects.get(pk=archive_item_pk)
-    archive_item.values.clear()
-    archive_item.values.add(*civ_pks)
+    archive_item.values.remove(*civ_pks_to_remove)
+    archive_item.values.add(*civ_pks_to_add)
