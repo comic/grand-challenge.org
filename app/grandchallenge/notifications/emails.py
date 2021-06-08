bf612af50683ca687672146598c11c261f706bf3
amickan <amickan1990@gmail.com>
2021-06-08 14:36:24 +0200

    added notification-email-opt-out, tests for notification emails

--- app/grandchallenge/notifications/emails.py
+++ app/grandchallenge/notifications/emails.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template.defaultfilters import pluralize
 
 
 def send_unread_notifications_email(recipients):
@@ -8,8 +9,9 @@ def send_unread_notifications_email(recipients):
     for user, n_notifications in recipients.items():
         msg = (
             f"Dear {user.user.username},\n\n"
-            f"You have {n_notifications} unread notifications."
-            "To read them and mark them as read, visit: https://grand-challenge.org/notifications/"
+            f"You have {n_notifications} new notification{pluralize(n_notifications)}."
+            "To read and manage your notifications, visit: https://grand-challenge.org/notifications/.\n\n"
+            "If you no longer wish to receive emails about notifications, you can disable them in your profile settings."
         )
         send_mail(
             subject=subject,
--- app/grandchallenge/notifications/tasks.py
+++ app/grandchallenge/notifications/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
+from django.db.models import Q
 from django.utils.timezone import now
 
 from grandchallenge.notifications.emails import send_unread_notifications_email
+from grandchallenge.notifications.models import Notification
 from grandchallenge.profiles.models import UserProfile
 
 
@@ -10,9 +12,22 @@ def send_unread_notification_emails():
     users = UserProfile.objects.filter(receive_notification_emails=True)
     recipients = {}
     for user in users:
-        if user.has_unread_notifications:
+        if (
+            user.notification_email_last_sent_at is None
+            and user.has_unread_notifications
+        ):
             recipients[user] = len(user.unread_notifications)
             user.notification_email_last_sent_at = now()
             user.save()
+        elif user.notification_email_last_sent_at is not None:
+            unread_notifications = Notification.objects.filter(
+                Q(user=user.user)
+                & Q(read=False)
+                & Q(action__timestamp__gt=user.notification_email_last_sent_at)
+            )
+            if unread_notifications:
+                recipients[user] = len(unread_notifications)
+                user.notification_email_last_sent_at = now()
+                user.save()
 
     send_unread_notifications_email(recipients)
