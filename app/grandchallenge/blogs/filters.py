1d4c0fcc4b7089e2794b218c6e79a4fbf032a993
amickan <amickan1990@gmail.com>
2021-06-11 16:38:52 +0200

    added content text search

--- app/grandchallenge/blogs/filters.py
+++ app/grandchallenge/blogs/filters.py
@@ -1,5 +1,9 @@
+from functools import reduce
+from operator import or_
+
 from django.contrib.auth.models import User
-from django_filters import FilterSet, ModelMultipleChoiceFilter
+from django.db.models import Q
+from django_filters import CharFilter, FilterSet, ModelMultipleChoiceFilter
 from django_select2.forms import Select2MultipleWidget
 
 from grandchallenge.blogs.models import Post, Tag
@@ -7,6 +11,7 @@ from grandchallenge.core.filters import FilterForm
 
 
 class BlogFilter(FilterSet):
+    search = CharFilter(method="search_filter", label="Title or Content")
     tags = ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(), widget=Select2MultipleWidget, label="Tags",
     )
@@ -21,4 +26,17 @@ class BlogFilter(FilterSet):
     class Meta:
         model = Post
         form = FilterForm
-        fields = ("tags", "authors")
+        fields = ("tags", "authors", "search")
+        search_fields = ("title", "content")
+
+    def search_filter(self, queryset, name, value):
+        return queryset.filter(
+            reduce(
+                or_,
+                [
+                    Q(**{f"{f}__icontains": value})
+                    for f in self.Meta.search_fields
+                ],
+                Q(),
+            )
+        )
