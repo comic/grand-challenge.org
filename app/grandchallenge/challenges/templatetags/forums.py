d83e48875978cfb652d3cb78f2e8b4ca9a027bac
amickan <amickan1990@gmail.com>
2021-06-16 10:05:16 +0200

    simplify follow list view

--- app/grandchallenge/challenges/templatetags/forums.py
+++ app/grandchallenge/challenges/templatetags/forums.py
@@ -1,38 +1,7 @@
-from typing import Union
-
 from django import template
 from django.contrib.contenttypes.models import ContentType
-from django.utils.html import format_html
-from machina.apps.forum.models import Forum
 
 register = template.Library()
-
-
-@register.filter
-def forum_link(forum: Union[Forum, None]) -> str:
-
-    if forum.challenge and forum.challenge.logo:
-        forum_name = forum.name
-        forum_url = forum.get_absolute_url()
-        challenge_logo = format_html(
-            (
-                '<img class="rounded-circle align-middle" loading="lazy" '
-                'src="{0}" alt="Challenge logo" '
-                'style="height: 1.33em;"/>'
-            ),
-            forum.challenge.logo.url,
-        )
-    else:
-        forum_name = forum.name
-        forum_url = forum.get_absolute_url()
-        challenge_logo = ""
-
-    return format_html(
-        '<span class="text-nowrap"><a href="{0}">{1}</a>&nbsp;&nbsp;<a href="{0}">{2}</a></span>',
-        forum_url,
-        challenge_logo,
-        forum_name,
-    )
 
 
 @register.simple_tag
