310f4447a885bd715da6852e3c25058ea9b37a86
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-07-01 09:35:10 +0200

    Migrate from docker hub to ECR public

--- cycle_docker_compose.sh
+++ cycle_docker_compose.sh
@@ -7,6 +7,10 @@ sleep 1
 export GIT_COMMIT_ID=$(git describe --always --dirty)
 export GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | sed "s/[^[:alnum:]]//g")
 export DOCKER_GID=$(getent group docker | cut -d: -f3)
+export GRAND_CHALLENGE_HTTP_REPOSITORY_URI=public.ecr.aws/m3y0m7n5/grand-challenge/http
+export GRAND_CHALLENGE_WEB_REPOSITORY_URI=public.ecr.aws/m3y0m7n5/grand-challenge/web
+export GRAND_CHALLENGE_WEB_BASE_REPOSITORY_URI=public.ecr.aws/m3y0m7n5/grand-challenge/web-base
+export GRAND_CHALLENGE_WEB_TEST_BASE_REPOSITORY_URI=public.ecr.aws/m3y0m7n5/grand-challenge/web-test-base
 
 make build_web_test
 make build_http
