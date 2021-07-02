0a1aa13b7109f7dda9fea2bfa6a3c5253bbccaad
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2021-07-02 13:49:32 +0200

    Remove duplicate 'created' field from Build model

--- app/grandchallenge/codebuild/models.py
+++ app/grandchallenge/codebuild/models.py
@@ -4,7 +4,6 @@ from grandchallenge.core.models import UUIDModel
 
 
 class Build(UUIDModel):
-    created = models.DateTimeField(auto_now_add=True)
     build_id = models.CharField(max_length=1024)
     project_name = models.CharField(max_length=1024)
     status = models.CharField(blank=True, max_length=256)
