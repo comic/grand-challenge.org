4ce9da4bee266dcfa3e3d34d7786c69e8e00b445
amickan <amickan1990@gmail.com>
2021-07-27 11:40:25 +0200

    send challenge creation emails to managers

--- app/grandchallenge/challenges/emails.py
+++ app/grandchallenge/challenges/emails.py
@@ -1,7 +1,5 @@
-from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
-from django.core.mail import send_mail
+from django.core.mail import mail_managers
 
 from grandchallenge.subdomains.utils import reverse
 
@@ -9,7 +7,7 @@ from grandchallenge.subdomains.utils import reverse
 def send_challenge_created_email(challenge):
     site = Site.objects.get_current()
     message = (
-        f"Dear {{}},\n\n"
+        f"Dear manager,\n\n"
         f"User {challenge.creator} has just created the challenge "
         f"{challenge.short_name} at {challenge.get_absolute_url()}.\n\n"
         f"Regards,\n"
@@ -17,15 +15,10 @@ def send_challenge_created_email(challenge):
         f"This is an automated service email from {site.domain}."
     )
 
-    staff = get_user_model().objects.filter(is_staff=True)
-
-    for s in staff:
-        send_mail(
-            subject=f"[{site.domain.lower()}] New Challenge Created",
-            message=message.format(s.username),
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[s.email],
-        )
+    mail_managers(
+        subject=f"[{site.domain.lower()}] New Challenge Created",
+        message=message,
+    )
 
 
 def send_external_challenge_created_email(challenge):
@@ -36,7 +29,7 @@ def send_external_challenge_created_email(challenge):
     )
 
     message = (
-        f"Dear {{}},\n\n"
+        f"Dear manager,\n\n"
         f"User {challenge.creator} has just created the challenge "
         f"{challenge.short_name}. You need to un-hide it before it is visible "
         f"on the all challenges page, you can do that here: {update_url}\n\n"
@@ -45,12 +38,7 @@ def send_external_challenge_created_email(challenge):
         f"This is an automated service email from {site.domain}."
     )
 
-    staff = get_user_model().objects.filter(is_staff=True)
-
-    for s in staff:
-        send_mail(
-            subject=f"[{site.domain.lower()}] New External Challenge",
-            message=message.format(s.username),
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[s.email],
-        )
+    mail_managers(
+        subject=f"[{site.domain.lower()}] New External Challenge",
+        message=message,
+    )
