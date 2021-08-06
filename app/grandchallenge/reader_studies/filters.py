e712f0fd2466357f6ab4e1ced15666457c461729
Anne Mickan <amickan1990@gmail.com>
2021-08-06 16:25:58 +0200

    Creator filter for reader study answers (#1951)

--- app/grandchallenge/reader_studies/filters.py
+++ app/grandchallenge/reader_studies/filters.py
@@ -1,7 +1,18 @@
+from django_filters import CharFilter
+from django_filters.rest_framework import FilterSet
+
 from grandchallenge.core.filters import TitleDescriptionModalityStructureFilter
-from grandchallenge.reader_studies.models import ReaderStudy
+from grandchallenge.reader_studies.models import Answer, ReaderStudy
 
 
 class ReaderStudyFilter(TitleDescriptionModalityStructureFilter):
     class Meta(TitleDescriptionModalityStructureFilter.Meta):
         model = ReaderStudy
+
+
+class AnswerFilter(FilterSet):
+    creator = CharFilter(field_name="creator__username", label="username")
+
+    class Meta:
+        model = Answer
+        fields = ("creator", "question__reader_study")
