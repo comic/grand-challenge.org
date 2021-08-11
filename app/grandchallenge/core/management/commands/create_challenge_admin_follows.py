75d562916249ce6cca19bfb74bfe93f0414cc547
amickan <amickan1990@gmail.com>
2021-08-11 08:43:18 +0200

    added management command to create phase follows

--- app/grandchallenge/core/management/commands/create_challenge_admin_follows.py
+++ app/grandchallenge/core/management/commands/create_challenge_admin_follows.py
@@ -21,7 +21,18 @@ class Command(BaseCommand):
                         f"Created follow: {str(admin.username)} --> {str(challenge.short_name)}"
                     )
                     total_follows += 1
-
+                for phase in challenge.phase_set.all():
+                    if not is_following(user=admin, obj=phase):
+                        follow(
+                            user=admin,
+                            obj=phase,
+                            actor_only=False,
+                            send_action=False,
+                        )
+                    self.stdout.write(
+                        f"Created follow: {str(admin.username)} --> {str(phase.title)}"
+                    )
+                    total_follows += 1
         if total_follows == 0:
             self.stdout.write(self.style.WARNING("No new follows created."))
         else:
