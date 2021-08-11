fce341e9a4db63b1f7ca6bd79438a91d86e063d8
amickan <amickan1990@gmail.com>
2021-08-11 12:49:42 +0200

    allow blog editors to add authors

--- app/grandchallenge/blogs/forms.py
+++ app/grandchallenge/blogs/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
 from django_select2.forms import Select2MultipleWidget
 from guardian.utils import get_anonymous_user
 
+from config import settings
 from grandchallenge.blogs.models import Post
 from grandchallenge.core.forms import SaveFormInitMixin
 from grandchallenge.core.widgets import MarkdownEditorWidget
@@ -12,7 +15,16 @@ class PostForm(SaveFormInitMixin, forms.ModelForm):
     def __init__(self, *args, authors, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields["authors"].queryset = authors
+        self.fields["authors"].queryset = (
+            get_user_model()
+            .objects.filter(
+                groups=Group.objects.get(
+                    name=settings.REGISTERED_USERS_GROUP_NAME
+                )
+            )
+            .all()
+            .order_by("username")
+        )
         self.fields["authors"].initial = authors
 
         self.fields["tags"].required = True
@@ -34,7 +46,7 @@ class PostForm(SaveFormInitMixin, forms.ModelForm):
         )
         widgets = {
             "tags": Select2MultipleWidget,
-            "authors": forms.MultipleHiddenInput,
+            "authors": Select2MultipleWidget,
         }
 
 
