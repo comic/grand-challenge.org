a985666909030d42a5a67eb388980156708ea9a0
amickan <amickan1990@gmail.com>
2021-08-18 14:30:28 +0200

    added author update  view

--- app/grandchallenge/blogs/forms.py
+++ app/grandchallenge/blogs/forms.py
@@ -1,30 +1,19 @@
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
 from django_select2.forms import Select2MultipleWidget
 from guardian.utils import get_anonymous_user
 
-from config import settings
 from grandchallenge.blogs.models import Post
 from grandchallenge.core.forms import SaveFormInitMixin
 from grandchallenge.core.widgets import MarkdownEditorWidget
+from grandchallenge.groups.forms import UserGroupForm
 
 
 class PostForm(SaveFormInitMixin, forms.ModelForm):
     def __init__(self, *args, authors, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields["authors"].queryset = (
-            get_user_model()
-            .objects.filter(
-                groups=Group.objects.get(
-                    name=settings.REGISTERED_USERS_GROUP_NAME
-                )
-            )
-            .all()
-            .order_by("username")
-        )
+        self.fields["authors"].queryset = authors
         self.fields["authors"].initial = authors
 
         self.fields["tags"].required = True
@@ -46,7 +35,7 @@ class PostForm(SaveFormInitMixin, forms.ModelForm):
         )
         widgets = {
             "tags": Select2MultipleWidget,
-            "authors": Select2MultipleWidget,
+            "authors": forms.MultipleHiddenInput,
         }
 
 
@@ -57,3 +46,7 @@ class PostUpdateForm(PostForm):
             **PostForm.Meta.widgets,
             "content": MarkdownEditorWidget,
         }
+
+
+class AuthorsForm(UserGroupForm):
+    role = "author"
--- app/grandchallenge/blogs/urls.py
+++ app/grandchallenge/blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from grandchallenge.blogs.views import (
+    AuthorsUpdate,
     PostCreate,
     PostDetail,
     PostList,
@@ -12,6 +13,11 @@ app_name = "blogs"
 urlpatterns = [
     path("", PostList.as_view(), name="list"),
     path("create/", PostCreate.as_view(), name="create"),
+    path(
+        "<slug>/authors/update/",
+        AuthorsUpdate.as_view(),
+        name="authors-update",
+    ),
     path("<slug>/", PostDetail.as_view(), name="detail"),
     path("<slug>/update/", PostUpdate.as_view(), name="update"),
 ]
