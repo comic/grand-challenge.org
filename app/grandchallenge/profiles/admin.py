6a69f4d7b4b0e30022ce3f3b00d3b1234927f8da
James <12661555+jmsmkn@users.noreply.github.com>
2021-09-02 09:50:16 +0200

    Remove auto verification (#1989)

--- app/grandchallenge/profiles/admin.py
+++ app/grandchallenge/profiles/admin.py
@@ -3,12 +3,22 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from grandchallenge.profiles.models import UserProfile
+from grandchallenge.profiles.tasks import deactivate_user
 
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     max_num = 1
     can_delete = False
+
+
+def deactivate_users(modeladmin, request, queryset):
+    for user in queryset:
+        deactivate_user.signature(kwargs={"user_pk": user.pk}).apply_async()
+
+
+deactivate_users.short_description = "Deactivate users"
+deactivate_users.allowed_permissions = ("change",)
 
 
 class UserProfileAdmin(UserAdmin):
@@ -24,6 +34,7 @@ class UserProfileAdmin(UserAdmin):
         "is_staff",
     )
     list_filter = ("is_staff", "is_superuser", "is_active")
+    actions = (deactivate_users,)
 
 
 User = get_user_model()
