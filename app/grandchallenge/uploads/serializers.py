c0560f07e2a15e21dbc0eeffd4bcc4d9692e3f74
James <12661555+jmsmkn@users.noreply.github.com>
2021-10-07 17:00:10 +0200

    Add S3 Multipart File Uploads (#2074)
    * Add multipart S3 uploads
    
    * Fix test and add view placeholders
    
    * Add DRF views for uploads
    
    * Add widget for uploading
    
    * Add methods for uppy API
    
    * Keep exceptions near the checks
    
    * Simplify models
    
    * Implement uppy drf views
    
    * Add upload id validation
    
    * Adds uppy implementation
    
    * Add support for single and multiple uploads
    
    * Display current values to user
    
    * Add the user pk to the prefix
    
    * Small change to layout

---
+++ app/grandchallenge/uploads/serializers.py
@@ -0,0 +1,112 @@
+from typing import Dict
+
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import (
+    CharField,
+    DateTimeField,
+    IntegerField,
+    ListField,
+    SerializerMethodField,
+)
+from rest_framework.serializers import ModelSerializer, Serializer
+
+from grandchallenge.uploads.models import UserUpload
+
+
+class UserUploadSerializer(ModelSerializer):
+    status = CharField(source="get_status_display", read_only=True)
+
+    class Meta:
+        model = UserUpload
+        fields = (
+            "pk",
+            "created",
+            "filename",
+            "key",
+            "s3_upload_id",
+            "status",
+        )
+        read_only_fields = (
+            "pk",
+            "created",
+            "filename",
+            "key",
+            "s3_upload_id",
+            "status",
+        )
+
+
+class UserUploadCreateSerializer(UserUploadSerializer):
+    class Meta(UserUploadSerializer.Meta):
+        read_only_fields = [
+            field
+            for field in UserUploadSerializer.Meta.read_only_fields
+            if field != "filename"
+        ]
+
+    def validate(self, data):
+        if data.get("creator") is None:
+            data["creator"] = self.context["request"].user
+        return data
+
+
+class PartSerializer(Serializer):
+    ETag = CharField()
+    PartNumber = IntegerField(min_value=1, max_value=10_000)
+    LastModified = DateTimeField()
+    Size = IntegerField(min_value=0)
+
+
+class UserUploadPartsSerializer(UserUploadSerializer):
+    parts = PartSerializer(source="list_parts", many=True, read_only=True)
+
+    class Meta(UserUploadSerializer.Meta):
+        fields = (
+            *UserUploadSerializer.Meta.fields,
+            "parts",
+        )
+
+
+class UserUploadPresignedURLsSerializer(UserUploadSerializer):
+    part_numbers = ListField(
+        child=IntegerField(min_value=1, max_value=10_000), write_only=True
+    )
+    presigned_urls = SerializerMethodField(read_only=True)
+
+    class Meta(UserUploadSerializer.Meta):
+        fields = (
+            *UserUploadSerializer.Meta.fields,
+            "part_numbers",
+            "presigned_urls",
+        )
+
+    def validate(self, data):
+        if "part_numbers" not in data:
+            raise ValidationError("The `part_numbers` field is required")
+        return data
+
+    def get_presigned_urls(self, obj: UserUpload) -> Dict[int, str]:
+        return obj.generate_presigned_urls(
+            part_numbers=self.validated_data["part_numbers"]
+        )
+
+
+class UserUploadCompleteSerializer(UserUploadSerializer):
+    parts = PartSerializer(many=True, write_only=True)
+
+    class Meta(UserUploadSerializer.Meta):
+        fields = (
+            *UserUploadSerializer.Meta.fields,
+            "parts",
+        )
+
+    def validate(self, data):
+        if "parts" not in data:
+            raise ValidationError("The `parts` field is required")
+        return data
+
+    def save(self, **kwargs):
+        self.instance.complete_multipart_upload(
+            parts=self.validated_data["parts"]
+        )
+        super().save(**kwargs)
---
+++ app/grandchallenge/uploads/static/js/user_upload.js
@@ -0,0 +1,191 @@
+"use strict";
+
+(function () {
+    document.addEventListener("DOMContentLoaded", function (event) {
+        const widgets = document.getElementsByClassName("user-upload");
+        for (const widget of widgets) {
+            initializeWidget(widget)
+        }
+    });
+
+    function initializeWidget(widget) {
+        const inputId = widget.getAttribute("data-input-id");
+        const inputName = widget.getAttribute("data-input-name");
+        const multiWidget = widget.getAttribute("data-multiple");
+
+        let uppy = new Uppy.Core({
+            id: `${window.location.pathname}-${inputId}`,
+            autoProceed: true
+        });
+
+        uppy.use(Uppy.DragDrop, {
+            target: `#${inputId}-drag-drop`
+        });
+
+        uppy.use(Uppy.ProgressBar, {
+            target: `#${inputId}-progress`,
+            hideAfterFinish: false
+        });
+
+        uppy.use(Uppy.AwsS3Multipart, {
+            getChunkSize: (file) => {
+                return 20 * 1024 * 1024
+            },
+            createMultipartUpload: createMultipartUpload,
+            listParts: listParts,
+            prepareUploadParts: prepareUploadParts,
+            abortMultipartUpload: abortMultipartUpload,
+            completeMultipartUpload: completeMultipartUpload
+        });
+
+        uppy.on("upload-success", (file, response) => {
+            const uploadedPK = file.s3Multipart.key.split("/")[2];
+            const fileList = document.getElementById(`${inputId}-file-list`);
+
+            if (multiWidget === null) {
+                fileList.innerHTML = "";
+                document.getElementById(inputId).value = uploadedPK
+            } else {
+                let input = document.createElement("input");
+                input.name = inputName;
+                input.type = "hidden";
+                input.value = uploadedPK;
+                widget.appendChild(input);
+            }
+
+            let newFile = document.createElement("li");
+            newFile.textContent = `${uploadedPK} (${file.name})`;
+            fileList.prepend(newFile);
+        });
+    }
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    function getPOSTParams() {
+        return {
+            uploadListView: JSON.parse(document.getElementById("uploadListView").textContent),
+            csrfToken: getCookie("_csrftoken")
+        };
+    }
+
+    function createMultipartUpload(file) {
+        const postParams = getPOSTParams();
+
+        return fetch(
+            postParams.uploadListView,
+            {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": postParams.csrfToken
+                },
+                body: JSON.stringify({
+                    "filename": file.name
+                })
+            }
+        ).then(response => response.json()
+        ).then(upload => {
+            return {
+                uploadId: upload.s3_upload_id,
+                key: upload.key
+            }
+        })
+    }
+
+    function listParts(file, {uploadId, key}) {
+        const postParams = getPOSTParams();
+        const uploadPK = key.split("/")[2];
+
+        return fetch(
+            `${postParams.uploadListView}${uploadPK}/${uploadId}/list-parts/`,
+            {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": postParams.csrfToken
+                }
+            }
+        ).then(response => response.json()
+        ).then(upload => {
+            return upload.parts
+        })
+    }
+
+    function prepareUploadParts(file, {uploadId, key, partNumbers}) {
+        const postParams = getPOSTParams();
+        const uploadPK = key.split("/")[2];
+
+        return fetch(
+            `${postParams.uploadListView}${uploadPK}/${uploadId}/generate-presigned-urls/`,
+            {
+                method: "PATCH",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": postParams.csrfToken
+                },
+                body: JSON.stringify({
+                    "part_numbers": partNumbers
+                })
+            }
+        ).then(response => response.json()
+        ).then(upload => {
+            return {
+                presignedUrls: upload.presigned_urls
+            }
+        })
+    }
+
+    function abortMultipartUpload(file, {uploadId, key}) {
+        const postParams = getPOSTParams();
+        const uploadPK = key.split("/")[2];
+
+        return fetch(
+            `${postParams.uploadListView}${uploadPK}/${uploadId}/abort-multipart-upload/`,
+            {
+                method: "PATCH",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": postParams.csrfToken
+                },
+            }
+        )
+    }
+
+    function completeMultipartUpload(file, {uploadId, key, parts}) {
+        const postParams = getPOSTParams();
+        const uploadPK = key.split("/")[2];
+
+        return fetch(
+            `${postParams.uploadListView}${uploadPK}/${uploadId}/complete-multipart-upload/`,
+            {
+                method: "PATCH",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": postParams.csrfToken
+                },
+                body: JSON.stringify({
+                    "parts": parts
+                })
+            }
+        )
+    }
+})();
