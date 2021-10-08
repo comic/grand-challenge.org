8050088d04c14d9edfae03d08ac0c502701dabd9
HarmvZ <harm.van.zeeland@gmail.com>
2021-10-08 12:06:32 +0200

    Remove Patient model

--- app/tests/patients_tests/test_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from tests.patients_tests.factories import PatientFactory
-
-
-@pytest.mark.django_db
-def test_study_str():
-    model = PatientFactory()
-    assert str(model) == f"<{model.__class__.__name__} {model.name}>"
