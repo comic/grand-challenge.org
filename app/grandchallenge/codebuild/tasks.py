02558baf1bcea1d30fcb65634120e0c7a9b52623
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-10-11 18:18:19 +0200

    Use acks late queues

--- app/grandchallenge/codebuild/tasks.py
+++ app/grandchallenge/codebuild/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
 from django.apps import apps
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 from django.db.transaction import on_commit
 
 from grandchallenge.algorithms.models import Algorithm, AlgorithmImage
+from grandchallenge.components.tasks import _retry
 
 
-@shared_task()
+@shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-micro-short"])
 def create_codebuild_build(*, pk):
     GitHubWebhookMessage = apps.get_model(  # noqa: N806
         app_label="github", model_name="GitHubWebhookMessage"
@@ -29,50 +32,58 @@ def create_codebuild_build(*, pk):
         # Repository is not linked to algorithm
         return
 
-    algorithm_image = AlgorithmImage.objects.create(
-        algorithm=algorithm,
-        requires_gpu=algorithm.image_requires_gpu,
-        requires_memory_gb=algorithm.image_requires_memory_gb,
-    )
-
-    build = Build.objects.create(
-        webhook_message=ghwm, algorithm_image=algorithm_image,
-    )
-
-    on_commit(
-        lambda: wait_for_build_completion.apply_async(
-            kwargs={"build_pk": str(build.pk)}
+    with transaction.atomic():
+        algorithm_image = AlgorithmImage.objects.create(
+            algorithm=algorithm,
+            requires_gpu=algorithm.image_requires_gpu,
+            requires_memory_gb=algorithm.image_requires_memory_gb,
         )
-    )
+        build = Build.objects.create(
+            webhook_message=ghwm, algorithm_image=algorithm_image,
+        )
+
+        on_commit(
+            lambda: wait_for_build_completion.apply_async(
+                kwargs={"build_pk": str(build.pk)}
+            )
+        )
 
 
-@shared_task(bind=True, max_retries=120)
-def wait_for_build_completion(self, *, build_pk):
+@shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-micro-short"])
+def wait_for_build_completion(*, build_pk, retries=0):
     Build = apps.get_model(  # noqa: N806
         app_label="codebuild", model_name="Build"
     )
 
     build = Build.objects.get(pk=build_pk)
-    build.refresh_status()
 
-    if build.status == build.BuildStatusChoices.IN_PROGRESS:
-        self.retry(countdown=30)
-    else:
-        build.refresh_logs()
-        build.save()
-        if build.status == build.BuildStatusChoices.SUCCEEDED:
-            on_commit(
-                lambda: add_image_to_algorithm.apply_async(
-                    kwargs={"build_pk": str(build_pk)}
-                )
+    with transaction.atomic():
+        build.refresh_status()
+
+        if build.status == build.BuildStatusChoices.IN_PROGRESS:
+            _retry(
+                task=wait_for_build_completion,
+                signature_kwargs={"kwargs": {"build_pk": build_pk}},
+                retries=retries,
             )
+        else:
+            build.refresh_logs()
+            build.save()
+            if build.status == build.BuildStatusChoices.SUCCEEDED:
+                on_commit(
+                    lambda: add_image_to_algorithm.apply_async(
+                        kwargs={"build_pk": str(build_pk)}
+                    )
+                )
 
 
-@shared_task
+@shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-2xlarge"])
 def add_image_to_algorithm(*, build_pk):
     Build = apps.get_model(  # noqa: N806
         app_label="codebuild", model_name="Build"
     )
 
     build = Build.objects.get(pk=build_pk)
-    build.add_image_to_algorithm()
+
+    if not build.algorithm_image.image:
+        build.add_image_to_algorithm()
