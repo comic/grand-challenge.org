e5b6c21a73afe94c1083b00c4c384deeeba4490c
amickan <amickan1990@gmail.com>
2021-10-28 15:32:13 +0200

    Add new outputs to job detail view

--- app/grandchallenge/components/models.py
+++ app/grandchallenge/components/models.py
@@ -278,24 +278,95 @@ class InterfaceKind:
                 "version": { "major": 1, "minor": 0 }
             }
 
-        Example json for Chart
+        Example json for Chart (for more examples, see `here<https://vega.github.io/vega-lite/examples/>` and `here<https://grand-challenge.org/blogs/visualisations-for-challenges/>`)
 
         .. code-block:: json
 
             {
-                "description": "A simple bar chart with embedded data.",
-                "data": {
-                    "values": [
-                        {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
-                        {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
-                        {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
-                    ]
-                },
-                "mark": "bar",
-                "encoding": {
-                    "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
-                    "y": {"field": "b", "type": "quantitative"}
-                }
+               "$schema":"https://vega.github.io/schema/vega-lite/v5.json",
+               "width":300,
+               "height":300,
+               "data":{
+                  "values":[
+                     {
+                        "target":"Negative",
+                        "prediction":"Negative",
+                        "value":198
+                     },
+                     {
+                        "target":"Negative",
+                        "prediction":"Positive",
+                        "value":9
+                     },
+                     {
+                        "target":"Positive",
+                        "prediction":"Negative",
+                        "value":159
+                     },
+                     {
+                        "target":"Positive",
+                        "prediction":"Positive",
+                        "value":376
+                     }
+                  ],
+                  "format":{
+                     "type":"json"
+                  }
+               },
+               "layer":[
+                  {
+                     "mark":"rect",
+                     "encoding":{
+                        "y":{
+                           "field":"target",
+                           "type":"ordinal"
+                        },
+                        "x":{
+                           "field":"prediction",
+                           "type":"ordinal"
+                        },
+                        "color":{
+                           "field":"value",
+                           "type":"quantitative",
+                           "title":"Count of Records",
+                           "legend":{
+                              "direction":"vertical",
+                              "gradientLength":300
+                           }
+                        }
+                     }
+                  },
+                  {
+                     "mark":"text",
+                     "encoding":{
+                        "y":{
+                           "field":"target",
+                           "type":"ordinal"
+                        },
+                        "x":{
+                           "field":"prediction",
+                           "type":"ordinal"
+                        },
+                        "text":{
+                           "field":"value",
+                           "type":"quantitative"
+                        },
+                        "color":{
+                           "condition":{
+                              "test":"datum['value'] < 40",
+                              "value":"black"
+                           },
+                           "value":"white"
+                        }
+                     }
+                  }
+               ],
+               "config":{
+                  "axis":{
+                     "grid":True,
+                     "tickBand":"extent"
+                  }
+               }
             }
 
         """
