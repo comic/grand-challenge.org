2c6da7674c77aa223e36275d026f714d15a8daf9
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-12-14 15:10:49 +0100

    Add UPLOADS_TIMEOUT_DAYS
    Sets how long uploads can exist for before being cleaned up.

--- app/grandchallenge/uploads/tasks.py
+++ app/grandchallenge/uploads/tasks.py
@@ -10,7 +10,7 @@ from grandchallenge.uploads.models import UserUpload
 
 @shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-micro-short"])
 def delete_old_user_uploads():
-    limit = now() - timedelta(days=2)
+    limit = now() - timedelta(days=settings.UPLOADS_TIMEOUT_DAYS)
     queryset = UserUpload.objects.filter(created__lt=limit).order_by(
         "-created"
     )
