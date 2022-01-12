b7d3fefa2371f3b70ba85b37a91d1f56f72c77b7
amickan <amickan1990@gmail.com>
2022-01-12 17:23:19 +0100

    Add action enum and turn send_bulk_email into celery task

--- app/grandchallenge/emails/admin.py
+++ app/grandchallenge/emails/admin.py
@@ -1,28 +1,35 @@
 from django.contrib import admin, messages
-from django.contrib.auth import get_user_model
+from django.db.transaction import on_commit
 from django.utils.timezone import now
 from django.utils.translation import ngettext
 from markdownx.admin import MarkdownxModelAdmin
 
-from grandchallenge.algorithms.models import Algorithm
-from grandchallenge.challenges.models import Challenge
 from grandchallenge.emails.models import Email
 from grandchallenge.emails.tasks import send_bulk_email
-from grandchallenge.reader_studies.models import ReaderStudy
+from grandchallenge.emails.utils import SendActionChoices
 
 
-def schedule_emails(modeladmin, queryset, request, receivers, action_string):
+def schedule_emails(modeladmin, queryset, request, action):
     emails = queryset.filter(sent=False)
     if emails:
         for email in emails:
-            send_bulk_email(receivers, email.subject, email.body)
+            send_admin_emails = send_bulk_email.signature(
+                kwargs={
+                    "action": action,
+                    "subject": email.subject,
+                    "body": email.body,
+                },
+                immutable=True,
+            )
+            on_commit(send_admin_emails.apply_async)
 
         updated = emails.update(sent=True, sent_at=now())
+
         modeladmin.message_user(
             request,
             ngettext(
-                f"%d email was successfully sent to {action_string}.",
-                f"%d emails were successfully sent to {action_string}.",
+                f"%d email was successfully sent to {action.label.lower()}.",
+                f"%d emails were successfully sent to {action.label.lower()}.",
                 updated,
             )
             % updated,
@@ -38,60 +45,51 @@ def schedule_emails(modeladmin, queryset, request, receivers, action_string):
 
 class EmailAdmin(MarkdownxModelAdmin):
     list_display = ("subject", "sent", "sent_at")
-    actions = [
-        "send_to_mailing_list",
-        "send_to_staff",
-        "send_to_challenge_admins",
-        "send_to_readerstudy_editors",
-        "send_to_algorithm_editors",
-    ]
+    actions = [*SendActionChoices]
 
     @admin.action(description="Send to mailing list")
     def send_to_mailing_list(self, request, queryset):
-        receivers = [
-            user
-            for user in get_user_model().objects.filter(
-                user_profile__receive_newsletter=True
-            )
-        ]
-        schedule_emails(self, queryset, request, receivers, "mailing list")
+        schedule_emails(
+            modeladmin=self,
+            queryset=queryset,
+            request=request,
+            action=SendActionChoices.MAILING_LIST,
+        )
 
     @admin.action(description="Send to staff")
     def send_to_staff(self, request, queryset):
-        receivers = [
-            user for user in get_user_model().objects.filter(is_staff=True)
-        ]
-        schedule_emails(self, queryset, request, receivers, "staff")
+        schedule_emails(
+            modeladmin=self,
+            queryset=queryset,
+            request=request,
+            action=SendActionChoices.STAFF,
+        )
 
     @admin.action(description="Send to challenge admins")
     def send_to_challenge_admins(self, request, queryset):
-        receivers = (
-            user
-            for challenge in Challenge.objects.all()
-            for user in challenge.admins_group.user_set.all()
+        schedule_emails(
+            modeladmin=self,
+            queryset=queryset,
+            request=request,
+            action=SendActionChoices.CHALLENGE_ADMINS,
         )
-        schedule_emails(self, queryset, request, receivers, "challenge admins")
 
     @admin.action(description="Send to reader study editors")
     def send_to_readerstudy_editors(self, request, queryset):
-        receivers = (
-            user
-            for rs in ReaderStudy.objects.all()
-            for user in rs.editors_group.user_set.all()
-        )
         schedule_emails(
-            self, queryset, request, receivers, "reader study editors"
+            modeladmin=self,
+            queryset=queryset,
+            request=request,
+            action=SendActionChoices.READER_STUDY_EDITORS,
         )
 
     @admin.action(description="Send to algorithm editors")
     def send_to_algorithm_editors(self, request, queryset):
-        receivers = (
-            user
-            for algorithm in Algorithm.objects.all()
-            for user in algorithm.editors_group.user_set.all()
-        )
         schedule_emails(
-            self, queryset, request, receivers, "algorithm editors"
+            modeladmin=self,
+            queryset=queryset,
+            request=request,
+            action=SendActionChoices.ALGORITHM_EDITORS,
         )
 
 
--- app/grandchallenge/emails/tasks.py
+++ app/grandchallenge/emails/tasks.py
@@ -1,15 +1,50 @@
 from celery import shared_task
+from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
 from django.core.mail import send_mass_mail
 from django.core.paginator import Paginator
 
 from config import settings
+from grandchallenge.algorithms.models import Algorithm
+from grandchallenge.challenges.models import Challenge
+from grandchallenge.emails.utils import SendActionChoices
+from grandchallenge.reader_studies.models import ReaderStudy
 from grandchallenge.subdomains.utils import reverse
 
 
-@shared_task
-def send_bulk_email(users, subject, body):
-    paginator = Paginator(users, 1000)
+@shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-micro-short"])
+def send_bulk_email(action, subject, body):
+    if action == SendActionChoices.MAILING_LIST:
+        receivers = [
+            user
+            for user in get_user_model().objects.filter(
+                user_profile__receive_newsletter=True
+            )
+        ]
+    elif action == SendActionChoices.STAFF:
+        receivers = [
+            user for user in get_user_model().objects.filter(is_staff=True)
+        ]
+    elif action == SendActionChoices.CHALLENGE_ADMINS:
+        receivers = [
+            user
+            for challenge in Challenge.objects.all()
+            for user in challenge.admins_group.user_set.all()
+        ]
+    elif action == SendActionChoices.READER_STUDY_EDITORS:
+        receivers = [
+            user
+            for rs in ReaderStudy.objects.all()
+            for user in rs.editors_group.user_set.all()
+        ]
+    elif action == SendActionChoices.ALGORITHM_EDITORS:
+        receivers = [
+            user
+            for algorithm in Algorithm.objects.all()
+            for user in algorithm.editors_group.user_set.all()
+        ]
+
+    paginator = Paginator(list(set(receivers)), 1000)
     site = Site.objects.get_current()
     messages = []
     for page_nr in paginator.page_range:
@@ -17,9 +52,9 @@ def send_bulk_email(users, subject, body):
             messages.append(
                 (
                     f"[{site.domain.lower()}] {subject}",
-                    f"Dear {recipient.username}, "
-                    f"\r\n\r\n {body} \r\n\r\n "
-                    f"Kind regards, \r\n Grand Challenge team \r\n\r\n "
+                    f"Dear {recipient.username},"
+                    f"\r\n\r\n{body}\r\n\r\n"
+                    f"Kind regards, \r\nGrand Challenge team \r\n\r\n"
                     f"To unsubscribe from this mailing list, "
                     f"uncheck 'Receive newsletter' in your profile settings:"
                     f" {reverse('profile-update', kwargs={'username': recipient.username})}",
