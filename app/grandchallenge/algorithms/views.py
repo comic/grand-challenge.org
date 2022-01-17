574b17f1e1703f60e1db1f79b94aebae32fa85f4
Mike Overkamp <57257130+MikeOverkamp-diag@users.noreply.github.com>
2022-01-17 15:50:49 +0100

    Don't raise a 404 if user does not have a GitHub token (#2244)
    * Don't raise a 404 if user does not have a GitHub token
    
    Display an ampty repo list instead.
    
    * Re-add template filename

--- app/grandchallenge/algorithms/views.py
+++ app/grandchallenge/algorithms/views.py
@@ -895,7 +895,11 @@ class AlgorithmAddRepo(
         """Return the keyword arguments for instantiating the form."""
         kwargs = super().get_form_kwargs()
 
-        user_token = get_object_or_404(GitHubUserToken, user=self.request.user)
+        try:
+            user_token = GitHubUserToken.objects.get(user=self.request.user)
+        except GitHubUserToken.DoesNotExist:
+            kwargs.update({"repos": []})
+            return kwargs
 
         if user_token.access_token_is_expired:
             user_token.refresh_access_token()
