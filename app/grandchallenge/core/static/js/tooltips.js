bd283a499748327d77cdc5ecfb574c4e5a946b70
Chris <chris.vanrun@radboudumc.nl>
2022-02-19 09:42:22 +0100

    Move in-line JS to a static file

---
+++ app/grandchallenge/core/static/js/tooltips.js
@@ -0,0 +1,8 @@
+// Bootstrap tooltips are opt-in
+// Set attribute 'data-toggle' to "tooltip" on elements that should have a tooltip
+// For example:
+//   <div data-toggle="tooltip" title="Tooltip content"></div>
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
