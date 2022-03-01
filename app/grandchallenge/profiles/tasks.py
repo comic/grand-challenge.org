b02183546dd4a7afa9cc84b205de9eee11e8309d
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-03-01 13:15:52 +0100

    Paginate session deletion

--- app/grandchallenge/profiles/tasks.py
+++ app/grandchallenge/profiles/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.sessions.models import Session
+from django.core.paginator import Paginator
 
 
 @shared_task(**settings.CELERY_TASK_DECORATOR_KWARGS["acks-late-micro-short"])
@@ -11,6 +12,13 @@ def deactivate_user(*, user_pk):
     user.is_active = False
     user.save()
 
-    for s in Session.objects.all():
-        if str(s.get_decoded().get("_auth_user_id")) == str(user.id):
-            s.delete()
+    queryset = Session.objects.order_by("expire_date")
+    paginator = Paginator(object_list=queryset, per_page=1000)
+
+    # Reverse iteration over the queryset as we're deleting objects
+    for idx in range(paginator.num_pages, 0, -1):
+        page = paginator.get_page(idx)
+
+        for s in page.object_list:
+            if str(s.get_decoded().get("_auth_user_id")) == str(user.id):
+                s.delete()
