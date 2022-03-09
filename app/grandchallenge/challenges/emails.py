e18d4c339a5fcbb5182da4871c5722fd6198c30e
amickan <amickan1990@gmail.com>
2022-03-09 14:24:24 +0100

    Escape user input in emails and remove reviewer group

--- app/grandchallenge/challenges/emails.py
+++ app/grandchallenge/challenges/emails.py
@@ -1,20 +1,25 @@
-from django.contrib.auth.models import Group
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
 from django.core.mail import mail_managers, send_mail
+from django.db.models import Q
+from django.utils.html import format_html
 
-from config import settings
 from grandchallenge.subdomains.utils import reverse
 
 
 def send_challenge_created_email(challenge):
     site = Site.objects.get_current()
-    message = (
-        f"Dear manager,\n\n"
-        f"User {challenge.creator} has just created the challenge "
-        f"{challenge.short_name} at {challenge.get_absolute_url()}.\n\n"
-        f"Regards,\n"
-        f"{site.name}\n\n"
-        f"This is an automated service email from {site.domain}."
+    message = format_html(
+        "Dear manager,\n\n"
+        "User {} has just created the challenge {} at {}.\n\n"
+        "Regards,\n{}\n\n"
+        "This is an automated service email from {}.",
+        challenge.creator,
+        challenge.short_name,
+        challenge.get_absolute_url(),
+        site.name,
+        site.domain,
     )
 
     mail_managers(
@@ -30,14 +35,18 @@ def send_external_challenge_created_email(challenge):
         kwargs={"short_name": challenge.short_name},
     )
 
-    message = (
-        f"Dear manager,\n\n"
-        f"User {challenge.creator} has just created the challenge "
-        f"{challenge.short_name}. You need to un-hide it before it is visible "
-        f"on the all challenges page, you can do that here: {update_url}\n\n"
-        f"Regards,\n"
-        f"{site.name}\n\n"
-        f"This is an automated service email from {site.domain}."
+    message = format_html(
+        "Dear manager,\n\n"
+        "User {} has just created the challenge {}. You need to un-hide it "
+        "before it is visible on the all challenges page, you can do that "
+        "here: {}\n\n"
+        "Regards,\n{}\n\n"
+        "This is an automated service email from {}.",
+        challenge.creator,
+        challenge.short_name,
+        update_url,
+        site.name,
+        site.domain,
     )
 
     mail_managers(
@@ -51,21 +60,22 @@ def send_challenge_requested_email_to_reviewers(challengerequest):
     update_url = reverse(
         "challenges:request-list",
     )
-    message = (
-        f"Dear reviewers,\n\n"
-        f"User {challengerequest.creator} has just requested the challenge "
-        f"{challengerequest.title}. To review the challenge, go here:"
-        f"{update_url}\n\n"
-        f"Regards,\n"
-        f"{site.name}\n\n"
-        f"This is an automated service email from {site.domain}."
+    message = format_html(
+        "Dear reviewers,\n\n"
+        "User {} has just requested the challenge "
+        "{}. To review the challenge, go here: {}\n\n"
+        "Regards,\n{}\n\n"
+        "This is an automated service email from {}.",
+        challengerequest.creator,
+        challengerequest.title,
+        update_url,
+        site.name,
+        site.domain,
     )
-    reviewers = (
-        Group.objects.filter(name=settings.CHALLENGE_REVIEWERS_GROUP_NAME)
-        .get()
-        .user_set.all()
+    reviewers = get_user_model().objects.filter(
+        Q(groups__permissions__codename="change_challengerequest")
+        | Q(user_permissions__codename="change_challengerequest")
     )
-
     send_mail(
         subject=f"[{site.domain.lower()}] New Challenge Requested",
         message=message,
@@ -90,16 +100,20 @@ def send_challenge_requested_email_to_requester(challengerequest):
             f"{budget}\n\n"
         )
 
-    message = (
-        f"Dear {challengerequest.creator.username},\n\n"
-        f"Your challenge request has been sent to the reviewers. You will "
-        f"receive an email informing you of our decision within the next 4 weeks. "
-        f"The reviewers might contact you for additional information during "
-        f"that time.\n"
-        f"{addition}"
-        f"Regards,\n"
-        f"{site.name} team\n\n"
-        f"This is an automated service email from {site.domain}."
+    message = format_html(
+        "Dear {},\n\n"
+        "Your challenge request has been sent to the reviewers. You will "
+        "receive an email informing you of our decision within the next 4 weeks. "
+        "The reviewers might contact you for additional information during "
+        "that time.\n"
+        "{}"
+        "Regards,\n"
+        "{} team\n\n"
+        "This is an automated service email from {}.",
+        challengerequest.creator.username,
+        addition,
+        site.name,
+        site.domain,
     )
 
     send_mail(
@@ -117,55 +131,62 @@ def send_challenge_status_update_email(challengerequest, challenge=None):
         challengerequest.status
         == challengerequest.ChallengeRequestStatusChoices.ACCEPTED
     ):
-        message = (
-            f"Dear {challengerequest.creator.username},\n\n"
-            f"We are happy to inform you that your challenge request has been "
-            f"accepted. For your convenience, we have already created the "
-            f"challenge page for you at {challenge.get_absolute_url()} "
-            f"Note that your challenge is currently still hidden and is not yet "
-            f"listed on our challenge overview page. Once your challenge is "
-            f"all set-up, you can make it public by going to Admin - General "
-            f"Settings and unchecking the 'hidden' box. \n"
-            f"To get your challenge ready, please have a close look at our "
-            f"documentation (https://grand-challenge.org/documentation/"
-            f"create-your-own-challenge/) and the steps below. \n\n"
-            f"Next steps:\n"
-            f"1. On your challenge page, go to Admin - General Settings and carefully "
-            f"review all information there, upload a logo and banner for your "
-            f"challenge, optionally enable the forum (recommended) and teams features "
-            f"and choose your preferred access request handling policy. All these "
-            f"options are described in our documentation.\n"
-            f"2. Add information about your challenge, your submission procedure, "
-            f"the challenge timeline etc to your challenge page by editing and adding"
-            f" custom subpages to your challenge. For more information, see the tab "
-            f"'Add custom pages' in our documentation.\n"
-            f"3. A first phase has been added to your challenge, to add more go "
-            f"to Admin - Phases - Add a new Phase. Please carefully read our "
-            f"documentation for details on how to set up your phases for a type 1"
-            f"or a type 2 challenge respectively. \n\n"
-            f"Feel free to contact support@grand-challenge.org if you have any "
-            f"questions or require assistance in setting up your challenge. \n"
-            f"Thank you for choosing Grand Challenge. We are looking forward to "
-            f"hosting your challenge. \n\n"
-            f"Regards,\n"
-            f"{site.name} team\n\n"
-            f"This is an automated service email from {site.domain}."
+        message = format_html(
+            "Dear {},\n\n"
+            "We are happy to inform you that your challenge request has been "
+            "accepted. For your convenience, we have already created the "
+            "challenge page for you at {} "
+            "Note that your challenge is currently still hidden and is not yet "
+            "listed on our challenge overview page. Once your challenge is "
+            "all set-up, you can make it public by going to Admin - General "
+            "Settings and unchecking the 'hidden' box. \n"
+            "To get your challenge ready, please have a close look at our "
+            "documentation (https://grand-challenge.org/documentation/"
+            "create-your-own-challenge/) and the steps below. \n\n"
+            "Next steps:\n"
+            "1. On your challenge page, go to Admin - General Settings and "
+            "carefully review all information there, upload a logo and banner for"
+            "your challenge, optionally enable the forum (recommended) and "
+            "teams features and choose your preferred access request handling "
+            "policy. All these options are described in our documentation.\n"
+            "2. Add information about your challenge, your submission procedure, "
+            "the challenge timeline etc to your challenge page by editing and "
+            "adding custom subpages to your challenge. For more information, "
+            "see the tab 'Add custom pages' in our documentation.\n"
+            "3. A first phase has been added to your challenge, to add more go "
+            "to Admin - Phases - Add a new Phase. Please carefully read our "
+            "documentation for details on how to set up your phases for a type 1"
+            "or a type 2 challenge respectively. \n\n"
+            "Feel free to contact support@grand-challenge.org if you have any "
+            "questions or require assistance in setting up your challenge. \n"
+            "Thank you for choosing Grand Challenge. We are looking forward to "
+            "hosting your challenge. \n\n"
+            "Regards,\n"
+            "{} team\n\n"
+            "This is an automated service email from {}.",
+            challengerequest.creator.username,
+            challenge.get_absolute_url(),
+            site.name,
+            site.domain,
         )
     elif (
         challengerequest.status
         == challengerequest.ChallengeRequestStatusChoices.REJECTED
     ):
-        message = (
-            f"Dear {challengerequest.creator.username},\n\n"
+        message = format_html(
+            "Dear {},\n\n"
             "We are very sorry to have to inform you that we will not be able to "
             "host your challenge on our platform. We can only support a limited "
             "number of challenges per year and hence have to be selective in "
-            "reviewing proposals. We would like to nevertheless thank you for your"
-            "submission and wish you the best of luck with organizing and hosting "
-            "your challenge elsewhere.\n\n"
-            f"Regards,\n"
-            f"{site.name} team\n\n"
-            f"This is an automated service email from {site.domain}."
+            "reviewing proposals. We would like to nevertheless thank you for "
+            "your submission and wish you the best of luck with organizing and "
+            "hosting your challenge elsewhere.\n\n"
+            "Regards,\n"
+            "{} team\n\n"
+            "This is an automated service email from {}.",
+            challengerequest.creator.username,
+            site.name,
+            site.domain,
         )
 
     send_mail(
