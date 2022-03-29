dc3b09d4fe6a1c5ed1abc0bd18d644085c6ab6fb
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-03-29 13:59:46 +0200

    Allow the groups users to be editable

--- app/grandchallenge/core/admin.py
+++ app/grandchallenge/core/admin.py
@@ -1,28 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import GroupAdmin
-from django.contrib.auth.models import Group
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.forms import FlatpageForm
 from django.contrib.flatpages.models import FlatPage
 
 from grandchallenge.core.widgets import MarkdownEditorAdminWidget
-
-
-class ReadOnlyUserInLine(admin.TabularInline):
-    model = get_user_model().groups.through
-    extra = 0
-    can_delete = False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_add_permission(self, request, obj=None):
-        return False
-
-
-class GroupWithUsers(GroupAdmin):
-    inlines = [ReadOnlyUserInLine]
 
 
 class MarkdownFlatPageForm(FlatpageForm):
@@ -33,9 +14,6 @@ class MarkdownFlatPageForm(FlatpageForm):
 class MarkdownFlatPageAdmin(FlatPageAdmin):
     form = MarkdownFlatPageForm
 
-
-admin.site.unregister(Group)
-admin.site.register(Group, GroupWithUsers)
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, MarkdownFlatPageAdmin)
--- app/grandchallenge/groups/admin.py
+++ app/grandchallenge/groups/admin.py
@@ -1,47 +1,17 @@
-from django import forms
 from django.contrib import admin
-from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.models import Group
 
-User = get_user_model()
+
+class UserInLine(admin.TabularInline):
+    model = get_user_model().groups.through
+    raw_id_fields = ("user",)
 
 
-class GroupAdminForm(forms.ModelForm):
-    class Meta:
-        model = Group
-        exclude = []
-
-    users = forms.ModelMultipleChoiceField(
-        queryset=User.objects.all(),
-        required=False,
-        widget=FilteredSelectMultiple("users", False),
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            self.fields["users"].initial = self.instance.user_set.all()
-
-    def save_m2m(self):
-        self.instance.user_set.set(self.cleaned_data["users"])
-
-    def save(self, *args, **kwargs):
-        instance = super().save()
-        # Save many-to-many data
-        self.save_m2m()
-        return instance
+class GroupWithUsers(GroupAdmin):
+    inlines = [UserInLine]
 
 
-# Create a new Group admin.
-class GroupAdmin(admin.ModelAdmin):
-    form = GroupAdminForm
-    filter_horizontal = ["permissions"]
-    search_fields = ("name",)
-    ordering = ("name",)
-
-
-# Unregister the original Group admin.
 admin.site.unregister(Group)
-# Register the new Group ModelAdmin.
-admin.site.register(Group, GroupAdmin)
+admin.site.register(Group, GroupWithUsers)
