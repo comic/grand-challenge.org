83545994214b6194e42ef8f5e59d47524e846958
amickan <amickan1990@gmail.com>
2022-04-01 16:52:26 +0200

    Show visualization on list view

--- app/grandchallenge/hanging_protocols/static/js/hanging_protocols/hanging_protocol_visualization.js
+++ app/grandchallenge/hanging_protocols/static/js/hanging_protocols/hanging_protocol_visualization.js
@@ -8,13 +8,11 @@ function removeAllChildNodes(parent) {
     }
 }
 
-function showOrHideVisualizationDiv(jsonString) {
-    console.log(jsonString)
+function showOrHideVisualizationDiv(divId, jsonString) {
     if (jsonString != '' && jsonString != 'null') {
-            console.log("show!")
-            document.getElementById("hpVisualization").style.display = 'block';
+            document.getElementById(divId).style.display = 'block';
         } else {
-            document.getElementById("hpVisualization").style.display = 'none';
+            document.getElementById(divId).style.display = 'none';
     }
 }
 
@@ -37,7 +35,7 @@ function getGridDimensions(json) {
     return [totalHeight, totalWidth]
 }
 
-function createViewportDiv(viewportSpec, totalHeight, totalWidth) {
+function createViewportDiv(divId, viewportSpec, totalHeight, totalWidth) {
     let viewportDiv = document.createElement("div");
     viewportDiv.setAttribute('id', viewportSpec.viewport_name);
     viewportDiv.style.opacity = '0.5';
@@ -47,23 +45,24 @@ function createViewportDiv(viewportSpec, totalHeight, totalWidth) {
     viewportDiv.style.height = (viewportSpec.h / parseFloat(totalHeight).toFixed(2))*100 + '%';
     viewportDiv.style.left = (viewportSpec.x / parseFloat(totalWidth).toFixed(2))*100 + '%';
     viewportDiv.style.top = (viewportSpec.y / parseFloat(totalHeight).toFixed(2))*100 + '%';
-    document.getElementById("hpVisualization").appendChild(viewportDiv).classList.add('bg-dark', 'rounded', 'border', 'border-2', 'd-flex', 'flex-column', 'justify-content-center', 'align-items-center');
+    document.getElementById(divId).appendChild(viewportDiv).classList.add('bg-dark', 'rounded', 'border', 'border-2', 'd-flex', 'flex-column', 'justify-content-center', 'align-items-center');
 
-    viewportDiv.innerHTML += '<h4>'+ viewportSpec.viewport_name + '</h4>';
+    viewportDiv.innerHTML += '<p style="font-size: 1.5em">'+ viewportSpec.viewport_name + '</p>';
     if (viewportSpec.fullsizable == true) {
         viewportDiv.innerHTML += '<i class="fas fa-expand m-1"></i>'
     }
 }
 
-function updateHangingProtocolVisualization(jsonString){
+function updateHangingProtocolVisualization(parentDivId, jsonString){
     jsonString = jsonString || document.getElementById("id_json").value;
-    showOrHideVisualizationDiv(jsonString);
+    parentDivId = parentDivId || "hpVisualization";
+    showOrHideVisualizationDiv(parentDivId, jsonString);
     try {
         let jsonSpec = JSON.parse(jsonString);
         [totalHeight, totalWidth] = getGridDimensions(jsonSpec);
-        removeAllChildNodes(document.getElementById("hpVisualization"));
+        removeAllChildNodes(document.getElementById(parentDivId));
         for (let i = 0; i < jsonSpec.length; i++) {
-            createViewportDiv(jsonSpec[i], totalHeight, totalWidth);
+            createViewportDiv(parentDivId, jsonSpec[i], totalHeight, totalWidth);
         }
     } catch (err) {
     }
@@ -72,14 +71,18 @@ function updateHangingProtocolVisualization(jsonString){
 $(document).ready(function () {
     if (window.location.href.includes("update") | window.location.href.includes("create")) {
         let jsonString = document.getElementById("id_json").value;
-        showOrHideVisualizationDiv(jsonString);
-        updateHangingProtocolVisualization(jsonString);
+        showOrHideVisualizationDiv("hpVisualization", jsonString);
+        updateHangingProtocolVisualization("hpVisualization", jsonString);
         document.getElementById('jsoneditor_id_json').addEventListener('input', function(){updateHangingProtocolVisualization()});
         document.getElementById('jsoneditor_id_json').addEventListener('keyup', function(){updateHangingProtocolVisualization()});
         document.getElementById('jsoneditor_id_json').addEventListener('paste', function(){updateHangingProtocolVisualization()});
     } else {
-        let jsonString = document.getElementById("id_json").innerHTML;
-        showOrHideVisualizationDiv(jsonString);
-        updateHangingProtocolVisualization(jsonString);
+        let jsonDivs = document.querySelectorAll("[id^='id_json']");
+        let hpVizDivs = document.querySelectorAll("[id^='hpVisualization']");
+        for (let i = 0; i < jsonDivs.length; i++) {
+            let jsonString = jsonDivs[i].innerHTML;
+            showOrHideVisualizationDiv(hpVizDivs[i].id, jsonString);
+            updateHangingProtocolVisualization(hpVizDivs[i].id, jsonString);
+        }
     }
 });
