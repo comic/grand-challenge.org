135abab4eae0ba7699158de16753a4225630e3f6
Mike Overkamp <mikeoverkamp.diag@gmail.com>
2022-04-04 13:25:42 +0200

    Add button to migrate to display sets

--- app/grandchallenge/reader_studies/management/commands/migrate_to_display_sets.py
+++ app/grandchallenge/reader_studies/management/commands/migrate_to_display_sets.py
@@ -1,20 +1,14 @@
 from django.core.management import BaseCommand
-from django.db import transaction
 
-from grandchallenge.cases.models import Image
-from grandchallenge.components.models import (
-    ComponentInterface,
-    ComponentInterfaceValue,
-)
-from grandchallenge.reader_studies.models import (
-    Answer,
-    DisplaySet,
-    ReaderStudy,
+from grandchallenge.reader_studies.models import ReaderStudy
+from grandchallenge.reader_studies.utils import (
+    check_for_new_answers,
+    migrate_reader_study_to_display_sets,
 )
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):  # noqa: C901
+    def handle(self, *args, **options):
         not_migrated = []
         reader_studies = ReaderStudy.objects.filter(use_display_sets=False)
         n_studies = len(reader_studies)
@@ -22,10 +16,8 @@ class Command(BaseCommand):
         count = 0
         for rs in reader_studies:
             count += 1
-            if not (
-                {x for item in rs.hanging_list for x in item}
-                in [{"main"}, {"main", "main-overlay"}]
-            ):
+            content_keys = {x for item in rs.hanging_list for x in item}
+            if not (content_keys in [{"main"}, {"main", "main-overlay"}]):
                 # Multiple viewports require more interfaces, this needs
                 # to be handled manually
                 not_migrated.append(str(rs.pk))
@@ -35,75 +27,21 @@ class Command(BaseCommand):
                     )
                 )
                 continue
-            with transaction.atomic():
-                image_interface = ComponentInterface.objects.get(
-                    slug="generic-medical-image"
-                )
-                overlay_interface = ComponentInterface.objects.get(
-                    slug="generic-overlay"
-                )
-                for item in rs.hanging_list:
-                    ds = DisplaySet.objects.create(reader_study=rs)
-                    images = []
-                    for key in item:
-                        try:
-                            image = Image.objects.get(name=item[key])
-                        except Image.DoesNotExist:
-                            continue
-                        images.append(image.pk)
-                        if key == "main":
-                            (
-                                civ,
-                                _,
-                            ) = ComponentInterfaceValue.objects.get_or_create(
-                                image=image, interface=image_interface
-                            )
-                        else:
-                            (
-                                civ,
-                                _,
-                            ) = ComponentInterfaceValue.objects.get_or_create(
-                                image=image, interface=overlay_interface
-                            )
-                        ds.values.add(civ)
+            view_content = {"main": "generic-medical-image"}
+            if "main-overlay" in content_keys:
+                view_content["main-overlay"] = "generic-overlay"
 
-                    answers = Answer.objects.filter(question__reader_study=rs)
-                    for image in images:
-                        answers = answers.filter(images=image)
-                    for answer in answers:
-                        answer.display_set = ds
-                        answer.save()
-                        answer.images.clear()
-                rs.use_display_sets = True
-                rs.images.clear()
-                rs.hanging_list = []
-                rs.save()
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully migrated {rs}. ({count}/{n_studies})"
-                    )
+            migrate_reader_study_to_display_sets(rs, view_content)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully migrated {rs}. ({count}/{n_studies})"
                 )
+            )
 
-            # Check of any new answers have been created during the migration
-            # and add them to the proper display set
-            for answer in Answer.objects.filter(
-                question__reader_study=rs, images__isnull=False
-            ):
-                ds = DisplaySet.objects.filter(reader_study=rs)
-                for im in answer.images.all():
-                    ds.filter(values__image=im)
-                ds = ds.first()
-                if ds:
-                    answer.display_set = ds
-                    answer.save()
-                    answer.images.clear()
-                else:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            f"Could not find a display set for answer "
-                            f"{answer.pk}."
-                        )
-                    )
+            try:
+                check_for_new_answers(rs)
+            except ValueError as e:
+                self.stdout.write(self.style.ERROR(str(e)))
 
         pk_str = "\n".join(not_migrated)
         self.stdout.write(
