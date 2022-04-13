786f44852317e0ef4a529a5b584a45a7a1031ca2
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-04-13 17:50:56 +0200

    Split interface overview into inputs and outputs
    This should provide users with more context, and provides more
    explanation on what an interface is.

--- app/grandchallenge/algorithms/views.py
+++ app/grandchallenge/algorithms/views.py
@@ -12,7 +12,7 @@ from django.core.exceptions import (
     PermissionDenied,
     ValidationError,
 )
-from django.db.models import OuterRef, Subquery
+from django.db.models import OuterRef, Subquery, TextChoices
 from django.forms.utils import ErrorList
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -25,6 +25,7 @@ from django.views.generic import (
     DetailView,
     FormView,
     ListView,
+    TemplateView,
     UpdateView,
 )
 from django_filters.rest_framework import DjangoFilterBackend
@@ -91,8 +92,28 @@ from grandchallenge.verifications.views import VerificationRequiredMixin
 logger = logging.getLogger(__name__)
 
 
+class ComponentInterfaceIOSwitch(LoginRequiredMixin, TemplateView):
+    template_name = "components/componentinterface_io_switch.html"
+
+
+class InterfaceListTypeOptions(TextChoices):
+    INPUT = "INPUT"
+    OUTPUT = "OUTPUT"
+
+
 class ComponentInterfaceList(LoginRequiredMixin, ListView):
     model = ComponentInterface
+    list_type = None
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context.update(
+            {
+                "list_type": self.list_type,
+                "list_type_options": InterfaceListTypeOptions,
+            }
+        )
+        return context
 
 
 class AlgorithmCreate(
