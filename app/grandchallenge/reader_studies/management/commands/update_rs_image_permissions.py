c454c557cdf7b52a82b27f35ceafc4eccd44992f
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-04-17 13:08:06 +0200

    Add update study permissions

--- app/grandchallenge/reader_studies/management/commands/update_rs_image_permissions.py
+++ app/grandchallenge/reader_studies/management/commands/update_rs_image_permissions.py
@@ -5,10 +5,11 @@ from grandchallenge.cases.models import Image
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for c, image in enumerate(
-            Image.objects.filter(
-                componentinterfacevalue__display_sets__isnull=False
-            ).distinct()
-        ):
-            self.stdout.write(c)
+        images = Image.objects.filter(
+            componentinterfacevalue__display_sets__isnull=False
+        ).distinct()
+        for c, image in enumerate(images):
+            if c % 100 == 0:
+                self.stdout.write(f"Updating {c} / {images.count()}")
+
             image.update_viewer_groups_permissions()
