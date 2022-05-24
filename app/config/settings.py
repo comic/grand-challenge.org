f767c4f0bf5153df9ab36669724d850437d0f7d4
amickan <amickan1990@gmail.com>
2022-05-24 16:37:25 +0200

    Add environment variable

--- app/config/settings.py
+++ app/config/settings.py
@@ -1116,7 +1116,9 @@ WORKSTATIONS_RENDERING_SUBDOMAINS = {
 # Number of minutes grace period before the container is stopped
 WORKSTATIONS_GRACE_MINUTES = 5
 # Feature Flag for reader study admin view
-READER_STUDY_VIEW_AS_USER_FEATURE = False
+READER_STUDY_VIEW_AS_USER_FEATURE = os.environ.get(
+    "READER_STUDY_VIEW_AS_USER_FEATURE", "False"
+)
 
 CELERY_BEAT_SCHEDULE = {
     "ping_google": {
