995f446050a63a4c28cab974ca1a16500eea095a
amickan <amickan1990@gmail.com>
2022-05-25 14:03:30 +0200

    Add test for workstation query

--- app/tests/workstations_tests/test_templatetags.py
+++ app/tests/workstations_tests/test_templatetags.py
@@ -4,7 +4,10 @@ from grandchallenge.workstations.templatetags.workstations import (
 from tests.algorithms_tests.factories import AlgorithmJobFactory
 from tests.archives_tests.factories import ArchiveItemFactory
 from tests.factories import ImageFactory, UserFactory, WorkstationConfigFactory
-from tests.reader_studies_tests.factories import ReaderStudyFactory
+from tests.reader_studies_tests.factories import (
+    DisplaySetFactory,
+    ReaderStudyFactory,
+)
 
 
 def test_workstation_query(settings):
@@ -16,6 +19,7 @@ def test_workstation_query(settings):
     config = WorkstationConfigFactory.build()
     archive_item = ArchiveItemFactory.build()
     user = UserFactory.build()
+    display_set = DisplaySetFactory.build(reader_study=reader_study)
 
     qs = workstation_query(image=image)
     assert "&" not in qs
@@ -75,12 +79,44 @@ def test_workstation_query(settings):
     )
     assert f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}={config.pk}" in qs
 
+    qs = workstation_query(display_set=display_set)
+    assert "&" in qs
+    assert (
+        f"{settings.WORKSTATIONS_DISPLAY_SET_QUERY_PARAM}={display_set.pk}"
+        in qs
+    )
+    assert (
+        f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}={reader_study.workstation_config.pk}"
+        in qs
+    )
+    assert f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}={config.pk}" not in qs
+
+    qs = workstation_query(display_set=display_set, config=config)
+    assert "&" in qs
+    assert (
+        f"{settings.WORKSTATIONS_DISPLAY_SET_QUERY_PARAM}={display_set.pk}"
+        in qs
+    )
+    assert (
+        f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}={reader_study.workstation_config.pk}"
+        not in qs
+    )
+    assert f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}={config.pk}" in qs
+
     reader_study.workstation_config = None
 
     qs = workstation_query(reader_study=reader_study)
     assert "&" not in qs
     assert (
         f"{settings.WORKSTATIONS_READY_STUDY_QUERY_PARAM}={reader_study.pk}"
+        in qs
+    )
+    assert f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}" not in qs
+
+    qs = workstation_query(display_set=display_set)
+    assert "&" not in qs
+    assert (
+        f"{settings.WORKSTATIONS_DISPLAY_SET_QUERY_PARAM}={display_set.pk}"
         in qs
     )
     assert f"{settings.WORKSTATIONS_CONFIG_QUERY_PARAM}" not in qs
