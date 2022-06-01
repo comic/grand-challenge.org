30e31f73ebf455af5dff6ce4344ce5a8de3796ec
Anne Mickan <amickan1990@gmail.com>
2022-06-01 17:43:29 +0200

    Add a challenge stats page (#2467)

--- app/grandchallenge/pages/urls.py
+++ app/grandchallenge/pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from grandchallenge.pages.views import (
     ChallengeHome,
+    ChallengeStatistics,
     PageCreate,
     PageDelete,
     PageDetail,
@@ -14,6 +15,7 @@ app_name = "pages"
 urlpatterns = [
     path("pages/", PageList.as_view(), name="list"),
     path("pages/create/", PageCreate.as_view(), name="create"),
+    path("statistics/", ChallengeStatistics.as_view(), name="statistics"),
     path("", ChallengeHome.as_view(), name="home"),
     path("<slug>/", PageDetail.as_view(), name="detail"),
     path("<slug>/update/", PageUpdate.as_view(), name="update"),
