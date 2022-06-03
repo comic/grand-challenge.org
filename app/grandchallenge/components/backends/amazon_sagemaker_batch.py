c8f7e42879d61658e2c5f405309ff5f6ff431c94
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-06-03 18:21:23 +0200

    Create the SageMaker model on provision

--- app/grandchallenge/components/backends/amazon_sagemaker_batch.py
+++ app/grandchallenge/components/backends/amazon_sagemaker_batch.py
@@ -1,6 +1,12 @@
+import logging
+
 import boto3
+import botocore
+from django.conf import settings
 
 from grandchallenge.components.backends.base import Executor
+
+logger = logging.getLogger(__name__)
 
 
 class AmazonSageMakerBatchExecutor(Executor):
@@ -18,19 +24,27 @@ class AmazonSageMakerBatchExecutor(Executor):
     @property
     def _sagemaker_client(self):
         if self.__sagemaker_client is None:
-            self.__sagemaker_client = boto3.client("sagemaker")
+            self.__sagemaker_client = boto3.client(
+                "sagemaker",
+                region_name=settings.COMPONENTS_AMAZON_SAGEMAKER_REGION,
+            )
         return self.__sagemaker_client
 
     @property
     def _logs_client(self):
         if self.__logs_client is None:
-            self.__logs_client = boto3.client("logs")
+            self.__logs_client = boto3.client(
+                "logs", region_name=settings.COMPONENTS_AMAZON_SAGEMAKER_REGION
+            )
         return self.__logs_client
 
     @property
     def _cloudwatch_client(self):
         if self.__cloudwatch_client is None:
-            self.__cloudwatch_client = boto3.client("cloudwatch")
+            self.__cloudwatch_client = boto3.client(
+                "cloudwatch",
+                region_name=settings.COMPONENTS_AMAZON_SAGEMAKER_REGION,
+            )
         return self.__cloudwatch_client
 
     @property
@@ -53,6 +67,38 @@ class AmazonSageMakerBatchExecutor(Executor):
         # TODO
         raise NotImplementedError
 
+    @property
+    def _model_name(self):
+        """
+        The SageMaker model name
+
+        These are quite restrictive, so we cannot use the container image name.
+        They must be max 63 chars and match ^[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?$
+        """
+        model_name = self._exec_image_repo_tag
+
+        # Assuming the registry prefix follows the recommendation of
+        # org-project-env
+        new_prefix = settings.COMPONENTS_REGISTRY_PREFIX.split("-", 1)[1]
+        model_name_replacements = {
+            f"{settings.COMPONENTS_REGISTRY_URL}/": "",
+            f"{settings.COMPONENTS_REGISTRY_PREFIX}/": f"{new_prefix}/",
+            "algorithms/algorithmimage": "A",
+            "evaluation/method": "M",
+            ":": "-",
+            "/": "-",
+            ".": "-",
+        }
+
+        for k, v in model_name_replacements.items():
+            model_name = model_name.replace(k, v)
+
+        return model_name
+
+    def provision(self, *, input_civs, input_prefixes):
+        super().provision(input_civs=input_civs, input_prefixes=input_prefixes)
+        self._create_model()
+
     def execute(self, *, input_civs, input_prefixes):
         # TODO
         raise NotImplementedError
@@ -60,3 +106,30 @@ class AmazonSageMakerBatchExecutor(Executor):
     def handle_event(self, *, event):
         # TODO
         raise NotImplementedError
+
+    def _create_model(self):
+        try:
+            self._sagemaker_client.create_model(
+                ModelName=self._model_name,
+                PrimaryContainer={"Image": self._exec_image_repo_tag},
+                ExecutionRoleArn=settings.COMPONENTS_AMAZON_SAGEMAKER_EXECUTION_ROLE_ARN,
+                EnableNetworkIsolation=False,  # Restricted by VPC
+                VpcConfig={
+                    "SecurityGroupIds": [
+                        settings.COMPONENTS_AMAZON_SAGEMAKER_SECURITY_GROUP_ID,
+                    ],
+                    "Subnets": settings.COMPONENTS_AMAZON_SAGEMAKER_SUBNETS,
+                },
+            )
+        except botocore.exceptions.ClientError as error:
+            if error.response["Error"][
+                "Code"
+            ] == "ValidationException" and error.response["Error"][
+                "Message"
+            ].startswith(
+                "Cannot create already existing model"
+            ):
+                logger.info("SageMaker model already exists")
+                # TODO - ensure that the config is up to date, maybe recreate the model?
+            else:
+                raise
--- app/tests/components_tests/test_amazon_sagemaker_batch_backend.py
+++ app/tests/components_tests/test_amazon_sagemaker_batch_backend.py
@@ -1,6 +1,13 @@
+import re
+from uuid import uuid4
+
+import pytest
+
+from grandchallenge.algorithms.models import AlgorithmImage
 from grandchallenge.components.backends.amazon_sagemaker_batch import (
     AmazonSageMakerBatchExecutor,
 )
+from grandchallenge.evaluation.models import Method
 
 
 def test_instantiation():
@@ -12,3 +19,33 @@ def test_instantiation():
         requires_gpu=False,
     )
     assert executor
+
+
+@pytest.mark.parametrize("model", [Method, AlgorithmImage])
+def test_model_name_generation(settings, model):
+    settings.COMPONENTS_REGISTRY_URL = (
+        "000000000000.dkr.ecr.regionregion.amazonaws.com"
+    )
+    settings.COMPONENTS_REGISTRY_PREFIX = "organisation-proj-env"
+    shimmed_version = "99.99.99"
+    pk = uuid4()
+    image = model(pk=pk, latest_shimmed_version=shimmed_version)
+
+    assert image.shimmed_repo_tag == (
+        "000000000000.dkr.ecr.regionregion.amazonaws.com/"
+        f"organisation-proj-env/{model._meta.app_label}/"
+        f"{model._meta.model_name}:{pk}-shm-{shimmed_version}"
+    )
+
+    executor = AmazonSageMakerBatchExecutor(
+        job_id="algorithms-job-00000000-0000-0000-0000-000000000000",
+        exec_image_repo_tag=image.shimmed_repo_tag,
+        memory_limit=4,
+        time_limit=60,
+        requires_gpu=False,
+    )
+
+    # Model name must be less than 64 chars and match the given regex
+    assert len(executor._model_name) <= 63
+    pattern = re.compile(r"^[a-zA-Z0-9]([\-a-zA-Z0-9]*[a-zA-Z0-9])?$")
+    assert pattern.match(executor._model_name)
