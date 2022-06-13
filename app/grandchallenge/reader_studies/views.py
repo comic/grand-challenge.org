cb42870d99d2672a7d4de11dfe85a63b3e543c1f
Anne Mickan <amickan1990@gmail.com>
2022-06-13 11:23:44 +0200

    Remove reader study feature flags (#2514)
    * Remove reader study feature flags
    
    * Remove feature flag from user progress

--- app/grandchallenge/reader_studies/views.py
+++ app/grandchallenge/reader_studies/views.py
@@ -52,7 +52,6 @@ from rest_framework.settings import api_settings
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 from rest_framework_guardian.filters import ObjectPermissionsFilter
 
-from config import settings
 from grandchallenge.archives.forms import AddCasesForm
 from grandchallenge.cases.forms import UploadRawImagesForm
 from grandchallenge.cases.models import Image, RawImageUploadSession
@@ -412,7 +411,6 @@ class ReaderStudyDisplaySetList(
             {
                 "form_media": media,
                 "reader_study": self.reader_study,
-                "reader_study_display_set_view_feature": settings.READER_STUDY_DISPLAY_SET_VIEW_FEATURE,
             }
         )
         return context
@@ -818,7 +816,6 @@ class UsersProgress(
             {
                 "reader_study": self.object,
                 "users": users,
-                "reader_study_view_as_user_feature": settings.READER_STUDY_VIEW_AS_USER_FEATURE,
             }
         )
 
