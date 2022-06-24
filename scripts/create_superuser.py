5f091ad8e7126323d3055239b391a4a2dbad6738
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-06-24 14:24:32 +0200

    Add some one time tokens for the dev user

--- scripts/create_superuser.py
+++ scripts/create_superuser.py
@@ -1,19 +1,33 @@
 from allauth.account.models import EmailAddress
 from django.contrib.auth import get_user_model
+from django_otp.plugins.otp_static.models import StaticToken
 
 from grandchallenge.verifications.models import Verification
 
 
 def run():
-    get_user_model().objects.filter(username="superuser").delete()
+    username = "superuser"
+
+    get_user_model().objects.filter(username=username).delete()
+
     su = get_user_model().objects.create_superuser(
-        "superuser", "superuser@example.com", "superuser"
+        username=username, email=f"{username}@example.com", password=username
     )
+    print(f"Created superuser with username and password: {username}")
+
     su.user_profile.receive_newsletter = False
     su.user_profile.save()
+
     EmailAddress.objects.filter(email=su.email).delete()
     EmailAddress.objects.create(
         user=su, email=su.email, verified=True, primary=True
     )
     Verification.objects.create(user=su, is_verified=True)
-    print("Created superuser with username and password: superuser")
+
+    su.totpdevice_set.create()
+    static_device = su.staticdevice_set.create(name="backup")
+
+    for _ in range(5):
+        token = StaticToken.random_token()
+        static_device.token_set.create(token=token)
+        print(f"Added one time token: {token}")
