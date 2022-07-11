33c4ab9d0f20f5d7bafb9c2fc6fa99f8d491f3bf
Anne Mickan <amickan1990@gmail.com>
2022-07-11 17:38:19 +0200

    Algorithm for challenge phase form and view (#2560)

--- app/tests/evaluation_tests/test_views.py
+++ app/tests/evaluation_tests/test_views.py
@@ -1,19 +1,33 @@
+import json
 from datetime import timedelta
 
 import factory
 import pytest
 from django.db.models import signals
 from django.utils import timezone
+from factory.django import ImageField
 from guardian.shortcuts import assign_perm, remove_perm
 
+from config import settings
+from grandchallenge.algorithms.models import Algorithm
 from grandchallenge.evaluation.models import Evaluation
+from grandchallenge.evaluation.utils import SubmissionKindChoices
+from grandchallenge.workstations.models import Workstation
+from tests.archives_tests.factories import ArchiveFactory
+from tests.components_tests.factories import ComponentInterfaceFactory
 from tests.evaluation_tests.factories import (
     EvaluationFactory,
     MethodFactory,
     PhaseFactory,
     SubmissionFactory,
 )
-from tests.factories import ChallengeFactory, UserFactory
+from tests.factories import (
+    ChallengeFactory,
+    UserFactory,
+    WorkstationConfigFactory,
+    WorkstationFactory,
+)
+from tests.hanging_protocols_tests.factories import HangingProtocolFactory
 from tests.utils import get_view_for_user
 from tests.verification_tests.factories import VerificationFactory
 
@@ -443,3 +457,274 @@ def test_hidden_phase_visible_for_admins_but_not_participants(client):
             assert (
                 f"<td>{hidden_phase.title}</td>" in response.rendered_content
             )
+
+
+@pytest.mark.django_db
+def test_create_algorithm_for_phase_permission(client):
+    phase = PhaseFactory()
+    admin, participant, user = UserFactory.create_batch(3)
+    phase.challenge.add_admin(admin)
+    phase.challenge.add_participant(participant)
+
+    # admin can make a submission only if they are verified
+    # and if the phase has been configured properly
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=admin,
+    )
+    assert response.status_code == 403
+    assert (
+        "You need to verify your account before you can do this, you can request this from your profile page."
+        in str(response.content)
+    )
+
+    VerificationFactory(user=admin, is_verified=True)
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=admin,
+    )
+    assert response.status_code == 403
+    assert "This phase is not configured for algorithm submission" in str(
+        response.content
+    )
+
+    phase.submission_kind = SubmissionKindChoices.ALGORITHM
+    phase.creator_must_be_verified = True
+    phase.archive = ArchiveFactory()
+    phase.algorithm_inputs.set([ComponentInterfaceFactory()])
+    phase.algorithm_outputs.set([ComponentInterfaceFactory()])
+    phase.save()
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=admin,
+    )
+    assert response.status_code == 200
+
+    # participant can only create algorithm when verified,
+    # when phase is open for submission and
+    # when the phase has been configured properly (already the case here)
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=participant,
+    )
+    assert response.status_code == 403
+    assert (
+        "You need to verify your account before you can do this, you can request this from your profile page."
+        in str(response.content)
+    )
+
+    VerificationFactory(user=participant, is_verified=True)
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=participant,
+    )
+    assert response.status_code == 403
+    assert "The phase is currently not open for submissions." in str(
+        response.content
+    )
+
+    phase.submission_limit = 1
+    phase.save()
+
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=participant,
+    )
+    assert response.status_code == 200
+
+    # normal user cannot create algorithm for phase
+    VerificationFactory(user=user, is_verified=True)
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=user,
+    )
+    assert response.status_code == 403
+    assert (
+        "You need to be either an admin or a participant of the challenge in order to create an algorithm for this phase."
+        in str(response.content)
+    )
+
+
+@pytest.mark.django_db
+def test_create_algorithm_for_phase_presets(client):
+    phase = PhaseFactory(challenge__logo=ImageField(filename="test.jpeg"))
+    admin = UserFactory()
+    phase.challenge.add_admin(admin)
+    VerificationFactory(user=admin, is_verified=True)
+
+    phase.submission_kind = SubmissionKindChoices.ALGORITHM
+    phase.creator_must_be_verified = True
+    phase.archive = ArchiveFactory()
+    ci1 = ComponentInterfaceFactory()
+    ci2 = ComponentInterfaceFactory()
+    phase.algorithm_inputs.set([ci1])
+    phase.algorithm_outputs.set([ci2])
+    phase.hanging_protocol = HangingProtocolFactory()
+    phase.workstation_config = WorkstationConfigFactory()
+    phase.view_content = {"main": [ci1.slug]}
+    phase.save()
+
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        user=admin,
+    )
+    assert response.context_data["form"]["inputs"].initial.get() == ci1
+    assert response.context_data["form"]["outputs"].initial.get() == ci2
+    assert response.context_data["form"][
+        "workstation"
+    ].initial == Workstation.objects.get(
+        slug=settings.DEFAULT_WORKSTATION_SLUG
+    )
+    assert (
+        response.context_data["form"]["hanging_protocol"].initial
+        == phase.hanging_protocol
+    )
+    assert (
+        response.context_data["form"]["workstation_config"].initial
+        == phase.workstation_config
+    )
+    assert (
+        response.context_data["form"]["view_content"].initial
+        == phase.view_content
+    )
+    assert (
+        response.context_data["form"]["contact_email"].initial == admin.email
+    )
+    assert response.context_data["form"]["display_editors"].initial
+    assert (
+        response.context_data["form"]["logo"].initial == phase.challenge.logo
+    )
+    assert list(response.context_data["form"]["modalities"].initial) == []
+    assert list(response.context_data["form"]["structures"].initial) == []
+
+    response = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        method=client.post,
+        user=admin,
+        data={
+            "title": "Test algorithm",
+            "image_requires_memory_gb": 1,
+            "inputs": [
+                response.context_data["form"]["inputs"].initial.get().pk
+            ],
+            "outputs": [
+                response.context_data["form"]["outputs"].initial.get().pk
+            ],
+            "workstation": response.context_data["form"][
+                "workstation"
+            ].initial.pk,
+            "hanging_protocol": response.context_data["form"][
+                "hanging_protocol"
+            ].initial.pk,
+            "workstation_config": response.context_data["form"][
+                "workstation_config"
+            ].initial.pk,
+            "view_content": json.dumps(
+                response.context_data["form"]["view_content"].initial
+            ),
+            "logo": response.context_data["form"]["logo"].initial,
+        },
+    )
+
+    assert response.status_code == 302
+    assert Algorithm.objects.count() == 1
+    algorithm = Algorithm.objects.get()
+    assert algorithm.inputs.get() == ci1
+    assert algorithm.outputs.get() == ci2
+    assert algorithm.hanging_protocol == phase.hanging_protocol
+    assert algorithm.workstation_config == phase.workstation_config
+    assert algorithm.view_content == phase.view_content
+    assert algorithm.workstation.slug == settings.DEFAULT_WORKSTATION_SLUG
+    assert algorithm.contact_email == admin.email
+    assert algorithm.display_editors
+    assert list(algorithm.structures.all()) == []
+    assert list(algorithm.modalities.all()) == []
+    assert algorithm.logo == phase.challenge.logo
+
+    # try to set different values
+    ci3, ci4 = ComponentInterfaceFactory.create_batch(2)
+    hp = HangingProtocolFactory()
+    ws = WorkstationFactory()
+    wsc = WorkstationConfigFactory()
+
+    _ = get_view_for_user(
+        viewname="evaluation:phase-algorithm-create",
+        reverse_kwargs={
+            "slug": phase.slug,
+            "challenge_short_name": phase.challenge.short_name,
+        },
+        client=client,
+        method=client.post,
+        user=admin,
+        data={
+            "title": "Test algorithm",
+            "image_requires_memory_gb": 1,
+            "inputs": [ci3.pk],
+            "outputs": [ci2.pk],
+            "workstation": ws.pk,
+            "hanging_protocol": hp.pk,
+            "workstation_config": wsc.pk,
+            "view_content": "{}",
+        },
+    )
+
+    # created algorithm has the initial values set, not the modified ones
+    alg2 = Algorithm.objects.last()
+    assert alg2.inputs.get() == ci1
+    assert alg2.outputs.get() == ci2
+    assert alg2.hanging_protocol == phase.hanging_protocol
+    assert alg2.workstation_config == phase.workstation_config
+    assert alg2.view_content == phase.view_content
+    assert alg2.workstation.slug == settings.DEFAULT_WORKSTATION_SLUG
+    assert alg2.inputs.get() != ci3
+    assert alg2.outputs.get() != ci4
+    assert alg2.hanging_protocol != hp
+    assert alg2.workstation_config != wsc
+    assert alg2.view_content != "{}"
+    assert alg2.workstation.slug != ws
+    assert alg2.logo == phase.challenge.logo
