b33bebfe22f2a9824742e2bceb5293b59d43470d
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-07-11 10:57:15 +0200

    Add no_proxy environment variable

--- app/grandchallenge/components/backends/amazon_sagemaker_batch.py
+++ app/grandchallenge/components/backends/amazon_sagemaker_batch.py
@@ -468,7 +468,8 @@ class AmazonSageMakerBatchExecutor(Executor):
                     "InstanceCount": 1,
                 },
                 Environment={  # Up to 16 pairs
-                    "LOGLEVEL": "INFO",
+                    "LOG_LEVEL": "INFO",
+                    "no_proxy": f"s3.{settings.AWS_DEFAULT_REGION}.amazonaws.com",
                 },
                 ModelClientConfig={
                     "InvocationsTimeoutInSeconds": self._time_limit,
