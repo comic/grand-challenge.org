bd6f13bf8ddf63ed39ee75f36d6941b27ef23541
HarmvZ <harm.van.zeeland@gmail.com>
2022-07-29 15:36:25 +0200

    Improve errors

--- app/grandchallenge/core/serializer_fields.py
+++ app/grandchallenge/core/serializer_fields.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
@@ -7,5 +9,20 @@ class PkToHyperlinkedRelatedField(serializers.HyperlinkedRelatedField):
     model when writing, but returns the URL of the object when reading.
     """
 
+    default_error_messages = {
+        "required": _("This field is required."),
+        "does_not_exist": _(
+            'Invalid pk "{pk_value}" - object does not exist.'
+        ),
+        "incorrect_type": _(
+            "Incorrect type. Expected pk value, received {data_type}."
+        ),
+    }
+
     def to_internal_value(self, data):
-        return self.get_queryset().get(pk=data)
+        try:
+            return self.get_queryset().get(pk=data)
+        except ObjectDoesNotExist:
+            self.fail("does_not_exist", pk_value=data)
+        except (TypeError, ValueError):
+            self.fail("incorrect_type", data_type=type(data).__name__)
