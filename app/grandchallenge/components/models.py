812ac0166892eca757f4d840553955a47aad2f0f
Chris van Run <chrisvanrun@users.noreply.github.com>
2023-01-09 12:52:56 +0100

    Update app/grandchallenge/components/models.py
    Co-authored-by: miriam-groeneveld <51329283+miriam-groeneveld@users.noreply.github.com>

--- app/grandchallenge/components/models.py
+++ app/grandchallenge/components/models.py
@@ -555,7 +555,7 @@ class InterfaceKind:
                     {
                         "name": "First Ellipse",
                         "major axis": [[10, 10, 0.5], [10, 20, 0]],
-                        "minor_axis: [[10, 20, 0.5], [10.6, 0, 0]],
+                        "minor_axis": [[10, 20, 0.5], [10.6, 0, 0]],
                         "probability": 0.82
                     },
                     {
