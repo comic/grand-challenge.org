74b1877082f70832b006bc36d2dc56de8778058e
Chris van Run <chrisvanrun@users.noreply.github.com>
2023-01-09 12:53:04 +0100

    Update app/grandchallenge/components/models.py
    Co-authored-by: miriam-groeneveld <51329283+miriam-groeneveld@users.noreply.github.com>

--- app/grandchallenge/components/models.py
+++ app/grandchallenge/components/models.py
@@ -569,7 +569,7 @@ class InterfaceKind:
                         "major axis": [[10, 10, 0.5], [10, 20, 0]],
                         "minor_axis: [[10, 20, 0.5], [10.6, 0, 0]],
                         "probability": 0.98
-                    },
+                    }
                 ],
                 "version": {"major": 1, "minor": 0}
             }
