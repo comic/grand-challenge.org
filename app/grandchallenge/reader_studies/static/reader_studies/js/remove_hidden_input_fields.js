09500484b5dfc6ff2cf812963b928dc4614f0768
amickan <amickan1990@gmail.com>
2023-01-16 15:51:49 +0100

    Remove duplicate interface values server side

--- app/grandchallenge/reader_studies/static/reader_studies/js/remove_hidden_input_fields.js
+++
@@ -1,9 +0,0 @@
-form_id = document.getElementById("form-id").dataset.formid
-document.querySelector('#form-'+form_id).addEventListener("submit", function(e){
-    // this removes the hidden interface input fields that flexible-image-widget.html and image-search-widget.html add
-    // to prevent multiple interface values from being submitted with the form
-    var elements = document.getElementsByClassName("current-interface-slug");
-    while (elements.length > 0) {
-        elements[0].remove()
-    }
-});
