7f45f95d563f3dc79ab555872cceeae90f589b0b
Chris van Run <Chris.vanrun@radboudumc.nl>
2023-01-17 17:23:15 +0100

    Selective copy of relevant base code from private

---
+++ app/grandchallenge/workstations/static/workstations/js/session-control.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function openWorkstationSession(element) {
+    return (event) => {
+        const windowIdentifier = element.dataset.workstationWindowIdentifier;
+        const url = element.dataset.createSessionUrl;
+        const query = element.dataset.workstationQuery;
+        const creationURI = `${url}?${query}`;
+
+        if (event.ctrlKey) {
+            window.open(creationURI);
+            return;
+        }
+
+        // PoC Hack: ToDo query any existing session
+        const sessionOrigin = element.dataset.sessionOrigin;
+
+        const workstationWindow = window.open('', windowIdentifier);
+
+        // check if we just opened a blank or existing context
+        let isBlankContext = false;
+        try {
+            isBlankContext = workstationWindow.document.location.href === "about:blank";
+        } catch (err) {
+            // ignore any errors with getting the href as it entails we did not open
+            // the context
+            console.warn(err);
+        }
+
+        if (workstationWindow === null || isBlankContext) {
+            window.open(creationURI, windowIdentifier);
+        } else {
+            workstationWindow.focus();
+            const msg = {
+                sessionControl: {
+                    loadQuery: query
+                }
+            }
+            workstationWindow.postMessage(msg, sessionOrigin);
+        }
+    }
+}
+
+function genSessionControllersHook() {
+     const data = document.currentScript.dataset;
+     const querySelector = (typeof data.sessionControlQuerySelector === 'undefined') ? '[data-session-control]' : data.sessionControlQuerySelector;
+     return () => {
+        const sessionControllerElements = document.querySelectorAll(querySelector);
+        for (let element of sessionControllerElements) {
+            element.onclick = openWorkstationSession(element);
+        }
+    }
+}
+
+let sessionControllersHook;
+if (typeof sessionControllersHook === 'undefined') { // singleton
+    sessionControllersHook = genSessionControllersHook();
+}
+
+$(document).ready(() => {
+    // Run default once
+    sessionControllersHook();
+
+    // Sometimes content insertion is deferred and might result in adding session-control elements later:
+    //  add listeners:
+
+    // ajax-based tables
+    $('#ajaxDataTable').on('draw.dt', () => {sessionControllersHook()});
+
+    // htmx-based tables
+    htmx.onLoad(function() {
+        sessionControllersHook()
+    });
+})
