9cdc7f85ddbf13124dbd2704c3fa23af3bf4a06e
HarmvZ <harm.van.zeeland@gmail.com>
2023-04-12 15:29:34 +0200

    Extend session create urls to support nested paths

--- app/grandchallenge/workstations/urls.py
+++ app/grandchallenge/workstations/urls.py
@@ -30,9 +30,19 @@ urlpatterns = [
         name="workstation-session-create",
     ),
     path(
+        "<slug>/sessions/create/<str:workstation_path>/<uuid:pk>/",
+        SessionCreate.as_view(),
+        name="workstation-session-create",
+    ),
+    path(
         "<slug>/sessions/debug/create/",
         DebugSessionCreate.as_view(),
         name="workstation-debug-session-create",
+    ),
+    path(
+        "<slug>/sessions/debug/create/<str:workstation_path>/<uuid:pk>/",
+        SessionCreate.as_view(),
+        name="workstation-session-create",
     ),
     path(
         "<slug>/editors/update/",
