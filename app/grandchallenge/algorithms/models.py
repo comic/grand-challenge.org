85c00284c3b697b30a0a884cdbeb4de846e1571e
amickan <amickan1990@gmail.com>
2023-05-24 16:21:31 +0200

    Remove active image from algorithm string method

--- app/grandchallenge/algorithms/models.py
+++ app/grandchallenge/algorithms/models.py
@@ -241,7 +241,7 @@ class Algorithm(UUIDModel, TitleSlugDescriptionModel, ViewContentMixin):
         ]
 
     def __str__(self):
-        return f"{self.title} (Active version: {self.active_image})"
+        return f"{self.title}"
 
     def get_absolute_url(self):
         return reverse("algorithms:detail", kwargs={"slug": self.slug})
