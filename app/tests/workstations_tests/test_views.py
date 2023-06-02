comic/grand-challenge.org

commit 99b426b46964a9904814948a6578ac2b16f02df5

Use path parameters in workstation session creation URLs (#2830)
Part of https://github.com/DIAGNijmegen/rse-roadmap/issues/225.

This PR changes the workstation session creation URLs to use path
parameters instead of query parameters.

app/grandchallenge/workstations/urls.py
@@ -30,9 +30,19 @@ urlpatterns = [
         name="workstation-session-create",
     ),
     path(
+        "<slug>/sessions/create/<path:workstation_path>",
+        SessionCreate.as_view(),
+        name="workstation-session-create-nested",
+    ),
+    path(
         "<slug>/sessions/debug/create/",
         DebugSessionCreate.as_view(),
         name="workstation-debug-session-create",
+    ),
+    path(
+        "<slug>/sessions/debug/create/<path:workstation_path>",
+        DebugSessionCreate.as_view(),
+        name="workstation-debug-session-create-nested",
     ),
     path(
         "<slug>/editors/update/",

app/config/settings.py
@@ -1069,14 +1069,13 @@ WORKBENCH_ADMIN_USERNAME = os.environ.get("WORKBENCH_ADMIN_USERNAME", "demo")
 DEFAULT_WORKSTATION_SLUG = os.environ.get(
     "DEFAULT_WORKSTATION_SLUG", "cirrus-core"
 )
-WORKSTATIONS_BASE_IMAGE_QUERY_PARAM = "image"
-WORKSTATIONS_OVERLAY_QUERY_PARAM = "overlay"
-WORKSTATIONS_READY_STUDY_QUERY_PARAM = "readerStudy"
-WORKSTATIONS_ALGORITHM_JOB_QUERY_PARAM = "algorithmJob"
-WORKSTATIONS_ARCHIVE_ITEM_QUERY_PARAM = "archiveItem"
+WORKSTATIONS_BASE_IMAGE_PATH_PARAM = "image"
+WORKSTATIONS_READY_STUDY_PATH_PARAM = "reader-study"
+WORKSTATIONS_ALGORITHM_JOB_PATH_PARAM = "algorithm-job"
+WORKSTATIONS_ARCHIVE_ITEM_PATH_PARAM = "archive-item"
 WORKSTATIONS_CONFIG_QUERY_PARAM = "config"
 WORKSTATIONS_USER_QUERY_PARAM = "viewAsUser"
-WORKSTATIONS_DISPLAY_SET_QUERY_PARAM = "displaySet"
+WORKSTATIONS_DISPLAY_SET_PATH_PARAM = "display-set"
 # The name of the network that the workstations will be attached to
 WORKSTATIONS_NETWORK_NAME = os.environ.get(
     "WORKSTATIONS_NETWORK_NAME", "grand-challengeorg_workstations"

app/grandchallenge/reader_studies/models.py
@@ -48,6 +48,9 @@ from grandchallenge.organizations.models import Organization
 from grandchallenge.publications.models import Publication
 from grandchallenge.reader_studies.metrics import accuracy_score
 from grandchallenge.subdomains.utils import reverse
+from grandchallenge.workstations.templatetags.workstations import (
+    get_workstation_path_and_query_string,
+)
 
 __doc__ = """
 A reader study enables you to have a set of readers answer a set of questions
@@ -148,10 +151,6 @@ The scores can then be compared on the ``leaderboard``. Statistics are also avai
 based on these scores: the average and total scores for each question as well
 as for each case are displayed in the ``statistics`` view.
 """
-
-from grandchallenge.workstations.templatetags.workstations import (
-    get_workstation_query_string,
-)
 
 CASE_TEXT_SCHEMA = {
     "type": "object",
@@ -865,8 +864,8 @@ class DisplaySet(UUIDModel):
             "workstations:workstation-session-create",
             kwargs={"slug": self.reader_study.workstation.slug},
         )
-        query = get_workstation_query_string(display_set=self)
-        return f"{url}?{query}"
+        pqs = get_workstation_path_and_query_string(display_set=self)
+        return f"{url}{pqs.path}?{pqs.query_string}"
 
     @property
     def description(self) -> str:

app/grandchallenge/workstations/static/workstations/js/session-control.mjs
@@ -3,7 +3,8 @@ function openWorkstationSession(element) {
         const windowIdentifier = element.dataset.workstationWindowIdentifier;
         const url = element.dataset.createSessionUrl;
         const query = element.dataset.workstationQuery;
-        const creationURI = `${url}?${query}`;
+        const path = element.dataset.workstationPath;
+        const creationURI = `${url}${path}?${query}`;
         const timeout = element.dataset.timeout || 1000;
 
         if (event.ctrlKey) {
@@ -12,7 +13,7 @@ function openWorkstationSession(element) {
         }
 
         if (event.altKey) {
-            copyTextToClipboard(query);
+            copyTextToClipboard(`${path}?${query}`);
             return;
         }
 
@@ -55,10 +56,10 @@ function openWorkstationSession(element) {
                     // the open() already focuses the window
                     workstationWindow.focus();
                     removeSpinner(element);
-                };
+                }
 
                 potentialSessionOrigins.forEach((origin) => {
-                    sendSessionControlMessage(workstationWindow, origin, {loadQuery: query}, onMessageIsSuccess);
+                    sendSessionControlMessage(workstationWindow, origin, {loadPath: path, loadQuery: query}, onMessageIsSuccess);
                 });
             }
         } catch(err) {

app/grandchallenge/workstations/templatetags/workstations.py
@@ -1,3 +1,5 @@
+from typing import NamedTuple
+
 from django import template
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -8,9 +10,13 @@ from grandchallenge.subdomains.utils import reverse
 register = template.Library()
 
 
-def get_workstation_query_string(  # noqa: C901
+class PathAndQueryString(NamedTuple):
+    path: str
+    query_string: str
+
+
+def get_workstation_path_and_query_string(  # noqa: C901
     image=None,
-    overlay=None,
     reader_study=None,
     algorithm_job=None,
     archive_item=None,
@@ -24,39 +30,23 @@ def get_workstation_query_string(  # noqa: C901
     Supports setting the image with overlay or a reader_study.
     """
     if image:
-        query = {
-            settings.WORKSTATIONS_BASE_IMAGE_QUERY_PARAM: getattr(
-                image, "pk", image
-            )
-        }
-        if overlay:
-            query.update(
-                {
-                    settings.WORKSTATIONS_OVERLAY_QUERY_PARAM: getattr(
-                        overlay, "pk", overlay
-                    )
-                }
-            )
+        path = f"{settings.WORKSTATIONS_BASE_IMAGE_PATH_PARAM}/{getattr(image, 'pk', image)}"
     elif display_set:
-        query = {settings.WORKSTATIONS_DISPLAY_SET_QUERY_PARAM: display_set.pk}
+        path = (
+            f"{settings.WORKSTATIONS_DISPLAY_SET_PATH_PARAM}/{display_set.pk}"
+        )
     elif reader_study:
-        query = {
-            settings.WORKSTATIONS_READY_STUDY_QUERY_PARAM: reader_study.pk
-        }
-        if user:
-            query.update(
-                {settings.WORKSTATIONS_USER_QUERY_PARAM: user.username}
-            )
+        path = (
+            f"{settings.WORKSTATIONS_READY_STUDY_PATH_PARAM}/{reader_study.pk}"
+        )
     elif algorithm_job:
-        query = {
-            settings.WORKSTATIONS_ALGORITHM_JOB_QUERY_PARAM: algorithm_job.pk
-        }
+        path = f"{settings.WORKSTATIONS_ALGORITHM_JOB_PATH_PARAM}/{algorithm_job.pk}"
     elif archive_item:
-        query = {
-            settings.WORKSTATIONS_ARCHIVE_ITEM_QUERY_PARAM: archive_item.pk
-        }
+        path = f"{settings.WORKSTATIONS_ARCHIVE_ITEM_PATH_PARAM}/{archive_item.pk}"
     else:
-        query = {}
+        path = ""
+
+    query = {}
 
     if config:
         # Explicit configs have precedence
@@ -79,8 +69,10 @@ def get_workstation_query_string(  # noqa: C901
                 settings.WORKSTATIONS_CONFIG_QUERY_PARAM: display_set.reader_study.workstation_config.pk
             }
         )
+    if reader_study and user:
+        query.update({settings.WORKSTATIONS_USER_QUERY_PARAM: user.username})
 
-    return urlencode(query)
+    return PathAndQueryString(path, urlencode(query))
 
 
 @register.simple_tag()
@@ -91,12 +83,13 @@ def workstation_session_control_data(
         "workstations:workstation-session-create",
         kwargs={"slug": workstation.slug},
     )
-    workstation_query_string = get_workstation_query_string(**kwargs)
+    pqs = get_workstation_path_and_query_string(**kwargs)
     window_identifier = f"workstation-{context_object._meta.app_label}"
     data_attrs = {
         "data-session-control": True,
         "data-create-session-url": create_session_url,
-        "data-workstation-query": workstation_query_string,
+        "data-workstation-path": pqs.path,
+        "data-workstation-query": pqs.query_string,
         "data-workstation-window-identifier": window_identifier,
         "data-timeout": timeout,
     }

app/tests/reader_studies_tests/test_models.py
@@ -628,7 +628,7 @@ def test_workstation_url():
 
     assert (
         display_set.workstation_url
-        == f"https://testserver/viewers/{workstation.slug}/sessions/create/?displaySet={display_set.pk}"
+        == f"https://testserver/viewers/{workstation.slug}/sessions/create/display-set/{display_set.pk}?"
     )
 
 

app/tests/templates/session_control.html
@@ -10,6 +10,7 @@
     <button class="btn btn-primary"
             data-session-control
             data-create-session-url='{% url 'new-session-test' %}'
+            data-workstation-path='path-param/12345'
             data-workstation-query='queryParam=12345'
             data-workstation-window-identifier='workstation-context'
             data-timeout="500"
@@ -20,6 +21,7 @@
     <button class="btn btn-primary"
             data-session-control
             data-create-session-url='{% url 'new-session-test' %}'
+            data-workstation-path='path-param/12345'
             data-workstation-query='queryParam=12345'
             data-workstation-window-identifier='workstation-context'
             data-timeout="0"

app/tests/urls/root.py
@@ -16,6 +16,11 @@ urlpatterns = [
     path(
         "new-session/", SessionCreationView.as_view(), name="new-session-test"
     ),
+    path(
+        "new-session/path-param/12345/",
+        SessionCreationView.as_view(),
+        name="new-session-test-with-path-param",
+    ),
     path("workstation/", WorkstationView.as_view(), name="workstation-mock"),
     *root_urlpatterns,
 ]

commit 7934c77067fe4f98ce95f4e3cb066e834b0c911f

Add workstation path to session start redirects (#2862)
Propagates path params in redirects when starting a session. Fixes
broken session start from #2830.

app/grandchallenge/workstations/templates/workstations/session_detail.html
@@ -68,11 +68,14 @@
     <script type="text/javascript">
         (function () {
             $('#sessionModal').modal({show: true});
-
+            const params = new URLSearchParams(window.location.search);
+            const path = decodeURIComponent(params.has("path") ? params.get("path") : "");
+            const qs = decodeURIComponent(params.has("qs") ? params.get("qs") : "");
+            const workstationUrl = `{{ object.workstation_url }}/${path}?${qs}`;
             getSessionStatus(
                 "{% url 'api:session-detail' pk=object.pk %}",
                 document.getElementById("sessionState"),
-                `{{ object.workstation_url }}${window.location.search}`,
+                workstationUrl,
             );
         })();
     </script>

app/grandchallenge/workstations/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
         name="workstation-session-create",
     ),
     path(
-        "<slug>/sessions/create/<path:workstation_path>",
+        "<slug>/sessions/create/<path:workstation_path>/",
         SessionCreate.as_view(),
         name="workstation-session-create-nested",
     ),
@@ -40,7 +40,7 @@ urlpatterns = [
         name="workstation-debug-session-create",
     ),
     path(
-        "<slug>/sessions/debug/create/<path:workstation_path>",
+        "<slug>/sessions/debug/create/<path:workstation_path>/",
         DebugSessionCreate.as_view(),
         name="workstation-debug-session-create-nested",
     ),

app/grandchallenge/workstations/views.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from urllib.parse import quote_plus
 
 from django.conf import settings
 from django.contrib.auth.mixins import PermissionRequiredMixin
@@ -301,10 +302,10 @@ class SessionCreate(
         )
 
         url = session.get_absolute_url()
-
+        url += f"?path={quote_plus(self.kwargs.get('workstation_path', ''))}"
         qs = self.request.META.get("QUERY_STRING", "")
         if qs:
-            url = f"{url}?{qs}"
+            url = f"{url}&qs={quote_plus(qs)}"
 
         return HttpResponseRedirect(url)
 

app/tests/utils.py
@@ -66,6 +66,8 @@ def get_http_host(*, url, kwargs):
     if urlparts[1]:
         kwargs.update({"HTTP_HOST": urlparts[1]})
         url = urlparts[2]
+        if urlparts.query:
+            url += "?" + urlparts.query
     return url, kwargs
 
 

app/tests/workstations_tests/test_permissions.py
@@ -178,7 +178,7 @@ def test_workstation_redirect_permissions(
 
         if test[1] == 302 and test[0] is not None:
             session = Session.objects.get(creator=test[0])
-            assert response.url == session.get_absolute_url()
+            assert response.url == session.get_absolute_url() + "?path="
 
 
 @pytest.mark.django_db

app/tests/workstations_tests/test_views.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import quote_plus
 
 import pytest
 from django.conf import settings
@@ -281,6 +282,76 @@ def test_session_create(client):
     assert sessions[0].workstation_image == wsi_new_ready
     assert sessions[0].extra_env_vars == []
     assert sessions[0].creator == user
+    assert response.url == sessions[0].get_absolute_url() + "?path="
+
+
+@pytest.mark.django_db
+def test_session_create_redirect_url(client):
+    user = UserFactory()
+    ws = WorkstationFactory()
+
+    ws.add_user(user=user)
+    WorkstationImageFactory(
+        workstation=ws,
+        is_manifest_valid=True,
+        is_in_registry=True,
+        is_desired_version=True,
+    )
+    assert Session.objects.count() == 0
+
+    test_path = "path/with/special_characters$?="
+    test_qs = "query=string&test"
+
+    response = get_view_for_user(
+        client=client,
+        method=client.post,
+        url=reverse(
+            "workstations:workstation-session-create", kwargs={"slug": ws.slug}
+        ),
+        user=user,
+        data={"region": "eu-central-1"},
+    )
+
+    assert response.status_code == 302
+    sessions = Session.objects.all()
+    assert response.url == sessions[0].get_absolute_url() + "?path="
+
+    response = get_view_for_user(
+        client=client,
+        method=client.post,
+        url=reverse(
+            "workstations:workstation-session-create-nested",
+            kwargs={"slug": ws.slug, "workstation_path": test_path},
+        ),
+        user=user,
+        data={"region": "eu-central-1"},
+    )
+
+    assert response.status_code == 302
+    assert (
+        response.url
+        == sessions[0].get_absolute_url() + f"?path={quote_plus(test_path)}"
+    )
+
+    response = get_view_for_user(
+        client=client,
+        method=client.post,
+        url=reverse(
+            "workstations:workstation-session-create-nested",
+            kwargs={"slug": ws.slug, "workstation_path": test_path},
+        )
+        + "?"
+        + test_qs,
+        user=user,
+        data={"region": "eu-central-1"},
+    )
+
+    assert response.status_code == 302
+    assert (
+        response.url
+        == sessions[0].get_absolute_url()
+        + f"?path={quote_plus(test_path)}&qs={quote_plus(test_qs)}"
+    )
 
 
 @pytest.mark.django_db
@@ -319,7 +390,7 @@ def test_debug_session_create(client):
     assert session.workstation_image == wsi
     assert session.creator == user
     assert session.extra_env_vars == env_vars
-    assert response.url == session.get_absolute_url()
+    assert response.url == session.get_absolute_url() + "?path="
 
 
 @pytest.mark.django_db