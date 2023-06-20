00f7fb52b723c18203d71454be5bfd68a9b35a79
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2023-06-20 14:21:59 +0200

    Replace tokens nonce

---
+++ app/grandchallenge/api_tokens/static/api_tokens/js/display_tokens.mjs
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.table').each(function () {
+        $(this).DataTable({
+            "order": [[1, "desc"]],
+            "paging": false,
+        });
+    });
+});
