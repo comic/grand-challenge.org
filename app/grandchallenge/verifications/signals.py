68ebde50d02328d90a51aff640f8877c88d97398
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2023-07-22 16:06:21 +0200

    Set max_age

--- app/grandchallenge/verifications/signals.py
+++ app/grandchallenge/verifications/signals.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from allauth.account.signals import user_logged_in
 from django.conf import settings
 from django.core.signing import Signer
@@ -7,22 +9,30 @@ from django.dispatch import receiver
 from grandchallenge.verifications.tasks import update_verification_user_set
 
 
+def set_verification_user_set_cookie(*, response, usernames):
+    response.set_cookie(
+        key=settings.VERIFICATIONS_USER_SET_COOKIE_NAME,
+        value=Signer().sign_object(usernames),
+        max_age=timedelta(days=28),
+        secure=settings.SESSION_COOKIE_SECURE,
+        httponly=True,
+        samesite="Strict",
+    )
+
+
 @receiver(user_logged_in)
 def handle_user_logged_in(*, request, user, response, **_):
-    cookie = request.COOKIES.get(settings.VERIFICATIONS_USER_SET_COOKIE_NAME)
-    signer = Signer()
-
-    if cookie is None:
-        response.set_cookie(
-            settings.VERIFICATIONS_USER_SET_COOKIE_NAME,
-            signer.sign_object([user.username]),
+    try:
+        cookie = request.COOKIES[settings.VERIFICATIONS_USER_SET_COOKIE_NAME]
+    except KeyError:
+        set_verification_user_set_cookie(
+            response=response, usernames=[user.username]
         )
     else:
-        usernames = {*signer.unsign_object(cookie)}
+        usernames = {*Signer().unsign_object(cookie)}
         usernames.add(user.username)
-        response.set_cookie(
-            settings.VERIFICATIONS_USER_SET_COOKIE_NAME,
-            signer.sign_object([*usernames]),
+        set_verification_user_set_cookie(
+            response=response, usernames=[*usernames]
         )
 
         if len(usernames) > 1:
