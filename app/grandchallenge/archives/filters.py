d10631cfd1f40941621cc241b1d166080355ffc2
miriam-groeneveld <51329283+miriam-groeneveld@users.noreply.github.com>
2023-07-25 09:43:10 +0200

    Filter on public algorithm/archive/reader study (#2945)

--- app/grandchallenge/archives/filters.py
+++ app/grandchallenge/archives/filters.py
@@ -1,7 +1,16 @@
+from django_filters import BooleanFilter
+
 from grandchallenge.archives.models import Archive
 from grandchallenge.core.filters import TitleDescriptionModalityStructureFilter
 
 
 class ArchiveFilter(TitleDescriptionModalityStructureFilter):
+    public = BooleanFilter(label="Public", field_name="public")
+
     class Meta(TitleDescriptionModalityStructureFilter.Meta):
         model = Archive
+
+        search_fields = (
+            *TitleDescriptionModalityStructureFilter.Meta.search_fields,
+            "public",
+        )
