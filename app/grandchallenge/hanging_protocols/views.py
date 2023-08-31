d113c721398850e39f98cfcb05dca96f99a8d096
HarmvZ <harm.van.zeeland@gmail.com>
2023-08-31 11:19:52 +0200

    Use svg template in list and detail views

--- app/grandchallenge/hanging_protocols/views.py
+++ app/grandchallenge/hanging_protocols/views.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.base import ContextMixin
 from guardian.mixins import LoginRequiredMixin
 
 from grandchallenge.core.guardian import ObjectPermissionRequiredMixin
@@ -9,7 +11,22 @@ from grandchallenge.hanging_protocols.models import HangingProtocol
 from grandchallenge.subdomains.utils import reverse
 
 
-class HangingProtocolList(LoginRequiredMixin, ListView):
+class HangingProtocolSvgIconContextDataMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data.update(
+            {
+                "svg_width_px": settings.HANGING_PROTOCOL_SVG_WIDTH,
+                "svg_height_px": settings.HANGING_PROTOCOL_SVG_HEIGHT,
+                "svg_stroke_width": settings.HANGING_PROTOCOL_SVG_WIDTH * 0.05,
+            }
+        )
+        return data
+
+
+class HangingProtocolList(
+    HangingProtocolSvgIconContextDataMixin, LoginRequiredMixin, ListView
+):
     model = HangingProtocol
     paginate_by = 50
 
@@ -33,7 +50,9 @@ class HangingProtocolCreate(
         return super().form_valid(form)
 
 
-class HangingProtocolDetail(LoginRequiredMixin, DetailView):
+class HangingProtocolDetail(
+    HangingProtocolSvgIconContextDataMixin, LoginRequiredMixin, DetailView
+):
     model = HangingProtocol
 
 
