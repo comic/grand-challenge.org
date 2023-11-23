5633bdddf3ac18b43a26e61fb72bfa138dab1aa1
amickan <amickan1990@gmail.com>
2023-11-23 16:18:09 +0100

    Use obj perms for pdf serving

--- app/grandchallenge/serving/views.py
+++ app/grandchallenge/serving/views.py
@@ -165,9 +165,7 @@ def serve_structured_challenge_submission_form(
     except ChallengeRequest.DoesNotExist:
         raise Http404("Challenge request not found.")
 
-    if request.user.groups.filter(
-        name=settings.CHALLENGES_REVIEWERS_GROUP_NAME
-    ).exists():
+    if request.user.has_perm("view_challengerequest", challenge_request):
         return protected_storage_redirect(
             name=challenge_request.structured_challenge_submission_form.name,
             creator=request.user,
