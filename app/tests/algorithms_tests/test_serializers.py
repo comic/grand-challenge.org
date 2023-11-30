bfd11382436cde87af0e7c96fe06035da57ffb77
amickan <amickan1990@gmail.com>
2023-11-30 14:36:05 +0100

    Add to test for serializer

--- app/tests/algorithms_tests/test_serializers.py
+++ app/tests/algorithms_tests/test_serializers.py
@@ -263,8 +263,24 @@ class TestJobCreateLimits:
             },
             context={"request": request},
         )
-
         assert serializer.is_valid()
+        assert algorithm_image.algorithm.get_jobs_limit(user=user) == 5
+
+        AlgorithmJobFactory.create_batch(
+            5,
+            algorithm_image=algorithm_image,
+            creator=user,
+            status=Job.SUCCESS,
+        )
+        serializer = JobPostSerializer(
+            data={
+                "algorithm": algorithm_image.algorithm.api_url,
+                "inputs": [],
+            },
+            context={"request": request},
+        )
+        assert not serializer.is_valid()
+        assert algorithm_image.algorithm.get_jobs_limit(user=user) == 0
 
     def test_form_valid_with_credits(self, rf):
         algorithm_image = AlgorithmImageFactory(
