01c19415aeacf8fd8b3ec59057210ac9ff057ad8
amickan <amickan1990@gmail.com>
2024-02-02 07:24:54 +0100

    Iterate over items and assign perm to group

--- app/grandchallenge/archives/migrations/0017_archive_item_delete_permission_granting.py
+++ app/grandchallenge/archives/migrations/0017_archive_item_delete_permission_granting.py
@@ -3,23 +3,17 @@ from guardian.shortcuts import assign_perm
 
 
 def add_archive_item_delete_permissions(apps, schema_editor):
-    Archive = apps.get_model("archives", "Archive")  # noqa: N806
     ArchiveItem = apps.get_model("archives", "ArchiveItem")  # noqa: N806
 
-    archives = (
-        Archive.objects.order_by("-created")
-        .prefetch_related("editors_group", "items")
-        .all()
+    items = ArchiveItem.objects.order_by("-created").prefetch_related(
+        "archive__editors_group"
     )
-    for batch in archives.iterator(chunk_size=100):
-        for archive in batch:
-            editors = archive.editors_group.user_set.all()
-            for editor in editors:
-                assign_perm(
-                    f"delete_{ArchiveItem._meta.model_name}",
-                    editor,
-                    archive.items.all(),
-                )
+    for item in items.iterator():
+        assign_perm(
+            f"delete_{ArchiveItem._meta.model_name}",
+            item.archive.editors_group,
+            item,
+        )
 
 
 class Migration(migrations.Migration):
