ceaa26bd174cc4229f0b12fa04677bc6bd06561d
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-02-02 16:43:31 +0100

    Remove outdated test

--- app/tests/codebuild_tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from grandchallenge.codebuild.models import Build
-
-
-def test_clean_build_log():
-    b = Build()
-    b.build_log = "foo\n[Container] bar\nbaz\n"
-    assert b.redacted_build_log == "foo\nbaz"
