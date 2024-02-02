c8ad213ebb2a084b8c09cf7c197b44997e8a9655
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-02-02 09:44:09 +0100

    Create the group permissions directly

--- app/grandchallenge/archives/migrations/0017_archive_item_delete_permission_granting.py
+++ app/grandchallenge/archives/migrations/0017_archive_item_delete_permission_granting.py
@@ -1,18 +1,33 @@
 from django.db import migrations
-from guardian.shortcuts import assign_perm
 
 
 def add_archive_item_delete_permissions(apps, schema_editor):
+    Archive = apps.get_model("archives", "Archive")  # noqa: N806
     ArchiveItem = apps.get_model("archives", "ArchiveItem")  # noqa: N806
-
-    items = ArchiveItem.objects.order_by("-created").prefetch_related(
-        "archive__editors_group"
+    ArchiveItemGroupObjectPermission = apps.get_model(  # noqa: N806
+        "archives", "ArchiveItemGroupObjectPermission"
     )
-    for item in items.iterator():
-        assign_perm(
-            f"delete_{ArchiveItem._meta.model_name}",
-            item.archive.editors_group,
-            item,
+    Permission = apps.get_model("auth", "Permission")  # noqa: N806
+
+    queryset = Archive.objects.order_by("-created").prefetch_related(
+        "editors_group", "items"
+    )
+
+    delete_permission = Permission.objects.get(
+        app=ArchiveItem._meta.app_name,
+        codename=f"delete_{ArchiveItem._meta.model_name}",
+    )
+
+    for archive in queryset.iterator():
+        ArchiveItemGroupObjectPermission.bulk_create(
+            [
+                ArchiveItemGroupObjectPermission(
+                    content_object=item,
+                    group=archive.editors_group,
+                    permission=delete_permission,
+                )
+                for item in archive.items.all()
+            ]
         )
 
 
