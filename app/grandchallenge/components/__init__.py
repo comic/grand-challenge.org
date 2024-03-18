5a08c7e84ef9b8d9add9684c2e76524ff0cdbbb1
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-03-18 14:18:58 +0100

    Move `DockerExecutor` to tests (#3273)
    This PR moves the `DockerExecutor` to the tests app so that it is not
    present in the production containers and renames it to
    `InsecureDockerExecutor`. It also adds a deployment check to make sure
    users are not using this in production, and raises
    `ImproperlyConfigured` if `DEBUG=False` and `pytest` is not being run.
    If `DEBUG=True` we add a critical error message on every initialisation.
    This should prevent users from using it in production.

---
+++ app/grandchallenge/components/__init__.py
@@ -0,0 +1,25 @@
+from django.core.checks import Error, register
+
+
+@register(deploy=True)
+def check_sagemaker_is_used(app_configs, **kwargs):
+    from django.conf import settings
+
+    from grandchallenge.components.backends.amazon_sagemaker_training import (
+        AmazonSageMakerTrainingExecutor,
+    )
+
+    expected_backend = f"{AmazonSageMakerTrainingExecutor.__module__}.{AmazonSageMakerTrainingExecutor.__name__}"
+
+    errors = []
+
+    if settings.COMPONENTS_DEFAULT_BACKEND != expected_backend:
+        errors.append(
+            Error(
+                f"{expected_backend} is not the default backend.",
+                hint=f"Set COMPONENTS_DEFAULT_BACKEND={expected_backend} in your environment.",
+                id="grandchallenge.components.E001",
+            )
+        )
+
+    return errors
