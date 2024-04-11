7056ee71dc7cd40328ae536ab5133a9765cbb228
Anne Mickan <amickan1990@gmail.com>
2024-04-11 12:58:45 +0200

    Fix drf ajax calls (#3301)
    DRF expects an element with id `drf_csrf` that contains the csrf token
    and header. That was removed at some point, breaking the ajax calls for
    updating and deleting notifications and subscriptions.
    
    Closes #3293

--- app/grandchallenge/core/static/js/drf_csrf.js
+++
@@ -1,7 +0,0 @@
-const csrfHeaderName = JSON.parse(document.getElementById('csrfHeaderName').textContent);
-const csrfToken = JSON.parse(document.getElementById('csrfToken').textContent);
-
-window.drf = {
-    csrfHeaderName: csrfHeaderName,
-    csrfToken: csrfToken
-};
---
+++ app/grandchallenge/core/templatetags/csrf.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def csrf_data(csrf_token, csrf_header_name):
+    return {
+        "csrfHeaderName": csrf_header_name,
+        "csrfToken": str(csrf_token),
+    }
