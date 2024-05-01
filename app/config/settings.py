50dfa02ddee2eaa0f4c6ea6edcb685aa49c673f4
amickan <amickan1990@gmail.com>
2024-05-01 15:23:22 +0200

    Remove 2fa urls

--- app/config/settings.py
+++ app/config/settings.py
@@ -644,9 +644,6 @@ LOGIN_URL = "/accounts/login/"
 LOGOUT_URL = "/accounts/logout/"
 LOGIN_REDIRECT_URL = "/users/profile/"
 
-# django-allauth-2fa
-ALLAUTH_2FA_ALWAYS_REVEAL_BACKUP_TOKENS = False
-
 ##############################################################################
 #
 # stdimage
