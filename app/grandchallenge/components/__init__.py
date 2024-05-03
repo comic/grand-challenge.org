0b6e6ecebfce7f1377a36b10c6c69b0bd2b56803
amickan <amickan1990@gmail.com>
2024-05-03 07:31:28 +0200

    Add deployment check that dummy provider is not configured

--- app/grandchallenge/components/__init__.py
+++ app/grandchallenge/components/__init__.py
@@ -23,3 +23,21 @@ def check_sagemaker_is_used(app_configs, **kwargs):
         )
 
     return errors
+
+
+@register(deploy=True)
+def check_dummy_provider_is_not_used(app_configs, **kwargs):
+    from django.conf import settings
+
+    errors = []
+
+    if "dummy" in settings.SOCIALACCOUNT_PROVIDERS.keys():
+        errors.append(
+            Error(
+                "The dummy social account provider is configured. This provider should only be used for testing purposes and not in production.",
+                hint="Remove the dummy provider from SOCIALACCOUNT_PROVIDERS in your settings.",
+                id="grandchallenge.components.E002",
+            )
+        )
+
+    return errors
--- app/tests/components_tests/test_checks.py
+++ app/tests/components_tests/test_checks.py
@@ -1,4 +1,7 @@
-from grandchallenge.components import check_sagemaker_is_used
+from grandchallenge.components import (
+    check_dummy_provider_is_not_used,
+    check_sagemaker_is_used,
+)
 from grandchallenge.components.backends.amazon_sagemaker_training import (
     AmazonSageMakerTrainingExecutor,
 )
@@ -13,3 +16,16 @@ def test_check_sagemaker_is_used(settings):
     assert len(errors) == 1
     assert errors[0].id == "grandchallenge.components.E001"
     assert errors[0].msg == f"{expected_backend} is not the default backend."
+
+
+def test_check_dummy_provider_is_not_used(settings):
+    settings.SOCIALACCOUNT_PROVIDERS = {"dummy": {}}
+
+    errors = check_dummy_provider_is_not_used(None)
+
+    assert len(errors) == 1
+    assert errors[0].id == "grandchallenge.components.E002"
+    assert (
+        errors[0].msg
+        == "The dummy social account provider is configured. This provider should only be used for testing purposes and not in production."
+    )
