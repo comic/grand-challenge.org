af12a728329b8099fe44412dc878750ddab9d01a
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-05-06 17:17:36 +0200

    Add test for archive item title integrity

--- app/tests/archives_tests/test_models.py
+++ app/tests/archives_tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.db import IntegrityError
 
 from tests.archives_tests.factories import ArchiveFactory, ArchiveItemFactory
 from tests.components_tests.factories import ComponentInterfaceValueFactory
@@ -15,3 +16,40 @@ def create_archive_items_for_images(images, archive):
         civ = ComponentInterfaceValueFactory(image=image)
         ai = ArchiveItemFactory(archive=archive)
         ai.values.add(civ)
+
+
+@pytest.mark.django_db(transaction=True)
+def test_archive_item_set_title():
+    archive = ArchiveFactory()
+    ai0 = ArchiveItemFactory(archive=archive)
+
+    # Default
+    assert ai0.title == ""
+
+    # Update
+    ai0.title = "An archive item title"
+    ai0.save()
+
+    # Sanity
+    ai1 = ArchiveItemFactory(
+        archive=ai0.archive,
+        title="Another archive item title",
+    )
+
+    # Duplication attempt via edit
+    ai1.title = ai0.title
+    with pytest.raises(IntegrityError):
+        ai1.save()
+
+    # Duplication attempt via save
+    with pytest.raises(IntegrityError):
+        ArchiveItemFactory(
+            archive=archive,
+            title=ai1.title,
+        )
+
+    # Other archive no problem
+    ArchiveItemFactory(
+        archive=ArchiveFactory(),
+        title=ai0.title,
+    )
