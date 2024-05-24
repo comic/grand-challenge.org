32ee500b21b428a031169fdbeb97d09eaf01193b
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-05-24 14:28:59 +0200

    Add exclude_from_ranking for additional score extraction from metrics

--- app/tests/evaluation_tests/test_utils.py
+++ app/tests/evaluation_tests/test_utils.py
@@ -137,6 +137,96 @@ def test_calculate_ranks(django_assert_max_num_queries):
 
 
 @pytest.mark.django_db
+def test_calculate_ranks_with_exclusion(django_assert_max_num_queries):
+    phase = PhaseFactory()
+
+    results = [
+        # Warning: Do not change this values without updating the
+        # expected_ranks below.
+        {"a": 0.0, "b": 0.0},
+        {"a": 0.5, "b": 0.2},
+        {"a": 1.0, "b": 0.3},
+        {"a": 0.7, "b": 0.4},
+        {"a": 0.5, "b": 0.5},
+        {"b": 0.3},  # Incomplete and should not be processed
+    ]
+
+    queryset = [
+        EvaluationFactory(submission__phase=phase, status=Evaluation.SUCCESS)
+        for _ in range(len(results))
+    ]
+
+    for e, r in zip(queryset, results, strict=True):
+        e.outputs.add(
+            ComponentInterfaceValue.objects.create(
+                interface=ComponentInterface.objects.get(
+                    slug="metrics-json-file"
+                ),
+                value=r,
+            )
+        )
+
+    b_included = False
+    b_excluded = True
+
+    only_a = {
+        "ranks": [5, 3, 1, 2, 3, 0],
+        "rank_scores": [5, 3, 1, 2, 3, 0],
+    }
+    expected = {
+        Phase.ABSOLUTE: {
+            b_included: only_a,
+            b_excluded: only_a,
+        },
+        Phase.MEDIAN: {
+            b_included: {
+                "ranks": [5, 4, 1, 1, 1, 0],
+                "rank_scores": [5, 3.5, 2, 2, 2, 0],
+            },
+            b_excluded: only_a,
+        },
+        Phase.MEAN: {
+            b_included: {
+                "ranks": [5, 4, 1, 1, 1, 0],
+                "rank_scores": [5, 3.5, 2, 2, 2, 0],
+            },
+            b_excluded: only_a,
+        },
+    }
+
+    for score_method in (Phase.ABSOLUTE, Phase.MEDIAN, Phase.MEAN):
+        for b_exclusion in (True, False, None):
+            phase.score_jsonpath = "a"
+            phase.scoring_method_choice = score_method
+            phase.score_default_sort = Phase.DESCENDING
+            phase.extra_results_columns = [
+                {
+                    "path": "b",
+                    "title": "b",
+                    "order": Phase.DESCENDING,
+                }
+            ]
+
+            if b_exclusion is not None:
+                phase.extra_results_columns[0][
+                    "exclude_from_ranking"
+                ] = b_exclusion
+            else:
+                b_exclusion = False  # for the expected lookup
+
+            phase.save()
+
+            with django_assert_max_num_queries(10):
+                assert calculate_ranks(phase_pk=phase.pk) is None
+
+            assert_ranks(
+                queryset,
+                expected[score_method][b_exclusion]["ranks"],
+                expected[score_method][b_exclusion]["rank_scores"],
+            )
+
+
+@pytest.mark.django_db
 def test_results_display():
     phase = PhaseFactory()
 
