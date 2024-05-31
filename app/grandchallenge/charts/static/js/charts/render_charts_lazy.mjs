a8b3421a10674f7eb18e3274f0ccc256acbab596
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-05-31 10:47:07 +0200

    Add lazy loading for vega charts

---
+++ app/grandchallenge/charts/static/js/charts/render_charts_lazy.mjs
@@ -0,0 +1,44 @@
+// This module will lazily embed vega-lite charts
+// Tag the containers with the class 'vega-lite-chart-lazy'
+
+function renderVegaLiteChart(element) {
+    const spec = JSON.parse(element.children[0].textContent);
+    vegaEmbed(element, spec);
+}
+
+const observer = new IntersectionObserver(handleInterSection, {
+  rootMargin: "0px",
+  threshold: 1.0,
+});
+
+const handledAttribute = "data-vega-chart-is-rendered";
+
+
+function handleInterSection(entries) {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio < 0.40) {
+            return;
+        }
+
+        const element = entry.target;
+
+
+        if (element.getAttribute(handledAttribute)) {
+            return;
+        }
+
+        renderVegaLiteChart(element);
+
+        // Tag the element as being handled
+        element.setAttribute(handledAttribute, "");
+        this.unobserve(element);
+        }
+    )
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    for (const element of document.getElementsByClassName("vega-lite-chart-lazy")) {
+        observer.observe(element, );
+    }
+});
