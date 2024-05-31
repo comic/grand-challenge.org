comic/grand-challenge.org

commit 43ba70e7af2a9ba8b8a042887b8592c2416f865e

Archive Item Titles (#3345)
Part of the pitch:
-
https://github.com/DIAGNijmegen/rse-roadmap/issues/313#event-12465816350

This is a copy of what was done for Display Items. I've opted to
abstract the CIV set creation and update form data processing into
`CIVSetCreateViewMixin` and `CIVSetCreaViewMixin` respectively as to
stay DRY.

app/grandchallenge/archives/models.py
@@ -1,10 +1,9 @@
-import os
-
 from actstream.models import Follow
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models import Q
 from django_extensions.db.models import TitleSlugDescriptionModel
 from guardian.models import GroupObjectPermissionBase, UserObjectPermissionBase
 from guardian.shortcuts import assign_perm, remove_perm
@@ -14,6 +13,7 @@ from grandchallenge.algorithms.models import Algorithm
 from grandchallenge.anatomy.models import BodyStructure
 from grandchallenge.components.models import (
     CIVForObjectMixin,
+    CIVSetStringRepresentationMixin,
     ComponentInterfaceValue,
     ValuesForInterfacesMixin,
 )
@@ -266,6 +266,9 @@ class Archive(
     def civ_set_model(self):
         return ArchiveItem
 
+    def create_civ_set(self, data):
+        return self.civ_set_model.objects.create(archive=self, **data)
+
     @property
     def create_civ_set_url(self):
         return reverse("archives:item-create", kwargs={"slug": self.slug})
@@ -283,22 +286,16 @@ class ArchiveGroupObjectPermission(GroupObjectPermissionBase):
     content_object = models.ForeignKey(Archive, on_delete=models.CASCADE)
 
 
-class ArchiveItem(CIVForObjectMixin, UUIDModel):
+class ArchiveItem(
+    CIVSetStringRepresentationMixin, CIVForObjectMixin, UUIDModel
+):
     archive = models.ForeignKey(
         Archive, related_name="items", on_delete=models.PROTECT
     )
     values = models.ManyToManyField(
         ComponentInterfaceValue, blank=True, related_name="archive_items"
     )
-
-    def __str__(self):
-        values = []
-        for value in self.values.all():
-            if value.image:
-                values.append(value.image.name)
-            if value.file:
-                values.append(os.path.basename(value.file.file.name))
-        return ", ".join(values)
+    title = models.CharField(max_length=255, default="", blank=True)
 
     def save(self, *args, **kwargs):
         adding = self._state.adding
@@ -307,6 +304,15 @@ class ArchiveItem(CIVForObjectMixin, UUIDModel):
 
         if adding:
             self.assign_permissions()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "archive"],
+                name="unique_archive_item_title",
+                condition=~Q(title=""),
+            )
+        ]
 
     def assign_permissions(self):
         # Archive editors, uploaders and users can view this archive item
@@ -338,11 +344,6 @@ class ArchiveItem(CIVForObjectMixin, UUIDModel):
     @property
     def base_object(self):
         return self.archive
-
-    @property
-    def is_editable(self):
-        # always editable
-        return True
 
     @property
     def update_url(self):

app/tests/archives_tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.db import IntegrityError, transaction
 
 from tests.archives_tests.factories import ArchiveFactory, ArchiveItemFactory
 from tests.components_tests.factories import ComponentInterfaceValueFactory
@@ -15,3 +16,90 @@ def create_archive_items_for_images(images, archive):
         civ = ComponentInterfaceValueFactory(image=image)
         ai = ArchiveItemFactory(archive=archive)
         ai.values.add(civ)
+
+
+@pytest.fixture(scope="function")
+def archive_item_with_title(db):
+    archive = ArchiveFactory()
+    ai = ArchiveItemFactory(archive=archive)
+
+    # Default
+    assert ai.title == ""
+
+    # Update
+    ai.title = "An Archive Item Title"
+    ai.save()
+
+    return ai
+
+
+@pytest.mark.django_db
+def test_archive_item_duplicate_title_edit(archive_item_with_title):
+    # Sanity
+    ai = ArchiveItemFactory(
+        archive=archive_item_with_title.archive,
+        title="Another Archive Item",
+    )
+
+    ai.title = archive_item_with_title.title
+    with pytest.raises(IntegrityError):
+        ai.save()
+
+
+@pytest.mark.django_db
+def test_archive_item_duplicate_title_create(archive_item_with_title):
+    with pytest.raises(IntegrityError):
+        ArchiveItemFactory(
+            archive=archive_item_with_title.archive,
+            title=archive_item_with_title.title,
+        )
+
+
+@pytest.mark.django_db
+def test_archive_item_duplicate_title_other_archive(
+    archive_item_with_title,
+):
+    # Another archive is not a problem
+    ArchiveItemFactory(
+        archive=ArchiveFactory(),
+        title=archive_item_with_title.title,
+    )
+
+
+@pytest.mark.django_db
+def test_archive_item_set_title():
+    archive = ArchiveFactory()
+    ai0 = ArchiveItemFactory(archive=archive)
+
+    # Default
+    assert ai0.title == ""
+
+    # Update
+    ai0.title = "An archive item title"
+    ai0.save()
+
+    # Sanity
+    ai1 = ArchiveItemFactory(
+        archive=ai0.archive,
+        title="Another archive item title",
+    )
+
+    # Duplication attempt via edit
+    ai1.title = ai0.title
+    with transaction.atomic():
+        with pytest.raises(IntegrityError):
+            ai1.save()
+
+    # Duplication attempt via save
+    with transaction.atomic():
+        with pytest.raises(IntegrityError):
+            ArchiveItemFactory(
+                archive=archive,
+                title=ai1.title,
+            )
+
+    # Other archive no problem
+    ArchiveItemFactory(
+        archive=ArchiveFactory(),
+        title=ai0.title,
+    )

app/grandchallenge/components/models.py
@@ -1962,6 +1962,30 @@ class ComponentImage(FieldChangeMixin, models.Model):
             self.size_in_registry = self.calculate_size_in_registry()
 
 
+class CIVSetStringRepresentationMixin:
+    def __str__(self):
+        result = [str(self.pk)]
+
+        if self.title:
+            result.append(f"{self.title!r}")
+
+        result.append(self.__content_str)
+        return ", ".join(result)
+
+    @property
+    def __content_str(self):
+        civs = self.values.all()
+        nr = len(civs)
+        if nr == 0:
+            return "No content"
+
+        if nr > 5:
+            return "5+ items"
+
+        content = [f"{civ.interface.title}: {civ.title[:30]}" for civ in civs]
+        return ", ".join(content)
+
+
 class CIVForObjectMixin:
     def create_civ(self, *, ci_slug, new_value, user=None):
         ci = ComponentInterface.objects.get(slug=ci_slug)

app/grandchallenge/reader_studies/models.py
@@ -26,6 +26,7 @@ from stdimage import JPEGField
 from grandchallenge.anatomy.models import BodyStructure
 from grandchallenge.components.models import (
     CIVForObjectMixin,
+    CIVSetStringRepresentationMixin,
     ComponentInterface,
     ComponentInterfaceValue,
     InterfaceKind,
@@ -735,6 +736,9 @@ class ReaderStudy(
     def civ_set_model(self):
         return DisplaySet
 
+    def create_civ_set(self, data):
+        return self.civ_set_model.objects.create(reader_study=self, **data)
+
     @property
     def create_civ_set_url(self):
         return reverse(
@@ -787,7 +791,9 @@ def delete_reader_study_groups_hook(*_, instance: ReaderStudy, using, **__):
         pass
 
 
-class DisplaySet(CIVForObjectMixin, UUIDModel):
+class DisplaySet(
+    CIVSetStringRepresentationMixin, CIVForObjectMixin, UUIDModel
+):
     reader_study = models.ForeignKey(
         ReaderStudy, related_name="display_sets", on_delete=models.PROTECT
     )
@@ -796,13 +802,6 @@ class DisplaySet(CIVForObjectMixin, UUIDModel):
     )
     order = models.PositiveIntegerField(default=0)
     title = models.CharField(max_length=255, default="", blank=True)
-
-    def __str__(self):
-        parts = [self._meta.verbose_name.title()]
-        if self.title:
-            parts.append(f"{self.title!r}")
-        parts.append(f"(ID: {self.id})")
-        return " ".join(parts)
 
     def save(self, *args, **kwargs):
         adding = self._state.adding
@@ -838,7 +837,7 @@ class DisplaySet(CIVForObjectMixin, UUIDModel):
         constraints = [
             models.UniqueConstraint(
                 fields=["title", "reader_study"],
-                name="unique_title",
+                name="unique_display_set_title",
                 condition=~Q(title=""),
             )
         ]

app/tests/reader_studies_tests/test_models.py
@@ -677,40 +677,51 @@ def test_display_set_description():
         assert ds.description == result[ds.pk]
 
 
-@pytest.mark.django_db(transaction=True)
-def test_display_set_title():
+@pytest.fixture(scope="function")
+def display_set_with_title(db):
     rs = ReaderStudyFactory()
-    ds0 = DisplaySetFactory(reader_study=rs)
+    ds = DisplaySetFactory(reader_study=rs)
 
     # Default
-    assert ds0.title == ""
+    assert ds.title == ""
 
     # Update
-    ds0.title = "A Display Set Title"
-    ds0.save()
+    ds.title = "A Display Set Title"
+    ds.save()
 
+    return ds
+
+
+@pytest.mark.django_db
+def test_display_set_duplicate_title_edit(display_set_with_title):
     # Sanity
-    ds1 = DisplaySetFactory(
-        reader_study=ds0.reader_study,
+    ds = DisplaySetFactory(
+        reader_study=display_set_with_title.reader_study,
         title="Another Display Set",
     )
 
-    # Duplication attempt via edit
-    ds1.title = ds0.title
+    ds.title = display_set_with_title.title
     with pytest.raises(IntegrityError):
-        ds1.save()
+        ds.save()
 
-    # Duplication attempt via save
+
+@pytest.mark.django_db
+def test_display_set_duplicate_title_create(display_set_with_title):
     with pytest.raises(IntegrityError):
         DisplaySetFactory(
-            reader_study=rs,
-            title=ds0.title,
+            reader_study=display_set_with_title.reader_study,
+            title=display_set_with_title.title,
         )
 
-    # Other reader study is no problem
+
+@pytest.mark.django_db
+def test_display_set_duplicate_title_other_reader_study(
+    display_set_with_title,
+):
+    # Another reader study is not problem
     DisplaySetFactory(
         reader_study=ReaderStudyFactory(),
-        title=ds0.title,
+        title=display_set_with_title.title,
     )
 
 

app/grandchallenge/archives/forms.py
@@ -17,10 +17,17 @@ from grandchallenge.archives.models import (
     ArchivePermissionRequest,
 )
 from grandchallenge.cases.forms import UploadRawImagesForm
+from grandchallenge.components.forms import (
+    CIVSetCreateFormMixin,
+    CIVSetUpdateFormMixin,
+    MultipleCIVForm,
+)
 from grandchallenge.components.models import ComponentInterface, InterfaceKind
 from grandchallenge.core.forms import (
     PermissionRequestUpdateForm,
     SaveFormInitMixin,
+    UniqueTitleCreateFormMixin,
+    UniqueTitleUpdateFormMixin,
     WorkstationUserFilterMixin,
 )
 from grandchallenge.core.guardian import get_objects_for_user
@@ -225,3 +232,37 @@ class AddCasesForm(UploadRawImagesForm):
             {"interface_pk": self.cleaned_data["interface"].pk}
         )
         return super().save(*args, **kwargs)
+
+
+class ArchiveItemFormMixin:
+    class Meta:
+        non_interface_fields = ("title",)
+
+    @property
+    def model(self):
+        return self.base_obj.civ_set_model
+
+    def unique_title_query(self, *args, **kwargs):
+        return (
+            super()
+            .unique_title_query(*args, **kwargs)
+            .filter(archive=self.base_obj)
+        )
+
+
+class ArchiveItemCreateForm(
+    ArchiveItemFormMixin,
+    UniqueTitleCreateFormMixin,
+    CIVSetCreateFormMixin,
+    MultipleCIVForm,
+):
+    pass
+
+
+class ArchiveItemUpdateForm(
+    ArchiveItemFormMixin,
+    UniqueTitleUpdateFormMixin,
+    CIVSetUpdateFormMixin,
+    MultipleCIVForm,
+):
+    pass

app/grandchallenge/archives/migrations/0018_archiveitem_title_and_more.py
@@ -1,0 +1,25 @@
+# Generated by Django 4.2.11 on 2024-05-06 13:29
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("archives", "0017_archive_item_delete_permission_granting"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="archiveitem",
+            name="title",
+            field=models.CharField(blank=True, default="", max_length=255),
+        ),
+        migrations.AddConstraint(
+            model_name="archiveitem",
+            constraint=models.UniqueConstraint(
+                condition=models.Q(("title", ""), _negated=True),
+                fields=("title", "archive"),
+                name="unique_archive_item_title",
+            ),
+        ),
+    ]

app/grandchallenge/archives/serializers.py
@@ -34,6 +34,7 @@ class ArchiveItemSerializer(serializers.ModelSerializer):
         model = ArchiveItem
         fields = (
             "pk",
+            "title",
             "archive",
             "values",
             "hanging_protocol",

commit e5212aa4a46acbb8cb7f7345e7071d11f9dd0a10

Make archive items editable again (#3361)
Fixes bug introduced in
https://github.com/comic/grand-challenge.org/pull/3345 which made
archive items uneditable.

app/grandchallenge/archives/models.py
@@ -359,6 +359,10 @@ class ArchiveItem(
             kwargs={"slug": self.base_object.slug, "pk": self.pk},
         )
 
+    @property
+    def is_editable(self):
+        return True
+
 
 class ArchiveItemUserObjectPermission(UserObjectPermissionBase):
     content_object = models.ForeignKey(ArchiveItem, on_delete=models.CASCADE)

app/tests/archives_tests/test_models.py
@@ -103,3 +103,9 @@ def test_archive_item_set_title():
         archive=ArchiveFactory(),
         title=ai0.title,
     )
+
+
+@pytest.mark.django_db
+def test_archive_item_editable():
+    ai = ArchiveItemFactory()
+    assert ai.is_editable