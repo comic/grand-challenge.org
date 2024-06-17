c0dadc5b9df6711cb320b61d22fa451970a4ceaa
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-06-17 20:15:07 +0200

    Only allow pre-selected hashes Fix tab selection when pressing back

--- app/grandchallenge/core/static/js/refresh_sidebar.mjs
+++ app/grandchallenge/core/static/js/refresh_sidebar.mjs
@@ -1,30 +1,51 @@
-if (window.location.hash === "" || window.location.hash === '#information') {
-  $('#information').addClass('active');
-  $('#v-pills-information-tab').addClass('active');
-}
+$(document).ready(function() {
+    const tabSelectors = $('#v-pills-tab a');
 
-$('#v-pills-tab a').click(function () {
-  const tab = $(this);
-  tab.siblings().removeClass('active');
-  tab.tab('show');
+    const allowedHashes = new Set(
+        tabSelectors.map(function() {
+            let href = this.getAttribute('href');
+            return href.startsWith('#') ? href.substring(1) : null;
+        }).get().filter(hash => hash !== null)
+    );
+
+    function isValidHash(hash) {
+        return allowedHashes.has(hash);
+    }
+
+    tabSelectors.click(function () {
+        const tab = $(this);
+        tab.siblings().removeClass('active');
+        tab.tab('show');
+    });
+
+    $("ul.nav-pills > a").on("shown.bs.tab", (e) => {
+        const hash = $(e.target).attr("href").substring(1);
+        if (isValidHash(hash)) {
+            history.pushState(null, null, `#${hash}`);
+        }
+    });
+
+    function activateLocation() {
+        const hash = window.location.hash.substring(1);
+        if (isValidHash(hash)) {
+            const tab = $(`#v-pills-tab a[href="#${hash}"]`);
+            tab.siblings().removeClass('active');
+            tab.tab('show');
+        } else {
+            // Fallback to a default tab if the hash is not valid
+            const defaultTab = $('#v-pills-tab a[href="#information"]');
+            defaultTab.siblings().removeClass('active');
+            defaultTab.tab('show');
+        }
+    }
+
+    window.addEventListener('popstate', function (event) {
+        activateLocation();
+    });
+
+    window.addEventListener('pageshow', function(event) {
+        activateLocation();
+    });
+
+    activateLocation();
 });
-
-// store the currently selected tab in the hash value
-$("ul.nav-pills > a").on("shown.bs.tab", (e) => {
-  const href = $(e.target).attr("href");
-  const newUrl = new URL(href, window.location);
-  history.pushState(null, null, newUrl.hash);
-});
-
-function activateLocation() {
-  const hash = encodeURIComponent(window.location.hash.substring(1));
-  const tab = $(`#v-pills-tab a[href="#${hash}"]`);
-  tab.siblings().removeClass('active');
-  tab.tab('show');
-}
-
-window.addEventListener('popstate', function (event) {
-  activateLocation();
-});
-
-activateLocation();
