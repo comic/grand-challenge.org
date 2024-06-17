b485c76130b1a063a731c0d38784b78507b6598d
Chris van Run <chrisvanrun@users.noreply.github.com>
2024-06-17 21:02:31 +0200

    Fix sometimes having an partial URL as hash (#3386)
    Closes: #3385 
    
    The `href` attribute was never parsed as a URL and incorrectly assumed
    to always be a hash-only string.
    
    ---------
    
    Co-authored-by: James Meakin <12661555+jmsmkn@users.noreply.github.com>

--- app/grandchallenge/core/static/js/refresh_sidebar.mjs
+++ app/grandchallenge/core/static/js/refresh_sidebar.mjs
@@ -1,29 +1,46 @@
-if (window.location.hash === "" || window.location.hash === '#information') {
-  $('#information').addClass('active');
-  $('#v-pills-information-tab').addClass('active');
-}
+$(document).ready(function() {
+    const tabSelectors = $('#v-pills-tab a');
 
-$('#v-pills-tab a').click(function () {
-  const tab = $(this);
-  tab.siblings().removeClass('active');
-  tab.tab('show');
+    const allowedHashes = new Set(
+        tabSelectors.map(function() {
+            let href = this.getAttribute('href');
+            return href.startsWith('#') ? href : null;
+        }).get().filter(hash => hash !== null)
+    );
+
+    function isValidHash(hash) {
+        return allowedHashes.has(hash);
+    }
+
+    function activateLocation() {
+        const hash = window.location.hash;
+        if (isValidHash(hash)) {
+            const tab = $(`#v-pills-tab a[href="${hash}"]`);
+            tab.siblings().removeClass('active');
+            tab.tab('show');
+        } else {
+            // Fallback to a default tab if the hash is not valid
+            const defaultTab = $('#v-pills-tab a[href="#information"]');
+            defaultTab.siblings().removeClass('active');
+            defaultTab.tab('show');
+        }
+    }
+
+    tabSelectors.click(function () {
+        const hash = $(this).attr("href");
+        if (isValidHash(hash)) {
+            history.pushState(null, null, hash);
+            activateLocation();
+        }
+    });
+
+    window.addEventListener('popstate', function (event) {
+        activateLocation();
+    });
+
+    window.addEventListener('pageshow', function(event) {
+        activateLocation();
+    });
+
+    activateLocation();
 });
-
-// store the currently selected tab in the hash value
-$("ul.nav-pills > a").on("shown.bs.tab", (e) => {
-  const newUrl = $(e.target).attr("href").substring(1);
-  history.pushState(null, null, `#${newUrl}`);
-});
-
-function activateLocation() {
-  const hash = encodeURIComponent(window.location.hash.substring(1));
-  const tab = $(`#v-pills-tab a[href="#${hash}"]`);
-  tab.siblings().removeClass('active');
-  tab.tab('show');
-}
-
-window.addEventListener('popstate', function(event) {
-  activateLocation();
-});
-
-activateLocation();
