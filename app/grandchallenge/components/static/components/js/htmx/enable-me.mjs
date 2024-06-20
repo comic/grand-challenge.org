31041b3d1340662c3b985f9f97a6413d59940a31
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-06-20 16:21:34 +0200

    Make HX indicators robust against validation failures

--- app/grandchallenge/components/static/components/js/htmx/enable-me.mjs
+++ app/grandchallenge/components/static/components/js/htmx/enable-me.mjs
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let elements = document.querySelectorAll('[hx-enable-me]');
+function enableElements() {
+    const elements = document.querySelectorAll('[hx-enable-me]');
     elements.forEach(function (element) {
         element.removeAttribute('disabled');
+        element.removeAttribute('hx-enable-me');
     });
-});
+}
+
+document.addEventListener('htmx:load', enableElements);
+
+enableElements();
