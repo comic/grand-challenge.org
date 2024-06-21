02e92e54faf193adb59c35476bbdb0a8bee70d25
Chris van Run <chrisvanrun@users.noreply.github.com>
2024-06-21 10:49:26 +0200

    Make HX indicators robust against validation failures (#3389)
    When adding a title to a display set / archive item that already exists,
    the validation error causes a partial update of the DOM, messing the the
    states of the 'Save' button.
    
    This then shows the permanent state of 'Loading...':
    
    ![image](https://github.com/comic/grand-challenge.org/assets/7871310/8e9d1de6-cd3d-4505-8bcd-76046322b347)
    
    The indicators handlers now:
    - Listen  (also) to: [htmx:load](https://htmx.org/events/#htmx:load)
    - Cleanup after themselves to make them robust for multiple subsequent
    calls

--- app/grandchallenge/components/static/components/js/htmx/display-me.mjs
+++ app/grandchallenge/components/static/components/js/htmx/display-me.mjs
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let elements = document.querySelectorAll('[hx-display-me]');
-    elements.forEach(function(element) {
+function displayElements() {
+    const elements = document.querySelectorAll('[hx-display-me]');
+    elements.forEach(function (element) {
         element.classList.remove('d-none');
+        element.removeAttribute('hx-display-me');
     });
-})
+}
+
+document.addEventListener('htmx:load', displayElements);
+document.addEventListener('DOMContentLoaded', displayElements);
--- app/grandchallenge/components/static/components/js/htmx/hide-me.mjs
+++ app/grandchallenge/components/static/components/js/htmx/hide-me.mjs
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let elements = document.querySelectorAll('[hx-hide-me]');
-    elements.forEach(function(element) {
+function hideElements() {
+    const elements = document.querySelectorAll('[hx-hide-me]');
+    elements.forEach(function (element) {
         element.classList.add('d-none');
+        element.removeAttribute('hx-hide-me');
     });
-});
+}
+
+document.addEventListener('htmx:load', hideElements);
+document.addEventListener('DOMContentLoaded', hideElements);
