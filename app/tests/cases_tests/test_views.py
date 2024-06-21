d4d7d1b533b3af66bbbb88ca1be294b8bd79e90b
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-06-21 12:18:31 +0200

    Remove unused test

--- app/tests/cases_tests/test_views.py
+++ app/tests/cases_tests/test_views.py
@@ -1,16 +1,8 @@
 import pytest
 from guardian.shortcuts import assign_perm, remove_perm
 
-from grandchallenge.cases.models import Image
 from grandchallenge.components.models import ComponentInterface
-from tests.cases_tests.factories import (
-    ImageFactoryWithImageFile,
-    ImageFactoryWithImageFile2DGray16Bit,
-    ImageFactoryWithImageFile3D,
-    ImageFactoryWithImageFile16Bit,
-    ImageFactoryWithoutImageFile,
-    RawImageUploadSessionFactory,
-)
+from tests.cases_tests.factories import RawImageUploadSessionFactory
 from tests.components_tests.factories import ComponentInterfaceFactory
 from tests.factories import ImageFactory, UserFactory
 from tests.utils import get_view_for_user
@@ -136,63 +128,6 @@ def test_imageviewset_empty_fields_filtering(
         response = get_view_for_user(**view_kwargs, data=data)
         assert response.status_code == 200
         assert len(response.data["results"]) == 0
-
-
-@pytest.mark.django_db
-class TestCSImageDetail:
-    def get_status_code(self, client, viewname, image):
-        u = UserFactory()
-        assign_perm("view_image", u, image)
-        response = get_view_for_user(
-            client=client,
-            viewname=f"cases:{viewname}-image-detail",
-            reverse_kwargs={"pk": image.pk},
-            user=u,
-        )
-        return response.status_code
-
-    @pytest.mark.parametrize(
-        "viewname,factory,kwargs",
-        [
-            (
-                "cs",
-                ImageFactoryWithoutImageFile,
-                {"color_space": Image.COLOR_SPACE_GRAY},
-            ),
-            (
-                "cs3d",
-                ImageFactoryWithoutImageFile,
-                {"color_space": Image.COLOR_SPACE_GRAY},
-            ),
-            (
-                "cs",
-                ImageFactoryWithImageFile,
-                {"color_space": Image.COLOR_SPACE_YCBCR},
-            ),
-        ],
-    )
-    def test_not_allowed(self, client, viewname, factory, kwargs):
-        assert self.get_status_code(client, viewname, factory(**kwargs)) == 404
-
-    @pytest.mark.parametrize("viewname", ["cs", "cs3d"])
-    @pytest.mark.parametrize(
-        "factory,kwargs",
-        [
-            (ImageFactoryWithImageFile2DGray16Bit, {}),
-            (
-                ImageFactoryWithImageFile,
-                {"color_space": Image.COLOR_SPACE_RGB},
-            ),
-            (
-                ImageFactoryWithImageFile,
-                {"color_space": Image.COLOR_SPACE_RGBA},
-            ),
-            (ImageFactoryWithImageFile16Bit, {}),
-            (ImageFactoryWithImageFile3D, {}),
-        ],
-    )
-    def test_allowed(self, client, viewname, factory, kwargs):
-        assert self.get_status_code(client, viewname, factory(**kwargs)) == 200
 
 
 @pytest.mark.django_db
