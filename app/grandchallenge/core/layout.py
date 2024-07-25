66a0d71c7f8191cc34948c4ee3cb3accda4d8a3c
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-07-25 08:51:52 +0200

    Address PR feedback for CategoricalOptions form (#3453)
    Quick implementation to address the PR feedback before I go. Not
    extensively tested but see what you think! This:
    
    - only adds an extra field on create
    - when not fully editable: disables the add button and removes the
    delete checkboxes

--- app/grandchallenge/core/layout.py
+++ app/grandchallenge/core/layout.py
@@ -5,8 +5,16 @@ from django.template.loader import render_to_string
 class Formset(LayoutObject):
     template = "layout/formset.html"
 
-    def __init__(self, formset):
-        self.formset = formset
+    def __init__(self, *args, formset, can_add_another, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._formset = formset
+        self._can_add_another = can_add_another
 
     def render(self, *args, **kwargs):
-        return render_to_string(self.template, {"formset": self.formset})
+        return render_to_string(
+            self.template,
+            {
+                "formset": self._formset,
+                "can_add_another": self._can_add_another,
+            },
+        )
