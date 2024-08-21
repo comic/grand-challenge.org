e570849e7fda530f462cfb4c991027d4fbf56e9e
Ammar Ammar <43293485+ammar257ammar@users.noreply.github.com>
2024-08-21 15:31:41 +0000

    Update markdown with html rendering test

--- app/tests/core_tests/test_markdown.py
+++ app/tests/core_tests/test_markdown.py
@@ -1,61 +1,190 @@
+import textwrap
+
 import pytest
 from django.conf import settings
 from markdown import markdown
 
 from grandchallenge.core.utils.markdown import BS4Treeprocessor
 
-TEST_MARKDOWN = """
-![](whatever.png)
 
-> Quote Me
+@pytest.mark.parametrize(
+    "markdown_with_html, expected_output",
+    (
+        (
+            textwrap.dedent(
+                """
+                ![](test.png)
 
-Markdown | Less | Pretty
---- | --- | ---
-*Still* | `renders` | **nicely**
-1 | 2 | 3
+                > Quote Me
 
-```python
- def test_function():
-    pass
-```
-"""
+                Markdown | Less | Pretty
+                --- | --- | ---
+                *Still* | `renders` | **nicely**
+                1 | 2 | 3
 
-EXPECTED_HTML = """<p><img alt="" class="img-fluid" src="whatever.png" /></p>
-<blockquote class="blockquote">
-<p>Quote Me</p>
-</blockquote>
-<table class="table table-hover table-borderless">
-<thead class="thead-light">
-<tr>
-<th>Markdown</th>
-<th>Less</th>
-<th>Pretty</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td><em>Still</em></td>
-<td><code class="codehilite">renders</code></td>
-<td><strong>nicely</strong></td>
-</tr>
-<tr>
-<td>1</td>
-<td>2</td>
-<td>3</td>
-</tr>
-</tbody>
-</table>
-<div class="codehilite"><pre><span></span><code> <span class="k">def</span> <span class="nf">test_function</span><span class="p">():</span>
-    <span class="k">pass</span>
-</code></pre></div>"""
+                ```python
+                def test_function():
+                    pass
+                ```"""
+            ),
+            textwrap.dedent(
+                """\
+                <p><img alt="" class="img-fluid" src="test.png" /></p>
+                <blockquote class="blockquote">
+                <p>Quote Me</p>
+                </blockquote>
+                <table class="table table-hover table-borderless">
+                <thead class="thead-light">
+                <tr>
+                <th>Markdown</th>
+                <th>Less</th>
+                <th>Pretty</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                <td><em>Still</em></td>
+                <td><code class="codehilite">renders</code></td>
+                <td><strong>nicely</strong></td>
+                </tr>
+                <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                </tr>
+                </tbody>
+                </table>
+                <div class="codehilite"><pre><span></span><code class="codehilite"><span class="k">def</span> <span class="nf">test_function</span><span class="p">():</span>
+                    <span class="k">pass</span>
+                </code></pre></div>"""
+            ),
+        ),
+        (
+            textwrap.dedent(
+                """
+                ![](test.png)
 
+                <img src="test-no-class.png"/>
 
-def test_markdown_rendering():
+                <img class="" src="test-empty-class.png"/>
+
+                <img class="ml-3"src="test-existing-class.png"/>
+
+                > Quote Me
+
+                <blockquote>
+                <p>Quote Me No Class</p>
+                </blockquote>
+                <blockquote class="ml-3">
+                <p>Quote Me Existing Class</p>
+                </blockquote>
+
+                Markdown | Less | Pretty
+                --- | --- | ---
+                *Still* | `renders` | **nicely**
+                1 | 2 | 3
+
+                <table>
+                <thead>
+                <tr>
+                <th>no class</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
+
+                <table class="ml-3">
+                <thead class="ml-3">
+                <tr>
+                <th>existing class</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
+
+                ```python
+                def test_function():
+                    pass
+                ```
+                <div><pre><code>no class</code></pre></div>
+                <div class="ml-3"><pre><code class="ml-3">existing class</code></pre></div>"""
+            ),
+            textwrap.dedent(
+                """\
+                <p><img alt="" class="img-fluid" src="test.png" /></p>
+                <p><img class="img-fluid" src="test-no-class.png"/></p>
+                <p><img class="img-fluid" src="test-empty-class.png"/></p>
+                <p><img class="ml-3 img-fluid" src="test-existing-class.png"/></p>
+                <blockquote class="blockquote">
+                <p>Quote Me</p>
+                </blockquote>
+                <blockquote class="blockquote">
+                <p>Quote Me No Class</p>
+                </blockquote>
+                <blockquote class="ml-3 blockquote">
+                <p>Quote Me Existing Class</p>
+                </blockquote>
+
+                <table class="table table-hover table-borderless">
+                <thead class="thead-light">
+                <tr>
+                <th>Markdown</th>
+                <th>Less</th>
+                <th>Pretty</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                <td><em>Still</em></td>
+                <td><code class="codehilite">renders</code></td>
+                <td><strong>nicely</strong></td>
+                </tr>
+                <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                </tr>
+                </tbody>
+                </table>
+                <table class="table table-hover table-borderless">
+                <thead class="thead-light">
+                <tr>
+                <th>no class</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
+
+                <table class="ml-3 table table-hover table-borderless">
+                <thead class="ml-3 thead-light">
+                <tr>
+                <th>existing class</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
+
+                <div class="codehilite"><pre><span></span><code class="codehilite"><span class="k">def</span> <span class="nf">test_function</span><span class="p">():</span>
+                    <span class="k">pass</span>
+                </code></pre></div>
+
+                <div><pre><code class="codehilite">no class</code></pre></div>
+                <div class="ml-3"><pre><code class="ml-3 codehilite">existing class</code></pre></div>"""
+            ),
+        ),
+    ),
+)
+def test_markdown_rendering(markdown_with_html, expected_output):
     output = markdown(
-        TEST_MARKDOWN, extensions=settings.MARKDOWNX_MARKDOWN_EXTENSIONS
+        text=markdown_with_html or "",
+        extensions=settings.MARKDOWNX_MARKDOWN_EXTENSIONS,
+        extension_configs=settings.MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS,
     )
-
-    assert output == EXPECTED_HTML
+    assert output == expected_output
 
 
 @pytest.mark.parametrize(
