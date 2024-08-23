93a3343cf4145316b3d6c206c6388a83e2d6b0da
Ammar Ammar <43293485+ammar257ammar@users.noreply.github.com>
2024-08-23 12:04:40 +0000

    Test accepting algorithm permission requests upon verification

--- app/tests/verification_tests/test_signals.py
+++ app/tests/verification_tests/test_signals.py
@@ -1,7 +1,10 @@
 import pytest
 
+from grandchallenge.algorithms.models import AlgorithmPermissionRequest
 from grandchallenge.verifications.models import VerificationUserSet
+from tests.algorithms_tests.factories import AlgorithmPermissionRequestFactory
 from tests.factories import UserFactory
+from tests.verification_tests.factories import VerificationFactory
 
 
 @pytest.mark.django_db
@@ -90,3 +93,18 @@ def test_auto_deactivate_reverse_set(
     u.refresh_from_db()
 
     assert u.is_active is not auto_deactivate
+
+
+@pytest.mark.django_db
+def test_auto_accept_permission_on_verification():
+    u = UserFactory()
+    alpr = AlgorithmPermissionRequestFactory(user=u)
+
+    assert alpr.status == AlgorithmPermissionRequest.PENDING
+
+    v = VerificationFactory(user=u, email_is_verified=True, is_verified=False)
+    v.is_verified = True
+    v.save()
+    alpr.refresh_from_db()
+
+    assert alpr.status == AlgorithmPermissionRequest.ACCEPTED
