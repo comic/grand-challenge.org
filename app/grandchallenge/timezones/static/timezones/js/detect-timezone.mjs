1bb0939b14f68bafdaa88b88faf14c87c3cfb8a0
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-08-28 15:23:09 +0200

    Support many columns in paginated tables
    See https://github.com/DIAGNijmegen/rse-grand-challenge-admin/issues/327

--- app/grandchallenge/timezones/static/timezones/js/detect-timezone.mjs
+++ app/grandchallenge/timezones/static/timezones/js/detect-timezone.mjs
@@ -1,27 +1,11 @@
+import { getCookie } from "../../js/get_cookie.mjs";
+
 const setTimezoneUrl = JSON.parse(
     document.getElementById("setTimezoneUrl").textContent,
 );
 const currentTimezone = JSON.parse(
     document.getElementById("currentTimezone").textContent,
 );
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-        const cookies = document.cookie.split(";");
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === `${name}=`) {
-                cookieValue = decodeURIComponent(
-                    cookie.substring(name.length + 1),
-                );
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
 async function updateTimezone() {
     const csrftoken = getCookie("_csrftoken");
