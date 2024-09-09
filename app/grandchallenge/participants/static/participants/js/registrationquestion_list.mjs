ef8639c3cab9a9c40913b4b2742fe89726c996b7
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-09-09 11:43:37 +0200

    Do not sort Edit and Remove columns

--- app/grandchallenge/participants/static/participants/js/registrationquestion_list.mjs
+++ app/grandchallenge/participants/static/participants/js/registrationquestion_list.mjs
@@ -2,5 +2,12 @@ $(document).ready(() => {
     $("#registrationQuestionsTable").DataTable({
         order: [[0, "asc"]],
         pageLength: 10,
+        columnDefs: [
+            {
+                targets: [3, 4],
+                searchable: false,
+                orderable: false,
+            },
+        ],
     });
 });
