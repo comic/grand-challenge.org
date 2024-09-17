189adcdda89af97d1bf4e31136ca22731c1e4f70
Chris van Run <chrisvanrun@users.noreply.github.com>
2024-09-17 11:22:28 +0200

    Phase parenting warnings (#3543)
    Closes: #3537

--- app/grandchallenge/evaluation/models.py
+++ app/grandchallenge/evaluation/models.py
@@ -559,7 +559,7 @@ class Phase(FieldChangeMixin, HangingProtocolMixin, UUIDModel):
         help_text=(
             "Is this phase dependent on another phase? If selected, submissions "
             "to the current phase will only be possible after a successful "
-            "submission has been made to the parent phase."
+            "submission has been made to the parent phase. "
             "<b>Bear in mind that if you require a successful submission to a "
             "sanity check phase in order to submit to a final test phase, "
             "it could prevent people from submitting to the test phase on deadline "
