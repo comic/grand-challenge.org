27cab7ef4d65b10390f9df150d3b3eb08b3383dd
Thomas Koopman <t.koopman2@uva.nl>
2024-10-14 10:44:55 +0200

    Pre-commit fixes
    Pre-commit hook was not installed yet, so had to run it manually.

--- app/grandchallenge/core/static/js/markdownx_full_page.js
+++ app/grandchallenge/core/static/js/markdownx_full_page.js
@@ -1,15 +1,16 @@
-$('document').ready(function(){
+$("document").ready(() => {
     // Sync the height of the preview element to the height of the editor element.
-    let ELEMENTS = document.getElementsByClassName('markdownx');
-    Object.values(ELEMENTS).map(function (element) {
-        let editor = element.querySelector('.markdownx-editor'), preview = element.querySelector('.markdownx-preview');
-        preview.style.height = editor.clientHeight + "px";
-        const resizeObserver = new ResizeObserver((entries) => {
-          for (const entry of entries) {
-            if (entry.contentBoxSize) {
-              preview.style.height = editor.clientHeight + "px";
+    const ELEMENTS = document.getElementsByClassName("markdownx");
+    Object.values(ELEMENTS).map(element => {
+        const editor = element.querySelector(".markdownx-editor");
+        const preview = element.querySelector(".markdownx-preview");
+        preview.style.height = `${editor.clientHeight}px`;
+        const resizeObserver = new ResizeObserver(entries => {
+            for (const entry of entries) {
+                if (entry.contentBoxSize) {
+                    preview.style.height = `${editor.clientHeight}px`;
+                }
             }
-          }
         });
         resizeObserver.observe(editor);
     });
