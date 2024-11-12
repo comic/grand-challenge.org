fd436da30d46a29dfabae04b0380ec2acd4437dc
Ammar Ammar <43293485+ammar257ammar@users.noreply.github.com>
2024-11-12 19:05:40 +0000

    test generate_view_content_example function

---
+++ app/tests/components_tests/test_utils.py
@@ -0,0 +1,90 @@
+import json
+
+import pytest
+
+from grandchallenge.components.models import (
+    ComponentInterface,
+    InterfaceKindChoices,
+)
+from grandchallenge.components.utils import generate_view_content_example
+from tests.components_tests.factories import ComponentInterfaceFactory
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "number_of_images, number_of_overlays, expected_example",
+    (
+        (
+            0,
+            0,
+            None,
+        ),
+        (
+            1,
+            0,
+            {"main": ["test-ci-image-0"]},
+        ),
+        (
+            0,
+            1,
+            None,
+        ),
+        (
+            5,
+            5,
+            {
+                "main": ["test-ci-image-0", "test-ci-overlay-0"],
+                "secondary": ["test-ci-image-1", "test-ci-overlay-1"],
+                "tertiary": ["test-ci-image-2", "test-ci-overlay-2"],
+                "quaternary": ["test-ci-image-3", "test-ci-overlay-3"],
+                "quinary": ["test-ci-image-4", "test-ci-overlay-4"],
+            },
+        ),
+        (
+            6,
+            3,
+            {
+                "main": ["test-ci-image-0", "test-ci-overlay-0"],
+                "secondary": ["test-ci-image-1", "test-ci-overlay-1"],
+                "tertiary": ["test-ci-image-2", "test-ci-overlay-2"],
+                "quaternary": ["test-ci-image-3"],
+                "quinary": ["test-ci-image-4"],
+                "senary": ["test-ci-image-5"],
+            },
+        ),
+        (
+            3,
+            6,
+            {
+                "main": ["test-ci-image-0", "test-ci-overlay-0"],
+                "secondary": ["test-ci-image-1", "test-ci-overlay-1"],
+                "tertiary": ["test-ci-image-2", "test-ci-overlay-2"],
+            },
+        ),
+    ),
+)
+def test_generate_view_content_example(
+    number_of_images, number_of_overlays, expected_example
+):
+
+    for i in range(number_of_images):
+        ComponentInterfaceFactory(
+            kind=InterfaceKindChoices.IMAGE, title=f"test-ci-image-{i}"
+        )
+
+    for i in range(number_of_overlays):
+        ComponentInterfaceFactory(
+            kind=InterfaceKindChoices.SEGMENTATION,
+            title=f"test-ci-overlay-{i}",
+        )
+
+    interfaces = ComponentInterface.objects.filter(
+        title__startswith="test-ci-"
+    ).all()
+
+    view_content_example = generate_view_content_example(interfaces)
+    view_content_example_json = (
+        json.loads(view_content_example) if view_content_example else None
+    )
+
+    assert view_content_example_json == expected_example
