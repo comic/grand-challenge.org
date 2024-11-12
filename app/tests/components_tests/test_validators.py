c7f8b9c7aa9bd24a372c0ff9f3c487d3445c07d9
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-11-12 15:45:47 +0100

    Add (non passing) tests for newick validation

--- app/tests/components_tests/test_validators.py
+++ app/tests/components_tests/test_validators.py
@@ -1,7 +1,12 @@
+from contextlib import nullcontext
+
 import pytest
 from django.core.exceptions import ValidationError
 
-from grandchallenge.components.validators import validate_safe_path
+from grandchallenge.components.validators import (
+    validate_newick,
+    validate_safe_path,
+)
 
 
 @pytest.mark.parametrize(
@@ -18,3 +23,57 @@ def test_invalid_paths(rel_path):
 )
 def test_valid_paths(rel_path):
     validate_safe_path(rel_path)
+
+
+@pytest.mark.parametrize(
+    "tree, context",
+    (
+        (
+            "();",  # Empty tree
+            nullcontext(),
+        ),
+        (
+            "()",  # Empty tree
+            nullcontext(),
+        ),
+        (
+            "((A,B),C);",
+            nullcontext(),
+        ),
+        (
+            "((A,B),C);((D,E),F);",  # Multiple trees are OK.
+            nullcontext(),
+        ),
+        (
+            "((,),);",
+            nullcontext(),
+        ),
+        (
+            "",  # Empty
+            pytest.raises(ValidationError),
+        ),
+        (
+            "((A,B),C;",  # Unbalanced Paranthesis
+            pytest.raises(ValidationError),
+        ),
+        (
+            "((A@B),C;",  # Unexpected characters
+            pytest.raises(ValidationError),
+        ),
+        (
+            "(:0.5,A:0.3);",  # Branch lengths without a node
+            pytest.raises(ValidationError),
+        ),
+        (
+            "(A,B,);",  # Missing Node
+            pytest.raises(ValidationError),
+        ),
+        (
+            "(A,A);",  # Duplicate  labels
+            pytest.raises(ValidationError),
+        ),
+    ),
+)
+def test_validate_newick(tree, context):
+    with context:
+        validate_newick(tree=tree)
