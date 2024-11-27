c0e919b5ac29c73af913138c3571c940a19adbc4
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-11-27 10:29:17 +0100

    Rogue debug print

--- app/grandchallenge/components/validate_scripts/validate_biom.py
+++ app/grandchallenge/components/validate_scripts/validate_biom.py
@@ -29,7 +29,6 @@ def _get_file_path():
     if len(sys.argv) == 2:
         biom_file_path = Path(sys.argv[1])
         if not biom_file_path.is_file():
-            print(biom_file_path, biom_file_path.exists(), file=sys.stderr)
             raise RuntimeError(
                 f"Provided BIOM file path is not an existing file: {biom_file_path}"
             )
