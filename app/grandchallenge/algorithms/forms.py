f90c1371f4b474de2ba3a6dd7da5bead0a2c2bd4
Thomas Koopman <koopman.t.pro@gmail.com>
2024-11-27 13:38:29 +0100

    Use array aggregation which is slightly faster

--- app/grandchallenge/algorithms/forms.py
+++ app/grandchallenge/algorithms/forms.py
@@ -15,6 +15,7 @@ from crispy_forms.layout import (
 from dal import autocomplete
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.postgres.aggregates import ArrayAgg
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.files.base import ContentFile
 from django.core.validators import (
@@ -418,11 +419,11 @@ class AlgorithmForm(
         choices = [GPUTypeChoices.NO_GPU, GPUTypeChoices.T4]
         choices.extend(
             chain.from_iterable(
-                self.relevant_phases.order_by()
-                .values_list(
-                    "algorithm_selectable_gpu_type_choices", flat=True
-                )
-                .distinct()
+                self.relevant_phases.aggregate(
+                    choices=ArrayAgg(
+                        "algorithm_selectable_gpu_type_choices", distinct=True
+                    )
+                )["choices"]
             )
         )
         choices_set = set(choices)
