0ac085ffbbd42a7d89431a6ad076ab46d530f27d
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-12-04 12:41:09 +0100

    Remove responsive components of evaluations datatable

--- app/grandchallenge/evaluation/static/js/evaluation/evaluations_table.js
+++ app/grandchallenge/evaluation/static/js/evaluation/evaluations_table.js
@@ -1,18 +1,5 @@
-$(document).ready(() => {
+document.addEventListener("DOMContentLoaded", () => {
     $("#evaluationsTable").DataTable({
         order: [[2, "desc"]],
-        responsive: {
-            details: {
-                renderer: (api, rowIdx, columns) => {
-                    const data = $.map(columns, (col, i) =>
-                        col.hidden
-                            ? `<tr data-dt-row="${col.rowIndex}" data-dt-column="${col.columnIndex}"><td class="font-weight-bold">${col.title}:</td> <td>${col.data}</td></tr>`
-                            : "",
-                    ).join("");
-
-                    return data ? $("<table/>").append(data) : false;
-                },
-            },
-        },
     });
 });
