2426e422f6ea91f32e01757b2901f46c2a64b9f0
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-12-05 13:29:07 +0100

    Remove columns adjusting for async loaded content

--- app/grandchallenge/core/static/js/datatables.defaults.mjs
+++ app/grandchallenge/core/static/js/datatables.defaults.mjs
@@ -2,10 +2,7 @@ $.extend(true, DataTable.defaults, {
     scrollX: true,
 });
 
-$(document).on("init.dt", event => {
-    // This is a work-around to get the table to resize properly on extra-large Bootstrap viewport
-    setTimeout($(event.target).DataTable().columns.adjust, 1000);
-
+$(document).on("init.dt", () => {
     // Set up floating scroll, note that the target class only shows up when scrollX is set to true
     $(".dataTables_scrollBody").floatingScroll();
 });
