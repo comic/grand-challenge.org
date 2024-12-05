b34bdfda50e5ff97741403615795defda256f368
Chris van Run <chrisvanrun@users.noreply.github.com>
2024-12-05 12:59:51 +0100

    Update app/grandchallenge/core/static/js/datatables.defaults.mjs
    Co-authored-by: James Meakin <12661555+jmsmkn@users.noreply.github.com>

--- app/grandchallenge/core/static/js/datatables.defaults.mjs
+++ app/grandchallenge/core/static/js/datatables.defaults.mjs
@@ -6,6 +6,6 @@ $(document).on("init.dt", event => {
     // This is a work-around to get the table to resize properly on extra-large Bootstrap viewport
     setTimeout($(event.target).DataTable().columns.adjust, 1000);
 
-    // // Set up floating scroll, note that the target class only shows up when scrollX is set to true
+    // Set up floating scroll, note that the target class only shows up when scrollX is set to true
     $(".dataTables_scrollBody").floatingScroll();
 });
