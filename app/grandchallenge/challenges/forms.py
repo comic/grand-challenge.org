5f5d81fd90d081117537bb055671c0d86ffad999
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2024-12-13 13:57:26 +0100

    Removes oninput for challenge request form
    The old functionality was to drop invalid values, this does not
    seem user friendly. Now, we use the default behavioud of
    the browser forcing the user to fill in a valid value
    when they try to submit the form instead.
    
    Closes #3748

--- app/grandchallenge/challenges/forms.py
+++ app/grandchallenge/challenges/forms.py
@@ -193,33 +193,6 @@ class ChallengeRequestForm(forms.ModelForm):
         widgets = {
             "start_date": forms.TextInput(attrs={"type": "date"}),
             "end_date": forms.TextInput(attrs={"type": "date"}),
-            "expected_number_of_teams": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "number_of_tasks": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "average_size_of_test_image_in_mb": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "inference_time_limit_in_minutes": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "phase_1_number_of_submissions_per_team": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "phase_2_number_of_submissions_per_team": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "phase_1_number_of_test_images": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "phase_2_number_of_test_images": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
-            "budget_for_hosting_challenge": forms.NumberInput(
-                attrs={"oninput": "validity.valid||(value='');"}
-            ),
         }
         labels = {
             "short_name": "Acronym",
