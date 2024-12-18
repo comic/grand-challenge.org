b4e624cdd2a62f231c562287474c54a1c582a1dc
Thomas Koopman <koopman.t.pro@gmail.com>
2024-12-18 19:49:54 +0100

    Use fullpage markdown widget for doc pages

--- app/grandchallenge/documentation/urls.py
+++ app/grandchallenge/documentation/urls.py
@@ -1,19 +1,30 @@
 from django.urls import path
 
 from grandchallenge.documentation.views import (
+    DocPageContentUpdate,
     DocPageCreate,
     DocPageDetail,
     DocPageList,
-    DocPageUpdate,
+    DocPageMetadataUpdate,
     DocumentationHome,
 )
 
 app_name = "documentation"
+
 
 urlpatterns = [
     path("", DocumentationHome.as_view(), name="home"),
     path("overview/", DocPageList.as_view(), name="list"),
     path("create/", DocPageCreate.as_view(), name="create"),
     path("<slug:slug>/", DocPageDetail.as_view(), name="detail"),
-    path("<slug:slug>/update/", DocPageUpdate.as_view(), name="update"),
+    path(
+        "<slug:slug>/content-update/",
+        DocPageContentUpdate.as_view(),
+        name="content-update",
+    ),
+    path(
+        "<slug:slug>/metadata-update/",
+        DocPageMetadataUpdate.as_view(),
+        name="metadata-update",
+    ),
 ]
