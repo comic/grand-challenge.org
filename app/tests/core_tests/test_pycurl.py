2f878ec4ab359d01e076a1d946aefc182e931eab
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-01-28 09:43:28 +0100

    Update pycurl

--- app/tests/core_tests/test_pycurl.py
+++ app/tests/core_tests/test_pycurl.py
@@ -1,4 +1,8 @@
 def test_import_pycurl():
+    # pycurl is used by celery[sqs] and requires
+    # an environment variable to be correctly set
+    # at install time, see #3252
     import pycurl
 
+    assert pycurl.version == ""
     assert pycurl
