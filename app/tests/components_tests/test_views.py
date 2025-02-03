27b3f2b8e4ab9187c40547fd768b387323d07d8d
Thomas Koopman <koopman.t.pro@gmail.com>
2025-02-03 15:27:42 +0100

    Move test

--- app/tests/components_tests/test_views.py
+++ app/tests/components_tests/test_views.py
@@ -2,9 +2,15 @@ import json
 
 import pytest
 from django.conf import settings
+from django.utils.html import format_html
+from factory.fuzzy import FuzzyChoice
 
 from grandchallenge.archives.models import ArchiveItem
-from grandchallenge.components.models import InterfaceKindChoices
+from grandchallenge.components.models import (
+    InterfaceKind,
+    InterfaceKindChoices,
+)
+from grandchallenge.components.widgets import FileWidgetChoices
 from grandchallenge.reader_studies.models import DisplaySet, ReaderStudy
 from grandchallenge.subdomains.utils import reverse
 from tests.algorithms_tests.factories import AlgorithmFactory
@@ -19,6 +25,7 @@ from tests.reader_studies_tests.factories import (
     DisplaySetFactory,
     ReaderStudyFactory,
 )
+from tests.uploads_tests.factories import UserUploadFactory
 from tests.utils import get_view_for_user
 
 
@@ -539,3 +546,112 @@ def test_interfaces_list_link_in_new_interface_form(
         user=u,
     )
     assert reverse(interface_list_viewname) in response.rendered_content
+
+
+@pytest.mark.django_db
+def test_file_widget_select_view(client):
+    user = UserFactory()
+    ci = ComponentInterfaceFactory(
+        kind=FuzzyChoice(InterfaceKind.interface_type_file())
+    )
+    response = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_SEARCH.name,
+            "prefixed-interface-slug": ci.slug,
+        },
+    )
+    assert '<input class="form-control" type="search"' in str(response.content)
+
+    response2 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_UPLOAD.name,
+            "prefixed-interface-slug": ci.slug,
+        },
+    )
+    assert 'class="user-upload"' in str(response2.content)
+
+    response3 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.UNDEFINED.name,
+            "prefixed-interface-slug": ci.slug,
+        },
+    )
+    assert response3.content == b""
+
+    civ = ComponentInterfaceValueFactory(interface=ci)
+    response4 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_SELECTED.name,
+            "prefixed-interface-slug": ci.slug,
+            "current-value": civ.pk,
+        },
+    )
+    assert format_html(
+        '<input type="hidden" name="{}" value="{}">', ci.slug, civ.pk
+    ) in str(response4.content)
+
+    user_upload = UserUploadFactory()
+    response5 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_SELECTED.name,
+            "prefixed-interface-slug": ci.slug,
+            "current-value": user_upload.pk,
+        },
+    )
+    assert format_html(
+        '<input type="hidden" name="{}" value="{}">', ci.slug, user_upload.pk
+    ) in str(response5.content)
+
+    civ_pk = civ.pk
+    civ.delete()
+    response6 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_SELECTED.name,
+            "prefixed-interface-slug": ci.slug,
+            "current-value": civ_pk,
+        },
+    )
+    assert response6.status_code == 404
+
+    response7 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci.slug}": FileWidgetChoices.FILE_SEARCH.name
+            + "foobar",
+            "prefixed-interface-slug": ci.slug,
+        },
+    )
+    assert response7.status_code == 404
+
+    ci_slug = ci.slug
+    ci.delete()
+    response8 = get_view_for_user(
+        viewname="components:select-file-widget",
+        client=client,
+        user=user,
+        data={
+            f"widget-choice-{ci_slug}": FileWidgetChoices.FILE_SEARCH.name,
+            "prefixed-interface-slug": ci_slug,
+        },
+    )
+    assert response8.status_code == 404
