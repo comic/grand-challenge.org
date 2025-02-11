08c0766f1272145a3fee5504fd09a929c7a8e1b8
Thomas Koopman <koopman.t.pro@gmail.com>
2025-02-11 17:49:07 +0100

    Fix id selector in javascript

--- app/grandchallenge/components/static/components/js/flexible_file_widget.mjs
+++ app/grandchallenge/components/static/components/js/flexible_file_widget.mjs
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    const elements = document.querySelectorAll("[id^=widgetSelect]");
+    const elements = document.querySelectorAll("[id^=widget-select]");
     for (let i = 0, len = elements.length; i < len; i++) {
         if (elements[i].value === "FILE_SEARCH") {
             htmx.trigger(elements[i], "widgetSelected");
