f55bc5585713270319f91e72e50ee28afecea958
Chris van Run <chris.vanrun@radboudumc.nl>
2025-02-17 10:21:20 +0100

    Hook signals correctly

--- app/grandchallenge/challenges/signals.py
+++ app/grandchallenge/challenges/signals.py
@@ -8,55 +8,59 @@ from grandchallenge.challenges.models import Challenge, OnboardingTask
 @receiver(post_save, sender=Challenge)
 def create_onboarding_tasks(sender, instance, created, **__):
     if created:  # Ensures tasks are only created on first save
-        OnboardingTask.objects.bulk_create(
-            [
-                OnboardingTask(
-                    title="Create Phases",
-                    description="Define, name and create the different phases of the challenge.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=1),
-                ),
-                OnboardingTask(
-                    title="Define Inputs and Outputs",
-                    description="To support, communicate the required input and output data formats for participant's algorithms.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=3),
-                ),
-                OnboardingTask(
-                    title="Create Archives",
-                    description="Create an archive per algorithm-type phase for the challenge.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.SUPPORT,
-                    deadline=instance.created + timedelta(weeks=3),
-                ),
-                OnboardingTask(
-                    title="Upload Data",
-                    description="Prepare and upload necessary datasets to archives, contact support if no archives are available.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=5),
-                ),
-                OnboardingTask(
-                    title="Example Algorithm",
-                    description="Implement and document a baseline example algorithm for participants to use as a reference.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=6, seconds=0),
-                ),
-                OnboardingTask(
-                    title="Evaluation Method",
-                    description="Implement and document the evaluation method for assessing participant submissions.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=6, seconds=1),
-                ),
-                OnboardingTask(
-                    title="Scoring",
-                    description="Configure the leaderboard scoring to accurately interpret the evaluation results.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=6, seconds=2),
-                ),
-                OnboardingTask(
-                    title="Test Evaluation",
-                    description="Run test evaluations using sample submissions to ensure the scoring system and evaluation method function correctly before launching the challenge.",
-                    responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
-                    deadline=instance.created + timedelta(weeks=6, seconds=3),
-                ),
-            ]
-        )
+        tasks = [
+            OnboardingTask(
+                title="Create Phases",
+                description="Define, name and create the different phases of the challenge.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=1),
+            ),
+            OnboardingTask(
+                title="Define Inputs and Outputs",
+                description="To support, communicate the required input and output data formats for participant's algorithms.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=3),
+            ),
+            OnboardingTask(
+                title="Create Archives",
+                description="Create an archive per algorithm-type phase for the challenge.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.SUPPORT,
+                deadline=instance.created + timedelta(weeks=3),
+            ),
+            OnboardingTask(
+                title="Upload Data",
+                description="Prepare and upload necessary datasets to archives, contact support if no archives are available.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=5),
+            ),
+            OnboardingTask(
+                title="Example Algorithm",
+                description="Implement and document a baseline example algorithm for participants to use as a reference.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=6, seconds=0),
+            ),
+            OnboardingTask(
+                title="Evaluation Method",
+                description="Implement and document the evaluation method for assessing participant submissions.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=6, seconds=1),
+            ),
+            OnboardingTask(
+                title="Scoring",
+                description="Configure the leaderboard scoring to accurately interpret the evaluation results.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=6, seconds=2),
+            ),
+            OnboardingTask(
+                title="Test Evaluation",
+                description="Run test evaluations using sample submissions to ensure the scoring system and evaluation method function correctly before launching the challenge.",
+                responsible_party=OnboardingTask.ResponsiblePartyChoices.CHALLENGE_ORGANIZERS,
+                deadline=instance.created + timedelta(weeks=6, seconds=3),
+            ),
+        ]
+
+        for t in tasks:  # Ensure sync between challenge and task creation
+            t.challenge = instance
+            t.created = instance.created
+
+        OnboardingTask.objects.bulk_create(tasks)
