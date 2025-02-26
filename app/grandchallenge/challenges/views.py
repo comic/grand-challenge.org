483df8652214ceb43aeddf15da53bfb668e27eb1
Thomas Koopman <koopman.t.pro@gmail.com>
2025-02-26 21:35:00 +0100

    Add indication of outstanding invoices above list

--- app/grandchallenge/challenges/views.py
+++ app/grandchallenge/challenges/views.py
@@ -370,6 +370,19 @@ class InvoiceList(
     raise_exception = True
     login_url = reverse_lazy("account_login")
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["outstanding_invoices"] = any(
+            (
+                obj.payment_type != obj.PaymentTypeChoices.COMPLIMENTARY
+                and obj.payment_status == obj.PaymentStatusChoices.ISSUED
+            )
+            for obj in context["object_list"]
+        )
+
+        return context
+
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter(challenge=self.request.challenge)
