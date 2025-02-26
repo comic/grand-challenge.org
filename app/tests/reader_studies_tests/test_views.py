8fc5c23b7a4a9e7ff3bd6a3b10e9f7acd118c414
Chris van Run <chrisvanrun@users.noreply.github.com>
2025-02-26 09:18:27 +0100

    Split off the Ground Truth page (#3853)
    Part of the pitch:
    - https://github.com/DIAGNijmegen/rse-roadmap/issues/385
    
    A quick pre-lude:
    - split the ground truth from being a tab in the the reader study detail
    to its own view
    - add a warning when the reader study is not configured for educational
    purposes
    
    Other than above, I did not change the content of the page.
    
    Considered to refactor all reader study views to have the configuration
    side-panel, much like the algorithms. But have put that out of scope
    (for now).
    
    
    
    ## Showcase
    
    The warning is no longer there.
    
    
    ![afbeelding](https://github.com/user-attachments/assets/f88398bc-e5b5-4f7f-99be-6edc538ac9ae)

--- app/tests/reader_studies_tests/test_views.py
+++ app/tests/reader_studies_tests/test_views.py
@@ -931,3 +931,30 @@ def test_display_set_upload_corrupt_image(
     notification = Notification.objects.get()
     assert notification.user == editor
     assert "1 file could not be imported" in notification.description
+
+
+@pytest.mark.django_db
+def test_ground_truth_view(client):
+    rs = ReaderStudyFactory()
+
+    editor, reader, a_user = UserFactory.create_batch(3)
+    rs.add_editor(editor)
+    rs.add_reader(reader)
+
+    for usr in [reader, a_user]:
+        response = get_view_for_user(
+            client=client,
+            viewname="reader-studies:ground-truth",
+            reverse_kwargs={"slug": rs.slug},
+            user=usr,
+        )
+        assert response.status_code == 403
+
+    response = get_view_for_user(
+        client=client,
+        viewname="reader-studies:ground-truth",
+        reverse_kwargs={"slug": rs.slug},
+        user=editor,
+    )
+
+    assert response.status_code == 200
