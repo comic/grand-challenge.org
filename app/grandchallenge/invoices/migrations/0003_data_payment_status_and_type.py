982450c741fb579249d5a8af67d883e178aec1d6
Thomas Koopman <koopman.t.pro@gmail.com>
2025-02-26 22:26:58 +0100

    Squash migrations

--- app/grandchallenge/invoices/migrations/0003_data_payment_status_and_type.py
+++ app/grandchallenge/invoices/migrations/0003_data_payment_status_and_type.py
@@ -15,7 +15,10 @@ def move_complimentary_status_to_type(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("invoices", "0002_invoice_payment_type_alter_invoice_payment_status"),
+        (
+            "invoices",
+            "0002_invoice_payment_type_alter_invoice_payment_status_and_more",
+        ),
     ]
 
     operations = [migrations.RunPython(move_complimentary_status_to_type)]
