8667063df042440fee5268747f684e13d684cdb1
Thomas Koopman <koopman.t.pro@gmail.com>
2025-03-12 15:07:39 +0100

    Simplify test

--- app/tests/invoices_tests/test_permissions.py
+++ app/tests/invoices_tests/test_permissions.py
@@ -1,8 +1,7 @@
 import pytest
-from django.conf import settings
-from django.contrib.auth.models import Group
-from guardian.shortcuts import get_perms
+from guardian.shortcuts import get_users_with_perms
 
+from tests.evaluation_tests.test_permissions import get_groups_with_set_perms
 from tests.invoices_tests.factories import InvoiceFactory
 
 
@@ -10,25 +9,7 @@ from tests.invoices_tests.factories import InvoiceFactory
 def test_invoice_permissions():
     invoice = InvoiceFactory()
 
-    g_reg_anon = Group.objects.get(
-        name=settings.REGISTERED_AND_ANON_USERS_GROUP_NAME
-    )
-    g_reg = Group.objects.get(name=settings.REGISTERED_USERS_GROUP_NAME)
-
-    reg_anon_perms = get_perms(g_reg_anon, invoice)
-    assert "view_invoice" not in reg_anon_perms
-    assert "change_invoice" not in reg_anon_perms
-
-    reg_perms = get_perms(g_reg, invoice)
-    assert "view_invoice" not in reg_perms
-    assert "change_invoice" not in reg_perms
-
-    participants_perms = get_perms(
-        invoice.challenge.participants_group, invoice
-    )
-    assert "view_invoice" not in participants_perms
-    assert "change_invoice" not in participants_perms
-
-    admins_perms = get_perms(invoice.challenge.admins_group, invoice)
-    assert "view_invoice" in admins_perms
-    assert "change_invoice" not in admins_perms
+    assert get_groups_with_set_perms(invoice) == {
+        invoice.challenge.admins_group: {"view_invoice"},
+    }
+    assert get_users_with_perms(invoice, with_group_users=False).count() == 0
