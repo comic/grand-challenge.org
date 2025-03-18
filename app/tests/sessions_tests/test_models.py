84aa7b30572a12cb35a78b7b31501fe8c65505a3
Thomas Koopman <koopman.t.pro@gmail.com>
2025-03-18 16:34:58 +0100

    Cleanup without fixture at start of test.

--- app/tests/sessions_tests/test_models.py
+++ app/tests/sessions_tests/test_models.py
@@ -4,13 +4,9 @@ from grandchallenge.browser_sessions.models import BrowserSession
 from tests.factories import UserFactory
 
 
-@pytest.fixture(scope="module", autouse=True)
-def clear_sessions():
-    BrowserSession.objects.all().delete()
-
-
 @pytest.mark.django_db
 def test_no_user_is_assigned_to_session(client):
+    BrowserSession.objects.all().delete()
     response = client.get("/")
 
     assert response.status_code == 200
@@ -20,6 +16,7 @@ def test_no_user_is_assigned_to_session(client):
 
 @pytest.mark.django_db
 def test_user_is_assigned_to_session_after_login(client):
+    BrowserSession.objects.all().delete()
     user = UserFactory()
 
     client.force_login(user)
