01606e1bbc954eb4cab6a532e7d3664270a39a9d
Thomas Koopman <koopman.t.pro@gmail.com>
2025-03-18 17:21:31 +0100

    Change naming for easier reading

--- app/config/settings.py
+++ app/config/settings.py
@@ -1245,8 +1245,8 @@ CELERY_BEAT_SCHEDULE = {
         "task": "grandchallenge.challenges.tasks.send_onboarding_task_reminder_emails",
         "schedule": crontab(day_of_week="mon", hour=6, minute=0),
     },
-    "send_challenge_outstanding_invoice_reminder_emails": {
-        "task": "grandchallenge.invoices.tasks.send_challenge_outstanding_invoice_reminder_emails",
+    "send_challenge_invoice_overdue_reminder_emails": {
+        "task": "grandchallenge.invoices.tasks.send_challenge_invoice_overdue_reminder_emails",
         "schedule": crontab(day_of_month=1, hour=6, minute=0),
     },
     "delete_users_who_dont_login": {
--- app/grandchallenge/invoices/tasks.py
+++ app/grandchallenge/invoices/tasks.py
@@ -5,17 +5,17 @@ from django.utils.timezone import now
 from grandchallenge.core.celery import acks_late_micro_short_task
 from grandchallenge.invoices.emails import (
     send_challenge_invoice_issued_notification,
-    send_challenge_outstanding_invoice_reminder,
+    send_challenge_invoice_overdue_reminder,
 )
 
 
 @acks_late_micro_short_task
 @transaction.atomic
-def send_challenge_outstanding_invoice_reminder_emails():
+def send_challenge_invoice_overdue_reminder_emails():
     from grandchallenge.invoices.models import Invoice
 
     _now = now()
-    outstanding_invoices = Invoice.objects.filter(
+    invoices_overdue = Invoice.objects.filter(
         payment_type__in=[
             Invoice.PaymentTypeChoices.PREPAID,
             Invoice.PaymentTypeChoices.POSTPAID,
@@ -23,8 +23,8 @@ def send_challenge_outstanding_invoice_reminder_emails():
         payment_status=Invoice.PaymentStatusChoices.ISSUED,
         issued_on__lt=_now - settings.CHALLENGE_INVOICE_OUTSTANDING_CUTOFF,
     )
-    for invoice in outstanding_invoices:
-        send_challenge_outstanding_invoice_reminder(invoice)
+    for invoice in invoices_overdue:
+        send_challenge_invoice_overdue_reminder(invoice)
 
 
 @acks_late_micro_short_task
