0eb6e7c2b71b46a1df01c35691735304c1616d70
Thomas Koopman <koopman.t.pro@gmail.com>
2025-03-18 14:58:55 +0100

    Cleanup browsersessions before test to remove possible orphans

--- app/tests/sessions_tests/test_models.py
+++ app/tests/sessions_tests/test_models.py
@@ -4,6 +4,11 @@ from grandchallenge.browser_sessions.models import BrowserSession
 from tests.factories import UserFactory
 
 
+@pytest.fixture(scope="module", autouse=True)
+def clear_sessions():
+    BrowserSession.objects.all().delete()
+
+
 @pytest.mark.django_db
 def test_no_user_is_assigned_to_session(client):
     response = client.get("/")
