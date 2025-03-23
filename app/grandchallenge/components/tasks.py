c67058f841c4f43fa861262efa5f1526dff334c7
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-03-23 12:54:49 +0100

    Set the content type on civ creation

--- app/grandchallenge/components/tasks.py
+++ app/grandchallenge/components/tasks.py
@@ -1156,9 +1156,14 @@ def civ_value_to_file(*, civ_pk):
         if civ.value is None:
             raise RuntimeError("CIV value is None")
 
-        civ.file = ContentFile(
+        content_file = ContentFile(
             json.dumps(civ.value).encode("utf-8"),
             name=Path(civ.interface.relative_path).name,
+        )
+        civ.file.save(
+            content_file.name,
+            content_file,
+            parameters={"ContentType": civ.interface.content_type},
         )
         civ.value = None
         civ.save()
