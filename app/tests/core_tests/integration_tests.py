84753a37745e1aa7da3a1ad4007baa0dca8a4d93
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-03-25 11:37:41 +0100

    Fix deprecation warning
    Closes #3899

--- app/tests/core_tests/integration_tests.py
+++ app/tests/core_tests/integration_tests.py
@@ -165,6 +165,7 @@ class GrandChallengeFrameworkTestCase(TestCase):
             "last_name": "test",
             "username": "test",
             "email": "test@test.com",
+            "email2": "test@test.com",
             "password1": password,
             "password2": password,
             "institution": "test",
