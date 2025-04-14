003696cd5cfbf92ffc64583cea6ffb5aca235250
Anne Mickan <30069334+amickan@users.noreply.github.com>
2025-04-14 08:48:52 +0200

    Initialize view_leaderboard permission (#3954)
    At the moment, noone has the newly introduced `view_leaderboard`
    permission. Simply opening the reader study settings and saving the form
    again (with or without changes) fixes that, but I think the cleaner way
    to initialize these permissions is via a migration.

---
+++ app/grandchallenge/reader_studies/migrations/0063_init_view_leaderboard_perm.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+def init_view_leaderboard_permission(apps, _schema_editor):
+    ReaderStudy = apps.get_model("reader_studies", "ReaderStudy")  # noqa: N806
+
+    for rs in ReaderStudy.objects.all():
+        rs.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        (
+            "reader_studies",
+            "0062_readerstudy_leaderboard_accessible_to_readers",
+        ),
+    ]
+
+    operations = [
+        migrations.RunPython(init_view_leaderboard_permission, elidable=True),
+    ]
