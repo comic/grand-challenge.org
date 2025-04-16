97dc68b0e2f6f4fd73370228c97fe89fab4eb825
Thomas Koopman <koopman.t.pro@gmail.com>
2025-04-16 17:40:43 +0200

    Avoid stopping session that are already stopped

--- app/tests/workstations_tests/test_models.py
+++ app/tests/workstations_tests/test_models.py
@@ -36,7 +36,7 @@ from tests.workstations_tests.factories import FeedbackFactory
 
 
 def stop_all_sessions():
-    sessions = Session.objects.all()
+    sessions = Session.objects.exclude(status=Session.STOPPED)
     for s in sessions:
         s.stop()
 
