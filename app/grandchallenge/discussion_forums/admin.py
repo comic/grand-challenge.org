3722deb490a5f423731b3d51310de38a22940878
Anne Mickan <30069334+amickan@users.noreply.github.com>
2025-05-08 13:23:51 +0200

    Update the admin

--- app/grandchallenge/discussion_forums/admin.py
+++ app/grandchallenge/discussion_forums/admin.py
@@ -6,16 +6,20 @@ from grandchallenge.discussion_forums.models import Forum, Post, Topic
 @admin.register(Forum)
 class ForumAdmin(admin.ModelAdmin):
     list_display = ("id", "parent_object")
+    readonly_fields = ("parent_object",)
 
     def parent_object(self, obj):
-        return obj.get_parent()
+        return obj.parent_object
 
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ("subject", "forum", "type", "creator")
+    list_display = ("subject", "linked_forum", "type", "creator")
     search_fields = ("subject", "creator__username")
-    list_filter = ("type", "forum__slug")
+    list_filter = ("type",)
+
+    def linked_forum(self, obj):
+        return f"Forum for {obj.forum.parent_object}"
 
 
 @admin.register(Post)
