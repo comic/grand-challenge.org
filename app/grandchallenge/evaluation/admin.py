6c931834bf7f24025b5787c8506efcd0ee8c968f
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-05-08 12:16:11 +0200

    Remove specific ordering in admin

--- app/grandchallenge/evaluation/admin.py
+++ app/grandchallenge/evaluation/admin.py
@@ -166,7 +166,6 @@ class SubmissionAdmin(admin.ModelAdmin):
 
 @admin.register(Evaluation)
 class EvaluationAdmin(admin.ModelAdmin):
-    ordering = ("-created",)
     list_display = (
         "pk",
         "created",
