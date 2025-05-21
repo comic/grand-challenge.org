fd8c714175cd8823e5b1dfa29ed65d95fe606dd9
Anne Mickan <30069334+amickan@users.noreply.github.com>
2025-05-21 12:23:40 +0200

    Call views through htmx and move templates back to main dir

--- app/grandchallenge/discussion_forums/views.py
+++ app/grandchallenge/discussion_forums/views.py
@@ -147,7 +147,6 @@ class ForumPostCreate(ObjectPermissionRequiredMixin, CreateView):
     permission_required = "create_topic_post"
     raise_exception = True
     form_class = ForumPostForm
-    template_name = "discussion_forums/partials/forumpost_form.html"
 
     @cached_property
     def forum(self):
@@ -179,7 +178,6 @@ class ForumPostDetail(ObjectPermissionRequiredMixin, DetailView):
     model = ForumPost
     permission_required = "view_forumpost"
     raise_exception = True
-    template_name = "discussion_forums/partials/forumpost_detail.html"
 
     @cached_property
     def forum(self):
@@ -198,7 +196,6 @@ class ForumPostDelete(ObjectPermissionRequiredMixin, DeleteView):
     permission_required = "delete_forumpost"
     raise_exception = True
     success_message = "Successfully deleted post."
-    template_name = "discussion_forums/partials/forumpost_confirm_delete.html"
 
     @cached_property
     def forum(self):
@@ -239,7 +236,6 @@ class ForumPostUpdate(ObjectPermissionRequiredMixin, UpdateView):
     permission_required = "change_forumpost"
     raise_exception = True
     form_class = ForumPostForm
-    template_name = "discussion_forums/partials/forumpost_form.html"
 
     @cached_property
     def forum(self):
