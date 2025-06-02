73174ac6c56d40ef3297de08bfcbb7903368bf29
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-06-02 15:55:08 +0200

    Update migrations

--- app/grandchallenge/utilization/migrations/0003_auto_20250520_1012.py
+++ app/grandchallenge/utilization/migrations/0003_auto_20250520_1012.py
@@ -1,5 +1,4 @@
 from django.db import migrations
-from django.db.models import F
 
 
 def create_evaluation_utilizations(apps, schema_editor):
@@ -7,25 +6,34 @@ def create_evaluation_utilizations(apps, schema_editor):
     EvaluationUtilization = apps.get_model(  # noqa: N806
         "utilization", "EvaluationUtilization"
     )
+
+    evaluations_to_create = []
+    n_created = 0
+
     for evaluation in (
-        Evaluation.objects.annotate(
-            duration=F("completed_at") - F("started_at")
-        )
-        .filter(evaluation_utilization__isnull=True)
-        .select_related(
+        Evaluation.objects.filter(evaluation_utilization__isnull=True)
+        .only(
+            "completed_at",
+            "started_at",
+            "compute_cost_euro_millicents",
             "submission",
             "submission__algorithm_image",
             "submission__phase",
+            "submission__phase__external_evaluation",
             "submission__phase__archive",
             "submission__phase__challenge",
             "submission__creator",
         )
-        .order_by()
-        .iterator(chunk_size=2000)
+        .order_by("created")
+        .iterator(chunk_size=1000)
     ):
+        if evaluation.completed_at and evaluation.started_at:
+            duration = evaluation.completed_at - evaluation.started_at
+        else:
+            duration = None
+
         kwargs = dict(
-            created=evaluation.created,
-            duration=evaluation.duration,
+            duration=duration,
             compute_cost_euro_millicents=evaluation.compute_cost_euro_millicents,
             external_evaluation=evaluation.submission.phase.external_evaluation,
             algorithm_image=evaluation.submission.algorithm_image,
@@ -35,11 +43,24 @@ def create_evaluation_utilizations(apps, schema_editor):
             evaluation=evaluation,
             phase=evaluation.submission.phase,
         )
+
         if evaluation.submission.algorithm_image is not None:
             kwargs.update(
                 algorithm=evaluation.submission.algorithm_image.algorithm,
             )
-        EvaluationUtilization.objects.create(**kwargs)
+
+        evaluations_to_create.append(EvaluationUtilization(**kwargs))
+
+        if len(evaluations_to_create) >= 1000:
+            EvaluationUtilization.objects.bulk_create(evaluations_to_create)
+            n_created += len(evaluations_to_create)
+            evaluations_to_create = []
+
+    if evaluations_to_create:
+        EvaluationUtilization.objects.bulk_create(evaluations_to_create)
+        n_created += len(evaluations_to_create)
+
+    print(f"Created {n_created} Evaluation Utilizations")
 
 
 def create_job_utilizations(apps, schema_editor):
@@ -47,26 +68,49 @@ def create_job_utilizations(apps, schema_editor):
     JobUtilization = apps.get_model(  # noqa: N806
         "utilization", "JobUtilization"
     )
+
+    job_utlizations_to_create = []
+    n_created = 0
+
     for job in (
-        Job.objects.annotate(duration=F("completed_at") - F("started_at"))
-        .filter(job_utilization__isnull=True)
-        .select_related(
+        Job.objects.filter(job_utilization__isnull=True)
+        .only(
+            "completed_at",
+            "started_at",
+            "compute_cost_euro_millicents",
             "algorithm_image",
             "algorithm_image__algorithm",
             "creator",
         )
-        .order_by()
-        .iterator(chunk_size=2000)
+        .order_by("created")
+        .iterator(chunk_size=1000)
     ):
-        JobUtilization.objects.create(
-            created=job.created,
-            duration=job.duration,
-            compute_cost_euro_millicents=job.compute_cost_euro_millicents,
-            algorithm=job.algorithm_image.algorithm,
-            algorithm_image=job.algorithm_image,
-            creator=job.creator,
-            job=job,
+        if job.completed_at and job.started_at:
+            duration = job.completed_at - job.started_at
+        else:
+            duration = []
+
+        job_utlizations_to_create.append(
+            JobUtilization(
+                duration=duration,
+                compute_cost_euro_millicents=job.compute_cost_euro_millicents,
+                algorithm=job.algorithm_image.algorithm,
+                algorithm_image=job.algorithm_image,
+                creator=job.creator,
+                job=job,
+            )
         )
+
+        if len(job_utlizations_to_create) >= 1000:
+            JobUtilization.objects.bulk_create(job_utlizations_to_create)
+            n_created += len(job_utlizations_to_create)
+            job_utlizations_to_create = []
+
+    if job_utlizations_to_create:
+        JobUtilization.objects.bulk_create(job_utlizations_to_create)
+        n_created += len(job_utlizations_to_create)
+
+    print(f"Created {n_created} Job Utilizations")
 
 
 def set_challenges_to_job_utilizations(apps, schema_editor):
