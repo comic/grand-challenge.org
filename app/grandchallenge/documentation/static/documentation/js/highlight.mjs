e7de0a9f2f05d66ce9131bdb5bd78b5e2a15d348
Thomas Koopman <koopman.t.pro@gmail.com>
2025-06-18 15:58:44 +0200

    Avoid highlighting rest of page if end text is not found

--- app/grandchallenge/documentation/static/documentation/js/highlight.mjs
+++ app/grandchallenge/documentation/static/documentation/js/highlight.mjs
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let startNode = null;
     let capturing = false;
+    let num_highlighted_elements = 0;
 
     for (const el of elements) {
         const text = el.textContent.replace(/\s+/g, " ");
@@ -25,7 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (capturing) {
             el.classList.add("highlight");
+            num_highlighted_elements += 1;
         }
+
+        if (num_highlighted_elements === 3) break;
 
         if (!endText && startNode) break;
 
