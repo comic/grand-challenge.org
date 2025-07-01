57550f444fc160010136427bcc12ef5b2f0c22e6
Thomas Koopman <koopman.t.pro@gmail.com>
2025-07-01 10:54:29 +0200

    Avoid clash on "pages" and "statistics"

--- app/grandchallenge/pages/urls.py
+++ app/grandchallenge/pages/urls.py
@@ -14,9 +14,11 @@ from grandchallenge.pages.views import (
 app_name = "pages"
 
 urlpatterns = [
-    path("pages/", PageList.as_view(), name="list"),
+    path("pages/all/", PageList.as_view(), name="list"),
     path("pages/create/", PageCreate.as_view(), name="create"),
-    path("statistics/", ChallengeStatistics.as_view(), name="statistics"),
+    path(
+        "pages/statistics/", ChallengeStatistics.as_view(), name="statistics"
+    ),
     path("", ChallengeHome.as_view(), name="home"),
     path("<slug>/", PageDetail.as_view(), name="detail"),
     path(
