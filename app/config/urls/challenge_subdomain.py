5863bed9099405386327d18794ab7a788a183cb2
Anne Mickan <30069334+amickan@users.noreply.github.com>
2025-07-14 08:15:57 +0200

    Change path for topic list view

--- app/config/urls/challenge_subdomain.py
+++ app/config/urls/challenge_subdomain.py
@@ -21,7 +21,7 @@ urlpatterns = [
         include("grandchallenge.evaluation.urls", namespace="evaluation"),
     ),
     path(
-        "discussion-forum/",
+        "forum/",
         include(
             "grandchallenge.discussion_forums.urls",
             namespace="discussion-forums",
