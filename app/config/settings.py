195b1d7f32d85e4dc763ffeb92cdec616317ef21
HarmvZ <harm.van.zeeland@gmail.com>
2025-07-15 15:57:05 +0200

    Configure webpack loader in django

--- app/config/settings.py
+++ app/config/settings.py
@@ -435,6 +435,16 @@ default_loaders = [
 
 cached_loaders = [("django.template.loaders.cached.Loader", default_loaders)]
 
+# Webpack Loader configuration
+WEBPACK_LOADER = {
+    "DEFAULT": {
+        "BUNDLE_DIR_NAME": "vendored/",
+        "STATS_FILE": os.path.join(
+            Path(__file__).resolve().parent.parent, "webpack-stats.json"
+        ),
+    }
+}
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -535,6 +545,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",  # to allow api communication from subdomains
     "markdownx",  # for editing markdown
     "compressor",  # for compressing css
+    "webpack_loader",  # for webpack integration
     "stdimage",
     "django_filters",
     "drf_spectacular",
