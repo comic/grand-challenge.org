8fed7906ee2fc40cc0f47c497a620b21fc285248
Thomas Koopman <koopman.t.pro@gmail.com>
2025-07-28 13:43:32 +0200

    Place phase nav dropdown in challenge topbar
    Normal dropdown but only 1 column. Having 2 columns is impossible like this.

--- app/grandchallenge/challenges/static/js/challenges/dropdown.js
+++
@@ -1,8 +0,0 @@
-$("#leaderboardPhaseNavTrigger").click(e => {
-    e.stopPropagation();
-    $("#leaderboardPhaseNavDropdown").dropdown("toggle");
-});
-$("#submissionPhaseNavTrigger").click(e => {
-    e.stopPropagation();
-    $("#submissionPhaseNavDropdown").dropdown("toggle");
-});
