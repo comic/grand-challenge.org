5551c25356a6c465c941a76d8d2ff31a09c080ff
Thomas Koopman <koopman.t.pro@gmail.com>
2025-08-27 17:26:14 +0200

    Fix forgotten usage of COMPONENTS_ECR_USD_MILLICENTS_PER_YEAR_PER_TB

--- app/grandchallenge/core/templatetags/costs.py
+++ app/grandchallenge/core/templatetags/costs.py
@@ -42,7 +42,8 @@ def storage_bytes_to_euro_per_year(storage_size):
 @register.filter
 def registry_bytes_to_euro_per_year(storage_size):
     return millicents_to_euro(
-        settings.COMPONENTS_ECR_USD_MILLICENTS_PER_YEAR_PER_TB
+        settings.COMPONENTS_S3_USD_MILLICENTS_PER_YEAR_PER_TB_EXCLUDING_TAX
+        * (1 + settings.COMPONENTS_TAX_RATE)
         * settings.COMPONENTS_USD_TO_EUR
         * storage_size
         / settings.TERABYTE
