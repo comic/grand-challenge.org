1aa7cdcd1e590cb6061a65489bc8a0f64b16e823
Harm van Zeeland <HarmvZ@users.noreply.github.com>
2025-09-11 17:31:44 +0200

    Open all mailto links on new page (#4299)
    For me, clicking a `mailto:...` link navigates to Gmail, which replaces
    the current page when `target="_blank"` is not in the link. It makes
    more sense to open it on a new window/tab like this.

--- app/grandchallenge/organizations/views.py
+++ app/grandchallenge/organizations/views.py
@@ -33,8 +33,9 @@ class OrganizationList(ListView):
                     (
                         "An organization is a group or institution who have "
                         "created an archive, reader study, challenge or "
-                        "algorithm on this site. Please <a href='{}'>contact "
-                        "us</a> if you would like to add your organization."
+                        "algorithm on this site. Please <a target='_blank' "
+                        "href='{}'>contact us</a> if you would like to add your "
+                        "organization."
                     ),
                     mark_safe(
                         random_encode("mailto:support@grand-challenge.org")
