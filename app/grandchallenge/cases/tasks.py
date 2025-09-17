6d2d025347d8f3c811b553e0f9318662284310af
Thomas Koopman <koopman.t.pro@gmail.com>
2025-09-17 16:18:38 +0200

    Remove unused RetryStep

--- app/grandchallenge/cases/tasks.py
+++ app/grandchallenge/cases/tasks.py
@@ -571,7 +571,7 @@ def import_dicom_to_healthimaging(*, dicom_imageset_upload_pk):
         upload.save()
 
 
-@acks_late_micro_short_task(retry_on=(LockNotAcquiredException, RetryStep))
+@acks_late_micro_short_task(retry_on=(LockNotAcquiredException,))
 @transaction.atomic
 def handle_healthimaging_import_job_event(*, event):
     job_name = event["jobName"]
