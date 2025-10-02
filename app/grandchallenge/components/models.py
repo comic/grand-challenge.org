5c3d751b6ead30e5b13f079950f676da2e91f4a2
Anne Mickan <30069334+amickan@users.noreply.github.com>
2025-10-02 15:56:36 +0200

    Order algorithm images and tarballs by creation date (#4329)
    Closes #4279 
    Closes #4260

--- app/grandchallenge/components/models.py
+++ app/grandchallenge/components/models.py
@@ -2723,6 +2723,7 @@ class Tarball(UUIDModel):
 
     class Meta:
         abstract = True
+        ordering = ("created",)
 
     def save(self, *args, **kwargs):
         adding = self._state.adding
