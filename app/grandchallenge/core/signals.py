5ee3cf8dca759556f00ccd72be984971bbc8ac65
Thomas Koopman <koopman.t.pro@gmail.com>
2025-10-15 11:11:17 +0200

    Revert notification cleanup

--- app/grandchallenge/core/signals.py
+++ app/grandchallenge/core/signals.py
@@ -4,7 +4,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models.signals import m2m_changed, post_save, pre_save
+from django.db.models import Q
+from django.db.models.signals import (
+    m2m_changed,
+    post_save,
+    pre_delete,
+    pre_save,
+)
 from django.dispatch import receiver
 from guardian.utils import get_anonymous_user
 
@@ -158,3 +164,17 @@ def update_editor_follows(  # noqa: C901
                 )
             elif action == "pre_remove" or action == "pre_clear":
                 unfollow(user=user, obj=obj, send_action=False)
+
+
+@receiver(pre_delete, sender=get_user_model())
+def clean_up_user_notifications(instance, **_):
+    ct = ContentType.objects.filter(
+        app_label=instance._meta.app_label, model=instance._meta.model_name
+    ).get()
+    Notification.objects.filter(
+        Q(actor_object_id=instance.pk) & Q(actor_content_type=ct)
+        | Q(action_object_object_id=instance.pk)
+        & Q(action_object_content_type=ct)
+        | Q(target_object_id=instance.pk) & Q(target_content_type=ct)
+        | Q(user_id=instance.pk)
+    ).delete()
