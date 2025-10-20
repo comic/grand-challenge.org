2300d4b1d5b67dd6820adddac2d43bfdec963610
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-10-20 16:27:51 +0200

    Generate a unique key for each existing row

--- app/grandchallenge/components/models.py
+++ app/grandchallenge/components/models.py
@@ -1632,6 +1632,7 @@ class ComponentJob(FieldChangeMixin, UUIDModel):
     signing_key = models.BinaryField(
         max_length=32,
         default=secrets.token_bytes,
+        unique=True,
         editable=False,
         help_text="The key used to sign the inference result file",
     )
