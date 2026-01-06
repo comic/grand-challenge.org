c05cdc0ee888ddc5173c894f6f45c38fbe0f538a
Thomas Koopman <koopman.t.pro@gmail.com>
2026-01-06 16:51:08 +0100

    fix

--- app/grandchallenge/algorithms/admin.py
+++ app/grandchallenge/algorithms/admin.py
@@ -284,7 +284,7 @@ class AlgorithmModelAdmin(admin.ModelAdmin):
         "model",
     )
 
-    def gb_in_storage(self, obj):
+    def naturalsize_in_storage(self, obj):
         return humanize.naturalsize(obj.size_in_storage)
 
 
