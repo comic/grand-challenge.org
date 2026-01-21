ad6d47143ddc22f87480000b89844f710f89edd0
Thomas Koopman <koopman.t.pro@gmail.com>
2026-01-21 18:46:44 +0100

    Remove test for current value in create forms
    Now using current socket value, which is never set on create.

--- app/tests/components_tests/test_forms.py
+++ app/tests/components_tests/test_forms.py
@@ -2,10 +2,7 @@ import pytest
 from django.core.exceptions import ValidationError
 from guardian.shortcuts import assign_perm
 
-from grandchallenge.archives.forms import (
-    ArchiveItemCreateForm,
-    ArchiveItemUpdateForm,
-)
+from grandchallenge.archives.forms import ArchiveItemUpdateForm
 from grandchallenge.components.forms import (
     INTERFACE_FORM_FIELD_PREFIX,
     FlexibleWidgetPrefixes,
@@ -15,12 +12,9 @@ from grandchallenge.components.models import (
     ComponentInterface,
     InterfaceKindChoices,
 )
-from grandchallenge.reader_studies.forms import (
-    DisplaySetCreateForm,
-    DisplaySetUpdateForm,
-)
+from grandchallenge.reader_studies.forms import DisplaySetUpdateForm
 from grandchallenge.uploads.models import UserUpload
-from tests.archives_tests.factories import ArchiveFactory, ArchiveItemFactory
+from tests.archives_tests.factories import ArchiveItemFactory
 from tests.cases_tests.factories import DICOMImageSetFactory
 from tests.components_tests.factories import (
     ComponentInterfaceFactory,
@@ -28,10 +22,7 @@ from tests.components_tests.factories import (
 )
 from tests.conftest import get_interface_form_data
 from tests.factories import ImageFactory, UserFactory
-from tests.reader_studies_tests.factories import (
-    DisplaySetFactory,
-    ReaderStudyFactory,
-)
+from tests.reader_studies_tests.factories import DisplaySetFactory
 from tests.uploads_tests.factories import (
     UserUploadFactory,
     create_completed_upload,
@@ -57,76 +48,6 @@ def test_interface_form_field_image_queryset_filter():
     assert im2 not in field.fields[0].queryset.all()
     assert upload1 in field.fields[1].queryset.all()
     assert upload2 not in field.fields[1].queryset.all()
-
-
-@pytest.mark.parametrize(
-    "form_class,base_object_factory,extra_form_kwargs",
-    (
-        (
-            DisplaySetCreateForm,
-            ReaderStudyFactory,
-            {"order": 1},
-        ),
-        (
-            ArchiveItemCreateForm,
-            ArchiveFactory,
-            {},
-        ),
-    ),
-)
-@pytest.mark.django_db
-def test_image_widget_current_value_in_archive_item_and_display_set_create_forms(
-    form_class, base_object_factory, extra_form_kwargs
-):
-    user = UserFactory()
-    image_ci = ComponentInterfaceFactory(
-        kind=ComponentInterface.Kind.PANIMG_IMAGE
-    )
-
-    image = ImageFactory()
-    assign_perm("cases.view_image", user, image)
-
-    user_upload = UserUploadFactory(creator=user)
-    user_upload.status = UserUpload.StatusChoices.COMPLETED
-    user_upload.save()
-
-    form1 = form_class(
-        user=user,
-        instance=None,
-        base_obj=base_object_factory(),
-        data={
-            **extra_form_kwargs,
-            **get_interface_form_data(
-                interface_slug=image_ci.slug, data=image.pk, existing_data=True
-            ),
-        },
-    )
-    assert form1.is_valid()
-    assert (
-        form1.fields[f"{INTERFACE_FORM_FIELD_PREFIX}{image_ci.slug}"]
-        .widget.attrs["current_value"][0]
-        .pk
-        == image.pk
-    )
-
-    form2 = form_class(
-        user=user,
-        instance=None,
-        base_obj=base_object_factory(),
-        data={
-            **extra_form_kwargs,
-            **get_interface_form_data(
-                interface_slug=image_ci.slug, data=user_upload.pk
-            ),
-        },
-    )
-    assert form2.is_valid()
-    assert (
-        form2.fields[f"{INTERFACE_FORM_FIELD_PREFIX}{image_ci.slug}"]
-        .widget.attrs["current_value"][0]
-        .pk
-        == user_upload.pk
-    )
 
 
 @pytest.mark.parametrize(
