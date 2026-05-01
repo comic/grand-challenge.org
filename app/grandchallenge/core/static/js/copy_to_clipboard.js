1d34eede788c3f12030e3dd677dffc74a349d4e8
Anne Mickan <30069334+amickan@users.noreply.github.com>
2026-05-01 11:12:58 +0200

    Add event listener to button

--- app/grandchallenge/core/static/js/copy_to_clipboard.js
+++ app/grandchallenge/core/static/js/copy_to_clipboard.js
@@ -2,29 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const activeTimers = {};
     const originalTitles = {};
 
-    document.addEventListener("click", e => {
-        const btn = e.target.closest(".copy-btn");
-        if (!btn) return;
+    for (const btn of document.querySelectorAll(".copy-btn")) {
+        btn.addEventListener("click", () => {
+            const id = btn.dataset.copy;
 
-        const id = btn.dataset.copy;
+            if (!originalTitles[id]) {
+                originalTitles[id] = btn.title;
+            }
+            clearTimeout(activeTimers[id]);
 
-        // Only store the title if we don't have it yet
-        if (!originalTitles[id]) {
-            originalTitles[id] = btn.title;
-        }
-        // Clear any timers that might already be present (e.g. because user double-clicked)
-        clearTimeout(activeTimers[id]);
+            navigator.clipboard.writeText(id).then(() => {
+                $(btn)
+                    .tooltip({ trigger: "manual" })
+                    .attr("data-original-title", "Copied to clipboard!")
+                    .tooltip("show");
 
-        navigator.clipboard.writeText(id).then(() => {
-            $(btn)
-                .tooltip({ trigger: "manual" })
-                .attr("data-original-title", "Copied to clipboard!")
-                .tooltip("show");
-
-            activeTimers[id] = setTimeout(() => {
-                $(btn).tooltip("dispose");
-                btn.title = originalTitles[id];
-            }, 2000);
+                activeTimers[id] = setTimeout(() => {
+                    $(btn)
+                        .attr("data-original-title", originalTitles[id])
+                        .tooltip("hide");
+                }, 2000);
+            });
         });
-    });
+    }
 });
