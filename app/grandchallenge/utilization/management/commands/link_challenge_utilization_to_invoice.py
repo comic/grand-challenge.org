738f5e1c301570c6a7d0961204710c8cc6c88fb1
Chris van Run <chris.vanrun@radboudumc.nl>
2026-05-19 14:04:20 +0200

    Remove arguments and invoice creations

--- app/grandchallenge/utilization/management/commands/link_challenge_utilization_to_invoice.py
+++ app/grandchallenge/utilization/management/commands/link_challenge_utilization_to_invoice.py
@@ -1,11 +1,8 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from grandchallenge.challenges.models import Challenge
-from grandchallenge.invoices.models import (
-    Invoice,
-    PaymentStatusChoices,
-    PaymentTypeChoices,
-)
+from grandchallenge.invoices.models import Invoice
 from grandchallenge.utilization.models import (
     EvaluationUtilization,
     JobUtilization,
@@ -19,67 +16,47 @@ CHALLENGE_UTILIZATION_MODELS = (
 )
 
 
+class NoInvoiceError(Exception):
+    pass
+
+
 class Command(BaseCommand):
-    help = "Links challenge utilizations to invoices, optionally creates a dummy invoice if none exists for a challenge"
+    help = "Links challenge utilizations to invoices"
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "challenge_short_names",
-            nargs="*",
-            type=str,
-        )
+    def handle(self, *_, **__):
+        challenges_with_unlinked_utilization = Challenge.objects.filter(
+            Q(job_utilizations__invoice__isnull=True)
+            | Q(evaluation_utilizations__invoice__isnull=True)
+            | Q(job_warm_pool_utilizations__invoice__isnull=True)
+        ).distinct()
 
-        parser.add_argument(
-            "--all_challenges",
-            action="store_true",
-            help="Link all challenge utilizations to invoices",
-        )
-
-        parser.add_argument(
-            "--create_missing_invoice",
-            action="store_true",
-            help="Create an invoice if none exists for a challenge",
-        )
-
-    def handle(self, *args, **options):
-        if options["all_challenges"]:
-            challenges = Challenge.objects.all()
-        else:
-            challenges = Challenge.objects.filter(
-                short_name__in=options["challenge_short_names"]
-            )
-
-        for challenge in challenges:
-            self._link_challenge_utilization(
-                challenge=challenge,
-                create_missing_invoice=options["create_missing_invoice"],
-            )
-
-        self.stdout.write(self.style.SUCCESS("Finished linking utilizations"))
-
-    def _link_challenge_utilization(
-        self, *, challenge, create_missing_invoice
-    ):
-        prefix = f"[{challenge.short_name}]"
-
-        has_unlinked = any(
-            utilization_model.objects.filter(
-                challenge=challenge, invoice__isnull=True
-            ).exists()
-            for utilization_model in CHALLENGE_UTILIZATION_MODELS
-        )
-        if not has_unlinked:
+        missing_invoice_challenges = []
+        for challenge in challenges_with_unlinked_utilization:
             self.stdout.write(
-                self.style.SUCCESS(
-                    f"{prefix} No unlinked utilizations found, skipping."
+                self.style.WARNING(
+                    f"[{challenge.short_name}] Has unlinked utilization."
                 )
             )
-            return
-        else:
+            try:
+                self._link_challenge_utilization(challenge=challenge)
+            except NoInvoiceError:
+                missing_invoice_challenges.append(challenge.short_name)
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"[{challenge.short_name}] Has no invoices, skipping."
+                    )
+                )
+
+        self.stdout.write(self.style.SUCCESS("Finished linking utilizations."))
+
+        if missing_invoice_challenges:
             self.stdout.write(
-                self.style.WARNING(f"{prefix} Found unlinked utilizations.")
+                self.style.ERROR(
+                    f"No invoice found for challenges: {', '.join(missing_invoice_challenges)}."
+                )
             )
 
+    def _link_challenge_utilization(self, *, challenge):
         invoice = (
             Invoice.objects.filter(challenge=challenge)
             .order_by("expires_on", "created")
@@ -87,33 +64,16 @@ class Command(BaseCommand):
         )
 
         if not invoice:
-            if not create_missing_invoice:
-                self.stdout.write(
-                    self.style.ERROR(f"{prefix} No invoice found.")
-                )
-                return
+            raise NoInvoiceError
+        else:
+            for utilization_model in CHALLENGE_UTILIZATION_MODELS:
+                utilization_model.objects.filter(
+                    challenge=challenge,
+                    invoice__isnull=True,
+                ).update(invoice=invoice)
 
             self.stdout.write(
-                self.style.WARNING(
-                    f"{prefix} No target invoice found, creating invoice."
+                self.style.SUCCESS(
+                    f"{challenge.short_name} Linked utilizations to invoice {invoice.pk}."
                 )
             )
-            invoice = Invoice.objects.create(
-                challenge=challenge,
-                support_costs_euros=0,
-                compute_costs_euros=0,
-                storage_costs_euros=0,
-                payment_type=PaymentTypeChoices.COMPLIMENTARY,
-                payment_status=PaymentStatusChoices.CANCELLED,
-                internal_comments="Created to link utilizations without existing invoices. This invoice represents historical compute usage that was not tracked under any invoice.",
-            )
-
-        for utilization_model in CHALLENGE_UTILIZATION_MODELS:
-            utilization_model.objects.filter(
-                challenge=challenge,
-                invoice__isnull=True,
-            ).update(invoice=invoice)
-
-        self.stdout.write(
-            self.style.SUCCESS(f"{prefix} Linked utilizations to invoice.")
-        )
