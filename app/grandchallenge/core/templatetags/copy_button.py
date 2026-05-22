6dbe0a97e677dcf8fde53b2ad3e6d23a7733cae4
Anne Mickan <30069334+amickan@users.noreply.github.com>
2026-05-22 13:09:04 +0200

    Move copy button to the right

--- app/grandchallenge/core/templatetags/copy_button.py
+++ app/grandchallenge/core/templatetags/copy_button.py
@@ -11,7 +11,7 @@ def copy_button(value, link=None):
     button = format_html(
         "<button "
         'type="button" '
-        'class="btn btn-link text-dark p-0 m-0 mr-1 copy-btn shadow-none" '
+        'class="btn btn-link text-dark p-0 mt-0 mb-1 mx-1 copy-btn shadow-none" '
         'data-copy="{value}" '
         'data-placement="left" '
         'title="Copy to clipboard">'
@@ -23,10 +23,13 @@ def copy_button(value, link=None):
     shortened = str(value).split("-", 1)[0]
 
     if link:
-        return button + format_html(
-            '<a href="{link}">{display_value}</a>',
-            link=link,
-            display_value=shortened,
+        return (
+            format_html(
+                '<a href="{link}">{display_value}</a>',
+                link=link,
+                display_value=shortened,
+            )
+            + button
         )
     else:
-        return button + format_html("{display_value}", display_value=shortened)
+        return format_html("{display_value}", display_value=shortened) + button
