f5371476467e9cb8c211280104cf841e0bc02703
Chris van Run <chris.vanrun@radboudumc.nl>
2026-06-05 15:44:34 +0200

    Fix open_for_submission test

--- app/tests/evaluation_tests/test_models.py
+++ app/tests/evaluation_tests/test_models.py
@@ -577,12 +577,10 @@ def test_open_for_submission(
     phase.submissions_close_at = submissions_close
     phase.save()
 
-    phase.challenge.compute_cost_euro_millicents = 5 * 1000 * 100
-    phase.challenge.save()
-
     InvoiceFactory(
         challenge=phase.challenge,
         compute_costs_euros=compute_costs_euros,
+        compute_cost_euro_millicents=5 * 1000 * 100,
         payment_type=PaymentTypeChoices.COMPLIMENTARY,
     )
 
