4020ca13ee62cbc3db9434411028870993c95a64
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2026-06-09 16:22:05 +0200

    Add a copy button for image pks

--- app/grandchallenge/core/templatetags/copy_button.py
+++ app/grandchallenge/core/templatetags/copy_button.py
@@ -1,35 +1,35 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.template.loader import render_to_string
 from django.utils.html import format_html
 
 register = template.Library()
 
 
-@register.filter
-@stringfilter
-def copy_button(value, link=None):
-    button = format_html(
+@register.simple_tag
+def copy_button_only(value, title="Copy to clipboard"):
+    return format_html(
         "<button "
         'type="button" '
         'class="btn btn-link text-dark p-0 mt-0 mb-1 mx-1 copy-btn shadow-none" '
         'data-copy="{value}" '
         'data-placement="left" '
-        'title="Copy to clipboard">'
+        'title="{title}">'
         '<i class="far fa-copy"></i>'
         "</button>",
         value=value,
+        title=title,
     )
 
-    shortened = str(value).split("-", 1)[0]
 
-    if link:
-        return (
-            format_html(
-                '<a href="{link}">{display_value}</a>',
-                link=link,
-                display_value=shortened,
-            )
-            + button
-        )
-    else:
-        return format_html("{display_value}", display_value=shortened) + button
+@register.filter
+@stringfilter
+def copy_button(value, link=None):
+    return render_to_string(
+        "grandchallenge/partials/copy_button.html",
+        context={
+            "button": copy_button_only(value=value),
+            "display_value": str(value).split("-", 1)[0],
+            "link": link,
+        },
+    )
