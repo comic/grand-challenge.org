d1e7dfc9ae7c9fe8916a2246f88821adf23e338f
Chris van Run <chris.vanrun@radboudumc.nl>
2026-06-15 10:45:31 +0200

    Fix test that checks email subject content

--- app/tests/invoices_tests/test_tasks.py
+++ app/tests/invoices_tests/test_tasks.py
@@ -638,7 +638,7 @@ def test_invoice_budget_alert_email(
     ]
     assert (
         challenge_admin_email[0].subject
-        == "[testserver] [test] over 70% Budget Consumed Alert"
+        == "[testserver] [test] over 70% Compute Budget Consumed Alert"
     )
     assert (
         "We would like to inform you that more than 70% of the compute budget "
@@ -676,7 +676,7 @@ def test_invoice_budget_alert_email(
     assert len(mail.outbox) != 0
     assert (
         mail.outbox[0].subject
-        == "[testserver] [test] over 90% Budget Consumed Alert"
+        == "[testserver] [test] over 90% Compute Budget Consumed Alert"
     )
 
 
@@ -727,7 +727,7 @@ def test_invoice_budget_alert_two_thresholds_one_email(
     }
     assert (
         mail.outbox[0].subject
-        == "[testserver] [test] over 90% Budget Consumed Alert"
+        == "[testserver] [test] over 90% Compute Budget Consumed Alert"
     )
 
 
