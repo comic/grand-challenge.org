6bec4d5dc22b456765cfedd3578ba8a3b7c3c033
Thomas Koopman <koopmant@users.noreply.github.com>
2026-06-16 13:21:56 +0200

    Update sagemaker-shim to 0.8.0 (#4801)
    Related to https://github.com/DIAGNijmegen/rse-roadmap/issues/481
    
    Follow up PR:
    https://github.com/DIAGNijmegen/rse-grand-challenge/pull/4797
    
    ---------
    
    Co-authored-by: James Meakin <12661555+jmsmkn@users.noreply.github.com>

--- app/grandchallenge/components/tasks.py
+++ app/grandchallenge/components/tasks.py
@@ -66,6 +66,8 @@ from grandchallenge.uploads.models import UserUpload
 @lambda_task
 def update_all_container_image_shims():
     """Updates existing images to new versions of sagemaker shim"""
+    n_tasks = 0
+
     for app_label, model_name in (
         ("algorithms", "algorithmimage"),
         ("evaluation", "method"),
@@ -80,6 +82,9 @@ def update_all_container_image_shims():
                 app_label=instance._meta.app_label,
                 model_name=instance._meta.model_name,
             )
+            n_tasks += 1
+
+    return n_tasks
 
 
 @lambda_task(queue=LambdaTaskQueueChoices.MEM8G)
@@ -217,7 +222,7 @@ def mark_desired_container_version(
         )
 
 
-@lambda_task
+@lambda_task(retry_on=(LockNotAcquiredException,))
 def update_container_image_shim(
     *,
     pk: str | UUID,
@@ -225,7 +230,21 @@ def update_container_image_shim(
     model_name: str,
 ):
     model = apps.get_model(app_label=app_label, model_name=model_name)
-    instance = model.objects.get(pk=pk)
+
+    with check_lock_acquired():
+        instance = model.objects.select_for_update(nowait=True).get(pk=pk)
+
+    from grandchallenge.algorithms.models import AlgorithmImage, Job
+    from grandchallenge.evaluation.models import Evaluation, Method
+
+    if isinstance(instance, AlgorithmImage):
+        if Job.objects.active().filter(algorithm_image=instance).exists():
+            raise RuntimeError("Algorithm image has an active job")
+    elif isinstance(instance, Method):
+        if Evaluation.objects.active().filter(method=instance).exists():
+            raise RuntimeError("Method has an active evaluation")
+    else:
+        raise NotImplementedError
 
     if (
         instance.is_in_registry
