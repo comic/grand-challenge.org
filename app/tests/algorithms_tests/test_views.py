c4f5964737e397529947a1474cbc7469fbcd2896
Thomas Koopman <koopman.t.pro@gmail.com>
2026-08-14 08:11:33 +0200

    Move AmazonSageMakerTrainingLogsService to training

--- app/tests/algorithms_tests/test_views.py
+++ app/tests/algorithms_tests/test_views.py
@@ -722,7 +722,7 @@ class TestJobLogsDetailPermissions:
         mock_service.execution_history = []
         mock_service.get_task_logs.return_value = []
         mocker.patch(
-            "grandchallenge.components.backends.amazon_sagemaker_base.AmazonSageMakerTrainingLogsService",
+            "grandchallenge.components.backends.amazon_sagemaker_training.AmazonSageMakerTrainingLogsService",
             return_value=mock_service,
         )
 
